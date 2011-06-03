998bc4df09c6fcb71c19e7d15b697adf28ae94e9
Josh Allen <jwallen@mit.edu>
2011-06-03 12:38:40 -0400

    Added functionality for generating and saving kinetics group values to script.
    There are at least two use cases for the "generate kinetics group additivity
    values" functionality: (1) generate and save the group values for all
    reaction families for use in RMG jobs and (2) generate and evalutate the
    group values for one or more individual families. Since a lot of the code
    that performs the kinetics group additivity value generation is the same in
    either case, I've placed functionality for both in the same file. You can
    invoke either using the command line, i.e.
    
    $ python kineticsGroups.py generate
    
    to generate and save group values for all families, or
    
    $ python kineticsGroups.py evaluate <family>
    
    to generate and evaluate group values for a single family (as specified as
    an argument). The argparse module makes this quite straightforward to set
    up.

--- kineticsGroups.py
+++ kineticsGroups.py
@@ -8,11 +8,11 @@ training or test set. You can change the reaction family and the training and
 test sets at the bottom of the file.
 """
 
+import time
 import math
 import numpy
 import pylab
 
-from rmgpy.data.rmg import RMGDatabase
 from rmgpy.data.base import LogicNode
 from rmgpy.molecule import Molecule
 from rmgpy.group import Group
@@ -146,28 +146,42 @@ def getForwardReactionForEntry(entry, database, family):
         
 ################################################################################
 
-def generateKineticsGroupValues(family, trainingSet, testSet, database):
+def getRateCoefficientUnits(family):
     """
-    Generate the kinetics group additivity values for the given reaction 
-    `family` using the specified lists of depository components `trainingSet`
-    as the training set and `testSet` as the test set. The already-loaded
-    RMG database should be given as the `database` parameter.
+    For the given reaction `family`, return the units of its forward kinetics.
+    This is hardcoding of reaction families, but at least it will fail loudly
+    if it encounters an unexpected family.
     """
+    if family in ['H_Abstraction', 'R_Addition_MultipleBond', 'R_Recombination', 'HO2_Elimination_from_PeroxyRadical', 'Disproportionation', '1+2_Cycloaddition', '2+2_cycloaddition_Cd', '2+2_cycloaddition_CO', '2+2_cycloaddition_CCO', 'Diels_alder_addition', '1,2_Insertion', '1,3_Insertion_CO2', '1,3_Insertion_ROR', 'R_Addition_COm', 'Oa_R_Recombination']:
+        return 'm^3/(mol*s)'
+    elif family in ['intra_H_migration', 'Birad_recombination', 'intra_OH_migration', 'Cyclic_Ether_Formation', 'Intra_R_Add_Exocyclic', 'Intra_R_Add_Endocyclic', '1,2-Birad_to_alkene', 'Intra_Disproportionation']:
+        return 's^-1'
+    else:
+        raise ValueError('Unable to determine units of rate coefficient for reaction family "{0}".'.format(family))
+
+def generateKineticsGroupValues(family, database, Tdata, trainingSetLabels, testSetLabels=None, plot=False):
+    """
+    Evaluate the kinetics group additivity values for the given reaction 
+    `family` using the specified lists of depository components 
+    `trainingSetLabels` as the training set and `testSetLabels` as the test 
+    set. The already-loaded RMG database should be given as the `database` 
+    parameter.
+    """
+    testSetLabels = testSetLabels or []
     
     groups = database.kinetics.groups[family]
     
     print 'Categorizing reactions in training and test sets for {0}'.format(family)
-    trainingSetLabels = trainingSet; trainingSet = []
+    trainingSet = []
     for label in trainingSetLabels:
         for entry in database.kinetics.depository['{0}/{1}'.format(family,label)].entries.values():
             trainingSet.append(getForwardReactionForEntry(entry=entry, database=database, family=family))
-    testSetLabels = testSet; testSet = []
+    testSet = []
     for label in testSetLabels:
         for entry in database.kinetics.depository['{0}/{1}'.format(family,label)].entries.values():
             testSet.append(getForwardReactionForEntry(entry=entry, database=database, family=family))
     
-    print 'Fitting new group additivity values...'
-    Tdata = numpy.array([500,1000,1500,2000], numpy.float64)
+    print 'Fitting new group additivity values for {0}...'.format(family)
     kdata_training = []
     for reaction, template, entry in trainingSet:
         if isinstance(reaction.kinetics, Arrhenius) or isinstance(reaction.kinetics, KineticsData):
@@ -178,9 +192,14 @@ def generateKineticsGroupValues(family, trainingSet, testSet, database):
             raise Exception('Unexpected kinetics model of type {0} for reaction {1}.'.format(reaction.kinetics.__class__, reaction))
     kdata_training = numpy.array(kdata_training, numpy.float64)
     
-    kunits = 'm^3/(mol*s)' if len(testSet[0][0].reactants) == 2 else 's^-1'
+    kunits = getRateCoefficientUnits(family)
     trainingTemplates = [template for reaction, template, entry in trainingSet]
     groupValues, groupUncertainties, groupCounts, kmodel = groups.fitGroupValues(trainingTemplates, Tdata, kdata_training, kunits)
+    # Add a note to the history of each item indicating that we've generated new group values
+    event = [time.asctime(),'jwallen','action','jwallen generated new group additivity values for this entry.']
+    for label, entry in groups.entries.iteritems():
+        if entry.data is not None:
+            entry.history.append(event)
     
     # Evaluate kmodel for the training set
     kmodel_training = []
@@ -203,35 +222,36 @@ def generateKineticsGroupValues(family, trainingSet, testSet, database):
     kdata_test = numpy.array(kdata_test, numpy.float64)
     kmodel_test = numpy.array(kmodel_test, numpy.float64)
     
-    # Print the group values
-    entries = groups.top[:]
-    for entry in groups.top:
-        entries.extend(groups.descendants(entry))
-    print '=============================== =========== =========== =========== ======='
-    print 'Group                           T (K)       k(T) (SI)   CI (95%)    Count'
-    print '=============================== =========== =========== =========== ======='
-    entry = groups.top[0]
-    for i in range(len(entry.data.Tdata.values)):
-        label = ', '.join(['%s' % (top.label) for top in groups.top]) if i == 0 else ''
-        T = Tdata[i]
-        value = groupValues[entry][i]
-        uncertainty = groupUncertainties[entry][i]
-        count = groupCounts[entry][i]
-        print '%-31s %-11g %-11.4e %-11.4e %-7i' % (label, T, value, uncertainty, count)
-    print '------------------------------- ----------- ----------- ----------- -------'
-    for entry in entries:
-        if entry.data is not None:
-            for i in range(len(entry.data.Tdata.values)):
-                label = entry.label if i == 0 else ''
-                T = Tdata[i]
-                value = groupValues[entry][i]
-                uncertainty = groupUncertainties[entry][i]
-                count = groupCounts[entry][i]
-                print '%-31s %-11g %-11.4e %-11.4e %-7i' % (label, T, value, uncertainty, count)
-    print '=============================== =========== =========== =========== ======='
-    
-    # Generate plots
-    generateParityPlots(Tdata, kdata_training, kmodel_training, kdata_test, kmodel_test, groups, trainingSet, testSet, family)
+    if plot:
+        # Print the group values
+        entries = groups.top[:]
+        for entry in groups.top:
+            entries.extend(groups.descendants(entry))
+        print '=============================== =========== =========== =========== ======='
+        print 'Group                           T (K)       k(T) (SI)   CI (95%)    Count'
+        print '=============================== =========== =========== =========== ======='
+        entry = groups.top[0]
+        for i in range(len(entry.data.Tdata.values)):
+            label = ', '.join(['%s' % (top.label) for top in groups.top]) if i == 0 else ''
+            T = Tdata[i]
+            value = groupValues[entry][i]
+            uncertainty = groupUncertainties[entry][i]
+            count = groupCounts[entry][i]
+            print '%-31s %-11g %-11.4e %-11.4e %-7i' % (label, T, value, uncertainty, count)
+        print '------------------------------- ----------- ----------- ----------- -------'
+        for entry in entries:
+            if entry.data is not None:
+                for i in range(len(entry.data.Tdata.values)):
+                    label = entry.label if i == 0 else ''
+                    T = Tdata[i]
+                    value = groupValues[entry][i]
+                    uncertainty = groupUncertainties[entry][i]
+                    count = groupCounts[entry][i]
+                    print '%-31s %-11g %-11.4e %-11.4e %-7i' % (label, T, value, uncertainty, count)
+        print '=============================== =========== =========== =========== ======='
+        
+        # Generate plots
+        generateParityPlots(Tdata, kdata_training, kmodel_training, kdata_test, kmodel_test, groups, trainingSet, testSet, family)
     
 ################################################################################
 
@@ -293,18 +313,63 @@ def generateParityPlots(Tdata, kdata_training, kmodel_training, kdata_test, kmod
 
 ################################################################################
 
+def generate(args, database):
+    """
+    This function is called when the "generate" command is given on the command
+    line. It causes group additivity kinetics values to be generated and saved
+    for all reaction families.
+    """
+    Tdata = numpy.array([300,400,500,600,800,1000,1500,2000], numpy.float64)
+    for family in database.kinetics.groups:
+        generateKineticsGroupValues(
+            database = database,
+            family = family,
+            Tdata = Tdata,
+            trainingSetLabels = ['rules'],
+            plot = False,
+        )
+    print 'Saving RMG database...'
+    database.save('input')
+    
+def evaluate(args, database):
+    """
+    This function is called when the "evaluate" command is given on the command
+    line. It causes group additivity kinetics values to be generated and 
+    evaluated for one reaction family.
+    """
+    family = args.family[0]
+    Tdata = numpy.array([500,1000,1500,2000], numpy.float64)
+    generateKineticsGroupValues(
+        database = database,
+        family = family,
+        Tdata = Tdata,
+        trainingSetLabels = ['rules'],
+        testSetLabels = ['PrIMe'],
+        plot = True,
+    )
+
+################################################################################
+
 if __name__ == '__main__':
 
+    import argparse
+    
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest='command', help='')
+    
+    generateParser = subparsers.add_parser('generate', help='generate and save kinetics group values for all families')
+    generateParser.set_defaults(run=generate)
+    
+    evaluateParser = subparsers.add_parser('evaluate', help='evaluate kinetics group values for one family')
+    evaluateParser.add_argument('family', metavar='<family>', type=str, nargs=1, help='the family to evaluate')
+    evaluateParser.set_defaults(run=evaluate)
+    
+    args = parser.parse_args()
+    
     print 'Loading RMG database...'
+    from rmgpy.data.rmg import RMGDatabase
     database = RMGDatabase()
     database.load('input')
 
-    generateKineticsGroupValues(
-        family = 'H_Abstraction',
-        #family = 'R_Addition_MultipleBond',
-        #family = 'R_Recombination',
-        trainingSet = ['rules'],
-        testSet = ['PrIMe'],
-        database = database,
-    )
+    args.run(args, database)
     
