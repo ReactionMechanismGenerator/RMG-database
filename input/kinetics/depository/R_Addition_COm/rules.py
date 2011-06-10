02b763bfbe835a6ad3c913ec8417250e1dab4ff4
Connie Gao <connieg@mit.edu>
2011-06-10 17:57:49 -0400

    Adding 4 rate rules to R_Addition_COm reaction template library
    MRH computed the rate coefficients for the following 4 reactions using the
    CBS-QB3 method + TST w/1d hindered rotor corrections + asymmetric Eckart
    tunneling:
    	CO + CH3 = CH3CO
    	CO + C2H5 = CH3CH2CO
    	CO + n-C3H7 = CH3CH2CH2CO
    	CO + iso-C3H7 = CH3CH(CH3)CO
    The CBS-QB3 calculations are faster than RMG's previous (Average of Average)
    estimates by as much as an order of magnitude at temperatures > 1000 K.  At
    lower temperatures, the new calculations are usually within a factor of three
    of the old RMG estimates.
    
    The butanol mechanism was sensitive to these types of reactions (in particular,
    the CO + n-C3H7 = CH3CH2CH2CO reaction) at the low temperature, high pressure
    conditions of the RCM.
    
    Mirrors commit 2954148fe922162af88240068d238d962a7bfadf in RMG-Java repository by mrharper.

--- input/kinetics/depository/R_Addition_COm/rules.py
+++ input/kinetics/depository/R_Addition_COm/rules.py
@@ -474,3 +474,210 @@ MRH 1-Sept-2009
     ],
 )
 
+entry(
+    index = 424,
+    label = "COm;C_methyl",
+    group1 =
+"""
+1 *1 C {2S,2T} {2,D}
+2 O 0 {1,D}
+""",
+    group2 =
+"""
+1 *2 C 1 {2,S}, {3,S}, {4,S}
+2 H 0 {1,S}
+3 H 0 {1,S}
+4 H 0 {1,S}
+""",
+    kinetics = ArrheniusEP(
+        A = (3.06e+06,"cm^3/(mol*s)","*|/",3),
+        n = 1.89,
+        alpha = 0,
+        E0 = (4.82,"kcal/mol","+/-",2),
+        Tmin = (600,"K"),
+        Tmax = (2000,"K"),
+    ),
+    reference = None,
+    referenceType = "",
+    rank = 3,
+    shortDesc = u"""MRH CBS-QB3 calculations with 1dHR corrections""",
+    longDesc =
+u"""
+CH3 + CO = CH3CO
+MRH CBS-QB3 calculations with 1D hindered rotor corrections [MRHCBSQB31DHR]_.
+
+Methyl (doublet): external symmetry number (EXTSYM) = 6
+CO (singlet): EXTSYM = 1
+TS (doublet): EXTSYM = 1, one hindered rotor (methyl group, symmetry = 3)
+CH3CO (doublet): EXTSYM = 1, one hindered rotor (methyl group, symmetry = 3)
+
+[MRHCBSQB31DHR] M.R. Harper (mrharper_at_mit_dot_edu or michael_dot_harper_dot_jr_at_gmail_dot_com)
+The geometries of all reactants, products, and the transition state were optimized using the CBS-QB3 method.
+The zero-point energy is that computed by the CBS-QB3 calculations. The frequencies were computed with B3LYP/CBSB7.
+In computing k(T), an asymmetric tunneling correction was employed, the calculated frequencies were scaled by 0.99, and the
+temperatures used were from 600 K to 2000 K (in 200 K increments).
+""",
+    history = [
+        ("Fri Jun 10 17:16:47 2011","connie","action","""connie imported this entry from the old RMG database."""),
+    ],
+)
+
+
+entry(
+    index = 425,
+    label = "COm;CH2CH3",
+    group1 =
+"""
+1 *1 C {2S,2T} {2,D}
+2 O 0 {1,D}
+""",
+    group2 =
+"""
+1 *2 C 1 {2,S} {3,S} {4,S}
+2 H 0 {1,S}
+3 H 0 {1,S}
+4 C 0 {1,S} {5,S} {6,S} {7,S}
+5 H 0 {4,S}
+6 H 0 {4,S}
+7 H 0 {4,S}
+""",
+    kinetics = ArrheniusEP(
+        A = (7.70e+07,"cm^3/(mol*s)","*|/",3),
+        n = 1.37,
+        alpha = 0,
+        E0 = (5.69,"kcal/mol","+/-",2),
+        Tmin = (600,"K"),
+        Tmax = (2000,"K"),
+    ),
+    reference = None,
+    referenceType = "",
+    rank = 3,
+    shortDesc = u"""MRH CBS-QB3 calculations with 1dHR corrections""",
+    longDesc =
+u"""
+CH3CH2 + CO = CH3CH2CO
+MRH CBS-QB3 calculations with 1D hindered rotor corrections [MRHCBSQB31DHR]_.
+
+Ethyl (doublet): external symmetry number (EXTSYM) = 1, one hindered rotor (methyl group, symmetry = 6)
+CO (singlet): EXTSYM = 1
+TS (doublet): EXTSYM = 1, two hindered rotors (methyl group, symmetry = 3; ethyl group, symmetry = 1)
+CH3CH2CO (doublet): EXTSYM = 1, two hindered rotors (methyl group, symmetry = 3; ethyl group, symmetry = 1)
+
+[MRHCBSQB31DHR] M.R. Harper (mrharper_at_mit_dot_edu or michael_dot_harper_dot_jr_at_gmail_dot_com)
+The geometries of all reactants, products, and the transition state were optimized using the CBS-QB3 method.
+The zero-point energy is that computed by the CBS-QB3 calculations. The frequencies were computed with B3LYP/CBSB7.
+In computing k(T), an asymmetric tunneling correction was employed, the calculated frequencies were scaled by 0.99, and the
+temperatures used were from 600 K to 2000 K (in 200 K increments).
+""",
+    history = [
+        ("Fri Jun 10 17:16:47 2011","connie","action","""connie imported this entry from the old RMG database."""),
+    ],
+)
+
+entry(
+    index = 426,
+    label = "COm;CH2CH2CH3",
+    group1 =
+"""
+1 *1 C {2S,2T} {2,D}
+2 O 0 {1,D}
+""",
+    group2 =
+"""
+1 *2 C 1 {2,S} {3,S} {4,S}
+2 H 0 {1,S}
+3 H 0 {1,S}
+4 C 0 {1,S} {5,S} {6,S} {7,S}
+5 H 0 {4,S}
+6 H 0 {4,S}
+7 C 0 {4,S} {8,S} {9,S} {10,S}
+8 H 0 {7,S}
+9 H 0 {7,S}
+10 H 0 {7,S}
+""",
+    kinetics = ArrheniusEP(
+        A = (6.51e+10,"cm^3/(mol*s)","*|/",3),
+        n = 0.45,
+        alpha = 0,
+        E0 = (6.68,"kcal/mol","+/-",2),
+        Tmin = (600,"K"),
+        Tmax = (2000,"K"),
+    ),
+    reference = None,
+    referenceType = "",
+    rank = 3,
+    shortDesc = u"""MRH CBS-QB3 calculations with 1dHR corrections""",
+    longDesc =
+u"""
+CH3CH2CH2 + CO = CH3CH2CH2CO
+MRH CBS-QB3 calculations with 1D hindered rotor corrections [MRHCBSQB31DHR]_.
+
+n-Propyl (doublet): external symmetry number (EXTSYM) = 1, two hindered rotors (methyl group, symmetry = 3; ethyl group, symmetry = 4)
+CO (singlet): EXTSYM = 1
+TS (doublet): EXTSYM = 1, three hindered rotors (methyl group, symmetry = 3; ethyl group, symmetry = 2; propyl group, symmetry = 1)
+CH3CH2CH2CO (doublet): EXTSYM = 1, three hindered rotors (methyl group, symmetry = 3; ethyl group, symmetry = 1; propyl group, symmetry = 1)
+
+[MRHCBSQB31DHR] M.R. Harper (mrharper_at_mit_dot_edu or michael_dot_harper_dot_jr_at_gmail_dot_com)
+The geometries of all reactants, products, and the transition state were optimized using the CBS-QB3 method.
+The zero-point energy is that computed by the CBS-QB3 calculations. The frequencies were computed with B3LYP/CBSB7.
+In computing k(T), an asymmetric tunneling correction was employed, the calculated frequencies were scaled by 0.99, and the
+temperatures used were from 600 K to 2000 K (in 200 K increments).
+""",
+    history = [
+        ("Fri Jun 10 17:16:47 2011","connie","action","""connie imported this entry from the old RMG database."""),
+    ],
+)
+
+entry(
+    index = 427,
+    label = "COm;CH[CH3]2",
+    group1 =
+"""
+1 *1 C {2S,2T} {2,D}
+2 O 0 {1,D}
+""",
+    group2 =
+"""
+1 *2 C 1 {2,S} {3,S} {4,S}
+2 H 0 {1,S}
+3 C 0 {1,S} {5,S} {6,S} {7,S}
+4 C 0 {1,S} {8,S} {9,S} {10,S}
+5 H 0 {3,S}
+6 H 0 {3,S}
+7 H 0 {3,S}
+8 H 0 {4,S}
+9 H 0 {4,S}
+10 H 0 {4,S}
+""",
+    kinetics = ArrheniusEP(
+        A = (8.61e+07,"cm^3/(mol*s)","*|/",3),
+        n = 1.36,
+        alpha = 0,
+        E0 = (4.80,"kcal/mol","+/-",2),
+        Tmin = (600,"K"),
+        Tmax = (2000,"K"),
+    ),
+    reference = None,
+    referenceType = "",
+    rank = 3,
+    shortDesc = u"""MRH CBS-QB3 calculations with 1dHR corrections""",
+    longDesc =
+u"""
+CH3CHCH3 + CO = CH3CH(CO)CH3
+MRH CBS-QB3 calculations with 1D hindered rotor corrections [MRHCBSQB31DHR]_.
+
+iso-Propyl (doublet): external symmetry number (EXTSYM) = 1, two hindered rotors (methyl group, symmetry = 6; methyl group, symmetry = 6)
+CO (singlet): EXTSYM = 1
+TS (doublet): EXTSYM = 1, three hindered rotors (methyl group, symmetry = 3; methyl group, symmetry = 3; propyl group, symmetry = 1)
+CH3CH(CO)CH3 (doublet): EXTSYM = 1, three hindered rotors (methyl group, symmetry = 3; methyl group, symmetry = 3; propyl group, symmetry = 1)
+
+[MRHCBSQB31DHR] M.R. Harper (mrharper_at_mit_dot_edu or michael_dot_harper_dot_jr_at_gmail_dot_com)
+The geometries of all reactants, products, and the transition state were optimized using the CBS-QB3 method.
+The zero-point energy is that computed by the CBS-QB3 calculations. The frequencies were computed with B3LYP/CBSB7.
+In computing k(T), an asymmetric tunneling correction was employed, the calculated frequencies were scaled by 0.99, and the
+temperatures used were from 600 K to 2000 K (in 200 K increments).
+""",
+    history = [
+        ("Fri Jun 10 17:16:47 2011","connie","action","""connie imported this entry from the old RMG database."""),
+    ],
+)
