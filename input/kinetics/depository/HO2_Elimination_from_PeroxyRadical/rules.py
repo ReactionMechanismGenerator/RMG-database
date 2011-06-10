2a47d0fd3ab54ebdbe85729c69d76be44549b24d
Connie Gao <connieg@mit.edu>
2011-06-10 17:37:00 -0400

    Adding new rate coefficient for HO2_Elimination_from_PeroxyRadical family
    The top level node for the HO2_Elimination_from_PeroxyRadical rxn family was
    R2OO, whose definition appears below:
    
    	1 *1 C 0 {2,S} {5,S}
    	2 *2 C 0 {1,S} {3,S}
    	3 *3 O 0 {2,S} {4,S}
    	4 *4 O 1 {3,S}
    	5 *5 H 0 {1,S}
    
    Note that the atom bonded to the H atom is forced to be a carbon atom.  Thus,
    if the species CH3-CH(OO)-OH was run against the rxn family template, only the
    products HO2 + CH2=CH-OH would be found (and not HO2 + CH3CHO).
    
    MRH has modified the R2OO definition to include "O" as a possible atom for the
    *1 site.  I have also added two nodes beneath the R2OO node: a child, called
    OCOO (which simply distinguishes between an O and C *1 node), and a grandchild,
    called HOCH[OO]CH3.
    
    I calculated the rate coefficient for the grandchild node, HOCH[OO]CH3 => HO2 +
    CH3CHO, using the CBS-QB3 method with 1-d hindered rotor corrections.  Note: I
    did not apply a tunneling correction (see my notes in the comments.rst file).
    
    Furthermore, I assigned this rate coefficient expression to the more general
    node OCOO.
    
    This type of reaction should be important in low-temperature chemistry of
    alcohol oxidation.
    
    Mirrors commit 29c96524782e7e6bb2a204382508d4145ab5bf59 in RMG-Java repository by mrharper.

--- input/kinetics/depository/HO2_Elimination_from_PeroxyRadical/rules.py
+++ input/kinetics/depository/HO2_Elimination_from_PeroxyRadical/rules.py
@@ -465,3 +465,113 @@ TS (and it is certainly better than leaving RMG to estimate via "Average of Aver
     ],
 )
 
+entry(
+    index = 847,
+    label = "R2OO_2H_HDe",
+    group1 =
+"""
+1  *1 C 0 {2,S} {5,S} {6,S} {7,S}
+2  *2 C 0 {1,S} {3,S} {8,S} {9,S}
+3  *3 O 0 {2,S} {4,S}
+4  *4 O 1 {3,S}
+5  *5 H 0 {1,S}
+6     H 0 {1,S}
+7     H 0 {1,S}
+8     H 0 {2,S}
+9     {Cd,Ct,Cb,CO} 0 {2,S}
+""",
+    kinetics = ArrheniusEP(
+        A = (825300,"cm^3/(mol*s)","*|/",5),
+        n = 1.829,
+        alpha = 0,
+        E0 = (24.247,"kcal/mol","+|-",2),
+        Tmin = (600,"K"),
+        Tmax = (2000,"K"),
+    ),
+    reference = None,
+    referenceType = "",
+    rank = 3,
+    shortDesc = u"""Same as node 845 (MRH assumption)""",
+    longDesc =
+u"""
+MRH CBS-QB3 calculations for the reaction CH3-CH(OO)-OH => CH3-CH=O + HO2
+
+Previous RMG estimate for this reaction was zero (RMG only allowed HO2 direct elimination
+to occur for species with the structure H-C-C-O-O* ... note the atom next to the hydrogen
+had to be a carbon).
+
+MRH calculated the rate coefficient using the CBS-QB3 method.  1-d hindered rotor
+corrections were applied and NO tunneling correction.  The reason no tunneling correction
+was applied is that the TS is lower in energy than the products, CH3CHO + HO2.
+
+da Silva, Bozzelli, Liang, and Farrell (dx.doi.org/10.1021/jp903210a) recently studied
+this reaction system (ethanol + O2).  In their calculations (G3B3), they determined a stable
+adduct existed between the reactant CH3CH(OO)OH and the products CH3CHO+HO2.  The adduct is
+stable due to H-bonding.  MRH believes his TS is for the reactant to the adduct.
+Comparing my k(T) with the da Silva et al. k(T) (for forming the adduct) shows very
+good agreement, within a factor of 2 over the valid temperature range.  Furthermore, the
+da Silva et al. calculation for the adduct going to product is between 2-5 orders of
+magnitude faster than reactants going to adduct, so it is a reasonable assumption
+to say the first step is the rate-limiting step.
+
+Comparing my k(T) with two other sources for this reaction (dx.doi.org/10.1021/jp003762p and
+I. Hermans et al., AIAA Journal, 109, (2005), 4303-4311) also shows good agreement.
+I am setting the rank for this k(T) to be 5 (very uncertain).
+
+Information on the TST calculations:
+
+Reactant: 3 hindered rotors were considered (the -OO, -CH3, and -OH torsions)
+TS: 1 hindered rotor was considered (the -CH3 torsion)
+Product: 1 hindered rotor was considered (the -CH3 torsion of CH3CHO)
+
+All external symmetry numbers were set equal to one.
+The k(T) was calculated from 600 - 2000 K, in 200 K intervals, and the fitted Arrhenius expression from CanTherm was:
+k(T) = 6.813e+10 * (T/1K)^0.493 * exp(-11.894 kcal/mol / RT) cm3/mol/s.
+""",
+    history = [
+        ("Fri Jun  10 17:16:47 2011","connie","action","""connie imported this entry from the old RMG database."""),
+    ],
+)
+
+entry(
+    index = 848,
+    label = "R2OO_2H_HDe",
+    group1 =
+"""
+1  *1 C 0 {2,S} {5,S} {6,S} {7,S}
+2  *2 C 0 {1,S} {3,S} {8,S} {9,S}
+3  *3 O 0 {2,S} {4,S}
+4  *4 O 1 {3,S}
+5  *5 H 0 {1,S}
+6     H 0 {1,S}
+7     H 0 {1,S}
+8     H 0 {2,S}
+9     {Cd,Ct,Cb,CO} 0 {2,S}
+""",
+    kinetics = ArrheniusEP(
+        A = (825300,"cm^3/(mol*s)","*|/",5),
+        n = 1.829,
+        alpha = 0,
+        E0 = (24.247,"kcal/mol","+|-",2),
+        Tmin = (600,"K"),
+        Tmax = (2000,"K"),
+    ),
+    reference = None,
+    referenceType = "",
+    rank = 3,
+    shortDesc = u"""Same as node 845 (MRH assumption)""",
+    longDesc =
+u"""
+MRH approximation for the general R2OO_2H_HDe node
+
+MRH computed the rate coefficient for the reaction CH3-CH(OO)-CH=CH2 => CH2=CH-CH=CH2 + HO2 (see node 845).
+The difference between the R2OO_2H_HDe and CH3CH(OO)CHCH2 nodes is defining the delocalized group (in the
+case of the CH3CH(OO)CHCH2 node, the -CH=CH2 functional group).  MRH thinks using the kinetics for node 845
+in the event node 846 is hit is reasonable, considering this part of the molecule does not play a role in the
+TS (and it is certainly better than leaving RMG to estimate via "Average of Average").
+""",
+    history = [
+        ("Fri Jun  10 17:16:47 2011","connie","action","""connie imported this entry from the old RMG database."""),
+    ],
+)
+
