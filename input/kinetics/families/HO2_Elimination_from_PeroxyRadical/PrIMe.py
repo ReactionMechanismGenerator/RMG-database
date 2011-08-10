5b0456cbfb238e1a64bf03aa8dc6f131e353ab43
Richard West <rwest@mit.edu>
2011-08-09 20:27:35 -0400

    HO2_Elimination_from_PeroxyRadical/PrIMe:5 Corrected T0 from 1K to 298K according to NIST database
    Change to kinetics/families/HO2_Elimination_from_PeroxyRadical/PrIMe entry 5 submitted through RMG website:
    Corrected T0 from 1K to 298K according to NIST database
    (should probably check the Ea too, as these often seem wrong)

--- input/kinetics/families/HO2_Elimination_from_PeroxyRadical/PrIMe.py
+++ input/kinetics/families/HO2_Elimination_from_PeroxyRadical/PrIMe.py
@@ -208,7 +208,7 @@ entry(
         A = (9.38e+16,"s^-1"),
         n = -7.86,
         Ea = (153236,"J/mol"),
-        T0 = (1,"K"),
+        T0 = (298,"K"),
     ),
     reference = Article(authors=["DeSain, J.D.", "Klippenstein, S.J.", "Miller, J.A.", "Taatjes, C.A."], title=u'Measurements, Theory, and Modeling of OH Formation in Ethyl + O_2 and Propyl + O_2 Reactions', journal="J. Phys. Chem. A", volume="107", pages="""4415-4427""", year="2003", url="http://warehouse.primekinetics.org/depository/reactions/data/r00012692/rk00000001.xml"),
     referenceType = "",
@@ -216,9 +216,11 @@ entry(
     longDesc = 
 u"""
 Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00012692/rk00000001.xml
+Referece T0 corrected from 1 to 298K according to NIST database at http://kinetics.nist.gov/kinetics/Detail?id=2003DES/KLI4415-4427:6
 """,
     history = [
         ("Tue May 17 14:34:57 2011","Josh Allen <jwallen@mit.edu>","action","""Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00012692/rk00000001.xml"""),
+        ("2011-08-05","Richard West <rwest@mit.edu>","action","""Corrected T0 from 1K to 298K according to NIST database"""),
     ],
 )
 
