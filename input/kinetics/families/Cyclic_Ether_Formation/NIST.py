66f94f3cc7e94bdd67896a869669e2c238da8933
stroiano <stroiano7@gmail.com>
2012-07-13 08:25:41 -0400

    Split Cyclic_Ether_Formation

---
+++ input/kinetics/families/Cyclic_Ether_Formation/NIST.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "Cyclic_Ether_Formation/NIST"
+shortDesc = u""
+longDesc = u"""
+
+"""
+recommended = False
+
+entry(
+    index = 1,
+    label = "1982BAL/HIS1615:8",
+    reactant1 = 
+"""
+1 *4 C 0 {2,S} {3,S} {4,S} {5,S}
+2    C 0 {1,S} {6,S}
+3    C 0 {1,S}
+4    C 0 {1,S}
+5 *1 C 1 {1,S}
+6 *2 O 0 {2,S} {7,S}
+7 *3 O 0 {6,S}
+""",
+    product1 = 
+"""
+1 *4 C 0 {2,S} {3,S} {4,S} {5,S}
+2    C 0 {1,S} {6,S}
+3 *1 C 0 {1,S} {6,S}
+4    C 0 {1,S}
+5    C 0 {1,S}
+6 *2 O 0 {2,S} {3,S}
+""",
+    product2 = 
+"""
+1 *3 O 1
+""",
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (2e+11,"s^-1"),
+        n = (0.0,""),
+        Ea = (73.001,"kJ/mol"),
+        T0 = (1.0,"K"),
+        Tmin = (653.0,"K"),
+        Tmax = (793.0,"K"),
+        Pmin = (66700.0,"Pa"),
+        Pmax = (66700.0,"Pa"),
+    ),
+    reference = Article(
+        authors = ["Baldwin, R.R.", "Hisham, M.W.M.", "Walker, R.W."],
+        title = u'Arrhenius parameters of elementary reactions involved in the oxidation of neopentane',
+        journal = "J. Chem. Soc. Faraday Trans. 1",
+        volume = "78",
+        pages = """1615""",
+        year = "1982",
+        url = "http://kinetics.nist.gov/kinetics/Detail?id=1982BAL/HIS1615:8",
+    ),
+    referenceType = "experiment",
+    shortDesc = u"""Derived from fitting to a complex mechanism""",
+    longDesc = 
+u"""
+PrIMe Reaction: r00016872
+PrIMe Kinetics: http://warehouse.primekinetics.org/depository/reactions/data/r00016872/rk00000002.xml
+Bath gas: N2
+Category: Experiment
+Data type: Derived from fitting to a complex mechanism
+Excitation technique: Thermal
+Analytical technique: Gas chromatography
+""",
+    history = [('Fri Jul 13 08:21:18 2012', 'Sean Troiano <stroiano7@gmail.com>', 'action', 'Imported from NIST database at http://kinetics.nist.gov/kinetics/Detail?id=1982BAL/HIS1615:8')],
+)
+
+entry(
+    index = 2,
+    label = "2004SUN/BOZ1694-1711:23",
+    reactant1 = 
+"""
+1 *4 C 0 {2,S} {3,S} {4,S} {5,S}
+2    C 0 {1,S} {6,S}
+3    C 0 {1,S}
+4    C 0 {1,S}
+5 *1 C 1 {1,S}
+6 *2 O 0 {2,S} {7,S}
+7 *3 O 0 {6,S}
+""",
+    product1 = 
+"""
+1 *4 C 0 {2,S} {3,S} {4,S} {5,S}
+2    C 0 {1,S} {6,S}
+3 *1 C 0 {1,S} {6,S}
+4    C 0 {1,S}
+5    C 0 {1,S}
+6 *2 O 0 {2,S} {3,S}
+""",
+    product2 = 
+"""
+1 *3 O 1
+""",
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (3.05e+35,"s^-1"),
+        n = (-7.46,""),
+        Ea = (100.362,"kJ/mol"),
+        T0 = (1.0,"K"),
+        Tmin = (300.0,"K"),
+        Tmax = (900.0,"K"),
+        Pmin = (101000.0,"Pa"),
+        Pmax = (101000.0,"Pa"),
+    ),
+    reference = Article(
+        authors = ["Sun, H.", "Bozzelli, J.W."],
+        title = u'Thermochemical and kinetic analysis on the reactions of neopentyl and hydroperoxy-neopentyl radicals with oxygen: Part I.  OH and initial stable HC product formation',
+        journal = "J. Phys. Chem. A",
+        volume = "108",
+        pages = """1694-1711""",
+        year = "2004",
+        url = "http://kinetics.nist.gov/kinetics/Detail?id=2004SUN/BOZ1694-1711:23",
+    ),
+    referenceType = "theory",
+    shortDesc = u"""Ab initio""",
+    longDesc = 
+u"""
+PrIMe Reaction: r00016872
+Category: Theory
+Data type: Ab initio
+Comments: Quantum calculations of energetics of pathways related to reaction between neopentyl radical and O2 (and subsequent reaction pathways).  This includes primary reaction pathways as well as subsequent secondary reactions.  Used ab initio CBS-Q method, as well as B3LYP/6-31G(d,p) for some molecules.  Many reaction channels are discussed in paper, for some, rate expressions are provided at low pressure limit, high pressure limit, and a number of intermediate pressures.  For some reaction channels, no rate expressions are given in paper, but are provided in auxillary information associated with the article in JPC.
+""",
+    history = [('Fri Jul 13 08:21:18 2012', 'Sean Troiano <stroiano7@gmail.com>', 'action', 'Imported from NIST database at http://kinetics.nist.gov/kinetics/Detail?id=2004SUN/BOZ1694-1711:23')],
+)
+
+entry(
+    index = 3,
+    label = "2004SUN/BOZ1694-1711:24",
+    reactant1 = 
+"""
+1 *4 C 0 {2,S} {3,S} {4,S} {5,S}
+2    C 0 {1,S} {6,S}
+3    C 0 {1,S}
+4    C 0 {1,S}
+5 *1 C 1 {1,S}
+6 *2 O 0 {2,S} {7,S}
+7 *3 O 0 {6,S}
+""",
+    product1 = 
+"""
+1 *4 C 0 {2,S} {3,S} {4,S} {5,S}
+2    C 0 {1,S} {6,S}
+3 *1 C 0 {1,S} {6,S}
+4    C 0 {1,S}
+5    C 0 {1,S}
+6 *2 O 0 {2,S} {3,S}
+""",
+    product2 = 
+"""
+1 *3 O 1
+""",
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (4.78e+17,"s^-1"),
+        n = (-2.45,""),
+        Ea = (48.103,"kJ/mol"),
+        T0 = (1.0,"K"),
+        Tmin = (900.0,"K"),
+        Tmax = (2500.0,"K"),
+        Pmin = (101000.0,"Pa"),
+        Pmax = (101000.0,"Pa"),
+    ),
+    reference = Article(
+        authors = ["Sun, H.", "Bozzelli, J.W."],
+        title = u'Thermochemical and kinetic analysis on the reactions of neopentyl and hydroperoxy-neopentyl radicals with oxygen: Part I.  OH and initial stable HC product formation',
+        journal = "J. Phys. Chem. A",
+        volume = "108",
+        pages = """1694-1711""",
+        year = "2004",
+        url = "http://kinetics.nist.gov/kinetics/Detail?id=2004SUN/BOZ1694-1711:24",
+    ),
+    referenceType = "theory",
+    shortDesc = u"""Ab initio""",
+    longDesc = 
+u"""
+PrIMe Reaction: r00016872
+Category: Theory
+Data type: Ab initio
+Comments: Quantum calculations of energetics of pathways related to reaction between neopentyl radical and O2 (and subsequent reaction pathways).  This includes primary reaction pathways as well as subsequent secondary reactions.  Used ab initio CBS-Q method, as well as B3LYP/6-31G(d,p) for some molecules.  Many reaction channels are discussed in paper, for some, rate expressions are provided at low pressure limit, high pressure limit, and a number of intermediate pressures.  For some reaction channels, no rate expressions are given in paper, but are provided in auxillary information associated with the article in JPC.
+""",
+    history = [('Fri Jul 13 08:21:18 2012', 'Sean Troiano <stroiano7@gmail.com>', 'action', 'Imported from NIST database at http://kinetics.nist.gov/kinetics/Detail?id=2004SUN/BOZ1694-1711:24')],
+)
