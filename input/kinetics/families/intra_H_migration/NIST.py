6f09b9adf35152e2cf897fb0b5dd546631459c83
Sean Troiano <stroiano7@gmail.com>
2012-07-24 18:45:32 -0400

    NIST Conversion: intra_H_migration/PrIMe/8
    New Entry: NIST/32 (2003MER/RAY4828-4833:9)

--- input/kinetics/families/intra_H_migration/NIST.py
+++ input/kinetics/families/intra_H_migration/NIST.py
@@ -1877,3 +1877,71 @@ The authors estimate uncertainties in calculated rates to be a factor of 5
     ],
 )
 
+entry(
+    index = 32,
+    label = "2003MER/RAY4828-4833:9",
+    reactant1 = 
+"""
+1 *2 C 0 {2,S} {4,S} {5,S} {7,S}
+2 *5 C 0 {1,S} {3,S}
+3    C 0 {2,S} {6,S}
+4    C 0 {1,S}
+5    C 0 {1,S}
+6 *4 C 0 {3,S} {8,S}
+7 *3 H 0 {1,S}
+8 *1 O 1 {6,S}
+""",
+    product1 = 
+"""
+1    C 0 {2,S} {3,S}
+2 *4 C 0 {1,S} {6,S}
+3 *5 C 0 {1,S} {7,S}
+4    C 0 {6,S}
+5    C 0 {6,S}
+6 *1 C 1 {2,S} {4,S} {5,S}
+7 *2 O 0 {3,S} {8,S}
+8 *3 H 0 {7,S}
+""",
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (8.7e+11,"s^-1","*|/",5),
+        n = 0,
+        Ea = (20.92,"kJ/mol"),
+        T0 = (1,"K"),
+        Tmin = (298,"K"),
+        Tmax = (298,"K"),
+    ),
+    reference = Article(
+        authors = ["Mereau, R.", "Rayez, M.T.", "Caralp, F.", "Rayez, J.C."],
+        title = u'Isomerization reactions of alkoxy radicals: theoretical study and structure-activity relationships',
+        journal = "Phys. Chem. Chem. Phys.",
+        volume = "5",
+        pages = """4828-4833""",
+        year = "2003",
+        url = "http://kinetics.nist.gov/kinetics/Detail?id=2003MER/RAY4828-4833:9",
+    ),
+    referenceType = "theory",
+    shortDesc = u"""Ab initio""",
+    longDesc = 
+u"""
+PrIMe Reaction: r00000092
+PrIMe Kinetics: http://warehouse.primekinetics.org/depository/reactions/data/r00000092/rk00000001.xml
+Uncertainty: 5.0
+Category: Theory
+Data type: Ab initio
+Pressure dependence: Rate constant is high pressure limit
+
+The authors have calculated thermochemical and kinetic parameters for 1,5-H isomerisation reactions of alkoxy radicals up to C8 using density functional theory. Tunneling corrections were applied and the pressure dependence was investigated using RRKM statistical methods.
+
+Generic rate parameters for alkoxy radical 1,5 H transfers from primary, secondary, and tertiary C-Hn groups are suggested for 298 K and 1 atm pressure:
+kisom(primary) = 6.2E5 s^-1
+kisom(secondary) = 9.3E8 s^-1
+kisom(tertiary) = 4.5E8 s^-1
+
+The authors estimate uncertainties in calculated rates to be a factor of 5
+""",
+    history = [
+        ("Tue Jul 24 18:45:23 2012","Sean Troiano <stroiano7@gmail.com>","action","""Imported from NIST database at http://kinetics.nist.gov/kinetics/Detail?id=2003MER/RAY4828-4833:9"""),
+    ],
+)
+
--- input/kinetics/families/intra_H_migration/PrIMe.py
+++ input/kinetics/families/intra_H_migration/PrIMe.py
@@ -9,58 +9,6 @@ longDesc = u"""
 recommended = False
 
 entry(
-    index = 8,
-    label = "r00000092",
-    reactant1 = 
-"""
-1 *2 C 0 {2,S} {4,S} {5,S} {7,S}
-2 *5 C 0 {1,S} {3,S}
-3    C 0 {2,S} {6,S}
-4    C 0 {1,S}
-5    C 0 {1,S}
-6 *4 C 0 {3,S} {8,S}
-7 *3 H 0 {1,S}
-8 *1 O 1 {6,S}
-""",
-    product1 = 
-"""
-1    C 0 {2,S} {3,S}
-2 *4 C 0 {1,S} {6,S}
-3 *5 C 0 {1,S} {7,S}
-4    C 0 {6,S}
-5    C 0 {6,S}
-6 *1 C 1 {2,S} {4,S} {5,S}
-7 *2 O 0 {3,S} {8,S}
-8 *3 H 0 {7,S}
-""",
-    degeneracy = 1,
-    kinetics = Arrhenius(
-        A = (8.7e+11,"s^-1"),
-        n = 0,
-        Ea = (82.6129,"J/mol"),
-        T0 = (1,"K"),
-    ),
-    reference = Article(
-        authors = ["Mereau, R.", "Rayez, M.T.", "Caralp, F.", "Rayez, J.C."],
-        title = u'Isomerization reactions of alkoxy radicals: theoretical study and structure-activity relationships',
-        journal = "Phys. Chem. Chem. Phys.",
-        volume = "5",
-        pages = """4828-4833""",
-        year = "2003",
-        url = "http://warehouse.primekinetics.org/depository/reactions/data/r00000092/rk00000001.xml",
-    ),
-    referenceType = "",
-    shortDesc = u"""""",
-    longDesc = 
-u"""
-Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00000092/rk00000001.xml
-""",
-    history = [
-        ("Tue May 17 14:33:24 2011","Josh Allen <jwallen@mit.edu>","action","""Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00000092/rk00000001.xml"""),
-    ],
-)
-
-entry(
     index = 9,
     label = "r00000093",
     reactant1 = 
