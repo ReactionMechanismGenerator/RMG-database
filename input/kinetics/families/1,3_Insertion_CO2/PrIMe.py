1aa5e39c945b69bf806ea328979e15a36c4e3a2a
Sean Troiano <stroiano7@gmail.com>
2012-07-24 14:59:02 -0400

    NIST Conversion: 1,3_Insertion_CO2/PrIMe/20
    New Entry: NIST/18 (1982ALB/BIG15:2)

--- input/kinetics/families/1,3_Insertion_CO2/PrIMe.py
+++ input/kinetics/families/1,3_Insertion_CO2/PrIMe.py
@@ -9,62 +9,6 @@ longDesc = u"""
 recommended = False
 
 entry(
-    index = 20,
-    label = "r00014891",
-    reactant1 = 
-"""
-1 *3 C 0 {2,S} {3,S} {4,S} {5,S}
-2    C 0 {1,S}
-3    C 0 {1,S}
-4    C 0 {1,S} {6,D}
-5 *1 C 0 {1,S} {7,S} {8,D}
-6    C 0 {4,D}
-7 *2 O 0 {5,S} {9,S}
-8    O 0 {5,D}
-9 *4 H 0 {7,S}
-""",
-    product1 = 
-"""
-1 *1 C 0 {2,D} {3,D}
-2 *2 O 0 {1,D}
-3    O 0 {1,D}
-""",
-    product2 = 
-"""
-1 *3 C 0 {2,S} {3,S} {4,S} {6,S}
-2    C 0 {1,S}
-3    C 0 {1,S}
-4    C 0 {1,S} {5,D}
-5    C 0 {4,D}
-6 *4 H 0 {1,S}
-""",
-    degeneracy = 1,
-    kinetics = Arrhenius(
-        A = (1.03e+11,"s^-1"),
-        n = 0,
-        Ea = (154649,"J/mol"),
-        T0 = (1,"K"),
-    ),
-    reference = Article(
-        authors = ["Al-Borno, A.", "Bigley, D.B."],
-        title = u'Studies in Decarboxylation. Par t 15. The Effects of 3-Substitution on the Rate of Decarboxylation of \u03b2\u03b2\u03c0\u03b2-Unsaturated Acids',
-        journal = "J. Chem. Soc. Perkin Trans. 2",
-        pages = """15""",
-        year = "1982",
-        url = "http://warehouse.primekinetics.org/depository/reactions/data/r00014891/rk00000002.xml",
-    ),
-    referenceType = "",
-    shortDesc = u"""""",
-    longDesc = 
-u"""
-Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00014891/rk00000002.xml
-""",
-    history = [
-        ("Tue May 17 14:35:16 2011","Josh Allen <jwallen@mit.edu>","action","""Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00014891/rk00000002.xml"""),
-    ],
-)
-
-entry(
     index = 22,
     label = "r00015736",
     reactant1 = 
