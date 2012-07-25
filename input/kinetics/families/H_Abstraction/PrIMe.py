d9e06584feffe1200ef416011c01ff49b792a0c6
Sean Troiano <stroiano7@gmail.com>
2012-07-25 12:12:39 -0400

    NIST Conversion: H_Abstraction/PrIMe/339
    New Entry: NIST/1139 (1981ALA/SEL1036:4)

--- input/kinetics/families/H_Abstraction/PrIMe.py
+++ input/kinetics/families/H_Abstraction/PrIMe.py
@@ -470,65 +470,6 @@ Imported from PrIMe database at http://warehouse.primekinetics.org/depository/re
 )
 
 entry(
-    index = 339,
-    label = "r00003018",
-    reactant1 = 
-"""
-1 *1 C 0 {2,S} {3,S}
-2    C 0 {1,S} {4,D}
-3 *2 H 0 {1,S}
-4    O 0 {2,D}
-""",
-    reactant2 = 
-"""
-1    C 0 {2,S} {3,S} {4,S} {5,S}
-2    C 0 {1,S}
-3    C 0 {1,S}
-4    C 0 {1,S}
-5 *3 O 1 {1,S}
-""",
-    product1 = 
-"""
-1    C 0 {2,S} {3,S} {4,S} {5,S}
-2    C 0 {1,S}
-3    C 0 {1,S}
-4    C 0 {1,S}
-5 *1 O 0 {1,S} {6,S}
-6 *2 H 0 {5,S}
-""",
-    product2 = 
-"""
-1 *3 C 1 {2,S}
-2    C 0 {1,S} {3,D}
-3    O 0 {2,D}
-""",
-    degeneracy = 3,
-    kinetics = Arrhenius(
-        A = (57900,"cm^3/(mol*s)"),
-        n = 0,
-        Ea = (15797.5,"J/mol"),
-        T0 = (1,"K"),
-    ),
-    reference = Article(
-        authors = ["Al Akeel, N.Y.", "Selby, K.", "Waddington, D.J."],
-        title = u'Reactions of Oxygenated Radicals in the Gas Phase. Part 8. Reactions of Alkoxyl Radicals with Aldehydes and Ketones',
-        journal = "J. Chem. Soc. Perkin Trans. 2",
-        pages = """1036""",
-        year = "1981",
-        url = "http://warehouse.primekinetics.org/depository/reactions/data/r00003018/rk00000001.xml",
-    ),
-    referenceType = "",
-    shortDesc = u"""""",
-    longDesc = 
-u"""
-Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00003018/rk00000001.xml
-""",
-    history = [
-        ("Tue May 17 14:33:36 2011","Josh Allen <jwallen@mit.edu>","action","""Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00003018/rk00000001.xml"""),
-    ],
-)
-
-entry(
     index = 350,
     label = "r00003205",
     reactant1 = 
