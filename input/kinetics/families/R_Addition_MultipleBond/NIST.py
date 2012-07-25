fe8ba77d3823253b80cfd3dbe699bbf96c4169d6
Sean Troiano <stroiano7@gmail.com>
2012-07-25 16:46:22 -0400

    NIST Conversion: R_Addition_MultipleBond/PrIMe/145
    New Entry: NIST/390 (1977BAL/BAR2483:1)

--- input/kinetics/families/R_Addition_MultipleBond/NIST.py
+++ input/kinetics/families/R_Addition_MultipleBond/NIST.py
@@ -21396,3 +21396,55 @@ Rate expression does not contain tunneling contributions.
     ],
 )
 
+entry(
+    index = 390,
+    label = "1977BAL/BAR2483:1",
+    reactant1 = 
+"""
+1 *3 C 0 {2,S}
+2 *1 C 0 {1,S} {3,S}
+3 *2 O 1 {2,S}
+""",
+    product1 = 
+"""
+1 *1 C 0 {2,D}
+2 *2 O 0 {1,D}
+""",
+    product2 = 
+"""
+1 *3 C 1
+""",
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (5.01e+13,"s^-1","*|/",3.16),
+        n = 0,
+        Ea = (90.628,"kJ/mol","+|-",4.523),
+        T0 = (1,"K"),
+        Tmin = (298,"K"),
+        Tmax = (450,"K"),
+    ),
+    reference = Article(
+        authors = ["Baldwin, A.C.", "Barker, J.R.", "Golden, D.M.", "Hendry, D.G."],
+        title = u'Photochemical smog. Rate parameter estimates and computer simulations',
+        journal = "J. Phys. Chem.",
+        volume = "81",
+        pages = """2483""",
+        year = "1977",
+        url = "http://kinetics.nist.gov/kinetics/Detail?id=1977BAL/BAR2483:1",
+    ),
+    referenceType = "theory",
+    shortDesc = u"""Estimated: thermochemical, kinetic, or other""",
+    longDesc = 
+u"""
+PrIMe Reaction: r00010632
+PrIMe Kinetics: http://warehouse.primekinetics.org/depository/reactions/data/r00010632/rk00000002.xml
+Uncertainty: 3.1600001
+Bath gas: N2
+Category: Theory
+Data type: Estimated: thermochemical, kinetic, or other
+""",
+    history = [
+        ("Wed Jul 25 16:45:53 2012","Sean Troiano <stroiano7@gmail.com>","action","""Imported from NIST database at http://kinetics.nist.gov/kinetics/Detail?id=1977BAL/BAR2483:1"""),
+    ],
+)
+
