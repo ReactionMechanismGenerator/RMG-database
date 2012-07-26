07f764c4d0e298808565e37dd0766787989dc1f2
Sean Troiano <stroiano7@gmail.com>
2012-07-26 18:28:02 -0400

    NIST Conversion: H_Abstraction/PrIMe/535
    New Entry: NIST/1246 (1986ATK555:11)

--- input/kinetics/families/H_Abstraction/NIST.py
+++ input/kinetics/families/H_Abstraction/NIST.py
@@ -74398,3 +74398,73 @@ Analytical technique: Gas chromatography
     ],
 )
 
+entry(
+    index = 1246,
+    label = "1986ATK555:11",
+    reactant1 = 
+"""
+1    C 0 {3,S} {5,S}
+2    C 0 {4,S} {6,S}
+3    C 0 {1,S} {4,S}
+4    C 0 {2,S} {3,S}
+5 *1 C 0 {1,S} {7,S} {9,S}
+6    C 0 {2,S} {8,S}
+7    C 0 {5,S}
+8    C 0 {6,S}
+9 *2 H 0 {5,S}
+""",
+    reactant2 = 
+"""
+1 *3 O 1
+""",
+    product1 = 
+"""
+1 *1 O 0 {2,S}
+2 *2 H 0 {1,S}
+""",
+    product2 = 
+"""
+1    C 0 {2,S} {4,S}
+2    C 0 {1,S} {3,S}
+3    C 0 {2,S} {5,S}
+4    C 0 {1,S} {6,S}
+5    C 0 {3,S} {8,S}
+6    C 0 {4,S}
+7    C 0 {8,S}
+8 *3 C 1 {5,S} {7,S}
+""",
+    degeneracy = 4,
+    kinetics = Arrhenius(
+        A = (5.2,"m^3/(mol*s)"),
+        n = 2,
+        Ea = (-2.569,"kJ/mol"),
+        T0 = (1,"K"),
+        Tmin = (250,"K"),
+        Tmax = (1000,"K"),
+        Pmin = (101000,"Pa"),
+        Pmax = (101000,"Pa"),
+    ),
+    reference = Article(
+        authors = ["Atkinson, R."],
+        title = u'Estimations of OH radical rate constants from H-atom abstraction from C-H and O-H bonds over the temperature range 250-1000K',
+        journal = "Int. J. Chem. Kinet.",
+        volume = "18",
+        pages = """555""",
+        year = "1986",
+        url = "http://kinetics.nist.gov/kinetics/Detail?id=1986ATK555:11",
+    ),
+    referenceType = "theory",
+    shortDesc = u"""Estimated: thermochemical, kinetic, or other""",
+    longDesc = 
+u"""
+PrIMe Reaction: r00005578
+PrIMe Kinetics: http://warehouse.primekinetics.org/depository/reactions/data/r00005578/rk00000002.xml
+Bath gas: N2
+Category: Theory
+Data type: Estimated: thermochemical, kinetic, or other
+""",
+    history = [
+        ("Thu Jul 26 18:27:56 2012","Sean Troiano <stroiano7@gmail.com>","action","""Imported from NIST database at http://kinetics.nist.gov/kinetics/Detail?id=1986ATK555:11"""),
+    ],
+)
+
