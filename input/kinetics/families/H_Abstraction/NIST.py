84a1acb356b7f6a7efc0ef7f87f104d9a16d12dc
Sean Troiano <stroiano7@gmail.com>
2012-07-26 18:30:17 -0400

    NIST Conversion: H_Abstraction/PrIMe/541
    New Entry: NIST/1249 (1986ATK555:13)

--- input/kinetics/families/H_Abstraction/NIST.py
+++ input/kinetics/families/H_Abstraction/NIST.py
@@ -74607,3 +74607,73 @@ Analytical technique: Gas chromatography
     ],
 )
 
+entry(
+    index = 1249,
+    label = "1986ATK555:13",
+    reactant1 = 
+"""
+1    C 0 {3,S} {5,S}
+2    C 0 {4,S} {6,S}
+3 *1 C 0 {1,S} {4,S} {9,S}
+4    C 0 {2,S} {3,S}
+5    C 0 {1,S} {7,S}
+6    C 0 {2,S} {8,S}
+7    C 0 {5,S}
+8    C 0 {6,S}
+9 *2 H 0 {3,S}
+""",
+    reactant2 = 
+"""
+1 *3 O 1
+""",
+    product1 = 
+"""
+1 *1 O 0 {2,S}
+2 *2 H 0 {1,S}
+""",
+    product2 = 
+"""
+1    C 0 {2,S} {5,S}
+2    C 0 {1,S} {7,S}
+3    C 0 {4,S} {6,S}
+4    C 0 {3,S} {8,S}
+5    C 0 {1,S} {8,S}
+6    C 0 {3,S}
+7    C 0 {2,S}
+8 *3 C 1 {4,S} {5,S}
+""",
+    degeneracy = 4,
+    kinetics = Arrhenius(
+        A = (5.2,"m^3/(mol*s)"),
+        n = 2,
+        Ea = (-3.201,"kJ/mol"),
+        T0 = (1,"K"),
+        Tmin = (250,"K"),
+        Tmax = (1000,"K"),
+        Pmin = (101000,"Pa"),
+        Pmax = (101000,"Pa"),
+    ),
+    reference = Article(
+        authors = ["Atkinson, R."],
+        title = u'Estimations of OH radical rate constants from H-atom abstraction from C-H and O-H bonds over the temperature range 250-1000K',
+        journal = "Int. J. Chem. Kinet.",
+        volume = "18",
+        pages = """555""",
+        year = "1986",
+        url = "http://kinetics.nist.gov/kinetics/Detail?id=1986ATK555:13",
+    ),
+    referenceType = "theory",
+    shortDesc = u"""Estimated: thermochemical, kinetic, or other""",
+    longDesc = 
+u"""
+PrIMe Reaction: r00005581
+PrIMe Kinetics: http://warehouse.primekinetics.org/depository/reactions/data/r00005581/rk00000002.xml
+Bath gas: N2
+Category: Theory
+Data type: Estimated: thermochemical, kinetic, or other
+""",
+    history = [
+        ("Thu Jul 26 18:30:01 2012","Sean Troiano <stroiano7@gmail.com>","action","""Imported from NIST database at http://kinetics.nist.gov/kinetics/Detail?id=1986ATK555:13"""),
+    ],
+)
+
--- input/kinetics/families/H_Abstraction/PrIMe.py
+++ input/kinetics/families/H_Abstraction/PrIMe.py
@@ -525,68 +525,6 @@ Imported from PrIMe database at http://warehouse.primekinetics.org/depository/re
 )
 
 entry(
-    index = 541,
-    label = "r00005581",
-    reactant1 = 
-"""
-1    C 0 {3,S} {5,S}
-2    C 0 {4,S} {6,S}
-3 *1 C 0 {1,S} {4,S} {9,S}
-4    C 0 {2,S} {3,S}
-5    C 0 {1,S} {7,S}
-6    C 0 {2,S} {8,S}
-7    C 0 {5,S}
-8    C 0 {6,S}
-9 *2 H 0 {3,S}
-""",
-    reactant2 = 
-"""
-1 *3 O 1
-""",
-    product1 = 
-"""
-1 *1 O 0 {2,S}
-2 *2 H 0 {1,S}
-""",
-    product2 = 
-"""
-1    C 0 {2,S} {5,S}
-2    C 0 {1,S} {7,S}
-3    C 0 {4,S} {6,S}
-4    C 0 {3,S} {8,S}
-5    C 0 {1,S} {8,S}
-6    C 0 {3,S}
-7    C 0 {2,S}
-8 *3 C 1 {4,S} {5,S}
-""",
-    degeneracy = 4,
-    kinetics = Arrhenius(
-        A = (5.20247,"cm^3/(mol*s)"),
-        n = 2,
-        Ea = (-3201.07,"J/mol"),
-        T0 = (1,"K"),
-    ),
-    reference = Article(
-        authors = ["Atkinson, R."],
-        title = u'Estimations of OH radical rate constants from H-atom abstraction from C-H and O-H bonds over the temperature range 250-1000K',
-        journal = "Int. J. Chem. Kinet.",
-        volume = "18",
-        pages = """555""",
-        year = "1986",
-        url = "http://warehouse.primekinetics.org/depository/reactions/data/r00005581/rk00000002.xml",
-    ),
-    referenceType = "",
-    shortDesc = u"""""",
-    longDesc = 
-u"""
-Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00005581/rk00000002.xml
-""",
-    history = [
-        ("Tue May 17 14:33:53 2011","Josh Allen <jwallen@mit.edu>","action","""Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00005581/rk00000002.xml"""),
-    ],
-)
-
-entry(
     index = 542,
     label = "r00005581",
     reactant1 = 
