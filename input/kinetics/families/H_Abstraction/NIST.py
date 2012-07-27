ec2f76c594ea27fba8aca01f207a233099905c52
Sean Troiano <stroiano7@gmail.com>
2012-07-27 09:51:51 -0400

    NIST Conversion: H_Abstraction/PrIMe/630
    New Entry: NIST/1268 (1991COH397-417:35)

--- input/kinetics/families/H_Abstraction/NIST.py
+++ input/kinetics/families/H_Abstraction/NIST.py
@@ -75865,3 +75865,74 @@ Data type: Transition state theory
     ],
 )
 
+entry(
+    index = 1268,
+    label = "1991COH397-417:35",
+    reactant1 = 
+"""
+1     C 0 {3,S} {7,S}
+2     C 0 {4,S} {8,S}
+3     C 0 {1,S} {5,S}
+4     C 0 {2,S} {6,S}
+5     C 0 {3,S} {6,S}
+6     C 0 {4,S} {5,S}
+7  *1 C 0 {1,S} {9,S} {11,S}
+8     C 0 {2,S} {10,S}
+9     C 0 {7,S}
+10    C 0 {8,S}
+11 *2 H 0 {7,S}
+""",
+    reactant2 = 
+"""
+1 *3 O 1
+""",
+    product1 = 
+"""
+1 *1 O 0 {2,S}
+2 *2 H 0 {1,S}
+""",
+    product2 = 
+"""
+1     C 0 {2,S} {6,S}
+2     C 0 {1,S} {4,S}
+3     C 0 {4,S} {5,S}
+4     C 0 {2,S} {3,S}
+5     C 0 {3,S} {7,S}
+6     C 0 {1,S} {8,S}
+7     C 0 {5,S} {10,S}
+8     C 0 {6,S}
+9     C 0 {10,S}
+10 *3 C 1 {7,S} {9,S}
+""",
+    degeneracy = 4,
+    kinetics = Arrhenius(
+        A = (1.99,"m^3/(mol*s)"),
+        n = 2,
+        Ea = (-2.494,"kJ/mol"),
+        T0 = (1,"K"),
+        Tmin = (298,"K"),
+        Tmax = (500,"K"),
+    ),
+    reference = Article(
+        authors = ["Cohen, N."],
+        title = u'Are reaction rate coefficients additive? Revised transition state theory calculations for OH + alkane reactions',
+        journal = "Int. J. Chem. Kinet.",
+        volume = "23",
+        pages = """397-417""",
+        year = "1991",
+        url = "http://kinetics.nist.gov/kinetics/Detail?id=1991COH397-417:35",
+    ),
+    referenceType = "theory",
+    shortDesc = u"""Transition state theory""",
+    longDesc = 
+u"""
+PrIMe Reaction: r00006130
+PrIMe Kinetics: http://warehouse.primekinetics.org/depository/reactions/data/r00006130/rk00000001.xml
+Category: Theory
+Data type: Transition state theory
+""",
+    history = [
+        ("Fri Jul 27 09:50:24 2012","Sean Troiano <stroiano7@gmail.com>","action","""Imported from NIST database at http://kinetics.nist.gov/kinetics/Detail?id=1991COH397-417:35"""),
+    ],
+)
+
