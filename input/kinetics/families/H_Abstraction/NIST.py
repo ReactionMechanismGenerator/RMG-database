9d22681028ae53080db4de482aef542a748769d4
Sean Troiano <stroiano7@gmail.com>
2012-07-27 09:29:14 -0400

    NIST Conversion: H_Abstraction/PrIMe/582
    New Entry: NIST/1257 (1991TSA221-273:54)

--- input/kinetics/families/H_Abstraction/NIST.py
+++ input/kinetics/families/H_Abstraction/NIST.py
@@ -75148,3 +75148,61 @@ The listed rate expression is a fit derived at NIST to the calculated rate const
     ],
 )
 
+entry(
+    index = 1257,
+    label = "1991TSA221-273:54",
+    reactant1 = 
+"""
+1    C 0 {2,S}
+2    C 0 {1,S} {3,D}
+3 *1 C 0 {2,D} {4,S}
+4 *2 H 0 {3,S}
+""",
+    reactant2 = 
+"""
+1 *3 O 1
+""",
+    product1 = 
+"""
+1 *1 O 0 {2,S}
+2 *2 H 0 {1,S}
+""",
+    product2 = 
+"""
+1    C 0 {2,S}
+2    C 0 {1,S} {3,D}
+3 *3 C 1 {2,D}
+""",
+    degeneracy = 2,
+    kinetics = Arrhenius(
+        A = (2.14,"m^3/(mol*s)","*|/",2),
+        n = 2,
+        Ea = (11.64,"kJ/mol"),
+        T0 = (1,"K"),
+        Tmin = (300,"K"),
+        Tmax = (2500,"K"),
+    ),
+    reference = Article(
+        authors = ["Tsang, W."],
+        title = u'Chemical kinetic data base for combustion chemistry. Part V. Propene',
+        journal = "J. Phys. Chem. Ref. Data",
+        volume = "20",
+        pages = """221-273""",
+        year = "1991",
+        url = "http://kinetics.nist.gov/kinetics/Detail?id=1991TSA221-273:54",
+    ),
+    referenceType = "review",
+    shortDesc = u"""Extensive literature review""",
+    longDesc = 
+u"""
+PrIMe Reaction: r00005689
+PrIMe Kinetics: http://warehouse.primekinetics.org/depository/reactions/data/r00005689/rk00000003.xml
+Uncertainty: 2.0
+Category: Review
+Data type: Extensive literature review
+""",
+    history = [
+        ("Fri Jul 27 09:28:59 2012","Sean Troiano <stroiano7@gmail.com>","action","""Imported from NIST database at http://kinetics.nist.gov/kinetics/Detail?id=1991TSA221-273:54"""),
+    ],
+)
+
