beeca189d12ad1ca87a4d9c68d848cc59ccdcc1d
Sean Troiano <stroiano7@gmail.com>
2012-07-27 12:03:27 -0400

    NIST Conversion: H_Abstraction/PrIMe/1136
    New Entry: NIST/1300 (1994BAU/COB847-1033:90)

--- input/kinetics/families/H_Abstraction/PrIMe.py
+++ input/kinetics/families/H_Abstraction/PrIMe.py
@@ -1276,58 +1276,6 @@ Imported from PrIMe database at http://warehouse.primekinetics.org/depository/re
 )
 
 entry(
-    index = 1136,
-    label = "r00013692",
-    reactant1 = 
-"""
-1 *1 O 0 {2,S} {3,S}
-2    O 1 {1,S}
-3 *2 H 0 {1,S}
-""",
-    reactant2 = 
-"""
-1    O 0 {2,S}
-2 *3 O 1 {1,S}
-""",
-    product1 = 
-"""
-1    O 0 {2,S}
-2 *1 O 0 {1,S} {3,S}
-3 *2 H 0 {2,S}
-""",
-    product2 = 
-"""
-1 *3 O 1 {2,S}
-2    O 1 {1,S}
-""",
-    degeneracy = 1,
-    kinetics = Arrhenius(
-        A = (4.22e+08,"cm^3/(mol*s)"),
-        n = 0,
-        Ea = (50136.3,"J/mol"),
-        T0 = (1,"K"),
-    ),
-    reference = Article(
-        authors = ["Baulch, D.L.", "Cobos, C.J.", "Cox, R.A.", "Frank, P.", "Hayman, G.", "Just, Th.", "Kerr, J.A.", "Murrells, T.", "Pilling, M.J.", "Troe, J.", "Walker, R.W.", "Warnatz, J."],
-        title = u'Evaluated kinetic data for combusion modelling. Supplement I',
-        journal = "J. Phys. Chem. Ref. Data",
-        volume = "23",
-        pages = """847-1033""",
-        year = "1994",
-        url = "http://warehouse.primekinetics.org/depository/reactions/data/r00013692/rk00000040.xml",
-    ),
-    referenceType = "",
-    shortDesc = u"""""",
-    longDesc = 
-u"""
-Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00013692/rk00000040.xml
-""",
-    history = [
-        ("Tue May 17 14:35:13 2011","Josh Allen <jwallen@mit.edu>","action","""Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00013692/rk00000040.xml"""),
-    ],
-)
-
-entry(
     index = 1137,
     label = "r00013692",
     reactant1 = 
