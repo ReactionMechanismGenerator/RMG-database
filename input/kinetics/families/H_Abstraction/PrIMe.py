1d098b1a2e0fdf99fa547a9caac9b0ee90197d6c
Sean Troiano <stroiano7@gmail.com>
2012-07-27 10:57:34 -0400

    NIST Conversion: H_Abstraction/PrIMe/930
    New Entry: NIST/276 (1996MA/LIU37-44:1)

--- input/kinetics/families/H_Abstraction/PrIMe.py
+++ input/kinetics/families/H_Abstraction/PrIMe.py
@@ -886,54 +886,6 @@ Imported from PrIMe database at http://warehouse.primekinetics.org/depository/re
 )
 
 entry(
-    index = 930,
-    label = "r00010849",
-    reactant1 = 
-"""
-1 *3 C 1
-""",
-    reactant2 = 
-"""
-1 *1 O 0 {2,S}
-2 *2 H 0 {1,S}
-""",
-    product1 = 
-"""
-1 *1 C 0 {2,S}
-2 *2 H 0 {1,S}
-""",
-    product2 = 
-"""
-1 *3 O 1
-""",
-    degeneracy = 2,
-    kinetics = Arrhenius(
-        A = (6.39058e-06,"cm^3/(mol*s)"),
-        n = 3.31,
-        Ea = (52547.5,"J/mol"),
-        T0 = (1,"K"),
-    ),
-    reference = Article(
-        authors = ["Ma, S.", "Liu, R."],
-        title = u'Theoretical studies on the reaction path and dynamics of the reaction CH_3+H_2O \u2192 CH_4+OH',
-        journal = "Sci. China Ser. B",
-        volume = "39",
-        pages = """37-44""",
-        year = "1996",
-        url = "http://warehouse.primekinetics.org/depository/reactions/data/r00010849/rk00000004.xml",
-    ),
-    referenceType = "",
-    shortDesc = u"""""",
-    longDesc = 
-u"""
-Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00010849/rk00000004.xml
-""",
-    history = [
-        ("Tue May 17 14:34:41 2011","Josh Allen <jwallen@mit.edu>","action","""Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00010849/rk00000004.xml"""),
-    ],
-)
-
-entry(
     index = 931,
     label = "r00010849",
     reactant1 = 
