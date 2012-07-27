3f3815e1466df2cf187bd9271b975154fecf7907
Sean Troiano <stroiano7@gmail.com>
2012-07-27 10:49:08 -0400

    NIST Conversion: H_Abstraction/PrIMe/800
    New Entry: NIST/195 (1996LI/WIL1017-1024:3)

--- input/kinetics/families/H_Abstraction/PrIMe.py
+++ input/kinetics/families/H_Abstraction/PrIMe.py
@@ -656,56 +656,6 @@ Imported from PrIMe database at http://warehouse.primekinetics.org/depository/re
 )
 
 entry(
-    index = 800,
-    label = "r00009476",
-    reactant1 = 
-"""
-1 *1 C 0 {2,S} {3,S}
-2    O 0 {1,S}
-3 *2 H 0 {1,S}
-""",
-    reactant2 = 
-"""
-1 *3 H 1
-""",
-    product1 = 
-"""
-1 *1 H 0 {2,S}
-2 *2 H 0 {1,S}
-""",
-    product2 = 
-"""
-1 *3 C 1 {2,S}
-2    O 0 {1,S}
-""",
-    degeneracy = 3,
-    kinetics = Arrhenius(
-        A = (16.4407,"cm^3/(mol*s)"),
-        n = 2,
-        Ea = (18873.9,"J/mol"),
-        T0 = (1,"K"),
-    ),
-    reference = Article(
-        authors = ["Li, S.C.", "Williams, F.A."],
-        title = u'Experimental and numerical studies of two-stage methanol flames',
-        journal = "Symp. Int. Combust. Proc.",
-        volume = "26",
-        pages = """1017-1024""",
-        year = "1996",
-        url = "http://warehouse.primekinetics.org/depository/reactions/data/r00009476/rk00000008.xml",
-    ),
-    referenceType = "",
-    shortDesc = u"""""",
-    longDesc = 
-u"""
-Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00009476/rk00000008.xml
-""",
-    history = [
-        ("Tue May 17 14:34:25 2011","Josh Allen <jwallen@mit.edu>","action","""Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00009476/rk00000008.xml"""),
-    ],
-)
-
-entry(
     index = 803,
     label = "r00009477",
     reactant1 = 
