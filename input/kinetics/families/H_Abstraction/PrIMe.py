e139efb56c589553a95619229262a53dff7396f6
Sean Troiano <stroiano7@gmail.com>
2012-07-27 10:24:13 -0400

    NIST Conversion: H_Abstraction/PrIMe/696
    New Entry: NIST/1283 (1991COH397-417:48)

--- input/kinetics/families/H_Abstraction/PrIMe.py
+++ input/kinetics/families/H_Abstraction/PrIMe.py
@@ -518,68 +518,6 @@ Imported from PrIMe database at http://warehouse.primekinetics.org/depository/re
 )
 
 entry(
-    index = 696,
-    label = "r00007395",
-    reactant1 = 
-"""
-1    C 0 {3,S} {4,S} {5,S} {6,S}
-2 *1 C 0 {3,S} {7,S} {8,S} {9,S}
-3    C 0 {1,S} {2,S}
-4    C 0 {1,S}
-5    C 0 {1,S}
-6    C 0 {1,S}
-7    C 0 {2,S}
-8    C 0 {2,S}
-9 *2 H 0 {2,S}
-""",
-    reactant2 = 
-"""
-1 *3 O 1
-""",
-    product1 = 
-"""
-1 *1 O 0 {2,S}
-2 *2 H 0 {1,S}
-""",
-    product2 = 
-"""
-1    C 0 {2,S} {3,S} {4,S} {5,S}
-2    C 0 {1,S} {8,S}
-3    C 0 {1,S}
-4    C 0 {1,S}
-5    C 0 {1,S}
-6    C 0 {8,S}
-7    C 0 {8,S}
-8 *3 C 1 {2,S} {6,S} {7,S}
-""",
-    degeneracy = 1,
-    kinetics = Arrhenius(
-        A = (1.698,"cm^3/(mol*s)"),
-        n = 1.9,
-        Ea = (-6069.56,"J/mol"),
-        T0 = (1,"K"),
-    ),
-    reference = Article(
-        authors = ["Cohen, N."],
-        title = u'Are reaction rate coefficients additive? Revised transition state theory calculations for OH + alkane reactions',
-        journal = "Int. J. Chem. Kinet.",
-        volume = "23",
-        pages = """397-417""",
-        year = "1991",
-        url = "http://warehouse.primekinetics.org/depository/reactions/data/r00007395/rk00000002.xml",
-    ),
-    referenceType = "",
-    shortDesc = u"""""",
-    longDesc = 
-u"""
-Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00007395/rk00000002.xml
-""",
-    history = [
-        ("Tue May 17 14:34:07 2011","Josh Allen <jwallen@mit.edu>","action","""Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00007395/rk00000002.xml"""),
-    ],
-)
-
-entry(
     index = 697,
     label = "r00007396",
     reactant1 = 
