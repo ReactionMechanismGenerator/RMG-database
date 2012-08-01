3a8eec5f1c3b39d60564192e28a21c335487ff35
Sean Troiano <stroiano7@gmail.com>
2012-08-01 12:10:11 -0400

    NIST Conversion: R_Recombination/PrIMe/494
    New Entry: NIST/494 (1986TSA/HAM1087:14)

--- input/kinetics/families/R_Recombination/PrIMe.py
+++ input/kinetics/families/R_Recombination/PrIMe.py
@@ -3840,49 +3840,6 @@ Imported from PrIMe database at http://warehouse.primekinetics.org/depository/re
 )
 
 entry(
-    index = 494,
-    label = "r00010887",
-    reactant1 = 
-"""
-1 *1 C 0 {2,S}
-2 *2 H 0 {1,S}
-""",
-    product1 = 
-"""
-1 * C 1
-""",
-    product2 = 
-"""
-1 * H 1
-""",
-    degeneracy = 8,
-    kinetics = Arrhenius(
-        A = (3.7e+15,"s^-1"),
-        n = 0,
-        Ea = (434015,"J/mol"),
-        T0 = (1,"K"),
-    ),
-    reference = Article(
-        authors = ["Tsang, W."],
-        title = u'Rate constants for the decomposition and formation of simple alkanes over extended temperature and pressure ranges',
-        journal = "Combust. Flame",
-        volume = "78",
-        pages = """71-86""",
-        year = "1989",
-        url = "http://warehouse.primekinetics.org/depository/reactions/data/r00010887/rk00000062.xml",
-    ),
-    referenceType = "",
-    shortDesc = u"""""",
-    longDesc = 
-u"""
-Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00010887/rk00000062.xml
-""",
-    history = [
-        ("Tue May 17 14:34:41 2011","Josh Allen <jwallen@mit.edu>","action","""Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00010887/rk00000062.xml"""),
-    ],
-)
-
-entry(
     index = 495,
     label = "r00010887",
     reactant1 = 
