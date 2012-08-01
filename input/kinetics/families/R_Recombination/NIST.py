6c33f60e02c6a483fd0efa79cd3b7042473d9dfe
Sean Troiano <stroiano7@gmail.com>
2012-08-01 11:48:15 -0400

    NIST Conversion: R_Recombination/PrIMe/463
    New Entry: NIST/479 (1985COB/TRO1010-1015:6)

--- input/kinetics/families/R_Recombination/NIST.py
+++ input/kinetics/families/R_Recombination/NIST.py
@@ -27583,3 +27583,54 @@ Data type: Estimated: thermochemical, kinetic, or other
     ],
 )
 
+entry(
+    index = 479,
+    label = "1985COB/TRO1010-1015:6",
+    reactant1 = 
+"""
+1 * C 1
+""",
+    reactant2 = 
+"""
+1 * O 1 {2,S}
+2   O 1 {1,S}
+""",
+    product1 = 
+"""
+1 *1 C 0 {2,S}
+2 *2 O 0 {1,S} {3,S}
+3    O 1 {2,S}
+""",
+    degeneracy = 4,
+    kinetics = Arrhenius(
+        A = (3.88e+06,"m^3/(mol*s)"),
+        n = 0,
+        Ea = (2.661,"kJ/mol"),
+        T0 = (1,"K"),
+        Tmin = (298,"K"),
+        Tmax = (1500,"K"),
+    ),
+    reference = Article(
+        authors = ["Cobos, C.J.", "Troe, J."],
+        title = u'Theory of thermal unimolecular reactions at high pressures. II. Analysis of experimental results',
+        journal = "J. Chem. Phys.",
+        volume = "83",
+        pages = """1010-1015""",
+        year = "1985",
+        url = "http://kinetics.nist.gov/kinetics/Detail?id=1985COB/TRO1010-1015:6",
+    ),
+    referenceType = "theory",
+    shortDesc = u"""Transition state theory""",
+    longDesc = 
+u"""
+PrIMe Reaction: r00010854
+PrIMe Kinetics: http://warehouse.primekinetics.org/depository/reactions/data/r00010854/rk00000028.xml
+Bath gas: Products
+Category: Theory
+Data type: Transition state theory
+""",
+    history = [
+        ("Wed Aug 01 11:48:03 2012","Sean Troiano <stroiano7@gmail.com>","action","""Imported from NIST database at http://kinetics.nist.gov/kinetics/Detail?id=1985COB/TRO1010-1015:6"""),
+    ],
+)
+
--- input/kinetics/families/R_Recombination/PrIMe.py
+++ input/kinetics/families/R_Recombination/PrIMe.py
@@ -3446,51 +3446,6 @@ Imported from PrIMe database at http://warehouse.primekinetics.org/depository/re
 )
 
 entry(
-    index = 463,
-    label = "r00010854",
-    reactant1 = 
-"""
-1 * C 1
-""",
-    reactant2 = 
-"""
-1 * O 1 {2,S}
-2   O 1 {1,S}
-""",
-    product1 = 
-"""
-1 *1 C 0 {2,S}
-2 *2 O 0 {1,S} {3,S}
-3    O 1 {2,S}
-""",
-    degeneracy = 4,
-    kinetics = Arrhenius(
-        A = (3.88e+06,"cm^3/(mol*s)"),
-        n = 0,
-        Ea = (2660.63,"J/mol"),
-        T0 = (1,"K"),
-    ),
-    reference = Article(
-        authors = ["Cobos, C.J.", "Troe, J."],
-        title = u'Theory of thermal unimolecular reactions at high pressures. II. Analysis of experimental results',
-        journal = "J. Chem. Phys.",
-        volume = "83",
-        pages = """1010-1015""",
-        year = "1985",
-        url = "http://warehouse.primekinetics.org/depository/reactions/data/r00010854/rk00000028.xml",
-    ),
-    referenceType = "",
-    shortDesc = u"""""",
-    longDesc = 
-u"""
-Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00010854/rk00000028.xml
-""",
-    history = [
-        ("Tue May 17 14:34:41 2011","Josh Allen <jwallen@mit.edu>","action","""Imported from PrIMe database at http://warehouse.primekinetics.org/depository/reactions/data/r00010854/rk00000028.xml"""),
-    ],
-)
-
-entry(
     index = 464,
     label = "r00010854",
     reactant1 = 
