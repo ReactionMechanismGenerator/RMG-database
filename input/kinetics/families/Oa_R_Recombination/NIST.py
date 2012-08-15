b0de3697476f2193c65a6537057139ab3e277330
Sean Troiano <stroiano7@gmail.com>
2012-08-15 11:27:30 -0400

    Added blank NIST depositories for remaining families

---
+++ input/kinetics/families/Oa_R_Recombination/NIST.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "Oa_R_Recombination/NIST"
+shortDesc = u""
+longDesc = u"""
+
+"""
+recommended = False
+
---
+++ input/kinetics/families/R_Addition_COm/NIST.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "R_Addition_COm/NIST"
+shortDesc = u""
+longDesc = u"""
+
+"""
+recommended = False
+
