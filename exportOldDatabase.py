4ef8bafde9a0d7fa43b7c87975c9f3b6d29faba7
Sean Troiano <stroiano7@gmail.com>
2012-08-20 11:52:41 -0400

    Modifications to exportOldDatabase script
    - Made script importable
    - Added command-line arguments for input and output paths

--- exportOldDatabase.py
+++ exportOldDatabase.py
@@ -2,11 +2,12 @@
 # encoding: utf-8
 
 """
-This script exports the database to the old RMG-Java format. The script 
-requires two command-line arguments: the path to the database to import, and 
+This script exports the database to the old RMG-Java format. The script
+requires two command-line arguments: the path to the database to import, and
 the path to save the old RMG-Java database to.
 """
 
+import os
 import sys
 
 from rmgpy.data.rmg import RMGDatabase
@@ -14,9 +15,34 @@ from rmgpy.data.base import Entry
 from rmgpy.reaction import Reaction
 from rmgpy.kinetics import Arrhenius, ArrheniusEP, KineticsData
 
-################################################################################
+###############################################################################
 
-def generateAdditionalRateRules(family, db):
+
+def main():
+
+    if len(sys.argv) != 3:
+        raise Exception('You must pass input and output '
+                        'directories as parameters.')
+
+    newPath = sys.argv[1]
+    oldPath = os.path.join(sys.argv[2], 'RMG_database')
+
+    print 'Loading the new RMG-Py database...'
+    database = RMGDatabase()
+    database.load(newPath)
+
+    print 'Constructing additional rate rules from kinetics depository...'
+    for family in database.kinetics.families.values():
+        generateRules(family, database)
+
+    print 'Saving old RMG-Java database...'
+    database.saveOld(oldPath)
+
+
+###############################################################################
+
+
+def generateRules(family, database):
     """
     For a given reaction `family` label, generate additional rate rules from
     the corresponding depository training set. This function does automatically
@@ -24,93 +50,70 @@ def generateAdditionalRateRules(family, db):
     kinetics found in the literature, i.e. determine the groups involved,
     adjust to a per-site basis, etc.
     """
-    
-    # Find the database components we will need
+
+    # Load rules and determine starting index
     rules = family.rules
-    groups = family.groups
-    
     index = max([entry.index for entry in rules.entries.values()] or [0]) + 1
-    
+
+    # Load training entries
     for depository in family.depositories:
         if 'training' in depository.name:
+            entries = sorted(depository.entries.values(),
+                             key=lambda entry: (entry.index, entry.label))
             break
-        
-    entries = depository.entries.values()
-    entries.sort(key=lambda x: (x.index, x.label))
-    
-    for entry0 in entries:
-        
-        reaction, template = db.kinetics.getForwardReactionForFamilyEntry(entry=entry0,
-                                                                          family=family.name,
-                                                                          thermoDatabase=database.thermo)
-        
-        # We must convert the kinetics to ArrheniusEP to finish our rate rule
-        kinetics = reaction.kinetics
-        # If kinetics are KineticsData, then first convert to Arrhenius
-        if isinstance(kinetics, KineticsData):
-            kinetics = Arrhenius().fitToData(
-                Tdata=kinetics.Tdata.values,
-                kdata=kinetics.kdata.values,
-                kunits=kinetics.kdata.units,
-                T0=1,
-            )
-        
-        # Now convert from Arrhenius to ArrheniusEP
-        # If not Arrhenius (or KineticsData), then skip this entry
-        if isinstance(kinetics, Arrhenius):
-            if kinetics.T0.value != 1:
-                kinetics.changeT0(1)
-            kinetics = ArrheniusEP(
-                A = kinetics.A,
-                n = kinetics.n,
-                alpha = 0,
-                E0 = kinetics.Ea,
-                Tmin = kinetics.Tmin,
-                Tmax = kinetics.Tmax,
-            )
-        else:
-            break
-        
-        # Put the kinetics on a per-site basis
-        kinetics.A.value /= reaction.degeneracy
-            
-        # Construct a new entry for the new rate rule
-        label = ';'.join([group.label for group in template])
-        item = Reaction(
-            reactants = template[:],
-            products = None,
-        )
-        entry = Entry(
-            index = index,
-            label = label,
-            item = item,
-            data = kinetics,
-            reference = entry0.reference,
-            rank = entry0.rank,
-            shortDesc = entry0.shortDesc,
-            longDesc = entry0.longDesc,
-            history = entry0.history,
-        )
 
-        # Add the new rate rule to the depository of rate rules
-        rules.entries[entry.index] = entry
-        
+    # Generate a rate rule for each training entry
+    for entry in entries:
+
+        # Load entry's reaction, template, and kinetics
+        reaction, template = database.kinetics.getForwardReactionForFamilyEntry(entry=entry,
+                                                                                family=family.name,
+                                                                                thermoDatabase=database.thermo)
+        kinetics = reaction.kinetics
+
+        # Convert KineticsData to Arrhenius
+        if isinstance(kinetics, KineticsData):
+            kinetics = Arrhenius().fitToData(Tdata=kinetics.Tdata.values,
+                                             kdata=kinetics.kdata.values,
+                                             kunits=kinetics.kdata.units,
+                                             T0=1)
+
+        # Ignore other kinetics types
+        if not isinstance(kinetics, Arrhenius):
+            continue
+
+        # Change reference temperature to 1 K if necessary
+        if kinetics.T0.value != 1:
+            kinetics = kinetics.changeT0(1)
+
+        # Convert kinetics to a per-site basis
+        kinetics.A.value /= reaction.degeneracy
+
+        # Convert to ArrheniusEP
+        kinetics = ArrheniusEP(A=kinetics.A,
+                               n=kinetics.n,
+                               alpha=0,
+                               E0=kinetics.Ea,
+                               Tmin=kinetics.Tmin,
+                               Tmax=kinetics.Tmax)
+
+        # Add new rate rule
+        rules.entries[index] = Entry(index=index,
+                                     label=';'.join([group.label for group in template]),
+                                     item=Reaction(reactants=template[:],
+                                                   products=None),
+                                     data=kinetics,
+                                     reference=entry.reference,
+                                     rank=entry.rank,
+                                     shortDesc=entry.shortDesc,
+                                     longDesc=entry.longDesc,
+                                     history=entry.history)
         index += 1
-            
-################################################################################
+
+
+###############################################################################
+
 
 if __name__ == '__main__':
 
-    oldPath = 'output/RMG_database'
-    newPath = 'input'
-    
-    print 'Loading the new RMG-Py database...'
-    database = RMGDatabase()
-    database.load(newPath)
-    
-    print 'Constructing additional rate rules from kinetics depository...'
-    for family in database.kinetics.families.values():
-        generateAdditionalRateRules(family, database)
-    
-    print 'Saving old RMG-Java database...'
-    database.saveOld(oldPath)
+    main()
