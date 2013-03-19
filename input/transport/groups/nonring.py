bc19535e4704e42930982eae6ad7b00db7d594b3
Jacob Barlow <barlow.ja@husky.neu.edu>
2013-03-19 15:18:26 -0400

    Added the nonring and ring group data to the transport groups database
    This data came from the old RMG database and I used a script to convert it into the new python format.  It represents the critical point group contributions.

---
+++ input/transport/groups/nonring.py
@@ -0,0 +1,744 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+entry(
+    index = 1,
+    label = "CsH3R",
+    molecule =
+"""
+1 * C 0 {2,S} {3,S} {4,S} {5,S}
+2 R!H 0 {1,S}
+3 H 0 {1,S}
+4 H 0 {1,S}
+5 H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0141,
+        Pc = -0.0012,
+        Vc = 65,
+        Tb = 23.58,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for CsH3R""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 2,
+    label = "CsH2R2",
+    molecule =
+"""
+1 * C 0 {2,S} {3,S} {4,S} {5,S}
+2 R!H 0 {1,S}
+3 R!H 0 {1,S}
+4 H 0 {1,S}
+5 H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0189,
+        Pc = 0,
+        Vc = 56,
+        Tb = 22.88,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for CsH2R2""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 3,
+    label = "CsHR3",
+    molecule =
+"""
+1 * C 0 {2,S} {3,S} {4,S} {5,S}
+2 R!H 0 {1,S}
+3 R!H 0 {1,S}
+4 R!H 0 {1,S}
+5 H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0164,
+        Pc = 0.0020,
+        Vc = 41,
+        Tb = 21.74,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for CsHR3""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 4,
+    label = "CsR4",
+    molecule =
+"""
+1 * C 0 {2,S} {3,S} {4,S} {5,S}
+2 R!H 0 {1,S}
+3 R!H 0 {1,S}
+4 R!H 0 {1,S}
+5 R!H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0067,
+        Pc = 0.0043,
+        Vc = 27,
+        Tb = 18.25,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for CsR4""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 5,
+    label = "CdH2R",
+    molecule =
+"""
+1 * C 0 {2,D} {3,S} {4,S}
+2 R!H 0 {1,D}
+3 H 0 {1,S}
+4 H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0113,
+        Pc = -0.0028,
+        Vc = 56,
+        Tb = 18.18,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for CdH2R""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 6,
+    label = "CdHR2",
+    molecule =
+"""
+1 * C 0 {2,D} {3,S} {4,S}
+2 R!H 0 {1,D}
+3 R!H 0 {1,S}
+4 H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0129,
+        Pc = -0.0006,
+        Vc = 46,
+        Tb = 24.96,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for CdHR2""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 7,
+    label = "CdR3",
+    molecule =
+"""
+1 * C 0 {2,D} {3,S} {4,S}
+2 R!H 0 {1,D}
+3 R!H 0 {1,S}
+4 R!H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0117,
+        Pc = 0.0011,
+        Vc = 38,
+        Tb = 24.14,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for CdR3""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 8,
+    label = "CddR2",
+    molecule =
+"""
+1 * C 0 {2,D} {3,D}
+2 R!H 0 {1,D}
+3 R!H 0 {1,D}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0026,
+        Pc = 0.0028,
+        Vc = 36,
+        Tb = 26.15,
+        structureIndex = 0,
+    ),
+    shortDesc = u"""nonring_library value for CddR2""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 9,
+    label = "CtHR",
+    molecule =
+"""
+1 * C 0 {2,T} {3,S}
+2 R!H 0 {1,T}
+3 H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0027,
+        Pc = -0.0008,
+        Vc = 46,
+        Tb = 9.20,
+        structureIndex = 0,
+    ),
+    shortDesc = u"""nonring_library value for CtHR""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 10,
+    label = "CtR2",
+    molecule =
+"""
+1 * C 0 {2,T} {3,S}
+2 R!H 0 {1,T}
+3 R!H 0 {1,S}
+
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0020,
+        Pc = 0.0016,
+        Vc = 37,
+        Tb = 27.38,
+        structureIndex = 0,
+    ),
+    shortDesc = u"""nonring_library value for CtR2""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 11,
+    label = "Alcohol",
+    molecule =
+"""
+1 * O 0 {2,S} {3,S}
+2 R!H 0 {1,S}
+3 H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0741,
+        Pc = 0.0112,
+        Vc = 28,
+        Tb = 92.88,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for Alcohol""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 12,
+    label = "Phenol",
+    molecule =
+"""
+1 * O 0 {2,S} {3,S}
+2 Cb 0 {1,S}
+3 H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0240,
+        Pc = 0.0184,
+        Vc = -25,
+        Tb = 76.34,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for Phenol""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 13,
+    label = "Ether",
+    molecule =
+"""
+1 * O 0 {2,S} {3,S}
+2 R!H 0 {1,S}
+3 R!H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0168,
+        Pc = 0.0015,
+        Vc = 18,
+        Tb = 22.42,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for Ether""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 14,
+    label = "Ketone_Ccentered",
+    molecule =
+"""
+1 * C 0 {2,D} {3,S} {4,S}
+2 O 0 {1,D}
+3 R!H 0 {1,S}
+4 R!H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0380,
+        Pc = 0.0031,
+        Vc = 62,
+        Tb = 76.75,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for Ketone_Ccentered""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 15,
+    label = "Ketone_Ocentered",
+    molecule =
+"""
+1 * O 0 {2,D}
+2 C 0 {1,D} {3,S} {4,S}
+3 R!H 0 {2,S}
+4 R!H 0 {2,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0,
+        Pc = 0,
+        Vc = 0,
+        Tb = 0,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for Ketone_Ocentered""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 16,
+    label = "Aldehyde_Ccentered",
+    molecule =
+"""
+1 * C 0 {2,D} {3,S} {4,S}
+2 O 0 {1,D}
+3 R!H 0 {1,S}
+4 H 0 {1,S}
+
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0379,
+        Pc = 0.0030,
+        Vc = 82,
+        Tb = 72.24,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for Aldehyde_Ccentered""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 17,
+    label = "Aldehyde_Ocentered",
+    molecule =
+"""
+1 * O 0 {2,D}
+2 C 0 {1,D} {3,S} {4,S}
+3 R!H 0 {2,S}
+4 H 0 {2,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0,
+        Pc = 0,
+        Vc = 0,
+        Tb = 0,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for Aldehyde_Ocentered""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 18,
+    label = "Acid_Ccentered",
+    molecule =
+"""
+1 * C 0 {2,D} {3,S} {4,S}
+2 O 0 {1,D}
+3 O 0 {1,S} {5,S}
+4 R!H 0 {1,S}
+5 H 0 {3,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0791,
+        Pc = 0.0077,
+        Vc = 89,
+        Tb = 169.09,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for Acid_Ccentered""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 19,
+    label = "Acid_Ocentered1",
+    molecule =
+"""
+1 * O 0 {2,S} {3,S}
+2 H 0 {1,S}
+3 C 0 {1,S} {4,D} {5,S}
+4 O 0 {3,D}
+5 R!H 0 {3,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0,
+        Pc = 0,
+        Vc = 0,
+        Tb = 0,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for Acid_Ocentered1""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 20,
+    label = "Acid_Ocentered2",
+    molecule =
+"""
+1 * O 0 {2,D}
+2 C 0 {1,D} {3,S} {4,S}
+3 R!H 0 {2,S}
+4 O 0 {2,S} {5,S}
+5 H 0 {4,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0,
+        Pc = 0,
+        Vc = 0,
+        Tb = 0,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for Acid_Ocentered2""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 21,
+    label = "Ester_Ccentered",
+    molecule =
+"""
+1 * C 0 {2,D} {3,S} {4,S}
+2 O 0 {1,D}
+3 O 0 {1,S} {5,S}
+4 R!H 0 {1,S}
+5 R!H 0 {3,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0481,
+        Pc = 0.0005,
+        Vc = 82,
+        Tb = 81.10,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for Ester_Ccentered""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 22,
+    label = "Ester_Ocentered1",
+    molecule =
+"""
+1 * O 0 {2,S} {3,S}
+2 R!H 0 {1,S}
+3 C 0 {1,S} {4,D} {5,S}
+4 O 0 {3,D}
+5 R!H 0 {3,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0,
+        Pc = 0,
+        Vc = 0,
+        Tb = 0,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for Ester_Ocentered1""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 23,
+    label = "Ester_Ocentered2",
+    molecule =
+"""
+1 * O 0 {2,D}
+2 C 0 {1,D} {3,S} {4,S}
+3 R!H 0 {2,S}
+4 O 0 {2,S} {5,S}
+5 R!H 0 {4,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0,
+        Pc = 0,
+        Vc = 0,
+        Tb = 0,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for Ester_Ocentered2""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 24,
+    label = "Od",
+    molecule =
+"""
+1 * O 0 {2,D}
+2 R!H 0 {1,D}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0143,
+        Pc = 0.0101,
+        Vc = 36,
+        Tb = -10.50,
+        structureIndex = 0,
+    ),
+    shortDesc = u"""nonring_library value for Od""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 25,
+    label = "Thiol",
+    molecule =
+"""
+1 * S 0 {2,S} {3,S}
+2 R!H 0 {1,S}
+3 H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0031,
+        Pc = 0.0084,
+        Vc = 63,
+        Tb = 63.56,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for Thiol""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 26,
+    label = "Thioether",
+    molecule =
+"""
+1 * S 0 {2,S} {3,S}
+2 R!H 0 {1,S}
+3 R!H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0119,
+        Pc = 0.0049,
+        Vc = 54,
+        Tb = 68.78,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""nonring_library value for Thioether""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:15:49","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+
+tree(
+"""
+L0: R
+    L1: C_centered
+        L2: CsH3R
+        L2: CsH2R2
+        L2: CsHR3
+        L2: CsR4
+        L2: CdH2R
+        L2: CdR3
+            L3: Ketone_Ccentered
+            L3: Acid_Ccentered
+            L3: Ester_Ccentered
+        L2: CdHR2
+            L3: Aldehyde_Ccentered
+        L2: CddR2
+        L2: CtHR
+        L2: CtR2
+        
+    L1: O_centered
+        L2: Alcohol
+            L3: Phenol
+            L3: Acid_Ocentered1
+        L2: Ether
+            L3: Ester_Ocentered1
+        L2: Od
+            L3: Ketone_Ocentered
+            L3: Aldehyde_Ocentered
+            L3: Acid_Ocentered2
+            L3: Ester_Ocentered2
+                
+    L1: S_centered
+        L2: Thiol
+        L2: Thioether
+"""
+)
---
+++ input/transport/groups/ring.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+entry(
+    index = 1,
+    label = "CsringH2R2",
+    molecule =
+"""
+1 * C 0 {2,S} {3,S} {4,S} {5,S}
+2 R!H 0 {1,S}
+3 R!H 0 {1,S}
+4 H 0 {1,S}
+5 H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0100,
+        Pc = 0.0025,
+        Vc = 48,
+        Tb = 27.15,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""ring_library value for CsringH2R2""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:53:03","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 2,
+    label = "CsringHR3",
+    molecule =
+"""
+1 * C 0 {2,S} {3,S} {4,S} {5,S}
+2 R!H 0 {1,S}
+3 R!H 0 {1,S}
+4 R!H 0 {1,S}
+5 H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0122,
+        Pc = 0.0004,
+        Vc = 38,
+        Tb = 21.78,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""ring_library value for CsringHR3""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:53:03","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 3,
+    label = "CsringR4",
+    molecule =
+"""
+1 * C 0 {2,S} {3,S} {4,S} {5,S}
+2 R!H 0 {1,S}
+3 R!H 0 {1,S}
+4 R!H 0 {1,S}
+5 R!H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0042,
+        Pc = 0.0061,
+        Vc = 27,
+        Tb = 21.32,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""ring_library value for CsringR4""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:53:03","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 4,
+    label = "CdringHR2",
+    molecule =
+"""
+1 * C 0 {2,D} {3,S} {4,S}
+2 R!H 0 {1,D}
+3 R!H 0 {1,S}
+4 H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0082,
+        Pc = 0.0011,
+        Vc = 41,
+        Tb = 26.73,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""ring_library value for CdringHR2""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:53:03","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 5,
+    label = "CdringR3",
+    molecule =
+"""
+1 * C 0 {2,D} {3,S} {4,S}
+2 R!H 0 {1,D}
+3 R!H 0 {1,S}
+4 R!H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0143,
+        Pc = 0.0008,
+        Vc = 32,
+        Tb = 31.01,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""ring_library value for CdringR3""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:53:03","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 6,
+    label = "Ether_ring",
+    molecule =
+"""
+1 * O 0 {2,S} {3,S}
+2 R!H 0 {1,S}
+3 R!H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0098,
+        Pc = 0.0048,
+        Vc = 13,
+        Tb = 31.22,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""ring_library value for Ether_ring""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:53:03","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 7,
+    label = "Ketone_ring",
+    molecule =
+"""
+1 * C 0 {2,D} {3,S} {4,S}
+2 O 0 {1,D}
+3 R!H 0 {1,S}
+4 R!H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0284,
+        Pc = 0.0028,
+        Vc = 55,
+        Tb = 94.97,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""ring_library value for Ketone_ring""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:53:03","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+entry(
+    index = 8,
+    label = "Thioether_ring",
+    molecule =
+"""
+1 * S 0 {2,S} {3,S}
+2 R!H 0 {1,S}
+3 R!H 0 {1,S}
+""",
+    transportGroup = CriticalPointGroupContribution(
+        Tc = 0.0019,
+        Pc = 0.0051,
+        Vc = 38,
+        Tb = 52.10,
+        structureIndex = 1,
+    ),
+    shortDesc = u"""ring_library value for Thioether_ring""",
+    LongDesc = 
+u"""
+
+""",
+    history = [
+        ("2013/03/14 12:53:03","Jake Barlow <barlow.ja@husky.neu.edu>","action",
+        """Jake Barlow <barlow.ja@husky.neu.edu> imported this entry from the old RMG database."""),    ],
+)
+
+
+tree(
+"""
+L0: R_ring
+    L1: C_centered_ring
+        L2: CsringH2R2
+        L2: CsringHR3
+        L2: CsringR4
+        L2: CdringR3
+            L3: Ketone_ring
+        L2: CdringHR2
+        //L2: CddringR2 (?) The lack of this node is causing RMG to throw a warning message: "Transport Group not found"
+    
+    L1: O_centered_ring
+        L2: Ether_ring
+        
+    L1: S_centered_ring
+        L2: Thioether_ring
+"""
+)
