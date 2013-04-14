bba3de04e881c4951969cc39d4dfae6d1ab062f1
Belinda Slakman <slakman.b@husky.neu.edu>
2013-04-14 15:54:14 -0400

    Added some solvents, and corrected a value for water.

--- input/solvation/libraries/solvent.py
+++ input/solvation/libraries/solvent.py
@@ -21,8 +21,128 @@ entry(
     	e_h = 9.91,
     	s_h = 2.836,
     	a_h = -32.010,	
-    	b_h = 41.816,	
+    	b_h = -41.816,	
     	l_h = -6.354
+    ),
+    shortDesc = u""" """,
+    longDesc = 
+u"""
+
+"""
+)
+
+entry(
+    index = 2,
+    label = "1-octanol",
+    solvent = SolventData(
+    	c_g = -0.12,
+    	e_g = -0.203,
+    	s_g = 0.56,
+    	a_g = 3.56,
+    	b_g = 0.702,
+    	l_g = 0.939,
+    	c_h = -6.49,
+    	e_h = -1.04,
+    	s_h = 5.89,
+    	a_h = -53.99,	
+    	b_h = -8.99,
+    	l_h = -9.18
+    ),
+    shortDesc = u""" """,
+    longDesc = 
+u"""
+
+"""
+)
+
+entry(
+    index = 3,
+    label = "benzene",
+    solvent = SolventData(
+    	c_g = 0.107,
+    	e_g = -0.313,
+    	s_g = 1.053,
+    	a_g = 0.457,
+    	b_g = 0.169,
+    	l_g = 1.02,
+    	c_h = -4.637,
+    	e_h = 4.446,
+    	s_h = -12.599,
+    	a_h = -9.775,	
+    	b_h =-4.023,	
+    	l_h = -8.488
+    ),
+    shortDesc = u""" """,
+    longDesc = 
+u"""
+
+"""
+)
+
+entry(
+    index = 4,
+    label = "cyclohexane",
+    solvent = SolventData(
+    	c_g = 0.163,
+    	e_g = -0.11,
+    	s_g = 0,
+    	a_g = 0,
+    	b_g = 0,
+    	l_g = 1.013,
+    	c_h = -6.507,
+    	e_h = 3.375,
+    	s_h = 0.0,
+    	a_h = 0.0,
+    	b_h = 0.0,	
+    	l_h = -9.078
+    ),
+    shortDesc = u""" """,
+    longDesc = 
+u"""
+
+"""
+)
+
+entry(
+    index = 5,
+    label = "dibutylether",
+    solvent = SolventData(
+    	c_g = 0.369,
+    	e_g = -0.216,
+    	s_g = 0.026,
+    	a_g = 2.626,
+    	b_g = -0.499,
+    	l_g = 1.124,
+    	c_h = -6.366,
+    	e_h = 3.943,
+    	s_h = -5.105,
+    	a_h = -33.970,	
+    	b_h = 0.0,	
+    	l_h = -9.325
+    ),
+    shortDesc = u""" """,
+    longDesc = 
+u"""
+
+"""
+)
+
+entry(
+    index = 6,
+    label = "octane",
+    solvent = SolventData(
+    	c_g = 0.215,
+    	e_g = -0.049,
+    	s_g = 0,
+    	a_g = 0,
+    	b_g = 0,
+    	l_g = 0.967,
+    	c_h = -6.708,
+    	e_h = 2.999,
+    	s_h = 0.0,
+    	a_h = 0.0,
+    	b_h = 0.0,
+    	l_h = -9.279
     ),
     shortDesc = u""" """,
     longDesc = 
