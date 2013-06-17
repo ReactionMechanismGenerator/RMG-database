a4d34265117a0c1860caf7638f2948ba3391feff
Belinda Slakman <slakman.b@husky.neu.edu>
2013-06-17 15:18:07 -0400

    Added more solutes to solute library.

--- input/solvation/libraries/solute.py
+++ input/solvation/libraries/solute.py
@@ -62,3 +62,801 @@ u"""
 
 """
 )
+
+entry(
+    index = 4,
+    label = "n-butane",
+    molecule = 'CCCC',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 1.615,
+        A = 0,
+        V = 0.6722
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+
+entry(
+    index = 5,
+    label = "2-methylpropane",
+    molecule = 'C(C)(C)C',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 1.409,
+        A = 0,
+        V = 0.6722
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+
+entry(
+    index = 6,
+    label = "n-pentane",
+    molecule = 'CCCCC',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 2.162,
+        A = 0,
+        V = 0.8131
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+
+entry(
+    index = 7,
+    label = "2-methylbutane",
+    molecule = 'C(CC)(C)C',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 2.013,
+        A = 0,
+        V = 0.8131
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+
+entry(
+    index = 8,
+    label = "2,2-dimethylpropane",
+    molecule = 'C(C)(C)(C)C',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 1.82,
+        A = 0,
+        V = 0.8131
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+
+entry(
+    index = 9,
+    label = "n-hexane",
+    molecule = 'CCCCCC',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 2.668,
+        A = 0,
+        V = 0.954
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 10,
+    label = "2-methylpentane",
+    molecule = 'C(CCC)(C)C',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 2.503,
+        A = 0,
+        V = 0.954
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 11,
+    label = "3-methylpentane",
+    molecule = 'C(CC)(CC)C',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 2.581,
+        A = 0,
+        V = 0.954
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 12,
+    label = "2,2-dimethylbutane",
+    molecule = 'C(CC)(C)(C)C',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 2.352,
+        A = 0,
+        V = 0.954
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 13,
+    label = "2,3-dimethylbutane",
+    molecule = 'C(C(C)C)(C)C',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 2.495,
+        A = 0,
+        V = 0.954
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 14,
+    label = "n-heptane",
+    molecule = 'CCCCCCC',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 3.173,
+        A = 0,
+        V = 1.0949
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 15,
+    label = "2-methylhexane",
+    molecule = 'C(CCCC)(C)C',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 3.001,
+        A = 0,
+        V = 1.0949
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 16,
+    label = "3-methylhexane",
+    molecule = 'C(CCC)(CC)C',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 3.044,
+        A = 0,
+        V = 1.0949
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 17,
+    label = "2,2-dimethylpentane",
+    molecule = 'C(CCC)(C)(C)C',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 2.796,
+        A = 0,
+        V = 1.0949
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 18,
+    label = "2,3-dimethylpentane",
+    molecule = 'C(C(C)C)(CC)C',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 3.016,
+        A = 0,
+        V = 1.0949
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 19,
+    label = "2,4-dimethylpentane",
+    molecule = 'C(CC(C)C)(C)C',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 2.809,
+        A = 0,
+        V = 1.0949
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 20,
+    label = "3,3-dimethylpentane",
+    molecule = 'C(CC)(CC)(C)C',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 2.809,
+        A = 0,
+        V = 1.0949
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 21,
+    label = "n-octane",
+    molecule = 'CCCCCCCC',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 3.677,
+        A = 0,
+        V = 1.2358
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 22,
+    label = "3-methylheptane",
+    molecule = 'C(CCCC)(CC)C',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 3.51,
+        A = 0,
+        V = 1.2358
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 23,
+    label = "2,2,4-trimethylpentane",
+    molecule = 'C(CC(C)C)(C)(C)C',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 3.106,
+        A = 0,
+        V = 1.2358
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 24,
+    label = "2,3,4-trimethylpentane",
+    molecule = 'C(C(C)C)(C(C)C)C',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 3.481,
+        A = 0,
+        V = 1.2358
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 25,
+    label = "n-nonane",
+    molecule = 'CCCCCCCCC',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 4.182,
+        A = 0,
+        V = 1.3767
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 26,
+    label = "2,2,5-trimethylhexane",
+    molecule = 'C(CCC(C)C)(C)(C)C',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 3.567,
+        A = 0,
+        V = 1.3767
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 27,
+    label = "n-decane",
+    molecule = 'CCCCCCCCCC',
+    solute = SoluteData(
+        S = 0,
+        B = 0,
+        E = 0,
+        L = 4.686,
+        A = 0,
+        V = 1.5176
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 28,
+    label = "cyclopropane",
+    molecule = 'C1CC1',
+    solute = SoluteData(
+        S = 0.15,
+        B = 0,
+        E = 0.180,
+        L = 1.314,
+        A = 0,
+        V = 0.4227
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 29,
+    label = "cyclopentane",
+    molecule = '',
+    solute = SoluteData(
+        S = 0.10,
+        B = 0,
+        E = 0.263,
+        L = 2.477,
+        A = 0,
+        V = 0.7045
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 30,
+    label = "methylcyclopentane",
+    molecule = 'C1(CCCC1)C',
+    solute = SoluteData(
+        S = 0.10,
+        B = 0,
+        E = 0.225,
+        L = 2.816,
+        A = 0,
+        V = 0.8454
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 31,
+    label = "n-propylcyclopentane",
+    molecule = 'C1(CCC)CCCC1',
+    solute = SoluteData(
+        S = 0.10,
+        B = 0.225,
+        E = 0,
+        L = 3.803,
+        A = 0,
+        V = 1.272
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 32,
+    label = "cyclohexane",
+    molecule = 'C1CCCCC1',
+    solute = SoluteData(
+        S = 0.10,
+        B = 0,
+        E = 0.305,
+        L = 2.964,
+        A = 0,
+        V = 0.8454
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 33,
+    label = "methylcyclohexane",
+    molecule = 'C1(CCCCC1)C',
+    solute = SoluteData(
+        S = 0.10,
+        B = 0,
+        E = 0.244,
+        L = 3.323,
+        A = 0,
+        V = 0.9863
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 34,
+    label = "1,2-dimethylcyclohexane",
+    molecule = 'C1(C(CCCC1)C)C',
+    solute = SoluteData(
+        S = 0.10,
+        B = 0,
+        E = 0.236,
+        L = 3.6665,
+        A = 0,
+        V = 1.272
+    ),
+    shortDesc = u"""E, L values are the average of those for cis and trans isomers.""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 35,
+    label = "ethene",
+    molecule = 'C=C',
+    solute = SoluteData(
+        S = 0.10,
+        B = 0.07,
+        E = 0.107,
+        L = 0.289,
+        A = 0,
+        V = 0.3474
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 36,
+    label = "propene",
+    molecule = 'CC=C',
+    solute = SoluteData(
+        S = 0.08,
+        B = 0.07,
+        E = 0.103,
+        L = 0.946,
+        A = 0,
+        V = 0.4883
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 37,
+    label = "1-butene",
+    molecule = 'C(C)C=C',
+    solute = SoluteData(
+        S = 0.08,
+        B = 0.07,
+        E = 0.1,
+        L = 1.491,
+        A = 0,
+        V = 0.6292
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 38,
+    label = "1-pentene",
+    molecule = 'C(CC=C)C',
+    solute = SoluteData(
+        S = 0.08,
+        B = 0.07,
+        E = 0.093,
+        L = 2.047,
+        A = 0,
+        V = 0.7701
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 39,
+    label = "2-pentene",
+    molecule = 'C(C)C=CC',
+    solute = SoluteData(
+        S = 0.08,
+        B = 0.07,
+        E = 0.141,
+        L = 2.211,
+        A = 0,
+        V = 0.7701
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 40,
+    label = "3-methylbut-1-ene",
+    molecule = 'C(C)(C)C=C',
+    solute = SoluteData(
+        S = 0.08,
+        B = 0.07,
+        E = 0.063,
+        L = 1.91,
+        A = 0,
+        V = 0.7701
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 41,
+    label = "2-methylbut-2-ene",
+    molecule = 'CC(=CC)C',
+    solute = SoluteData(
+        S = 0.08,
+        B = 0.07,
+        E = 0.159,
+        L = 2.226,
+        A = 0,
+        V = 0.7701
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 42,
+    label = "1-hexene",
+    molecule = 'C(CC)CC=C',
+    solute = SoluteData(
+        S = 0.08,
+        B = 0.07,
+        E = 0.078,
+        L = 2.572,
+        A = 0,
+        V = 0.911
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 43,
+    label = "2-methylpent-1-ene",
+    molecule = 'C(CC(=C)C)C',
+    solute = SoluteData(
+        S = 0.08,
+        B = 0.07,
+        E = 0.09,
+        L = 2.588,
+        A = 0,
+        V = 0.911
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 44,
+    label = "1-heptene",
+    molecule = 'C(CCC=C)CC',
+    solute = SoluteData(
+        S = 0.08,
+        B = 0.07,
+        E = 0.092,
+        L = 3.063,
+        A = 0,
+        V = 1.0519
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 45,
+    label = "1-octene",
+    molecule = 'C(CCC)CCC=C',
+    solute = SoluteData(
+        S = 0.08,
+        B = 0.07,
+        E = 0.094,
+        L = 3.568,
+        A = 0,
+        V = 1.1928
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 46,
+    label = "1-nonene",
+    molecule = 'C(CCCC)CCC=C',
+    solute = SoluteData(
+        S = 0.08,
+        B = 0.07,
+        E = 0.09,
+        L = 4.073,
+        A = 0,
+        V = 1.3337
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
+entry(
+    index = 47,
+    label = "1,3-butadiene",
+    molecule = 'C(=C)C=C',
+    solute = SoluteData(
+        S = 0.23,
+        B = 0.1,
+        E = 0.32,
+        L = 1.543,
+        A = 0,
+        V = 0.5862
+    ),
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+"""
+)
