184618c542430b06b059cf1aecc1144643117238
Pierre Bhoorasingh <pierreyb@gmail.com>
2013-06-30 06:44:15 -0400

    Convert H_Abstraction/TS_training.py to use new DistanceData class.
    Conversion using a scrip to ensure consistency in
    the changes.

--- input/kinetics/families/H_Abstraction/TS_training.py
+++ input/kinetics/families/H_Abstraction/TS_training.py
@@ -47,11 +47,9 @@ entry(
 4    H 0 {1,S}
 5 *2 H 0 {1,S}
 """,
-    distances = {
-        'd12' : 1.30821129476,
-        'd23' : 1.37712245821,
-        'd13' : 2.68276888272,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.30821129476, 'd23' : 1.37712245821, 'd13' : 2.68276888272},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -109,11 +107,9 @@ entry(
 4    H 0 {1,S}
 5 *2 H 0 {1,S}
 """,
-    distances = {
-        'd12' : 1.31063324722,
-        'd23' : 1.37920181409,
-        'd13' : 2.68792471359,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.31063324722, 'd23' : 1.37920181409, 'd13' : 2.68792471359},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -177,11 +173,9 @@ entry(
 4    H 0 {1,S}
 5 *2 H 0 {1,S}
 """,
-    distances = {
-        'd12' : 1.31200348227,
-        'd23' : 1.37931646413,
-        'd13' : 2.69070299055,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.31200348227, 'd23' : 1.37931646413, 'd13' : 2.69070299055},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -245,11 +239,9 @@ entry(
 4    H 0 {1,S}
 5 *2 H 0 {1,S}
 """,
-    distances = {
-        'd12' : 1.31597367556,
-        'd23' : 1.37992230669,
-        'd13' : 2.69552023665,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.31597367556, 'd23' : 1.37992230669, 'd13' : 2.69552023665},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -303,11 +295,9 @@ entry(
 4    H 0 {1,S}
 5 *2 H 0 {1,S}
 """,
-    distances = {
-        'd12' : 1.32103245203,
-        'd23' : 1.35951459954,
-        'd13' : 2.67705175771,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.32103245203, 'd23' : 1.35951459954, 'd13' : 2.67705175771},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -359,11 +349,9 @@ entry(
 7    H 0 {2,S}
 8 *2 H 0 {1,S}
 """,
-    distances = {
-        'd12' : 1.37713399973,
-        'd23' : 1.30819156289,
-        'd13' : 2.68278131422,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.37713399973, 'd23' : 1.30819156289, 'd13' : 2.68278131422},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -427,11 +415,9 @@ entry(
 7    H 0 {2,S}
 8 *2 H 0 {1,S}
 """,
-    distances = {
-        'd12' : 1.34592645405,
-        'd23' : 1.34540756838,
-        'd13' : 2.68976068861,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.34592645405, 'd23' : 1.34540756838, 'd13' : 2.68976068861},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -501,11 +487,9 @@ entry(
 7    H 0 {2,S}
 8 *2 H 0 {1,S}
 """,
-    distances = {
-        'd12' : 1.34734208010,
-        'd23' : 1.34638652623,
-        'd13' : 2.69267562849,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.34734208010, 'd23' : 1.34638652623, 'd13' : 2.69267562849},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -575,11 +559,9 @@ entry(
 7    H 0 {2,S}
 8 *2 H 0 {1,S}
 """,
-    distances = {
-        'd12' : 1.34764127963,
-        'd23' : 1.34162534895,
-        'd13' : 2.68778427670,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.34764127963, 'd23' : 1.34162534895, 'd13' : 2.68778427670},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -639,11 +621,9 @@ entry(
 7    H 0 {2,S}
 8 *2 H 0 {1,S}
 """,
-    distances = {
-        'd12' : 1.35801928986,
-        'd23' : 1.32864318762,
-        'd13' : 2.68078750257,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.35801928986, 'd23' : 1.32864318762, 'd13' : 2.68078750257},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -701,11 +681,9 @@ entry(
 10    H 0 {3,S}
 11 *2 H 0 {2,S}
 """,
-    distances = {
-        'd12' : 1.37914883843,
-        'd23' : 1.31063441583,
-        'd13' : 2.68787275261,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.37914883843, 'd23' : 1.31063441583, 'd13' : 2.68787275261},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -769,11 +747,9 @@ entry(
 10    H 0 {3,S}
 11 *2 H 0 {2,S}
 """,
-    distances = {
-        'd12' : 1.34541799426,
-        'd23' : 1.34597083831,
-        'd13' : 2.68979141822,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.34541799426, 'd23' : 1.34597083831, 'd13' : 2.68979141822},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -849,11 +825,9 @@ entry(
 10    H 0 {3,S}
 11 *2 H 0 {2,S}
 """,
-    distances = {
-        'd12' : 1.34656921583,
-        'd23' : 1.34394599296,
-        'd13' : 2.68905921144,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.34656921583, 'd23' : 1.34394599296, 'd13' : 2.68905921144},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -919,11 +893,9 @@ entry(
 10    H 0 {3,S}
 11 *2 H 0 {2,S}
 """,
-    distances = {
-        'd12' : 1.35242499306,
-        'd23' : 1.34612283019,
-        'd13' : 2.69841510129,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.35242499306, 'd23' : 1.34612283019, 'd13' : 2.69841510129},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -981,11 +953,9 @@ entry(
 10    H 0 {3,S}
 11    H 0 {3,S}
 """,
-    distances = {
-        'd12' : 1.40716757820,
-        'd23' : 1.28550527301,
-        'd13' : 2.68933612152,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.40716757820, 'd23' : 1.28550527301, 'd13' : 2.68933612152},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -1049,11 +1019,9 @@ entry(
 10    H 0 {3,S}
 11    H 0 {3,S}
 """,
-    distances = {
-        'd12' : 1.37352706397,
-        'd23' : 1.32070123588,
-        'd13' : 2.68865422259,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.37352706397, 'd23' : 1.32070123588, 'd13' : 2.68865422259},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -1123,11 +1091,9 @@ entry(
 10    H 0 {3,S}
 11    H 0 {3,S}
 """,
-    distances = {
-        'd12' : 1.37534926838,
-        'd23' : 1.31645349828,
-        'd13' : 2.68924761910,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.37534926838, 'd23' : 1.31645349828, 'd13' : 2.68924761910},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -1203,11 +1169,9 @@ entry(
 10    H 0 {3,S}
 11    H 0 {3,S}
 """,
-    distances = {
-        'd12' : 1.37067593134,
-        'd23' : 1.31924962137,
-        'd13' : 2.68744916354,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.37067593134, 'd23' : 1.31924962137, 'd13' : 2.68744916354},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -1283,11 +1247,9 @@ entry(
 10    H 0 {3,S}
 11    H 0 {3,S}
 """,
-    distances = {
-        'd12' : 1.37729313657,
-        'd23' : 1.31606569056,
-        'd13' : 2.69319121592,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.37729313657, 'd23' : 1.31606569056, 'd13' : 2.69319121592},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -1353,11 +1315,9 @@ entry(
 10    H 0 {3,S}
 11    H 0 {3,S}
 """,
-    distances = {
-        'd12' : 1.38731570737,
-        'd23' : 1.30207330006,
-        'd13' : 2.68718303364,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.38731570737, 'd23' : 1.30207330006, 'd13' : 2.68718303364},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -1405,11 +1365,9 @@ entry(
 3    H 0 {1,S}
 4    H 0 {1,S}
 """,
-    distances = {
-        'd12' : 1.42490792824,
-        'd23' : 1.28503916731,
-        'd13' : 2.70582358565,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.42490792824, 'd23' : 1.28503916731, 'd13' : 2.70582358565},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -1463,11 +1421,9 @@ entry(
 6    H 0 {2,S}
 7    H 0 {2,S}
 """,
-    distances = {
-        'd12' : 1.38946362815,
-        'd23' : 1.31635615242,
-        'd13' : 2.68885330909,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.38946362815, 'd23' : 1.31635615242, 'd13' : 2.68885330909},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -1527,11 +1483,9 @@ entry(
 9     H 0 {3,S}
 10    H 0 {3,S}
 """,
-    distances = {
-        'd12' : 1.39480230036,
-        'd23' : 1.31403057143,
-        'd13' : 2.69680491933,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.39480230036, 'd23' : 1.31403057143, 'd13' : 2.69680491933},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -1597,11 +1551,9 @@ entry(
 12    H 0 {4,S}
 13    H 0 {4,S}
 """,
-    distances = {
-        'd12' : 1.39427571589,
-        'd23' : 1.31945648860,
-        'd13' : 2.69931215056,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.39427571589, 'd23' : 1.31945648860, 'd13' : 2.69931215056},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -1667,11 +1619,9 @@ entry(
 12    H 0 {4,S}
 13    H 0 {4,S}
 """,
-    distances = {
-        'd12' : 1.39571486669,
-        'd23' : 1.31301772418,
-        'd13' : 2.70603863261,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.39571486669, 'd23' : 1.31301772418, 'd13' : 2.70603863261},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
@@ -1727,11 +1677,9 @@ entry(
 7    H 0 {2,S}
 8    H 0 {3,S}
 """,
-    distances = {
-        'd12' : 1.40646608200,
-        'd23' : 1.30692490313,
-        'd13' : 2.70366151951,
-    },
+    distances = DistanceData(
+      distances = {'d12' : 1.40646608200, 'd23' : 1.30692490313, 'd13' : 2.70366151951},
+    ),
     reference = None,
     referenceType = "",
     rank = 3,
