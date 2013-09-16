2ae9e5648cbc684fcfc238a1a35345cb498c8cff
Connie Gao <connieg@mit.edu>
2013-09-16 15:41:43 -0400

    Script for converting RMG-Java individual kinetics libraries to Py format.

---
+++ importKineticsLibrary.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+This script imports an individual RMG-Java kinetics library from a local directory and saves the output
+kinetics library py file into a path of the user's choosing.  This py file can be added to the 
+input/kinetics/libraries folder to be used as an RMG-Py kinetics library.  
+"""
+
+import argparse
+import time
+from rmgpy.data.kinetics import KineticsLibrary
+from importOldDatabase import getUsername
+          
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('inputPath', metavar='INPUT', type=str, nargs=1,
+        help='the input path of the RMG-Java kinetics library directory')
+    parser.add_argument('outputPath', metavar='OUTPUT', type=str, nargs=1,
+        help='the libraryname.py output file for the RMG-Py format kinetics library')   
+    
+    args = parser.parse_args()
+    inputPath = args.inputPath[0]
+    outputPath = args.outputPath[0]
+    
+    library = KineticsLibrary()
+    library.loadOld(inputPath)
+    
+    # Assign history to entries
+    user = getUsername()    # Pulls username from current git repository
+    #user = '{0} <{1}>'.format(name, email)   # If not in git repository, then enter user information manually
+    event = [time.asctime(),user,'action','{0} imported this entry from the old RMG database.'.format(user)]
+    for label, entry in library.entries.iteritems():
+        entry.history.append(event)
+        
+    # Save in Py format
+    library.save(outputPath)
