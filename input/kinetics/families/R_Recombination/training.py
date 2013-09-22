d10b9e4c8fed49560a6914390156e8a38463f572
Shamel Merchant <shamel@mit.edu>
2013-09-22 11:37:09 -0400

    New Entry: R_Recombination/training/1
    Rate rule for CO[O] = [CH3] + O2
    
    Submitted through the RMG website.

--- input/kinetics/families/R_Recombination/training.py
+++ input/kinetics/families/R_Recombination/training.py
@@ -9,3 +9,44 @@ group additivity values in this file.
 """
 recommended = True
 
+entry(
+    index = 1,
+    reactant1 = 
+"""
+1   C 0 {2,S}
+2   O 0 {1,S} {3,S}
+3 * O 1 {2,S}
+""",
+    product1 = 
+"""
+1 O 1 {2,S}
+2 O 1 {1,S}
+""",
+    product2 = 
+"""
+1 C 1
+""",
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (109000000000000.0, 's^-1'),
+        n = 0.25,
+        Ea = (33.3, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+    reference = None,
+    referenceType = "Quantum Calculation",
+    shortDesc = u"""Method CBS-QB3 w/ 1-d Hindered rotor corrections""",
+    longDesc = 
+u"""
+High-Pressure Rate Rules for Alkyl + O2 Reactions. 1. The Dissociation, Concerted Elimination, and Isomerization Channels of the Alkyl Peroxy Radical
+Stephanie M. Villano, Lam K. Huynh, Hans-Heinrich Carstensen, and Anthony M. Dean
+The Journal of Physical Chemistry A 2011 115 (46), 13425-13442
+dx.doi.org/10.1021/jp2079204
+
+Method CBS-QB3 w/ 1-d Hindered rotor corrections
+""",
+    history = [
+        ("Sun Sep 22 11:37:09 2013","Shamel Merchant <shamel@mit.edu>","action","""New entry. Rate rule for CO[O] = [CH3] + O2"""),
+    ],
+)
+
