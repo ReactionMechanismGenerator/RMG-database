c837cc40f433e9178563ee6fe14a396bffb82c66
Shamel Merchant <shamel@mit.edu>
2013-09-22 11:45:54 -0400

    New Entry: R_Recombination/training/2
    Rate rule for CCO[O] = C[CH2] + O2
    
    Submitted through the RMG website.

--- input/kinetics/families/R_Recombination/training.py
+++ input/kinetics/families/R_Recombination/training.py
@@ -50,3 +50,41 @@ Method CBS-QB3 w/ 1-d Hindered rotor corrections
     ],
 )
 
+entry(
+    index = 2,
+    reactant1 = 
+"""
+1   C 0 {3,S}
+2   O 0 {3,S} {4,S}
+3   C 0 {1,S} {2,S}
+4 * O 1 {2,S}
+""",
+    product1 = 
+"""
+1 O 1 {2,S}
+2 O 1 {1,S}
+""",
+    product2 = 
+"""
+1 C 0 {2,S}
+2 C 1 {1,S}
+""",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(9.49e+21, 's^-1'), n=-2.41, Ea=(35.8, 'kcal/mol'), T0=(1, 'K')),
+    reference = None,
+    referenceType = "Quantum Calculation",
+    shortDesc = u"""Method CBS-QB3 w/ 1-d Hindered rotor corrections""",
+    longDesc = 
+u"""
+High-Pressure Rate Rules for Alkyl + O2 Reactions. 1. The Dissociation, Concerted Elimination, and Isomerization Channels of the Alkyl Peroxy Radical
+Stephanie M. Villano, Lam K. Huynh, Hans-Heinrich Carstensen, and Anthony M. Dean
+The Journal of Physical Chemistry A 2011 115 (46), 13425-13442
+dx.doi.org/10.1021/jp2079204
+
+Method CBS-QB3 w/ 1-d Hindered rotor corrections
+""",
+    history = [
+        ("Sun Sep 22 11:45:54 2013","Shamel Merchant <shamel@mit.edu>","action","""New entry. Rate rule for CCO[O] = C[CH2] + O2"""),
+    ],
+)
+
