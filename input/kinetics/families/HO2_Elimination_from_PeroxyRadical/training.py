8e17fc5be2b18fca5d6765d8cbd106cb0d2639e0
Connie Gao <connieg@mit.edu>
2013-11-19 12:46:55 -0500

    New Entry: HO2_Elimination_from_PeroxyRadical/training/2
    Updated rate rule for CCCC(O[O])O = CCCC=O + HO2
    
    Submitted through the RMG website.

--- input/kinetics/families/HO2_Elimination_from_PeroxyRadical/training.py
+++ input/kinetics/families/HO2_Elimination_from_PeroxyRadical/training.py
@@ -13,42 +13,39 @@ entry(
     index = 1,
     reactant1 = 
 """
-acetylperoxy
-1  *1 C     0 {2,S} {4,S}
-2  *2 C     0 {1,S} {3,S} {5,D}
-3  *3 O     0 {2,S} {6,S}
-4  *5 H     0 {1,S}
-5     O     0 {2,D}
-6  *4 O     1 {3,S}
+1 *1 C 0 {2,S} {4,S}
+2 *2 C 0 {1,S} {3,S} {5,D}
+3 *3 O 0 {2,S} {6,S}
+4 *5 H 0 {1,S}
+5    O 0 {2,D}
+6 *4 O 1 {3,S}
 """,
     product1 = 
 """
-ketene
-1  *1 C     0 {2,D}
-2  *2 C     0 {1,D} {3,D}
-3     O     0 {2,D}
+1 *1 C 0 {2,D}
+2 *2 C 0 {1,D} {3,D}
+3    O 0 {2,D}
 """,
     product2 = 
 """
-HO2
-1  *4 O     0 {2,S} {3,S}
-2  *5 H     0 {1,S}
-3  *3 O     1 {1,S}
+1 *4 O 0 {2,S} {3,S}
+2 *5 H 0 {1,S}
+3 *3 O 1 {1,S}
 """,
     degeneracy = 3,
     kinetics = Arrhenius(
-        A = (2.6e9,"s^-1","*|/",10**0.4),
+        A = (2600000000.0, 's^-1', '*|/', 2.51189),
         n = 1.2,
-        Ea = (34.1,"kcal/mol"),
-        T0 = (1,"K"),
+        Ea = (34.1, 'kcal/mol'),
+        T0 = (1, 'K'),
     ),
     reference = Article(
         authors = ["J. W. Allen", "C. F. Goldsmith", "W. H. Green"],
-        title = u"""Automatic Estimation of Pressure-Dependent Rate Coefficients""",
+        title = u'Automatic Estimation of Pressure-Dependent Rate Coefficients',
         journal = "Phys. Chem. Chem. Phys.",
         volume = "???",
+        pages = """???-???""",
         year = "2011 (accepted)",
-        pages = "???-???",
     ),
     referenceType = "theory",
     shortDesc = u"""CFG VTST calculations at RQCISD(T)/CBS//B3LYP/6-311++G(d,p) level""",
@@ -58,4 +55,53 @@ Quantum chemistry calculations at the RQCISD(T)/CBS//B3LYP/6-311++G(d,p) level
 using Gaussian 03 and MOLPRO. High-pressure-limit rate coefficient computed
 using Variflex.
 """,
+    history = [
+    ],
 )
+
+entry(
+    index = 2,
+    reactant1 = 
+"""
+1 O 0 {2,S}
+2 C 0 {1,S} {3,S} {5,S}
+3 C 0 {2,S} {6,S}
+4 C 0 {6,S}
+5 O 0 {2,S} {7,S}
+6 C 0 {3,S} {4,S}
+7 O 1 {5,S}
+""",
+    product1 = 
+"""
+1    C 0 {2,S} {3,S}
+2    C 0 {1,S} {4,S}
+3    C 0 {1,S}
+4 *2 C 0 {2,S} {5,D}
+5 *1 O 0 {4,D}
+""",
+    product2 = 
+"""
+1 *4 O 0 {2,S} {3,S}
+2 *3 O 1 {1,S}
+3 *5 H 0 {1,S}
+""",
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (2900000000.0, 's^-1'),
+        n = 0.843,
+        Ea = (10.653, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+    reference = None,
+    referenceType = "",
+    shortDesc = u"""CBS-QB3 w/ 1d-HR""",
+    longDesc = 
+u"""
+Reference: Low-Temperature Combustion Chemistry of n-Butanol: Principal Oxidation Pathways of Hydroxybutyl Radicals 
+DOI: 10.1021/jp403792t
+""",
+    history = [
+        ("Tue Nov 19 12:46:54 2013","Connie Gao <connieg@mit.edu>","action","""New entry. Updated rate rule for CCCC(O[O])O = CCCC=O + HO2"""),
+    ],
+)
+
