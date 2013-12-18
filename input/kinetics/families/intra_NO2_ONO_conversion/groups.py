24ec09cfc9217915ffdf625ed031eec30cbce2ec
Beat Buesser <bbuesser@mit.edu>
2013-12-18 18:11:17 -0500

    update tree of intra_NO2_ONO_conversion

--- input/kinetics/families/intra_NO2_ONO_conversion/groups.py
+++ input/kinetics/families/intra_NO2_ONO_conversion/groups.py
@@ -23,10 +23,10 @@ entry(
     label = "RNO2",
     group = 
 """
-1 *1 Cs 0 {2,S}
+1 *1 R   0 {2,S}
 2 *2 N5d 0 {1,S} {3,S} {4,D}
-3 *3 Os 0 {2,S}
-4    Od 0 {2,D}
+3 *3 Os  0 {2,S}
+4    Od  0 {2,D}
 """,
     kinetics = None,
     reference = None,
