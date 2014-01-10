38a2eb00186f51e167569836162792d4e6fb6d85
enoch dames <enoch.dames@gmail.com>
2014-01-10 15:08:46 -0500

    New Entry: R_Addition_MultipleBond/training/3
    test_reaction update
    
    Submitted through the RMG website.

--- input/kinetics/families/R_Addition_MultipleBond/training.py
+++ input/kinetics/families/R_Addition_MultipleBond/training.py
@@ -13,35 +13,42 @@ entry(
     index = 1,
     reactant1 = 
 """
-1  *1 C     0 {2,T}
-2  *2 C     0 {1,T}
+1 *1 C 0 {2,T}
+2 *2 C 0 {1,T}
 """,
     reactant2 = 
 """
-1     C     0 {4,S}
-2     C     0 {4,S}
-3     C     0 {4,S}
-4  *3 C     1 {1,S} {2,S} {3,S}
+1    C 0 {4,S}
+2    C 0 {4,S}
+3    C 0 {4,S}
+4 *3 C 1 {1,S} {2,S} {3,S}
 """,
     product1 = 
 """
-1  *3 C     0 {2,S} {3,S} {4,S} {5,S}
-2     C     0 {1,S}
-3     C     0 {1,S}
-4     C     0 {1,S}
-5  *1 C     0 {1,S} {6,D}
-6  *2 C     1 {5,D}
+1 *3 C 0 {2,S} {3,S} {4,S} {5,S}
+2    C 0 {1,S}
+3    C 0 {1,S}
+4    C 0 {1,S}
+5 *1 C 0 {1,S} {6,D}
+6 *2 C 1 {5,D}
 """,
     degeneracy = 2,
     kinetics = Arrhenius(
-        A = (5.01e+10,"cm^3/(mol*s)"),
+        A = (50100000000.0, 'cm^3/(mol*s)'),
         n = 0,
-        Ea = (5.31,"kcal/mol"),
-        T0 = (1,"K"),
-        Tmin = (373,"K"),
-        Tmax = (493,"K"),
+        Ea = (5.31, 'kcal/mol'),
+        T0 = (1, 'K'),
+        Tmin = (373, 'K'),
+        Tmax = (493, 'K'),
     ),
-    reference = Article(authors=["Garcia Dominguez, J.A.", "Trotman-Dickenson, A.F."], title=u'The reactions of alkyl radicals. Part IX. The addition of methyl, ethyl, isopropyl, and t-butyl radicals to acetylene and the isomerization of alkenyl radicals', journal="J. Chem. Soc.", pages="""940-944""", year="1962", url="http://kinetics.nist.gov/kinetics/Detail?id=1962GAR/TRO940-944:1"),
+    reference = Article(
+        authors = ["Garcia Dominguez, J.A.", "Trotman-Dickenson, A.F."],
+        title = u'The reactions of alkyl radicals. Part IX. The addition of methyl, ethyl, isopropyl, and t-butyl radicals to acetylene and the isomerization of alkenyl radicals',
+        journal = "J. Chem. Soc.",
+        pages = """940-944""",
+        year = "1962",
+        url = "http://kinetics.nist.gov/kinetics/Detail?id=1962GAR/TRO940-944:1",
+    ),
     referenceType = "",
     shortDesc = u"""Dominguez et al. (1962). Data derived from fitting a complex mechanism.""",
     longDesc = 
@@ -62,40 +69,37 @@ entry(
     index = 2,
     reactant1 = 
 """
-hydroperoxyl-vinoxy
-1  *1 C     0 {2,S} {3,S} {5,D}
-2  *2 C     1 {1,S}
-3  *3 O     0 {1,S} {4,S}
-4     O     0 {3,S}
-5     O     0 {1,D}
+1 *1 C 0 {2,S} {3,S} {5,D}
+2 *2 C 1 {1,S}
+3 *3 O 0 {1,S} {4,S}
+4    O 0 {3,S}
+5    O 0 {1,D}
 """,
     product1 = 
 """
-ketene
-1  *2 C     0 {2,D}
-2  *1 C     0 {1,D} {3,D}
-3     O     0 {2,D}
+1 *2 C 0 {2,D}
+2 *1 C 0 {1,D} {3,D}
+3    O 0 {2,D}
 """,
     product2 = 
 """
-HO2
-1     O     0 {2,S}
-2  *3 O     1 {1,S}
+1    O 0 {2,S}
+2 *3 O 1 {1,S}
 """,
     degeneracy = 1,
     kinetics = Arrhenius(
-        A = (5.3e16,"s^-1","*|/",10**0.4),
-        n = -1.0,
-        Ea = (29.5,"kcal/mol"),
-        T0 = (1,"K"),
+        A = (5.3e+16, 's^-1', '*|/', 2.51189),
+        n = -1,
+        Ea = (29.5, 'kcal/mol'),
+        T0 = (1, 'K'),
     ),
     reference = Article(
         authors = ["J. W. Allen", "C. F. Goldsmith", "W. H. Green"],
-        title = u"""Automatic Estimation of Pressure-Dependent Rate Coefficients""",
+        title = u'Automatic Estimation of Pressure-Dependent Rate Coefficients',
         journal = "Phys. Chem. Chem. Phys.",
         volume = "???",
+        pages = """???-???""",
         year = "2011 (accepted)",
-        pages = "???-???",
     ),
     referenceType = "theory",
     shortDesc = u"""CFG VTST calculations at RQCISD(T)/CBS//B3LYP/6-311++G(d,p) level""",
@@ -105,4 +109,56 @@ Quantum chemistry calculations at the RQCISD(T)/CBS//B3LYP/6-311++G(d,p) level
 using Gaussian 03 and MOLPRO. High-pressure-limit rate coefficient computed
 using Variflex.
 """,
+    history = [
+    ],
 )
+
+entry(
+    index = 3,
+    reactant1 = 
+"""
+1 C 1 {2,S}
+2 O 0 {1,S}
+""",
+    product1 = 
+"""
+1 C 0 {2,D}
+2 O 0 {1,D}
+""",
+    product2 = 
+"""
+1 H 1
+""",
+    degeneracy = 1,
+    kinetics = Troe(
+        arrheniusHigh = Arrhenius(
+            A = (73700000000.0, 's^-1'),
+            n = 0.811,
+            Ea = (39578, 'cal/mol'),
+            T0 = (1000, 'K'),
+        ),
+        arrheniusLow = Arrhenius(
+            A = (3.5e+21, 'cm^3/(mol*s)'),
+            n = -1.99,
+            Ea = (24000, 'cal/mol'),
+            T0 = (1000, 'K'),
+        ),
+        alpha = 0.844,
+        T3 = (900, 'K'),
+        T1 = (1, 'K'),
+        T2 = (3315, 'K'),
+        efficiencies = {'C': 2.0, 'CO': 3.0, 'CC': 3.0, 'O': 6.0, 'C=O': 2.5, '[H][H]': 2.0, 'C(=O)=O': 2.0, '[C]=O': 1.5},
+        comment = 'Dames and Golden 2013',
+    ),
+    reference = None,
+    referenceType = "",
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+Dames and Golden 2013
+""",
+    history = [
+        ("Fri Jan 10 15:08:46 2014","enoch dames <enoch.dames@gmail.com>","action","""New entry. test_reaction update """),
+    ],
+)
+
