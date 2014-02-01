49371a5d1e34f7450255a9a1d1311d3b18c37d71
Richard West <r.west@neu.edu>
2014-01-31 20:24:12 -0500

    Change-detection in importOldDatabase script.
    It attempts to detect changes since the last import, and set the history items
    accordingly. Not really working yet, but I think a good start..

--- importOldDatabase.py
+++ importOldDatabase.py
@@ -53,37 +53,136 @@ def getUsername():
     
     return '{0} <{1}>'.format(name,email)
 
-def setHistory(database, user):
+
+from cStringIO import StringIO
+def compare(entry1,entry2,saver):
+    """Compare entries entry1 and entry2 according to the saveEntry function saver"""
+    history1, history2 = entry1.history, entry2.history
+    entry1.history = []
+    entry2.history = []
+    s1 = StringIO()
+    saver(s1,entry1)
+    s2 = StringIO()
+    saver(s2,entry2)
+    equal = False
+    if s1.getvalue() == s2.getvalue():
+        equal = True
+    s1.close()
+    s2.close()
+    entry1.history, entry2.history = history1, history2
+    return equal
+
+def setHistory(database, current_database, user):
     """
     For each entry in each component of the database, add a note to the history
-    indicating the import event and its time.
+    indicating the import event and its time, if it's different than what's in the current_database
     """
     # Add history item to each entry in each database
-    event = [time.asctime(),user,'action','{0} imported this entry from the old RMG database.'.format(user)]
+    event = [time.asctime(),user,'action','{0} updated this entry by importing the old RMG database.'.format(user)]
+    event_new = [time.asctime(),user,'action','{0} added this entry by importing the old RMG database.'.format(user)]
 
     for depository in database.thermo.depository.values():
         for label, entry in depository.entries.iteritems():
+            raise NotImplementedError("Wasn't expecting depositories in the old database, so haven't implemented change detection")
             entry.history.append(event)
-    for library in database.thermo.libraries.values():
+
+    for key, library in database.thermo.libraries.iteritems():
+        try:
+            old_library = current_database.thermo.libraries[key].entries
+        except KeyError:
+            old_library = {}
         for label, entry in library.entries.iteritems():
-            entry.history.append(event)
-    for groups in database.thermo.groups.values():
+            try:
+                old_entry = old_library[label]
+            except KeyError:
+                entry.history.append(event_new)
+                continue # next in for loop
+            entry.history.extend(old_entry.history)
+            if compare(old_entry, entry, library.saveEntry):
+                print key, label, "has not changed."
+            else:
+                entry.history.append(event)
+
+    for key, groups in database.thermo.groups.iteritems():
+        try:
+            old_groups = current_database.thermo.groups[key].entries
+        except KeyError:
+            old_groups = {}
         for label, entry in groups.entries.iteritems():
-            entry.history.append(event)
-    
-    for family in database.kinetics.families.values():
+            try:
+                old_entry = old_groups[label]
+            except KeyError:
+                entry.history.append(event_new)
+                continue # next in for loop
+            entry.history.extend(old_entry.history)
+            if compare(old_entry, entry, groups.saveEntry):
+                print key, label, "has not changed."
+            else:
+                entry.history.append(event)
+
+    for key, family in database.kinetics.families.iteritems():
+        try:
+            old_groups = current_database.kinetics.families[key].groups.entries
+        except KeyError:
+            old_groups = {}
         for label, entry in family.groups.entries.iteritems():
-            entry.history.append(event)
+            try:
+                old_entry = old_groups[label]
+            except KeyError:
+                entry.history.append(event_new)
+                continue # next in for loop
+            entry.history.extend(old_entry.history)
+            if compare(old_entry, entry, family.rules.saveEntry): # family.groups has no .saveEntry?
+                print key, label, "has not changed."
+            else:
+                entry.history.append(event)
+        try:
+            old_rules = current_database.kinetics.families[key].groups.entries
+        except KeyError:
+            old_rules = {}
         for label, entry in family.rules.entries.iteritems():
+            try:
+                old_entry = old_rules[label]
+            except KeyError:
+                for item in entry:
+                    item.history.append(event_new)
+                continue # next in for loop
             for item in entry:
-                item.history.append(event)
+                if old_entry.index == item.index:
+                    "Found what's meant to be the same thing"
+                    item.history.extend(old_entry.history)
+                    if compare(old_entry, item, family.rules.saveEntry):
+                        print key, label, "has not changed."
+                    else:
+                        item.history.append(event)
+                    break
+                else:
+                    "Couldn't find what was meant to be the same thing"
+                    item.history.append(event_new)
+
         for depository in family.depositories.values():
             for label, entry in depository.entries.iteritems():
+                raise NotImplementedError("Wasn't expecting depositories in the old database, so haven't implemented change detection")
                 entry.history.append(event)
-    for library in database.kinetics.libraries.values():
-        for label, entry in library.entries.iteritems():
-            entry.history.append(event)
     
+    for key, library in database.kinetics.libraries.iteritems():
+        try:
+            old_library = current_database.kinetics.libraries[key].entries
+        except KeyError:
+            old_library = {}
+        for label, entry in library.entries.iteritems():
+            try:
+                old_entry = old_library[label]
+            except KeyError:
+                entry.history.append(event_new)
+                continue # next in for loop
+            if compare(old_entry, entry, library.saveEntry):
+                print key, label, "has not changed."
+            else:
+                entry.history.extend(old_entry.history)
+                entry.history.append(event)
+
+
     groups = database.statmech.groups
     for label, entry in groups.entries.iteritems():
         entry.history.append(event)
@@ -110,8 +209,12 @@ if __name__ == '__main__':
     database = RMGDatabase()
     database.loadOld(oldPath)
     
+    print 'Loading the current database to look for changes...'
+    current_database = RMGDatabase()
+    current_database.load(newPath)
+    
     print 'Setting history of all entries in database...'
-    setHistory(database, user=user)
+    setHistory(database, current_database, user=user)
       
     print 'Saving the new RMG-Py database...'
     database.save(newPath)
