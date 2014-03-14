2dea6c4a9de50932d1761c48725e5b23d9cca53b
Beat Buesser <bbuesser@mit.edu>
2014-03-14 13:44:41 -0400

    Update forbiddenStructures with multiplicity label

--- input/forbiddenStructures.py
+++ input/forbiddenStructures.py
@@ -11,6 +11,7 @@ recommended = False
 
 entry(
     label = "Ods",
+    multiplicity = [1,2,3,4,5],
     group = 
 """
 1 O {0,1,2,3,4} {2,D} {3,S}
@@ -29,6 +30,7 @@ u"""
 
 entry(
     label = "Od_rad",
+    multiplicity = [1,2,3,4,5],
     group = 
 """
 1 O 1           {2,D}
@@ -46,6 +48,7 @@ u"""
 
 entry(
     label = "N_birad_RsRsRs",
+    multiplicity = [1,2,3,4,5],
     group = 
 """
 1 N 1         {2,S} {3,S} {4,S}
@@ -65,6 +68,7 @@ u"""
 
 entry(
     label = "N_birad_RsRs",
+    multiplicity = [1,2,3,4,5],
     group = 
 """
 1 N 2         {2,S} {3,S}
@@ -81,41 +85,44 @@ u"""
     ],
 )
 
-entry(
-    label = "C_quintet",
-    group = 
-"""
-1 C 4V
-""",
-    shortDesc = u"""""",
-    longDesc = 
-u"""
-
-""",
-    history = [
-        ("Mon Nov 18 12:27:50 2013","Beat Buesser <bbuesser@mit.edu>","action","""Beat Buesser <bbuesser@mit.edu> created this entry."""),
-    ],
-)
-
-entry(
-    label = "CH_doublet",
-    group = 
-"""
-1 C 3D {2,S}
-2 H 0  {1,S}
-""",
-    shortDesc = u"""""",
-    longDesc = 
-u"""
-
-""",
-    history = [
-        ("Mon Nov 18 12:27:50 2013","Beat Buesser <bbuesser@mit.edu>","action","""Beat Buesser <bbuesser@mit.edu> created this entry."""),
-    ],
-)
+#entry(
+#    label = "C_(V)",
+#    multiplicity = [5],
+#    group = 
+#"""
+#1 C 4
+#""",
+#    shortDesc = u"""""",
+#    longDesc = 
+#u"""
+#
+#""",
+#    history = [
+#        ("Mon Nov 18 12:27:50 2013","Beat Buesser <bbuesser@mit.edu>","action","""Beat Buesser <bbuesser@mit.edu> created this entry."""),
+#    ],
+#)
+#
+#entry(
+#    label = "CH_(D)",
+#    multiplicity = [2],
+#    group = 
+#"""
+#1 C 3 {2,S}
+#2 H 0 {1,S}
+#""",
+#    shortDesc = u"""""",
+#    longDesc = 
+#u"""
+#
+#""",
+#    history = [
+#        ("Mon Nov 18 12:27:50 2013","Beat Buesser <bbuesser@mit.edu>","action","""Beat Buesser <bbuesser@mit.edu> created this entry."""),
+#    ],
+#)
 
 entry(
     label = "C8H7S2J",
+    multiplicity = [1,2,3,4,5],
     group = 
 """
 1  S 0 {5,S} {11,S}
@@ -149,6 +156,7 @@ u"""
 
 entry(
     label = "C8H7S2J(2)",
+    multiplicity = [1,2,3,4,5],
     group = 
 """
 1  S 0 {5,S} {11,S}
@@ -182,10 +190,11 @@ u"""
 
 entry(
     label = "Carbene_D",
+    multiplicity = [1,3],
     group = 
 """
-1 C {2T,2S} {2,D}
-2 C 0       {1,D}
+1 C 2 {2,D}
+2 C 0 {1,D}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -199,10 +208,11 @@ u"""
 
 entry(
     label = "Carbene_S",
+    multiplicity = [1,3],
     group = 
 """
-1 C   {2T,2S} {2,S}
-2 R!H 0       {1,S}
+1 C   2 {2,S}
+2 R!H 0 {1,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -216,6 +226,7 @@ u"""
 
 entry(
     label = "O3",
+    multiplicity = [1,2,3,4,5],
     group = 
 """
 1 O 0 {2,S}
@@ -234,6 +245,7 @@ u"""
 
 entry(
     label = "O3.",
+    multiplicity = [1,2,3,4,5],
     group = 
 """
 1 O 0 {2,S}
@@ -252,6 +264,7 @@ u"""
 
 entry(
     label = "O3..",
+    multiplicity = [1,2,3,4,5],
     group = 
 """
 1 O 1 {2,S}
@@ -270,6 +283,7 @@ u"""
 
 entry(
     label = "O4",
+    multiplicity = [1,2,3,4,5],
     group = 
 """
 1 O 0 {2,S}
@@ -289,6 +303,7 @@ u"""
 
 entry(
     label = "O4.",
+    multiplicity = [1,2,3,4,5],
     group = 
 """
 1 O 0 {2,S}
@@ -308,6 +323,7 @@ u"""
 
 entry(
     label = "O4..",
+    multiplicity = [1,2,3,4,5],
     group = 
 """
 1 O 1 {2,S}
@@ -327,6 +343,7 @@ u"""
 
 entry(
     label = "S3",
+    multiplicity = [1,2,3,4,5],
     group = 
 """
 1 S 0 {2,S}
@@ -345,6 +362,7 @@ u"""
 
 entry(
     label = "S3..",
+    multiplicity = [1,2,3,4,5],
     group = 
 """
 1 S 1 {2,S}
@@ -363,6 +381,7 @@ u"""
 
 entry(
     label = "cyclic-C3O",
+    multiplicity = [1,2,3,4,5],
     group = 
 """
 1 C 0 {2,D} {3,S} {4,S}
@@ -382,6 +401,7 @@ u"""
 
 entry(
     label = "cyclopropyne",
+    multiplicity = [1],
     group = 
 """
 1 C 0 {2,T} {3,S}
