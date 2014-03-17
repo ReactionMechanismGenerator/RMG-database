b8cb73a5fd3123c3a2027a3d0b43cfe85b54d57d
Nathan Yee <nyee@mit.edu>
2014-03-17 13:49:24 -0400

    Add wrapper for testing database
    Running the function EvaluateKinetics.py will use the new checkWellFormed
    database and make an output file called DatabaseWellFormedSummary.txt in
    the RMG-database folder

--- EvaluateKinetics.py
+++ EvaluateKinetics.py
@@ -421,6 +421,47 @@ def LeaveOneOut(FullDatabase, trialDir):
     print 'These families had errors:', familiesWithErrors
     return
 
+def checkFamilies(FullDatabase):
+    familyStatus={}
+    for family in FullDatabase.kinetics.families:
+        print family
+        familyStatus[family]=FullDatabase.kinetics.families[family].checkWellFormed()
+        
+    with open(r'DatabaseWellFormedSummary.txt', 'wb') as outputFile:
+        for family, problems in familyStatus.iteritems():
+            problemsExist=[]
+            for problem in problems:
+                problemsExist.append(not problem==[] and not problem=={})
+            if not problemsExist==[False, False, False, False, False]:
+                outputFile.write(family + '\n')
+                if problemsExist[0]:
+                    outputFile.write('\n' + 'These groups exist in rules.py but not groups.py:' + '\n')
+                    for group in problems[0]:
+                        outputFile.write(group + '\n')
+                if problemsExist[1]:
+                    outputFile.write('\n' + 'These groups do not match the definition in the rule' + '\n')
+                    for rule, groups in problems[1].iteritems():
+                        for group in groups:
+                            if group==groups[-1]:
+                                if len(groups)>1:
+                                    outputFile.write('and ')
+                                outputFile.write(group + ' ')
+                            else:
+                                outputFile.write(group +', ' )
+                        outputFile.write('in ' + rule + '\n')
+                if problemsExist[2]:
+                    outputFile.write('\n' + 'These groups are not in the tree:' + '\n')
+                    for group in problems[2]:
+                        outputFile.write(group + '\n')
+                if problemsExist[3]:
+                    outputFile.write('\n' + 'These groups are not actually subgroups of their parent' + '\n')
+                    for group, parent in problems[3].iteritems():
+                        outputFile.write('Child: ' + group + ', Parent: ' + parent + '\n') 
+                if problemsExist[4]:
+                    outputFile.write('\n' + 'These groups are probably products, but you should check them anyway' + '\n')
+                    for group in problems[4]:
+                        outputFile.write(group + '\n')
+                outputFile.write('\n\n')
 if __name__ == '__main__':
 
     
@@ -434,9 +475,9 @@ if __name__ == '__main__':
     path='C:\RMG-database\input'
 #     FullDatabase.load(thermoLibraries=)
     FullDatabase.load(path)
+    checkFamilies(FullDatabase)
 #     trialDir=r'C:\Users\User1\Dropbox\Research\RMG\kinetics\LeaveOneOut\test'
-    trialDir=r'C:\RMG-database\input_test'
-    FullDatabase.save(trialDir)
+#     trialDir=r'C:\RMG-database\input_test'
 #     family=FullDatabase.kinetics.families['Disproportionation']
 #     entryKey='Y_1centerbirad;O_Cdrad'
 #     
@@ -447,6 +488,6 @@ if __name__ == '__main__':
     
 #     NISTExact(FullDatabase, trialDir)
 #     countNodesAll(NISTDatabase, trialDir)
-    consistencyTest(FullDatabase)
+#     consistencyTest(FullDatabase)
 #     LeaveOneOut(FullDatabase, trialDir)
     
