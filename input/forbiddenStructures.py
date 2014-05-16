7a92fb92bec30d20695bd047e2ae02055f87addc
Beat Buesser <bbuesser@mit.edu>
2014-05-16 17:27:20 -0400

    Rewrite adjacency lists in new format in forbiddenStructures.py
    Unfortunately saveDatabse() reordered the entries.

--- input/forbiddenStructures.py
+++ input/forbiddenStructures.py
@@ -6,129 +6,29 @@ shortDesc = u""
 longDesc = u"""
 
 """
-
-entry(
-    label = "Ods",
-    multiplicity = [1,2,3,4,5],
-    group = 
-"""
-1 O {0,1,2,3,4} {2,D} {3,S}
-2 R {0,1,2,3,4} {1,D}
-3 R {0,1,2,3,4} {1,S}
-""",
-    shortDesc = u"""""",
-    longDesc = 
-u"""
-
-""",
-)
-
-entry(
-    label = "Od_rad",
-    multiplicity = [1,2,3,4,5],
-    group = 
-"""
-1 O 1           {2,D}
-2 R {0,1,2,3,4} {1,D}
-""",
-    shortDesc = u"""""",
-    longDesc = 
-u"""
-
-""",
-)
-
-entry(
-    label = "N_birad_RsRsRs",
-    multiplicity = [1,2,3,4,5],
-    group = 
-"""
-1 N 1         {2,S} {3,S} {4,S}
-2 R {0,1,2,3} {1,S}
-3 R {0,1,2,3} {1,S}
-4 R {0,1,2,3} {1,S}
-""",
-    shortDesc = u"""""",
-    longDesc = 
-u"""
-
-""",
-)
-
-entry(
-    label = "N_birad_RsRs",
-    multiplicity = [1,2,3,4,5],
-    group = 
-"""
-1 N 2         {2,S} {3,S}
-2 R {0,1,2,3} {1,S}
-3 R {0,1,2,3} {1,S}
-""",
-    shortDesc = u"""""",
-    longDesc = 
-u"""
-
-""",
-)
-
-#entry(
-#    label = "C_(V)",
-#    multiplicity = [5],
-#    group = 
-#"""
-#1 C 4
-#""",
-#    shortDesc = u"""""",
-#    longDesc = 
-#u"""
-#
-#""",
-#    history = [
-#        ("Mon Nov 18 12:27:50 2013","Beat Buesser <bbuesser@mit.edu>","action","""Beat Buesser <bbuesser@mit.edu> created this entry."""),
-#    ],
-#)
-#
-#entry(
-#    label = "CH_(D)",
-#    multiplicity = [2],
-#    group = 
-#"""
-#1 C 3 {2,S}
-#2 H 0 {1,S}
-#""",
-#    shortDesc = u"""""",
-#    longDesc = 
-#u"""
-#
-#""",
-#    history = [
-#        ("Mon Nov 18 12:27:50 2013","Beat Buesser <bbuesser@mit.edu>","action","""Beat Buesser <bbuesser@mit.edu> created this entry."""),
-#    ],
-#)
-
 entry(
     label = "C8H7S2J",
-    multiplicity = [1,2,3,4,5],
+    multiplicity = [1, 2, 3, 4, 5],
     group = 
 """
-1  S 0 {5,S} {11,S}
-2  C 0 {3,D} {6,S} {18,S}
-3  C 0 {2,D} {4,S} {13,S}
-4  C 0 {3,S} {5,D} {7,S}
-5  C 0 {1,S} {4,D} {8,S}
-6  H 0 {2,S}
-7  H 0 {4,S}
-8  H 0 {5,S}
-9  S 0 {10,S} {13,S}
-10 C 0 {9,S} {11,S} {14,S} {15,S}
-11 C 0 {1,S} {10,S} {12,S} {16,S}
-12 C 0 {11,S} {13,D} {17,S}
-13 C 0 {3,S} {9,S} {12,D}
-14 H 0 {10,S}
-15 H 0 {10,S}
-16 H 0 {11,S}
-17 H 0 {12,S}
-18 H 0 {2,S}
+1  S U0 {5,S} {11,S}
+2  C U0 {3,D} {6,S} {18,S}
+3  C U0 {2,D} {4,S} {13,S}
+4  C U0 {3,S} {5,D} {7,S}
+5  C U0 {1,S} {4,D} {8,S}
+6  H U0 {2,S}
+7  H U0 {4,S}
+8  H U0 {5,S}
+9  S U0 {10,S} {13,S}
+10 C U0 {9,S} {11,S} {14,S} {15,S}
+11 C U0 {1,S} {10,S} {12,S} {16,S}
+12 C U0 {11,S} {13,D} {17,S}
+13 C U0 {3,S} {9,S} {12,D}
+14 H U0 {10,S}
+15 H U0 {10,S}
+16 H U0 {11,S}
+17 H U0 {12,S}
+18 H U0 {2,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -139,27 +39,56 @@ u"""
 
 entry(
     label = "C8H7S2J(2)",
-    multiplicity = [1,2,3,4,5],
+    multiplicity = [1, 2, 3, 4, 5],
     group = 
 """
-1  S 0 {5,S} {11,S}
-2  C 0 {3,D} {6,S} {18,S}
-3  C 0 {2,D} {4,S} {13,S}
-4  C 0 {3,S} {5,D} {7,S}
-5  C 0 {1,S} {4,D} {8,S}
-6  H 0 {2,S}
-7  H 0 {4,S}
-8  H 0 {5,S}
-9  S 0 {10,S} {13,S}
-10 C 0 {9,S} {11,S} {14,S} {15,S}
-11 C 0 {1,S} {10,S} {12,S} {16,S}
-12 C 0 {11,S} {13,D} {17,S}
-13 C 0 {3,S} {9,S} {12,D}
-14 H 0 {10,S}
-15 H 0 {10,S}
-16 H 0 {11,S}
-17 H 0 {12,S}
-18 H 0 {2,S}
+1  S U0 {5,S} {11,S}
+2  C U0 {3,D} {6,S} {18,S}
+3  C U0 {2,D} {4,S} {13,S}
+4  C U0 {3,S} {5,D} {7,S}
+5  C U0 {1,S} {4,D} {8,S}
+6  H U0 {2,S}
+7  H U0 {4,S}
+8  H U0 {5,S}
+9  S U0 {10,S} {13,S}
+10 C U0 {9,S} {11,S} {14,S} {15,S}
+11 C U0 {1,S} {10,S} {12,S} {16,S}
+12 C U0 {11,S} {13,D} {17,S}
+13 C U0 {3,S} {9,S} {12,D}
+14 H U0 {10,S}
+15 H U0 {10,S}
+16 H U0 {11,S}
+17 H U0 {12,S}
+18 H U0 {2,S}
+""",
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+""",
+)
+
+entry(
+    label = "CH_(D)",
+    multiplicity = [2],
+    group = 
+"""
+1 C U3 {2,S}
+2 H U0 {1,S}
+""",
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+""",
+)
+
+entry(
+    label = "C_(V)",
+    multiplicity = [5],
+    group = 
+"""
+1 C U4
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -170,11 +99,11 @@ u"""
 
 entry(
     label = "Carbene_D",
-    multiplicity = [1,3],
+    multiplicity = [1, 3],
     group = 
 """
-1 C 2 {2,D}
-2 C 0 {1,D}
+1 C U2 {2,D}
+2 C U0 {1,D}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -185,11 +114,44 @@ u"""
 
 entry(
     label = "Carbene_S",
-    multiplicity = [1,3],
+    multiplicity = [1],
     group = 
 """
-1 C   2 {2,S}
-2 R!H 0 {1,S}
+1 C   U2 {2,S}
+2 R!H U0 {1,S}
+""",
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+""",
+)
+
+entry(
+    label = "N_birad_RsRs",
+    multiplicity = [1, 2, 3, 4, 5],
+    group = 
+"""
+1 N U2         {2,S} {3,S}
+2 R U{0,1,2,3} {1,S}
+3 R U{0,1,2,3} {1,S}
+""",
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+""",
+)
+
+entry(
+    label = "N_birad_RsRsRs",
+    multiplicity = [1, 2, 3, 4, 5],
+    group = 
+"""
+1 N U1         {2,S} {3,S} {4,S}
+2 R U{0,1,2,3} {1,S}
+3 R U{0,1,2,3} {1,S}
+4 R U{0,1,2,3} {1,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -200,12 +162,12 @@ u"""
 
 entry(
     label = "O3",
-    multiplicity = [1,2,3,4,5],
+    multiplicity = [1, 2, 3, 4, 5],
     group = 
 """
-1 O 0 {2,S}
-2 O 0 {1,S} {3,S}
-3 O 0 {2,S}
+1 O U0 {2,S}
+2 O U0 {1,S} {3,S}
+3 O U0 {2,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -216,12 +178,12 @@ u"""
 
 entry(
     label = "O3.",
-    multiplicity = [1,2,3,4,5],
+    multiplicity = [1, 2, 3, 4, 5],
     group = 
 """
-1 O 0 {2,S}
-2 O 0 {1,S} {3,S}
-3 O 1 {2,S}
+1 O U0 {2,S}
+2 O U0 {1,S} {3,S}
+3 O U1 {2,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -232,12 +194,12 @@ u"""
 
 entry(
     label = "O3..",
-    multiplicity = [1,2,3,4,5],
+    multiplicity = [1, 2, 3, 4, 5],
     group = 
 """
-1 O 1 {2,S}
-2 O 0 {1,S} {3,S}
-3 O 1 {2,S}
+1 O U1 {2,S}
+2 O U0 {1,S} {3,S}
+3 O U1 {2,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -248,13 +210,13 @@ u"""
 
 entry(
     label = "O4",
-    multiplicity = [1,2,3,4,5],
+    multiplicity = [1, 2, 3, 4, 5],
     group = 
 """
-1 O 0 {2,S}
-2 O 0 {1,S} {3,S}
-3 O 0 {2,S} {4,S}
-4 O 0 {3,S}
+1 O U0 {2,S}
+2 O U0 {1,S} {3,S}
+3 O U0 {2,S} {4,S}
+4 O U0 {3,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -265,13 +227,13 @@ u"""
 
 entry(
     label = "O4.",
-    multiplicity = [1,2,3,4,5],
+    multiplicity = [1, 2, 3, 4, 5],
     group = 
 """
-1 O 0 {2,S}
-2 O 0 {1,S} {3,S}
-3 O 0 {2,S} {4,S}
-4 O 1 {3,S}
+1 O U0 {2,S}
+2 O U0 {1,S} {3,S}
+3 O U0 {2,S} {4,S}
+4 O U1 {3,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -282,13 +244,44 @@ u"""
 
 entry(
     label = "O4..",
-    multiplicity = [1,2,3,4,5],
+    multiplicity = [1, 2, 3, 4, 5],
     group = 
 """
-1 O 1 {2,S}
-2 O 0 {1,S} {3,S}
-3 O 0 {2,S} {4,S}
-4 O 1 {3,S}
+1 O U1 {2,S}
+2 O U0 {1,S} {3,S}
+3 O U0 {2,S} {4,S}
+4 O U1 {3,S}
+""",
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+""",
+)
+
+entry(
+    label = "Od_rad",
+    multiplicity = [1, 2, 3, 4, 5],
+    group = 
+"""
+1 O U1           {2,D}
+2 R U{0,1,2,3,4} {1,D}
+""",
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+
+""",
+)
+
+entry(
+    label = "Ods",
+    multiplicity = [1, 2, 3, 4, 5],
+    group = 
+"""
+1 O U{0,1,2,3,4} {2,D} {3,S}
+2 R U{0,1,2,3,4} {1,D}
+3 R U{0,1,2,3,4} {1,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -299,12 +292,12 @@ u"""
 
 entry(
     label = "S3",
-    multiplicity = [1,2,3,4,5],
+    multiplicity = [1, 2, 3, 4, 5],
     group = 
 """
-1 S 0 {2,S}
-2 S 0 {1,S} {3,S}
-3 S 0 {2,S}
+1 S U0 {2,S}
+2 S U0 {1,S} {3,S}
+3 S U0 {2,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -315,12 +308,12 @@ u"""
 
 entry(
     label = "S3..",
-    multiplicity = [1,2,3,4,5],
+    multiplicity = [1, 2, 3, 4, 5],
     group = 
 """
-1 S 1 {2,S}
-2 S 0 {1,S} {3,S}
-3 S 1 {2,S}
+1 S U1 {2,S}
+2 S U0 {1,S} {3,S}
+3 S U1 {2,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -331,13 +324,13 @@ u"""
 
 entry(
     label = "cyclic-C3O",
-    multiplicity = [1,2,3,4,5],
+    multiplicity = [1, 2, 3, 4, 5],
     group = 
 """
-1 C 0 {2,D} {3,S} {4,S}
-2 O 0 {1,D}
-3 C 0 {1,S} {4,T}
-4 C 0 {1,S} {3,T}
+1 C U0 {2,D} {3,S} {4,S}
+2 O U0 {1,D}
+3 C U0 {1,S} {4,T}
+4 C U0 {1,S} {3,T}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -351,11 +344,11 @@ entry(
     multiplicity = [1],
     group = 
 """
-1 C 0 {2,T} {3,S}
-2 C 0 {1,T} {3,S}
-3 C 0 {1,S} {2,S} {4,S} {5,S}
-4 H 0 {3,S}
-5 H 0 {3,S}
+1 C U0 {2,T} {3,S}
+2 C U0 {1,T} {3,S}
+3 C U0 {1,S} {2,S} {4,S} {5,S}
+4 H U0 {3,S}
+5 H U0 {3,S}
 """,
     shortDesc = u"""""",
     longDesc = 
