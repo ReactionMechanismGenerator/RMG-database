c3d105c8fdf9c7f8835e1c2b0e094c667056593d
Connie Gao <connieg@mit.edu>
2014-09-09 17:24:09 -0400

    Update forbidden groups with lone pairs in substitutionS and substitution_O families.

--- input/kinetics/families/Substitution_O/groups.py
+++ input/kinetics/families/Substitution_O/groups.py
@@ -6689,7 +6689,19 @@ forbidden(
     label = "birad_singlet",
     group = 
 """
-1 *3 R!H 2S
+1 *3 {C,N,Si} 2S 0
+""",
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+""",
+)
+
+forbidden(
+    label = "OS_birad_singlet",
+    group = 
+"""
+1 *3 {O,S} 2S 2
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -6701,11 +6713,10 @@ forbidden(
     label = "quadrad_singlet",
     group = 
 """
-1 *3 R!H 4S
+1 *3 {C,N,Si} 4S 0
 """,
     shortDesc = u"""""",
     longDesc = 
 u"""
 """,
 )
-
