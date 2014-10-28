9220ba4a77fcda727e9098b0e4ec53de2368373d
Belinda Slakman <slakman.b@husky.neu.edu>
2014-10-28 16:04:03 -0700

    Edited silicon hydride groups in the H_Abstraction kinetic groups.
    SiH2_singlet, like CH2_singlet, does not have unpaired electrons, so it does not fall
    under allowed abstracting radicals
    
    Added SiH2_triplet to the tree, since it wasn't there.
    
    Conflicts:
    	input/kinetics/families/H_Abstraction/groups.py

--- input/kinetics/families/H_Abstraction/groups.py
+++ input/kinetics/families/H_Abstraction/groups.py
@@ -6806,23 +6806,6 @@ u"""
 )
 
 entry(
-    index = 501,
-    label = "SiH2_singlet",
-    group = 
-"""
-1 *3 Si u0 p1 c0 {2,S} {3,S}
-2    H  u0 p0 c0 {1,S}
-3    H  u0 p0 c0 {1,S}
-""",
-    kinetics = None,
-    shortDesc = u"""""",
-    longDesc = 
-u"""
-
-""",
-)
-
-entry(
     index = 494,
     label = "SiH2_singlet_H",
     group = 
@@ -7135,9 +7118,7 @@ L1: Y_rad_birad_trirad_quadrad
         L3: O_atom_triplet
         L3: CH2_triplet
         L3: NH_triplet
-        L3: NH_singlet
 	L3: SiH2_triplet
-	L3: SiH2_singlet
     L2: Y_rad
         L3: H_rad
         L3: Y_2centeradjbirad
