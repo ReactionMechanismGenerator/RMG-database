33ecf6c83da0cf97540545a95647d3b677f0b457
Connie Gao <connieg@mit.edu>
2014-11-03 19:19:33 -0500

    Export to new-adjacency-list style: Part 11. Forbidden Structures.

--- input/forbiddenStructures.py
+++ input/forbiddenStructures.py
@@ -6,14 +6,13 @@ shortDesc = u""
 longDesc = u"""
 
 """
-
 entry(
     label = "Ods",
     group = 
 """
-1 O X {2,D} {3,S}
-2 R X {1,D}
-3 R X {1,S}
+1 O ux {2,D} {3,S}
+2 R ux {1,D}
+3 R ux {1,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -26,8 +25,8 @@ entry(
     label = "Od_rad",
     group = 
 """
-1 O 1           {2,D}
-2 R X {1,D}
+1 O u1 {2,D}
+2 R ux {1,D}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -40,10 +39,10 @@ entry(
     label = "N_monorad_3singleBonds",
     group = 
 """
-1 N 1 0 {2,S} {3,S} {4,S}
-2 R X {1,S}
-3 R X {1,S}
-4 R X {1,S}
+1 N u1 p0 {2,S} {3,S} {4,S}
+2 R ux {1,S}
+3 R ux {1,S}
+4 R ux {1,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -56,9 +55,9 @@ entry(
     label = "N_birad_singlet_2singleBonds",
     group = 
 """
-1 N 2S 0 {2,S} {3,S}
-2 R X {1,S}
-3 R X {1,S}
+1 N u0 p1 {2,S} {3,S}
+2 R ux {1,S}
+3 R ux {1,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -71,9 +70,9 @@ entry(
     label = "N_birad_triplet_2singleBonds",
     group = 
 """
-1 N 2T 0 {2,S} {3,S}
-2 R X {1,S}
-3 R X {1,S}
+1 N u2 p0 {2,S} {3,S}
+2 R ux {1,S}
+3 R ux {1,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -86,7 +85,7 @@ entry(
     label = "C_quintet",
     group = 
 """
-1 C 4V 0
+1 C u4 p0
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -99,8 +98,8 @@ entry(
     label = "CH_doublet",
     group = 
 """
-1 C 3D 0 {2,S}
-2 H 0  {1,S}
+1 C u1 p1 {2,S}
+2 H u0 {1,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -113,24 +112,24 @@ entry(
     label = "C8H7S2J",
     group = 
 """
-1  S 0 {5,S} {11,S}
-2  C 0 {3,D} {6,S} {18,S}
-3  C 0 {2,D} {4,S} {13,S}
-4  C 0 {3,S} {5,D} {7,S}
-5  C 0 {1,S} {4,D} {8,S}
-6  H 0 {2,S}
-7  H 0 {4,S}
-8  H 0 {5,S}
-9  S 0 {10,S} {13,S}
-10 C 0 {9,S} {11,S} {14,S} {15,S}
-11 C 0 {1,S} {10,S} {12,S} {16,S}
-12 C 0 {11,S} {13,D} {17,S}
-13 C 0 {3,S} {9,S} {12,D}
-14 H 0 {10,S}
-15 H 0 {10,S}
-16 H 0 {11,S}
-17 H 0 {12,S}
-18 H 0 {2,S}
+1  S u0 {5,S} {11,S}
+2  C u0 {3,D} {6,S} {18,S}
+3  C u0 {2,D} {4,S} {13,S}
+4  C u0 {3,S} {5,D} {7,S}
+5  C u0 {1,S} {4,D} {8,S}
+6  H u0 {2,S}
+7  H u0 {4,S}
+8  H u0 {5,S}
+9  S u0 {10,S} {13,S}
+10 C u0 {9,S} {11,S} {14,S} {15,S}
+11 C u0 {1,S} {10,S} {12,S} {16,S}
+12 C u0 {11,S} {13,D} {17,S}
+13 C u0 {3,S} {9,S} {12,D}
+14 H u0 {10,S}
+15 H u0 {10,S}
+16 H u0 {11,S}
+17 H u0 {12,S}
+18 H u0 {2,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -143,24 +142,24 @@ entry(
     label = "C8H7S2J(2)",
     group = 
 """
-1  S 0 {5,S} {11,S}
-2  C 0 {3,D} {6,S} {18,S}
-3  C 0 {2,D} {4,S} {13,S}
-4  C 0 {3,S} {5,D} {7,S}
-5  C 0 {1,S} {4,D} {8,S}
-6  H 0 {2,S}
-7  H 0 {4,S}
-8  H 0 {5,S}
-9  S 0 {10,S} {13,S}
-10 C 0 {9,S} {11,S} {14,S} {15,S}
-11 C 0 {1,S} {10,S} {12,S} {16,S}
-12 C 0 {11,S} {13,D} {17,S}
-13 C 0 {3,S} {9,S} {12,D}
-14 H 0 {10,S}
-15 H 0 {10,S}
-16 H 0 {11,S}
-17 H 0 {12,S}
-18 H 0 {2,S}
+1  S u0 {5,S} {11,S}
+2  C u0 {3,D} {6,S} {18,S}
+3  C u0 {2,D} {4,S} {13,S}
+4  C u0 {3,S} {5,D} {7,S}
+5  C u0 {1,S} {4,D} {8,S}
+6  H u0 {2,S}
+7  H u0 {4,S}
+8  H u0 {5,S}
+9  S u0 {10,S} {13,S}
+10 C u0 {9,S} {11,S} {14,S} {15,S}
+11 C u0 {1,S} {10,S} {12,S} {16,S}
+12 C u0 {11,S} {13,D} {17,S}
+13 C u0 {3,S} {9,S} {12,D}
+14 H u0 {10,S}
+15 H u0 {10,S}
+16 H u0 {11,S}
+17 H u0 {12,S}
+18 H u0 {2,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -173,8 +172,8 @@ entry(
     label = "Carbene_D_triplet",
     group = 
 """
-1 C 2T 0 {2,D}
-2 C 0       {1,D}
+1 C u2 p0 {2,D}
+2 C u0 {1,D}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -187,8 +186,8 @@ entry(
     label = "Carbene_D_singlet",
     group = 
 """
-1 C 2S 0 {2,D}
-2 C 0       {1,D}
+1 C u0 p1 {2,D}
+2 C u0 {1,D}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -201,8 +200,8 @@ entry(
     label = "Carbene_S_triplet",
     group = 
 """
-1 C   2T 0 {2,S}
-2 R!H 0       {1,S}
+1 C   u2 p0 {2,S}
+2 R!H u0 {1,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -215,8 +214,8 @@ entry(
     label = "Carbene_S_singlet",
     group = 
 """
-1 C 2S 0 {2,S}
-2 R!H 0       {1,S}
+1 C   u0 p1 {2,S}
+2 R!H u0 {1,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -229,9 +228,9 @@ entry(
     label = "O3",
     group = 
 """
-1 O 0 {2,S}
-2 O 0 {1,S} {3,S}
-3 O 0 {2,S}
+1 O u0 {2,S}
+2 O u0 {1,S} {3,S}
+3 O u0 {2,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -244,9 +243,9 @@ entry(
     label = "O3.",
     group = 
 """
-1 O 0 {2,S}
-2 O 0 {1,S} {3,S}
-3 O 1 {2,S}
+1 O u0 {2,S}
+2 O u0 {1,S} {3,S}
+3 O u1 {2,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -259,9 +258,9 @@ entry(
     label = "O3..",
     group = 
 """
-1 O 1 {2,S}
-2 O 0 {1,S} {3,S}
-3 O 1 {2,S}
+1 O u1 {2,S}
+2 O u0 {1,S} {3,S}
+3 O u1 {2,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -274,10 +273,10 @@ entry(
     label = "O4",
     group = 
 """
-1 O 0 {2,S}
-2 O 0 {1,S} {3,S}
-3 O 0 {2,S} {4,S}
-4 O 0 {3,S}
+1 O u0 {2,S}
+2 O u0 {1,S} {3,S}
+3 O u0 {2,S} {4,S}
+4 O u0 {3,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -290,10 +289,10 @@ entry(
     label = "O4.",
     group = 
 """
-1 O 0 {2,S}
-2 O 0 {1,S} {3,S}
-3 O 0 {2,S} {4,S}
-4 O 1 {3,S}
+1 O u0 {2,S}
+2 O u0 {1,S} {3,S}
+3 O u0 {2,S} {4,S}
+4 O u1 {3,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -306,10 +305,10 @@ entry(
     label = "O4..",
     group = 
 """
-1 O 1 {2,S}
-2 O 0 {1,S} {3,S}
-3 O 0 {2,S} {4,S}
-4 O 1 {3,S}
+1 O u1 {2,S}
+2 O u0 {1,S} {3,S}
+3 O u0 {2,S} {4,S}
+4 O u1 {3,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -322,9 +321,9 @@ entry(
     label = "S3",
     group = 
 """
-1 S 0 {2,S}
-2 S 0 {1,S} {3,S}
-3 S 0 {2,S}
+1 S u0 {2,S}
+2 S u0 {1,S} {3,S}
+3 S u0 {2,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -337,9 +336,9 @@ entry(
     label = "S3..",
     group = 
 """
-1 S 1 {2,S}
-2 S 0 {1,S} {3,S}
-3 S 1 {2,S}
+1 S u1 {2,S}
+2 S u0 {1,S} {3,S}
+3 S u1 {2,S}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -352,10 +351,10 @@ entry(
     label = "cyclic-C3O",
     group = 
 """
-1 C 0 {2,D} {3,S} {4,S}
-2 O 0 {1,D}
-3 C 0 {1,S} {4,T}
-4 C 0 {1,S} {3,T}
+1 C u0 {2,D} {3,S} {4,S}
+2 O u0 {1,D}
+3 C u0 {1,S} {4,T}
+4 C u0 {1,S} {3,T}
 """,
     shortDesc = u"""""",
     longDesc = 
@@ -368,11 +367,11 @@ entry(
     label = "cyclopropyne",
     group = 
 """
-1 C 0 {2,T} {3,S}
-2 C 0 {1,T} {3,S}
-3 C 0 {1,S} {2,S} {4,S} {5,S}
-4 H 0 {3,S}
-5 H 0 {3,S}
+1 C u0 {2,T} {3,S}
+2 C u0 {1,T} {3,S}
+3 C u0 {1,S} {2,S} {4,S} {5,S}
+4 H u0 {3,S}
+5 H u0 {3,S}
 """,
     shortDesc = u"""""",
     longDesc = 
