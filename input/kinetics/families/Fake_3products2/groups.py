a0bf7686cf3c669861b4bd37019adff1fdf4135d
Richard West <r.west@neu.edu>
2014-12-04 20:52:56 -0500

    Made another fake family with 3 products.
    This one (Fake_3products2) is like the previous (Fake_3products1)
    except there is already a radical, and you form a double bond.

---
+++ input/kinetics/families/Fake_3products2/groups.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "Fake_3products2/groups"
+shortDesc = u""
+longDesc = u"""
+
+"""
+
+template(reactants=["HOORRRj"], products=["P1", "P2", "P3"], ownReverse=False)
+
+reverse = "trimolecular2"
+
+recipe(actions=[
+    ['GAIN_RADICAL', '*2', '1'],
+    ['BREAK_BOND', '*2', 'S', '*3'],
+    ['CHANGE_BOND', '*3', '1', '*4'],
+    ['BREAK_BOND', '*4', 'S', '*5'],
+    ['CHANGE_BOND', '*5', '1', '*6'],
+    ['LOSE_RADICAL', '*6', '1'],
+])
+
+entry(
+    index = 1,
+    label = "HOORRRj",
+    group = """
+1 *1 H  0 {2,S}
+2 *2 O  0 {1,S} {3,S}
+3 *3 O  0 {2,S} {4,S}
+4 *4 R  0 {3,S} {5,S}
+5 *5 R  0 {4,S} {6,S}
+6 *6 R  1 {5,S}
+""",
+    kinetics = None,
+    reference = None,
+    referenceType = "",
+    shortDesc = u"""""",
+    longDesc = u"""""",
+    history = [
+        ("Thu Dec 4 2014","Richard West","action","""Created to use only for importing other groups chemkin files"""),
+    ],
+)
+
+
+tree(
+"""
+L1: HOORRRj
+"""
+)
