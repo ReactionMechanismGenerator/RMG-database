258484b5c52ab60f3f59a8503885d6e1a0d8216d
Enoch Dames <enoch.dames@gmail.com>
2015-06-16 17:40:27 -0400

    add two training reactions to intra_H migration family
    I have quite a few more, but wanted to start with just two reactions to
    be sure i'm doing this correctly.

--- input/kinetics/families/intra_H_migration/training/reactions.py
+++ input/kinetics/families/intra_H_migration/training/reactions.py
@@ -35,3 +35,58 @@ using Variflex.
 """,
 )
 
+entry(
+    index = 2,
+    label = "S1C4 <=> S1C4b",
+    degeneracy = 3,
+    kinetics = Arrhenius(
+        A = (1.775e+09, 's^-1', '*|/', 3.0),
+        n = 0.686,
+        Ea = (6.774, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+    reference = Article(
+        authors = ["E. E. Dames", "W. H. Green"],
+        title = u'THE EFFECT OF ALCOHOL AND CARBONYL FUNCTIONAL GROUPS ON THE COMPETITION BETWEEN UNIMOLECULAR DECOMPOSITION AND ISOMERIZATION IN C 1 -C 5 ALKOXY RADICALS',
+        journal = "Intl. J. Chem. Kin.",
+        volume = "???",
+        pages = """???-???""",
+        year = "2015",
+    ),
+    referenceType = "theory",
+    shortDesc = u"""TST calculations at M08SO/MG3S level""",
+    longDesc = 
+u"""
+Quantum chemistry calculations at the M08SO/MG3S level
+using Qchem. High-pressure-limit rate coefficient computed
+using Cantherm with 1D hindered rotor treatment.
+""",
+)
+
+entry(
+    index = 3,
+    label = "S2C4 <=> S2C4b",
+    degeneracy = 3,
+    kinetics = Arrhenius(
+        A = (1.540e+04, 's^-1', '*|/', 3.0),
+        n = 2.338,
+        Ea = (7.127, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+    reference = Article(
+        authors = ["E. E. Dames", "W. H. Green"],
+        title = u'THE EFFECT OF ALCOHOL AND CARBONYL FUNCTIONAL GROUPS ON THE COMPETITION BETWEEN UNIMOLECULAR DECOMPOSITION AND ISOMERIZATION IN C 1 -C 5 ALKOXY RADICALS',
+        journal = "Intl. J. Chem. Kin.",
+        volume = "???",
+        pages = """???-???""",
+        year = "2015",
+    ),
+    referenceType = "theory",
+    shortDesc = u"""TST calculations at M08SO/MG3S level""",
+    longDesc = 
+u"""
+Quantum chemistry calculations at the M08SO/MG3S level
+using Qchem. High-pressure-limit rate coefficient computed
+using Cantherm with 1D hindered rotor treatment.
+""",
+)
