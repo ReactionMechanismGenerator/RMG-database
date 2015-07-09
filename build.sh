364de3e85927457113132a52601238ef4c5c03d4
Connie Gao <connieg@mit.edu>
2015-07-09 11:23:11 -0400

    Create rmgrc file in rmgpy site-packages for conda build
    Conda build should not be used with RMG-Py built from source.
    A manually created rmgrc file would be needed since this would only
    drop rmgrc into the conda site-packages, rather than the source
    folder intended by the user.

--- build.sh
+++ build.sh
@@ -5,3 +5,8 @@ $PYTHON setup.py install
 cd ${PREFIX}/share/
 mkdir rmgdatabase
 cp -R ${SRC_DIR}/input/* rmgdatabase/
+
+# Create rmgrc file that points to this folder in site-packages/rmgpy directory
+cd ${SP_DIR}
+mkdir rmgpy
+echo 'database.directory: ' ${PREFIX}/share/rmgdatabase > ${SP_DIR}/rmgpy/rmgrc
