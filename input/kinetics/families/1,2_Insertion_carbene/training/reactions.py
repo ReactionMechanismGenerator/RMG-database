ddb0155d57da207a7145469797976851fd28b402
Nathan Yee <nyee@mit.edu>
2015-08-27 12:35:03 -0400

    Fix unit error on A factor for 1,2_Insertion carbene

--- input/kinetics/families/1,2_Insertion_carbene/training/reactions.py
+++ input/kinetics/families/1,2_Insertion_carbene/training/reactions.py
@@ -13,7 +13,7 @@ entry(
     label = "CH2 + C2H2 <=> CH3CCH",
     degeneracy = 2,
     kinetics = Arrhenius(
-        A = (6.63e+7, 's^-1', '*|/', 0.25),
+        A = (6.63e+7, 'cm^3/(mol*s)', '*|/', 0.25),
         n = 1.475,
         Ea = (-1.651, 'kcal/mol'),
         T0 = (1, 'K'),
@@ -40,7 +40,7 @@ entry(
     label = "CH2 + C2H4 <=> CH3CHCH2",
     degeneracy = 4,
     kinetics = Arrhenius(
-        A = (5.3e+12, 's^-1', '*|/', 0.25),
+        A = (5.3e+12, 'cm^3/(mol*s)', '*|/', 0.25),
         n = 0.0073,
         Ea = (-1.054, 'kcal/mol'),
         T0 = (1, 'K'),
@@ -67,7 +67,7 @@ entry(
     label = "CH2 + CH3CCH_r1 <=> CH3CCCH3",
     degeneracy = 1,
     kinetics = Arrhenius(
-        A = (2.03e+8, 's^-1', '*|/', 0.25),
+        A = (2.03e+8, 'cm^3/(mol*s)', '*|/', 0.25),
         n = 1.249,
         Ea = (-2.214, 'kcal/mol'),
         T0 = (1, 'K'),
@@ -94,7 +94,7 @@ entry(
     label = "CH2 + CH3CCH_r2 <=> CH3CH2CCH",
     degeneracy = 3,
     kinetics = Arrhenius(
-        A = (9.18e+10, 's^-1', '*|/', 0.25),
+        A = (9.18e+10, 'cm^3/(mol*s)', '*|/', 0.25),
         n = 0.524,
         Ea = (-0.711, 'kcal/mol'),
         T0 = (1, 'K'),
@@ -121,7 +121,7 @@ entry(
     label = "CH2 + CH3CHCH2_r1 <=> CH3CHCHCH3",
     degeneracy = 2,
     kinetics = Arrhenius(
-        A = (3.97e+13, 's^-1', '*|/', 0.25),
+        A = (3.97e+13, 'cm^3/(mol*s)', '*|/', 0.25),
         n = -0.324,
         Ea = (-0.935, 'kcal/mol'),
         T0 = (1, 'K'),
@@ -148,7 +148,7 @@ entry(
     label = "CH2 + CH3CHCH2_r2 <=> CH2C(CH3)CH3",
     degeneracy = 1,
     kinetics = Arrhenius(
-        A = (2.13e+13, 's^-1', '*|/', 0.25),
+        A = (2.13e+13, 'cm^3/(mol*s)', '*|/', 0.25),
         n = -0.274,
         Ea = (-0.686, 'kcal/mol'),
         T0 = (1, 'K'),
@@ -175,7 +175,7 @@ entry(
     label = "CH2 + CH3CHCH2_r3 <=> CH3CH2CHCH2",
     degeneracy = 3,
     kinetics = Arrhenius(
-        A = (1.87e+13, 's^-1', '*|/', 0.25),
+        A = (1.87e+13, 'cm^3/(mol*s)', '*|/', 0.25),
         n = -0.146,
         Ea = (0.00284, 'kcal/mol'),
         T0 = (1, 'K'),
@@ -202,7 +202,7 @@ entry(
     label = "CH2 + CH2CCH2 <=> CH3CHCCH2",
     degeneracy = 4,
     kinetics = Arrhenius(
-        A = (2.80e+11, 's^-1', '*|/', 0.25),
+        A = (2.80e+11, 'cm^3/(mol*s)', '*|/', 0.25),
         n = 0.465,
         Ea = (-1.742, 'kcal/mol'),
         T0 = (1, 'K'),
@@ -229,7 +229,7 @@ entry(
     label = "CH2 + CH2CHCHCH2_r1 <=> CH3CHCHCHCH2",
     degeneracy = 4,
     kinetics = Arrhenius(
-        A = (5.26e+11, 's^-1', '*|/', 0.25),
+        A = (5.26e+11, 'cm^3/(mol*s)', '*|/', 0.25),
         n = 0.313,
         Ea = (-1.854, 'kcal/mol'),
         T0 = (1, 'K'),
@@ -256,7 +256,7 @@ entry(
     label = "CH2 + CH2CHCHCH2_r2 <=> CH2C(CH3)CHCH2",
     degeneracy = 2,
     kinetics = Arrhenius(
-        A = (7.34e+10, 's^-1', '*|/', 0.25),
+        A = (7.34e+10, 'cm^3/(mol*s)', '*|/', 0.25),
         n = 0.498,
         Ea = (-1.758, 'kcal/mol'),
         T0 = (1, 'K'),
@@ -283,7 +283,7 @@ entry(
     label = "CH2 + CH3CCCH3_r1 <=> CH3CH2CCCH3",
     degeneracy = 6,
     kinetics = Arrhenius(
-        A = (2.96e+10, 's^-1', '*|/', 0.25),
+        A = (2.96e+10, 'cm^3/(mol*s)', '*|/', 0.25),
         n = 0.797,
         Ea = (-1.174, 'kcal/mol'),
         T0 = (1, 'K'),
@@ -310,7 +310,7 @@ entry(
     label = "CH2 + benzene <=> toluene",
     degeneracy = 6,
     kinetics = Arrhenius(
-        A = (6.48e+13, 's^-1', '*|/', 0.25),
+        A = (6.48e+13, 'cm^3/(mol*s)', '*|/', 0.25),
         n = -0.272,
         Ea = (-1.274, 'kcal/mol'),
         T0 = (1, 'K'),
