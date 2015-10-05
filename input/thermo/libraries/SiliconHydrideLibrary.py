e27d89f6a82b2831683ec17237e1cdff2c924982
Belinda Slakman <slakman.b@husky.neu.edu>
2015-10-05 19:56:39 -0400

    Modify silicon hydride thermo library with correct symmetry contributions.

--- input/thermo/libraries/SiliconHydrideLibrary.py
+++ input/thermo/libraries/SiliconHydrideLibrary.py
@@ -11,13 +11,13 @@ entry(
 		""",
 		thermo = ThermoData(
 			Tdata = ([300,400,500,600,800,1000,1500],'K'),
-			Cpdata = ([10.27, 12.30, 14.14, 15.75, 18.33, 20.2, 22.83],'cal/(mol*K)'),
-			H298 = (8.2, 'kcal/mol'),
-			S298 = (48.913, 'cal/(mol*K)'),
+			Cpdata = ([10.294,12.197,14.028,15.663,18.268,20.118,22.738],'cal/(mol*K)'),
+			H298 = (5.572, 'kcal/mol'),
+			S298 = (48.865, 'cal/(mol*K)'),
 			),
 		shortDesc = u"silane",
 		longDesc = u"""
-		From NIST Webbook
+		CBS-QB3 calculations using g09 and Cantherm, by Belinda Slakman 09/17/15; symm=12
 		""",
 		)
 
@@ -93,13 +93,13 @@ entry(
 		""",
 		thermo = ThermoData(
 			Tdata = ([300,400,500,600,800,1000,1500],'K'),
-			Cpdata = ([9.579,10.781,11.968,13.031,14.702,15.93,17.732],'cal/(mol*K)'),
-			H298 = (45.879, 'kcal/mol'),
-			S298 = (54.005, 'cal/(mol*K)')
+			Cpdata = ([9.589,10.795,11.96,13.01,14.705,15.934,17.717],'cal/(mol*K)'),
+			H298 = (45.894, 'kcal/mol'),
+			S298 = (51.821, 'cal/(mol*K)')
 		),
 		shortDesc = u"Silyl radical",
 		longDesc = u"""
-		CBS-QB3 calculations using g03 and CanTherm, by Belinda Slakman 10/24/14
+		CBS-QB3 calculations using g09 and CanTherm, by Belinda Slakman 10/03/15; symm=3
 		""",
 		)
 
@@ -119,13 +119,13 @@ entry(
 		""",
 		thermo = ThermoData(
 			Tdata = ([300,400,500,600,800,1000,1500],'K'),
-			Cpdata = ([19.193,22.953,25.955,28.5,32.496,35.321,39.159],'cal/(mol*K)'),
-			H298 = (15.954, 'kcal/mol'),
-			S298 = (65.546, 'J/(mol*K)'),
+			Cpdata = ([19.08,22.854,26.004,28.602,32.517,35.236,39.146],'cal/(mol*K)'),
+			H298 = (15.974, 'kcal/mol'),
+			S298 = (65.426, 'cal/(mol*K)'),
 			),
 		shortDesc = u"Disilane",
 		longDesc = u"""
-		CBS-QB3 calculations using g03 and CanTherm, by Belinda Slakman 10/24/14
+		CBS-QB3 calculations using g09 and CanTherm, by Belinda Slakman 9/16/15; symm=6
 		""",
 		)
 
@@ -142,11 +142,11 @@ entry(
 			Tdata = ([300,400,500,600,800,1000,1500],'K'),
 			Cpdata = ([8.35,8.821,9.381,9.954,10.921,11.651,12.734],'cal/(mol*K)'),
 			H298 = (62.875, 'kcal/mol'),
-			S298 = (50.932, 'cal/(mol*K)'),
+			S298 = (49.554, 'cal/(mol*K)'),
 			),
 		shortDesc = u"",
 		longDesc = u"""
-		CBS-QB3 calculations using g03 and CanTherm, by Belinda Slakman 10/24/14
+		CBS-QB3 calculations using g09 and CanTherm, by Belinda Slakman 10/03/15; symm=2
 		""",
 		)
 
@@ -161,13 +161,13 @@ entry(
 		""",
 		thermo = ThermoData(
 			Tdata = ([300,400,500,600,800,1000,1500],'K'),
-			Cpdata = ([8.472,8.926,9.439,9.959,10.855,11.549,12.629],'cal/(mol*K)'),
-			H298 = (84.868, 'kcal/mol'),
-			S298 = (52.781, 'cal/(mol*K)'),
+			Cpdata = ([8.471,8.935,9.442,9.944,10.841,11.547,12.622],'cal/(mol*K)'),
+			H298 = (84.884, 'kcal/mol'),
+			S298 = (51.402, 'cal/(mol*K)'),
 			),
 		shortDesc = u"",
 		longDesc = u"""
-		CBS-QB3 calculations using g03 and CanTherm, by Belinda Slakman 10/24/14
+		CBS-QB3 calculations using g09 and CanTherm, by Belinda Slakman 10/03/15; symm=2
 		""",
 		)
 
@@ -206,13 +206,61 @@ entry(
 		""",
 		thermo = ThermoData(
 			Tdata = ([300,400,500,600,800,1000,1500],'K'),
-			Cpdata = ([15.554,17.003,18.243,19.305,20.98,22.175,23.834], 'cal/(mol*K)'),
-			H298 = (95.251, 'kcal/mol'),
-			S298 = (68.744, 'cal/(mol*K)'),
+			Cpdata = ([15.501,17.076,18.319,19.344,20.949,22.12,23.855], 'cal/(mol*K)'),
+			H298 = (95.238, 'kcal/mol'),
+			S298 = (69.912, 'cal/(mol*K)'),
 			),
 		shortDesc = u"",
 		longDesc = u"""
-		CBS-QB3 calculations using g03 and CanTherm, by Belinda Slakman 11/6/14 
+		CBS-QB3 calculations using g09 and CanTherm, by Belinda Slakman 10/03/15; symm=1 
+		""",
+		)
+
+entry(
+		index = 11,
+		label = 'Si2H4',
+		molecule = 
+		"""
+		1 Si u0 p0 c0 {2,S} {3,D} {6,S}
+		2 H u0 p0 c0 {1,S}
+		3 Si u0 p0 c0 {1,D} {4,S} {5,S}
+		4 H u0 p0 c0 {3,S}
+		5 H u0 p0 c0 {3,S}
+                6 H u0 p0 c0 {1,S}
+		""",
+		thermo = ThermoData(
+			Tdata = ([300,400,500,600,800,1000,1500],'K'),
+			Cpdata = ([16.524,19.06,21.043,22.599,24.858,26.427,28.838], 'cal/(mol*K)'),
+			H298 = (64.556, 'kcal/mol'),
+			S298 = (64.263, 'cal/(mol*K)'),
+			),
+		shortDesc = u"",
+		longDesc = u"""
+		CBS-QB3 calculations using g09 and CanTherm, by Belinda Slakman 9/16/15; symm=2 
+		""",
+		)
+
+entry(
+		index = 12,
+		label = 'H3SiSiH',
+		molecule = 
+		"""
+		1 Si u0 p0 c0 {2,S} {3,S} {4,S} {5,S}
+		2 H u0 p0 c0 {1,S}
+		3 Si u0 p1 c0 {1,S} {6,S}
+		4 H u0 p0 c0 {1,S}
+		5 H u0 p0 c0 {1,S}
+                6 H u0 p0 c0 {3,S}
+		""",
+		thermo = ThermoData(
+			Tdata = ([300,400,500,600,800,1000,1500],'K'),
+			Cpdata = ([16.597,18.912,20.863,22.48,24.913,26.585,28.948], 'cal/(mol*K)'),
+			H298 = (72.429, 'kcal/mol'),
+			S298 = (70.02, 'cal/(mol*K)'),
+			),
+		shortDesc = u"",
+		longDesc = u"""
+		CBS-QB3 calculations using g09 and CanTherm, by Belinda Slakman 10/05/15; symm=1
 		""",
 		)
 
