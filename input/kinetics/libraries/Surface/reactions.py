c7f739e9d13607a6d11cd7889c74e6c9a063bb14
Franklin Goldsmith <franklin_goldsmith@brown.edu>
2016-05-16 21:01:07 -0400

    New reaction to stop non-dissociative adsorption of O2:
    I added a new reaction to stop non-dissociative adsorption of O2:
    
    O2 + Ni <=> O2X
    I set the pre-exponential sticking coefficient to zero:
    A = 0
    Otherwise, Surface_Adsorption_Single creates:
     O*
     |
     O
     |
    ~X~
    which is not helpful.

--- input/kinetics/libraries/Surface/reactions.py
+++ input/kinetics/libraries/Surface/reactions.py
@@ -22,9 +22,22 @@ entry(
 )
 
 
-
 entry(
     index = 2,
+    label = "O2 + Ni <=> O2X",
+    kinetics = StickingCoefficient(
+        A = 0.0,
+        n = 0,
+        Ea=(0, 'kcal/mol'),
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
+    ),
+    shortDesc = u"""Default""",
+    longDesc = u"""Made up"""
+)
+
+entry(
+    index = 3,
     label = "CH4 + Ni + Ni <=> CH3X + HX",
     kinetics = StickingCoefficient(
         A = 0.1,
