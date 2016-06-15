6fcb052edcc90d902832b767e8d93b84611812e0
Nathan Yee <nyee@mit.edu>
2016-06-15 17:53:51 -0400

    Standardize CS atomType usage for radical.py in thermo groups
    Changed 12 instances of Cd=Sd to CS

--- input/thermo/groups/radical.py
+++ input/thermo/groups/radical.py
@@ -3771,10 +3771,9 @@ entry(
     group = 
 """
 1 * Cs u1 {2,S} {3,S} {4,S}
-2   Cd u0 {1,S} {5,D}
+2   CS u0 {1,S}
 3   Ss u0 {1,S}
 4   H  u0 {1,S}
-5   Sd u0 {2,D}
 """,
     thermo = ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
@@ -3907,9 +3906,8 @@ entry(
 """
 1 * Cs u1 {2,S} {3,S} {4,S}
 2   Cs u0 {1,S}
-3   Cd u0 {1,S} {5,D}
+3   CS u0 {1,S}
 4   Ss u0 {1,S}
-5   Sd u0 {3,D}
 """,
     thermo = ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
@@ -4067,10 +4065,9 @@ entry(
     group = 
 """
 1 * Cs u1 {2,S} {3,S} {4,S}
-2   Cd u0 {1,S} {5,D}
+2   CS u0 {1,S}
 3   Ss u0 {1,S}
 4   Ss u0 {1,S}
-5   Sd u0 {2,D}
 """,
     thermo = None,
     shortDesc = u"""""",
@@ -4444,8 +4441,7 @@ entry(
     label = "C=SJ",
     group = 
 """
-1 * Cd u1 {2,D}
-2   Sd u0 {1,D}
+1 * CS u1
 """,
     thermo = None,
     shortDesc = u"""""",
@@ -4460,9 +4456,8 @@ entry(
     label = "C=SJ-H",
     group = 
 """
-1 * Cd u1 {2,S} {3,D}
+1 * CS u1 {2,S}
 2   H  u0 {1,S}
-3   Sd u0 {1,D}
 """,
     thermo = ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
@@ -4482,9 +4477,8 @@ entry(
     label = "C=SJ-C",
     group = 
 """
-1 * Cd u1 {2,S} {3,D}
+1 * CS u1 {2,S}
 2   C  u0 {1,S}
-3   Sd u0 {1,D}
 """,
     thermo = None,
     shortDesc = u"""""",
@@ -4499,9 +4493,8 @@ entry(
     label = "C=SJ-Cs",
     group = 
 """
-1 * Cd u1 {2,S} {3,D}
+1 * CS u1 {2,S}
 2   Cs u0 {1,S}
-3   Sd u0 {1,D}
 """,
     thermo = ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
@@ -4521,9 +4514,8 @@ entry(
     label = "C=SJ-Cd",
     group = 
 """
-1 * Cd u1 {2,S} {3,D}
+1 * CS u1 {2,S}
 2   Cd u0 {1,S}
-3   Sd u0 {1,D}
 """,
     thermo = ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
@@ -4543,9 +4535,8 @@ entry(
     label = "C=SJ-Ss",
     group = 
 """
-1 * Cd u1 {2,S} {3,D}
+1 * CS u1 {2,S}
 2   Ss u0 {1,S}
-3   Sd u0 {1,D}
 """,
     thermo = None,
     shortDesc = u"""""",
@@ -5427,8 +5418,7 @@ entry(
     group = 
 """
 1 * Ss u1 {2,S}
-2   Cd u0 {1,S} {3,D}
-3   Sd u0 {2,D}
+2   CS u0 {1,S}
 """,
     thermo = ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
@@ -5770,8 +5760,7 @@ entry(
     label = "CdJ2-Sd_triplet",
     group = 
 """
-1 * Cd u2 {2,D}
-2   Sd u0 {1,D}
+1 * CS u2
 """,
     thermo = ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
@@ -6077,8 +6066,7 @@ entry(
     label = "CdJ2-Sd_singlet",
     group = 
 """
-1 * Cd u0 p1 {2,D}
-2   Sd u0 {1,D}
+1 * CS u0 p1
 """,
     thermo = ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
