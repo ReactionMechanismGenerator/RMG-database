1c5986c2365b6e74fa7ed6a0210e199008f64223
Richard West <r.west@northeastern.edu>
2016-06-21 11:35:25 -0400

    Switch to using ThermoData in groups/adsorption.py
    These are added just like regular group additive thermo.
    There needn't be anything special about them.
    Easier to re-use existing code if we keep them
    the same as normal group thermo data objects.

--- input/thermo/groups/adsorption.py
+++ input/thermo/groups/adsorption.py
@@ -69,11 +69,11 @@ entry(
 2 R u0 p0 {1,S}
 3 X u0 p0 {1,T}
 """,
-    thermo = AdsorptionCorrection(
+    thermo=ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
-        Delta_Cpdata = ([-2.67,-1.82,-1.11,-0.48,0.49,1.16, 2.05],'cal/(mol*K)'),
-        Delta_H298 = (-135.97,'kcal/mol'),
-        Delta_S298 = (-39.38,'cal/(mol*K)'),
+        Cpdata=([-2.67, -1.82, -1.11, -0.48, 0.49, 1.16, 2.05], 'cal/(mol*K)'),
+        H298=(-135.97, 'kcal/mol'),
+        S298=(-39.38, 'cal/(mol*K)'),
         ),
     shortDesc = u"""C-R triple-bonded on nickel""",
     longDesc =  u"""Estimated via CFG-TiC
@@ -93,11 +93,11 @@ entry(
 3 R u0 p0 {1,S}
 4 X u0 p0 {1,D}
 """,
-    thermo = AdsorptionCorrection(
+    thermo=ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
-        Delta_Cpdata = ([-2.14,-0.93,0.01,0.74,1.76,2.39, 3.18],'cal/(mol*K)'),
-        Delta_H298 = (-76.05,'kcal/mol'),
-        Delta_S298 = (-41.84,'cal/(mol*K)'),
+        Cpdata=([-2.14, -0.93, 0.01, 0.74, 1.76, 2.39, 3.18], 'cal/(mol*K)'),
+        H298=(-76.05, 'kcal/mol'),
+        S298=(-41.84, 'cal/(mol*K)'),
         ),
     shortDesc = u"""CR2 doubled-bonded on nickel""",
     longDesc =  u"""Estimated via CFG-TiC
@@ -118,11 +118,11 @@ entry(
 4 R u0 p0 {1,S}
 5 X u0 p0 {1,S}
 """,
-    thermo = AdsorptionCorrection(
+    thermo=ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
-        Delta_Cpdata = ([0.97,2.02,2.63,3.00,3.40,3.60, 3.80],'cal/(mol*K)'),
-        Delta_H298 = (-29.96,'kcal/mol'),
-        Delta_S298 = (-36.94,'cal/(mol*K)'),
+        Cpdata=([0.97, 2.02, 2.63, 3.00, 3.40, 3.60, 3.80], 'cal/(mol*K)'),
+        H298=(-29.96, 'kcal/mol'),
+        S298=(-36.94, 'cal/(mol*K)'),
     ),
     shortDesc = u"""CR3 single-bonded on nickle""",
     longDesc = u"""Estimated via CFG-TiC
@@ -146,11 +146,11 @@ entry(
 5 R u0 p0 {1,S}
 6 X u0 p0 {1,vdW}
 """,
-    thermo = AdsorptionCorrection(
+    thermo=ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
-        Delta_Cpdata = ([-1.17,-1.10,-1.06,-1.04,-1.02,-1.01, -1.00],'cal/(mol*K)'),
-        Delta_H298 = (-0.46,'kcal/mol'),
-        Delta_S298 = (-19.25,'cal/(mol*K)'),
+        Cpdata=([-1.17, -1.10, -1.06, -1.04, -1.02, -1.01, -1.00], 'cal/(mol*K)'),
+        H298=(-0.46, 'kcal/mol'),
+        S298=(-19.25, 'cal/(mol*K)'),
     ),
     shortDesc = u"""CR4 physisorbed on nickel""",
     longDesc =  u"""Estimated via CFG-TiC
@@ -170,11 +170,11 @@ entry(
 3 R u0 p0 {1,S}
 4 X u0 p0 {1,vdW}
 """,
-    thermo = AdsorptionCorrection(
+    thermo=ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
-        Delta_Cpdata = ([-0.99,-0.99,-0.99,-0.99,-0.99,-0.99, -0.99],'cal/(mol*K)'),
-        Delta_H298 = (-0.46,'kcal/mol'),
-        Delta_S298 = (-14.96,'cal/(mol*K)'),
+        Cpdata=([-0.99, -0.99, -0.99, -0.99, -0.99, -0.99, -0.99], 'cal/(mol*K)'),
+        H298=(-0.46, 'kcal/mol'),
+        S298=(-14.96, 'cal/(mol*K)'),
     ),
     shortDesc = u"""R2O physisorbed on nickel""",
     longDesc =  u"""Estimated via CFG-TiC
@@ -194,11 +194,11 @@ entry(
 3 R u0 p0 {1,S}
 4 X u0 p0 {1,D} {2,S}
 """,
-    thermo = AdsorptionCorrection(
+    thermo=ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
-        Delta_Cpdata = ([-3.85,-2.46,-1.42,-0.65,0.42,1.14, 2.25],'cal/(mol*K)'),
-        Delta_H298 = (-41.48,'kcal/mol'),
-        Delta_S298 = (-51.44,'cal/(mol*K)'),
+        Cpdata=([-3.85, -2.46, -1.42, -0.65, 0.42, 1.14, 2.25], 'cal/(mol*K)'),
+        H298=(-41.48, 'kcal/mol'),
+        S298=(-51.44, 'cal/(mol*K)'),
     ),
     shortDesc = u"""RCR di-sigma adsorbed on nickel""",
     longDesc =  u"""Estimated via CFG-TiC
@@ -218,11 +218,11 @@ entry(
 3 R u0 p0 {2,S}
 4 X u0 p0 {1,T}
 """,
-    thermo = AdsorptionCorrection(
+    thermo=ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
-        Delta_Cpdata = ([1.24,2.09,2.62,2.96,3.36,3.56, 3.78],'cal/(mol*K)'),
-        Delta_H298 = (-48.39,'kcal/mol'),
-        Delta_S298 = (-43.58,'cal/(mol*K)'),
+        Cpdata=([1.24, 2.09, 2.62, 2.96, 3.36, 3.56, 3.78], 'cal/(mol*K)'),
+        H298=(-48.39, 'kcal/mol'),
+        S298=(-43.58, 'cal/(mol*K)'),
     ),
     shortDesc = u"""COR adsorbed on nickel: SUBSET OF CR*!""",
     longDesc =  u"""Estimated via CFG-TiC
@@ -245,11 +245,11 @@ entry(
 5 X u0 p0 {1,S}
 6 X u0 p0 {2,S}
 """,
-    thermo = AdsorptionCorrection(
+    thermo=ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
-        Delta_Cpdata = ([2.27,2.92,3.26,3.47,3.68,3.78, 3.88],'cal/(mol*K)'),
-        Delta_H298 = (-4.61,'kcal/mol'),
-        Delta_S298 = (-39.81,'cal/(mol*K)'),
+        Cpdata=([2.27, 2.92, 3.26, 3.47, 3.68, 3.78, 3.88], 'cal/(mol*K)'),
+        H298=(-4.61, 'kcal/mol'),
+        S298=(-39.81, 'cal/(mol*K)'),
     ),
     shortDesc = u"""CR2-R di-sigma adsorbed on nickel""",
     longDesc =  u"""Estimated via CFG-TiC
@@ -270,11 +270,11 @@ entry(
 4 R u0 p0 {2,S}
 5 X u0 p0 {1,D}
 """,
-    thermo = AdsorptionCorrection(
+    thermo=ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
-        Delta_Cpdata = ([1.68,2.50,2.96,3.24,3.54,3.69, 3.84],'cal/(mol*K)'),
-        Delta_H298 = (-55.31,'kcal/mol'),
-        Delta_S298 = (-45.59,'cal/(mol*K)'),
+        Cpdata=([1.68, 2.50, 2.96, 3.24, 3.54, 3.69, 3.84], 'cal/(mol*K)'),
+        H298=(-55.31, 'kcal/mol'),
+        S298=(-45.59, 'cal/(mol*K)'),
     ),
     shortDesc = u"""CROR double-bonded on nickel: SUBSET OF CR2*""",
     longDesc =  u"""Estimated via CFG-TiC
@@ -297,11 +297,11 @@ entry(
 5 R u0 p0 {2,S}
 6 X u0 p0 {1,S}
 """,
-    thermo = AdsorptionCorrection(
+    thermo=ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
-        Delta_Cpdata = ([2.76,3.26,3.50,3.64,3.78,3.85, 3.92],'cal/(mol*K)'),
-        Delta_H298 = (-43.79,'kcal/mol'),
-        Delta_S298 = (-40.59,'cal/(mol*K)'),
+        Cpdata=([2.76, 3.26, 3.50, 3.64, 3.78, 3.85, 3.92], 'cal/(mol*K)'),
+        H298=(-43.79, 'kcal/mol'),
+        S298=(-40.59, 'cal/(mol*K)'),
     ),
     shortDesc = u"""CR3O adsorbed on nickel""",
     longDesc =  u"""Estimated via CFG-TiC
@@ -326,11 +326,11 @@ entry(
 5 R u0 p0 {4,S}
 6 X u0 p0 {1,S}
 """,
-    thermo = AdsorptionCorrection(
+    thermo=ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
-        Delta_Cpdata = ([3.07,3.45,3.63,3.73,3.83,3.88, 3.93],'cal/(mol*K)'),
-        Delta_H298 = (-23.05,'kcal/mol'),
-        Delta_S298 = (-40.81,'cal/(mol*K)'),
+        Cpdata=([3.07, 3.45, 3.63, 3.73, 3.83, 3.88, 3.93], 'cal/(mol*K)'),
+        H298=(-23.05, 'kcal/mol'),
+        S298=(-40.81, 'cal/(mol*K)'),
     ),
     shortDesc = u"""CR2OR adsorbed on nickel: SUBSET OF CR3""",
     longDesc =  u"""Estimated via CFG-TiC
@@ -355,11 +355,11 @@ entry(
 6 R u0 p0 {2,S}
 7 X u0 p0 {1,vdW}
 """,
-    thermo = AdsorptionCorrection(
+    thermo=ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
-        Delta_Cpdata = ([0.98,0.99,0.99,0.99,0.99,0.99, 0.99],'cal/(mol*K)'),
-        Delta_H298 = (-0.69,'kcal/mol'),
-        Delta_S298 = (-17.57,'cal/(mol*K)'),
+        Cpdata=([0.98, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99], 'cal/(mol*K)'),
+        H298=(-0.69, 'kcal/mol'),
+        S298=(-17.57, 'cal/(mol*K)'),
     ),
     shortDesc = u"""CR3OR physisorbed on nickel: SUBSET OF R2O""",
     longDesc =  u"""Estimated via CFG-TiC
@@ -383,11 +383,11 @@ entry(
 5 X u0 p0 {1,D}
 6 X u0 p0 {2,S}
 """,
-    thermo = AdsorptionCorrection(
+    thermo=ThermoData(
         Tdata = ([300,400,500,600,800,1000,1500],'K'),
-        Delta_Cpdata = ([-4.42,-2.87,-1.69,-0.80,0.45,1.28, 2.47],'cal/(mol*K)'),
-        Delta_H298 = (-55.31,'kcal/mol'),
-        Delta_S298 = (-54.21,'cal/(mol*K)'),
+        Cpdata=([-4.42, -2.87, -1.69, -0.80, 0.45, 1.28, 2.47], 'cal/(mol*K)'),
+        H298=(-55.31, 'kcal/mol'),
+        S298=(-54.21, 'cal/(mol*K)'),
     ),
     shortDesc = u"""HOCO adsorbed on nickel: SUBSET OF RCR*""",
     longDesc =  u"""Estimated via CFG-TiC
