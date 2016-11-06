3ff426e636f29153e7e1f8c41cd23ebb9877bb4c
alongd <alongd@mit.edu>
2016-11-06 16:21:34 -0500

    Added dummy thermo for the new `N1sc` atomType

--- input/thermo/groups/group.py
+++ input/thermo/groups/group.py
@@ -41993,6 +41993,22 @@ u"""
 
 entry(
     index = 10001,
+    label = "N1sc",
+    group = 
+"""
+1 * N1sc u0 p3 {2,S}
+2   R    ux px {1,S}
+""",
+    thermo = u'N1s',
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+Nitrogen with two lone pairs and a single bond
+""",
+)
+
+entry(
+    index = 10001,
     label = "N1s",
     group = 
 """
