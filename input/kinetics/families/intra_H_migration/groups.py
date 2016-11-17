ba1ec2567a78728eef9c765afa39cf5e0f518404
zjburas <zjburas@mit.edu>
2016-11-17 18:30:37 -0500

    Modified intra_H_migration kinetics group for inew <=> i4 reaction on phenyl+propene surface

--- input/kinetics/families/intra_H_migration/groups.py
+++ input/kinetics/families/intra_H_migration/groups.py
@@ -5413,6 +5413,7 @@ Forbid a radical 2 atoms away from a phenyl side group from abstracting an H fro
 because the TS would be far too strained. Resonance form 3.
 """,
 )
+
 ##########
 
 ##########
