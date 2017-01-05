2be89b1ffc41f409e2ea2cfd9f47fbef921ba6e1
zjburas <zjburas@mit.edu>
2017-01-05 16:31:50 -0500

    Removed singlet  carbenes from globally forbidden structures because propargyl recombination involves singlet  carbene intermediates

--- input/forbiddenStructures.py
+++ input/forbiddenStructures.py
@@ -156,59 +156,59 @@ u"""
 
 entry(
     label = "Carbene_D_triplet",
-    group = 
+    group =
 """
 1 C u2 p0 {2,D}
 2 C u0 {1,D}
 """,
     shortDesc = u"""""",
-    longDesc = 
+    longDesc =
 u"""
 restricts C2O, see RMG-Py issue #514
 """,
 )
 
-entry(
-    label = "Carbene_D_singlet",
-    group = 
-"""
-1 C u0 p1 {2,D}
-2 C u0 {1,D}
-""",
-    shortDesc = u"""""",
-    longDesc = 
-u"""
-restricts H2CC, see RMG-Py issue #514
-""",
-)
+# entry(
+#     label = "Carbene_D_singlet",
+#     group =
+# """
+# 1 C u0 p1 {2,D}
+# 2 C u0 {1,D}
+# """,
+#     shortDesc = u"""""",
+#     longDesc =
+# u"""
+#
+# """,
+# )
 
 entry(
     label = "Carbene_S_triplet",
-    group = 
+    group =
 """
 1 C   u2 p0 {2,S}
 2 R!H u0 {1,S}
 """,
     shortDesc = u"""""",
-    longDesc = 
+    longDesc =
 u"""
 
 """,
 )
 
-entry(
-    label = "Carbene_S_singlet",
-    group = 
-"""
-1 C   u0 p1 {2,S}
-2 R!H u0 {1,S}
-""",
-    shortDesc = u"""""",
-    longDesc = 
-u"""
-restricts HCOH, see RMG-Py issue #514
-""",
-)
+# entry(
+#     label = "Carbene_S_singlet",
+#     group =
+# """
+# 1 C   u0 p1 {2,S}
+# 2 R!H u0 {1,S}
+# """,
+#     shortDesc = u"""""",
+#     longDesc =
+# u"""
+#
+# """,
+# )
 
 entry(
     label = "O3",
