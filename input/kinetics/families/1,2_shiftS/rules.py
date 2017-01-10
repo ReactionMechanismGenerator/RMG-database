2015cc5b477ed17d415d6bcf62ebe424e961c1d7
Nathan Yee <nyee@mit.edu>
2017-01-10 16:35:05 -0500

    Remove top level OR end group in 1,2_shiftS and replaced it with its only child
    This now conforms to the fact that end groups must be groups without removing any relevant groups.

--- input/kinetics/families/1,2_shiftS/rules.py
+++ input/kinetics/families/1,2_shiftS/rules.py
@@ -8,7 +8,7 @@ longDesc = u"""
 """
 entry(
     index = 1,
-    label = "XSYJ;YJ-Ss;X-Ss",
+    label = "XSYJ;YJ-Ss;C-Ss",
     kinetics = ArrheniusEP(
         A = (1e+08, 's^-1'),
         n = 2,
