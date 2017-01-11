9a4f4ef3cfa7a283358ebc66fb15a1ab04070aa1
zjburas <zjburas@mit.edu>
2017-01-11 11:21:29 -0500

    Created new reaction family: Intra_5_membered_conjugated_C=C_C=C_addition
    New family performas a concerted intramolecular addition of two alkene groups in a 5-membered conjugated backbone (C=C=C-C=C) to form a 5-membered conjugated cyclic singlet carbene

---
+++ input/kinetics/families/Intra_5_membered_conjugated_C=C_C=C_addition/rules.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "Intra_5_membered_conjugated_C=C_C=C_addition/rules"
+shortDesc = u"Concerted intramolecular addition of two alkene groups in a 5-membered conjugated backbone (C=C=C-C=C) to" \
+            u"form a 5-membered conjugated cyclic singlet carbene"
+longDesc = u"""
+From 2003 Miller and Klippenstein Propargyl recombination PES:
+
+Miller, J. A.; Klippenstein, S. J., The Recombination of Propargyl Radicals and Other Reactions on a C6H6 Potential.
+J. Phys. Chem. A 2003, 107, 7783-7799.
+"""
