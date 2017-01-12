7b9dfab71b5dbae4273d042055c02738f976b7d2
zjburas <zjburas@mit.edu>
2017-01-11 20:24:35 -0500

    Created new reaction family: Concerted_Intra_Diels_alder_monocyclic_1,2_shift
    New family performs intramolecular ring-closure by concerted addition of
    two alkene end groups in  a 1_3_hexadien_5_yne backbone (C=C-C=C-C#C)
    with simultaneous 1,2-H shift over unsaturated bond to form an
    unsaturated singlet carbene cyclohexane product. Taken from 2003 Miller
    Propargyl recombination surface

---
+++ input/kinetics/families/Concerted_Intra_Diels_alder_monocyclic_1,2_shiftH/rules.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "Concerted_Intra_Diels_alder_monocyclic_1,2_shiftH/groups"
+shortDesc = u"Intramolecular ring-closure by concerted addition of two alkene end groups in  a 1_3_hexadien_5_yne backbone " \
+            u"(C=C-C=C-C#C) with simultaneous 1,2-H shift over an unsaturated bond to form an unsaturated singlet carbene " \
+            u"cyclohexane product"
+longDesc = u"""
+From 2003 Miller and Klippenstein Propargyl recombination PES:
+
+Miller, J. A.; Klippenstein, S. J., The Recombination of Propargyl Radicals and Other Reactions on a C6H6 Potential.
+J. Phys. Chem. A 2003, 107, 7783-7799.
+"""
+
