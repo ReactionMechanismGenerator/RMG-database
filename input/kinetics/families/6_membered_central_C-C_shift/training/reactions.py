eedcf237ed7b7738ec63096f11a5e3f9e10e39b5
zjburas <zjburas@mit.edu>
2017-01-15 20:29:54 -0500

    Added training reactions for 8 new reaction families from TST calculations on 2003 Miller and Klippenstein Propargyl Recombination PES

--- input/kinetics/families/6_membered_central_C-C_shift/training/reactions.py
+++ input/kinetics/families/6_membered_central_C-C_shift/training/reactions.py
@@ -7,3 +7,17 @@ longDesc = u"""
 Put kinetic parameters for reactions to use as a training set for fitting
 group additivity values in this file.
 """
+
+entry(
+    index = 1,
+    label = "C6H6 <=> C6H6-2",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(1.873e+11, 's^-1'), n=0.278, Ea=(35.198, 'kcal/mol'), T0=(1, 'K')),
+    rank = 3,
+    shortDesc = u"""Training reaction from kinetics library: 2003_Miller_Propargyl_Recomb_High_P""",
+    longDesc = 
+u"""
+Taken from entry: I <=> II
+""",
+)
+
--- input/kinetics/families/Intra_2+2_cycloaddition_Cd/training/reactions.py
+++ input/kinetics/families/Intra_2+2_cycloaddition_Cd/training/reactions.py
@@ -7,3 +7,18 @@ longDesc = u"""
 Put kinetic parameters for reactions to use as a training set for fitting
 group additivity values in this file.
 """
+
+
+entry(
+    index = 1,
+    label = "C6H6 <=> C6H6-2",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(2.606e+11, 's^-1'), n=0.285, Ea=(29.433, 'kcal/mol'), T0=(1, 'K')),
+    rank = 3,
+    shortDesc = u"""Training reaction from kinetics library: 2003_Miller_Propargyl_Recomb_High_P""",
+    longDesc = 
+u"""
+Taken from entry: II <=> III
+""",
+)
+
