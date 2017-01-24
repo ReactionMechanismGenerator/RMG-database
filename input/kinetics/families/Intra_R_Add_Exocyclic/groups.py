f163b7327b4d21876e7b5a5455bba190b1b83e10
zjburas <zjburas@mit.edu>
2017-01-24 15:10:23 -0500

    Made more specific kinetics groups to match reactions in Fulvene_H libray

--- input/kinetics/families/Intra_R_Add_Exocyclic/groups.py
+++ input/kinetics/families/Intra_R_Add_Exocyclic/groups.py
@@ -2777,9 +2777,220 @@ entry(
 )
 
 entry(
+<<<<<<< HEAD
     index = 251,
     label = "R10",
     group = 
+=======
+    index = 243,
+    label = "R4_intra_6_member_ring",
+    group =
+"""
+1 *1 R!H              u1 {2,[S,D,T]} {6,[S,D,T]}
+2 *4 R!H              ux {1,[S,D,T]} {3,S}
+3 *2 [Cd,Ct,CO,N]     u0 {2,S} {4,[D,T]}
+4 *3 [Cd,Ct,Od,Sd,Cdd,N] u0 {3,[D,T]} {5,[S,D,T]}
+5    R!H              ux {4,[S,D,T]} {6,[S,D,T]}
+6    R!H              ux {5,[S,D,T]} {1,[S,D,T]}
+""",
+    kinetics = None,
+)
+
+tree(
+"""
+L1: Rn
+    L2: R4
+        L3: R4_intra_6_member_ring
+        L3: R4_S
+            L4: R4_S_D
+            L4: R4_S_T
+            L4: R4_S_CO
+        L3: R4_D
+            L4: R4_D_D
+            L4: R4_D_T
+            L4: R4_D_CO
+        L3: R4_T
+            L4: R4_T_D
+            L4: R4_T_T
+            L4: R4_T_CO
+    L2: R5
+        L3: R5_SS
+            L4: R5_SS_D
+            L4: R5_SS_T
+            L4: R5_SS_CO
+        L3: R5_SM
+            L4: R5_SD
+                L5: R5_SD_D
+                L5: R5_SD_T
+                L5: R5_SD_CO
+            L4: R5_ST
+                L5: R5_ST_D
+                L5: R5_ST_T
+                L5: R5_ST_CO
+        L3: R5_MS
+            L4: R5_DS
+                L5: R5_DS_D
+                L5: R5_DS_T
+                L5: R5_DS_CO
+            L4: R5_TS
+                L5: R5_TS_D
+                L5: R5_TS_T
+                L5: R5_TS_CO
+        L3: R5_MM
+    L2: R6
+        L3: R6_RSR
+            L4: R6_SSR
+                L5: R6_SSS
+                    L6: R6_SSS_D
+                    L6: R6_SSS_T
+                    L6: R6_SSS_CO
+                L5: R6_SSM
+                    L6: R6_SSM_D
+                    L6: R6_SSM_T
+                    L6: R6_SSM_CO
+            L4: R6_MSR
+                L5: R6_DSR
+                    L6: R6_DSS
+                        L7: R6_DSS_D
+                        L7: R6_DSS_T
+                        L7: R6_DSS_CO
+                    L6: R6_DSM
+                        L7: R6_DSM_D
+                        L7: R6_DSM_T
+                        L7: R6_DSM_CO
+                L5: R6_TSR
+                    L6: R6_TSS
+                        L7: R6_TSS_D
+                        L7: R6_TSS_T
+                        L7: R6_TSS_CO
+                    L6: R6_TSM
+                        L7: R6_TSM_D
+                        L7: R6_TSM_T
+                        L7: R6_TSM_CO
+        L3: R6_SMS
+            L4: R6_SMS_D
+            L4: R6_SMS_T
+            L4: R6_SMS_CO
+        L3: R6_SMM
+    L2: R7plus
+        L3: R7
+            L4: R7_RSSR
+                L5: R7_SSSR
+                    L6: R7_SSSS
+                        L7: R7_SSSS_D
+                        L7: R7_SSSS_T
+                        L7: R7_SSSS_CO
+                    L6: R7_SSSM
+                        L7: R7_SSSM_D
+                        L7: R7_SSSM_T
+                        L7: R7_SSSM_CO
+                L5: R7_MSSR
+                    L6: R7_DSSR
+                        L7: R7_DSSS
+                            L8: R7_DSSS_D
+                            L8: R7_DSSS_T
+                            L8: R7_DSSS_CO
+                        L7: R7_DSSM
+                            L8: R7_DSSM_D
+                            L8: R7_DSSM_T
+                            L8: R7_DSSM_CO
+                    L6: R7_TSSR
+                        L7: R7_TSSS
+                            L8: R7_TSSS_D
+                            L8: R7_TSSS_T
+                            L8: R7_TSSS_CO
+                        L7: R7_TSSM
+                            L8: R7_TSSM_D
+                            L8: R7_TSSM_T
+                            L8: R7_TSSM_CO
+            L4: R7_RSMS
+                L5: R7_SSMS
+                    L6: R7_SSMS_D
+                    L6: R7_SSMS_T
+                    L6: R7_SSMS_CO
+                L5: R7_MSMS
+                    L6: R7_DSMS
+                        L7: R7_DSMS_D
+                        L7: R7_DSMS_T
+                        L7: R7_DSMS_CO
+                    L6: R7_TSMS
+                        L7: R7_TSMS_D
+                        L7: R7_TSMS_T
+                        L7: R7_TSMS_CO
+            L4: R7_SMSR
+                L5: R7_SMSS
+                    L6: R7_SMSS_D
+                    L6: R7_SMSS_T
+                    L6: R7_SMSS_CO
+                L5: R7_SMSM
+                    L6: R7_SMSM_D
+                    L6: R7_SMSM_T
+                    L6: R7_SMSM_CO
+            L4: R7_MMSR
+            L4: R7_RSMM
+            L4: R7_SMMS
+        L3: R8
+        L3: R9
+L1: multiplebond_intra
+    L2: doublebond_intra
+        L3: doublebond_intra_2H
+            L4: doublebond_intra_2H_pri
+            L4: doublebond_intra_2H_secNd
+            L4: doublebond_intra_2H_secDe
+        L3: doublebond_intra_HNd
+            L4: doublebond_intra_HNd_pri
+            L4: doublebond_intra_HNd_secNd
+            L4: doublebond_intra_HNd_secDe
+        L3: doublebond_intra_HDe
+            L4: doublebond_intra_HDe_pri
+                L5: doublebond_intra_HCd_pri
+                L5: doublebond_intra_HCt_pri
+            L4: doublebond_intra_HDe_secNd
+            L4: doublebond_intra_HDe_secDe
+        L3: doublebond_intra_NdNd
+            L4: doublebond_intra_NdNd_pri
+            L4: doublebond_intra_NdNd_secNd
+            L4: doublebond_intra_NdNd_secDe
+        L3: doublebond_intra_NdDe
+            L4: doublebond_intra_NdDe_pri
+                L5: doublebond_intra_NdCd_pri
+                L5: doublebond_intra_NdCt_pri
+            L4: doublebond_intra_NdDe_secNd
+            L4: doublebond_intra_NdDe_secDe
+        L3: doublebond_intra_DeDe
+            L4: doublebond_intra_DeDe_pri
+            L4: doublebond_intra_DeDe_secNd
+            L4: doublebond_intra_DeDe_secDe
+    L2: triplebond_intra
+        L3: triplebond_intra_H
+        L3: triplebond_intra_Nd
+        L3: triplebond_intra_De
+    L2: carbonylbond_intra
+        L3: carbonylbond_intra_H
+        L3: carbonylbond_intra_Nd
+        L3: carbonylbond_intra_De
+L1: radadd_intra
+    L2: radadd_intra_cs
+        L3: radadd_intra_cs2H
+        L3: radadd_intra_csHNd
+        L3: radadd_intra_csHDe
+            L4: radadd_intra_csHCd
+            L4: radadd_intra_csHCt
+        L3: radadd_intra_csNdNd
+        L3: radadd_intra_csNdDe
+            L4: radadd_intra_csNdCd
+            L4: radadd_intra_csNdCt
+        L3: radadd_intra_csDeDe
+    L2: radadd_intra_O
+    L2: radadd_intra_Cb
+    L2: radadd_intra_cdsingle
+        L3: radadd_intra_cdsingleH
+        L3: radadd_intra_cdsingleNd
+        L3: radadd_intra_cdsingleDe
+    L2: radadd_intra_cddouble
+    L2: radadd_intra_CO
+    L2: radadd_intra_Ct
+>>>>>>> 5a55549... Added missing training reactions from Fulvene_H library
 """
 1 *1 R!H           u1 {2,[S,D,T,B]}
 2 *4 R!H           ux {1,[S,D,T,B]} {3,[S,D,T,B]}
