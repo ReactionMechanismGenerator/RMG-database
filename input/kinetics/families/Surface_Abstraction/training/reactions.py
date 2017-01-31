296fbe6a2eb4e37b11406d9b677bb2963082c91a
Franklin Goldsmith <franklin_goldsmith@brown.edu>
2017-01-31 10:55:45 -0500

    Three reactions added to training for Surface_Abstraction.
    R24: CH2+OH=CH3+O
    R26: CH+OH=CH2+O
    R28: C+OH=CH+O
    
    R24 and R26 are exothermic. R28 is endothermic. I tried to enter R27 (exothermic direction for R28), but it threw up an error.

--- input/kinetics/families/Surface_Abstraction/training/reactions.py
+++ input/kinetics/families/Surface_Abstraction/training/reactions.py
@@ -9,9 +9,9 @@ group additivity values in this file.
 """
 
 entry(
-    index = 1,
+    index = 24,
     label = "CH2X_1 + HOX_3 <=> CH3X_4 + OX_5",
-    degeneracy = 3,
+    degeneracy = 1,
     kinetics = SurfaceArrhenius(
         A=(1.39E17, 'm^2/(mol*s)'),
         n = 0.101,
@@ -27,3 +27,59 @@ Delgado et al
 Catalysts, 2015, 5, 871-904. Reaction R24
 """
 )
+
+entry(
+    index = 26,
+    label = "CHX_1 + HOX_3 <=> CH2X_4 + OX_5",
+    kinetics = SurfaceArrhenius(
+        A=(4.40E18, 'm^2/(mol*s)'),
+        n = 0.101,
+        Ea=(42400.0, 'J/mol'),
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
+    ),
+    shortDesc = u"""Default""",
+    longDesc = u"""
+"Surface Reaction Kinetics of Steam- and CO2-Reforming as well as Oxidation of Methane over Nickel-Based Catalysts"
+Delgado et al
+Catalysts, 2015, 5, 871-904. Reaction R26
+"""
+)
+
+entry(
+    index = 28,
+    label = "CX_1 + HOX_3 <=> CHX_4 + OX_5",
+    kinetics = SurfaceArrhenius(
+        A=(2.43E17, 'm^2/(mol*s)'),
+        n = -0.312,
+        Ea=(118900.0, 'J/mol'),
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
+    ),
+    shortDesc = u"""Default""",
+    longDesc = u"""
+"Surface Reaction Kinetics of Steam- and CO2-Reforming as well as Oxidation of Methane over Nickel-Based Catalysts"
+Delgado et al
+Catalysts, 2015, 5, 871-904. Reaction R28
+"""
+)
+
+#For some reason, it wasn't happen with the reaction in this direction, so I entered the reverse direction, R28, even though that is the endothermic direction.
+#entry(
+#    index = 27,
+#    label = "OX_1 + CHX_3 <=> HOX_4 + CX_5 ",
+#    kinetics = SurfaceArrhenius(
+#        A=(2.47E17, 'm^2/(mol*s)'),
+#        n = 0.312,
+#        Ea=(57700.0, 'J/mol'),
+#        Tmin = (200, 'K'),
+#        Tmax = (3000, 'K'),
+#    ),
+#    shortDesc = u"""Default""",
+#    longDesc = u"""
+#"Surface Reaction Kinetics of Steam- and CO2-Reforming as well as Oxidation of Methane over Nickel-Based Catalysts"
+#Delgado et al
+#Catalysts, 2015, 5, 871-904. Reaction R27
+#"""
+#)
+
