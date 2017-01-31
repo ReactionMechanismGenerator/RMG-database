5f5121bb79626ee0e3991e65ea4ffad50d01c032
Franklin Goldsmith <franklin_goldsmith@brown.edu>
2017-01-31 14:02:48 -0500

    Moved several reactions to training data.
    Slightly less than half of the reactions have been commented out because they were moved to the training data for Surface_Dissociation and Surface_Abstraction.
    More detailed comments were added.

--- input/kinetics/libraries/Deutschmann_Ni/reactions.py
+++ input/kinetics/libraries/Deutschmann_Ni/reactions.py
@@ -28,7 +28,7 @@ entry(
 
 #skip R2 - reverse included
 
-#CFG: I think we need to treat O2 separately
+#CFG: O2 is a special case: we need to treat it separately
 entry(
     index = 3,
     label = "O2 + Ni + Ni <=> OX + OX",
@@ -49,7 +49,7 @@ entry(
 #skip R7 vdW
 #skip R8 vdW
 
-#CFG: I think we need to treat CO separately
+#CFG: CO is a special case: we need to treat it separately
 entry(
     index = 9,
     label = "CO + Ni <=> OCX",
@@ -70,6 +70,7 @@ entry(
 #skip R13
 
 #CFG: Modified version of R14: reverse of dissociative adsorption
+#since vdW is not yet functioning, we include this reaction in library
 entry(
     index = 14,
     label = "CH3X + HX <=> CH4 + Ni + Ni",
@@ -85,37 +86,38 @@ entry(
 )
 
 #skip R15
-
-entry(
-    index = 16,
-    label = "CH2X + HX <=> CH3X + Ni",
-    kinetics = SurfaceArrhenius(
-        A=(3.09E19, 'm^2/(mol*s)'),
-        n = -0.087,
-        Ea=(57200.0, 'J/mol'),
-        Tmin = (200, 'K'),
-        Tmax = (3000, 'K'),
-    ),
-    shortDesc = u"""Default""",
-    longDesc = u"""R16"""
-)
+# moved to Surface_Dissociation/training
+#entry(
+#    index = 16,
+#    label = "CH2X + HX <=> CH3X + Ni",
+#    kinetics = SurfaceArrhenius(
+#        A=(3.09E19, 'm^2/(mol*s)'),
+#        n = -0.087,
+#        Ea=(57200.0, 'J/mol'),
+#        Tmin = (200, 'K'),
+#        Tmax = (3000, 'K'),
+#    ),
+#    shortDesc = u"""Default""",
+#    longDesc = u"""R16"""
+#)
 
 #skip R17
+# moved to Surface_Dissociation/training
+#entry(
+#    index = 18,
+#    label = "CHX + HX <=> CH2X + Ni",
+#    kinetics = SurfaceArrhenius(
+#        A=(9.77E20, 'm^2/(mol*s)'),
+#        n = -0.087,
+#        Ea=(81000.0, 'J/mol'),
+#        Tmin = (200, 'K'),
+#        Tmax = (3000, 'K'),
+#    ),
+#    shortDesc = u"""Default""",
+#    longDesc = u"""R18"""
+#)
 
-entry(
-    index = 18,
-    label = "CHX + HX <=> CH2X + Ni",
-    kinetics = SurfaceArrhenius(
-        A=(9.77E20, 'm^2/(mol*s)'),
-        n = -0.087,
-        Ea=(81000.0, 'J/mol'),
-        Tmin = (200, 'K'),
-        Tmax = (3000, 'K'),
-    ),
-    shortDesc = u"""Default""",
-    longDesc = u"""R18"""
-)
-
+# doesn't work right now in Surface_Dissociation/training -- although it should.
 entry(
     index = 19,
     label = "CHX + Ni <=> CX + HX",
@@ -134,6 +136,7 @@ entry(
 #skip R21
 
 #CFG: Modified version of R22: CH4* now goes directly to CH4(g)
+#since vdW is not yet functioning, we include this reaction in library
 entry(
     index = 22,
     label = "CH3X + HOX <=> CH4 + OX + Ni",
@@ -198,7 +201,7 @@ entry(
 #skip R28
 #skip R29 vdW
 
-
+#since vdW is not yet functioning, we include this reaction in library
 entry(
     index = 30,
     label = "HX + HOX <=> H2O + Ni + Ni",
@@ -214,21 +217,22 @@ entry(
 )
 
 #skip R31
+#moved to Surface_Dissociation/training
+#entry(
+#    index = 32,
+#    label = "HOX + Ni <=> HX + OX",
+#    kinetics = SurfaceArrhenius(
+#        A=(2.25E16, 'm^2/(mol*s)'),
+#        n = 0.188,
+#        Ea=(29600.0, 'J/mol'),
+#        Tmin = (200, 'K'),
+#        Tmax = (3000, 'K'),
+#    ),
+#    shortDesc = u"""Default""",
+#    longDesc = u"""R32"""
+#)
 
-entry(
-    index = 32,
-    label = "HOX + Ni <=> HX + OX",
-    kinetics = SurfaceArrhenius(
-        A=(2.25E16, 'm^2/(mol*s)'),
-        n = 0.188,
-        Ea=(29600.0, 'J/mol'),
-        Tmin = (200, 'K'),
-        Tmax = (3000, 'K'),
-    ),
-    shortDesc = u"""Default""",
-    longDesc = u"""R32"""
-)
-
+#since vdW is not yet functioning, we include this reaction in library
 entry(
     index = 33,
     label = "HOX + HOX <=> H2O + OX + Ni",
@@ -246,6 +250,8 @@ entry(
 #skip R34 vdW
 #skip R35
 
+#We don't yet have a template for breaking a double bond for surface dissociation.
+#when we do, this should move there.
 entry(
     index = 36,
     label = "OCX + Ni <=> CX + OX",
@@ -262,6 +268,7 @@ entry(
 
 #skip R37
 
+#not an elementary reaction according to our families
 entry(
     index = 38,
     label = "CX + HOX <=> HX + OCX",
@@ -276,6 +283,7 @@ entry(
     longDesc = u"""R38"""
 )
 
+#since vdW is not yet functioning, we include this reaction in library
 entry(
     index = 39,
     label = "OCX + OCX <=> CO2 + Ni + CX",
@@ -292,6 +300,7 @@ entry(
 
 #skip R40
 
+#since vdW is not yet functioning, we include this reaction in library
 entry(
     index = 41,
     label = "OCX + OX <=> CO2 + Ni + Ni",
@@ -309,23 +318,24 @@ entry(
 
 #skip R42
 #skip R43
-
-entry(
-    index = 44,
-    label = "HOCXO + Ni <=> OCX + HOX",
-    kinetics = SurfaceArrhenius(
-        A=(1.46E20, 'm^2/(mol*s)'),
-        n = -0.213,
-        Ea=(54300.0, 'J/mol'),
-        Tmin = (200, 'K'),
-        Tmax = (3000, 'K'),
-    ),
-    shortDesc = u"""Default""",
-    longDesc = u"""R44"""
-)
+# moved to Surface_Dissociation/training
+#entry(
+#    index = 44,
+#    label = "HOCXO + Ni <=> OCX + HOX",
+#    kinetics = SurfaceArrhenius(
+#        A=(1.46E20, 'm^2/(mol*s)'),
+#        n = -0.213,
+#        Ea=(54300.0, 'J/mol'),
+#        Tmin = (200, 'K'),
+#        Tmax = (3000, 'K'),
+#    ),
+#    shortDesc = u"""Default""",
+#    longDesc = u"""R44"""
+#)
 
 #skip R45
 
+#since vdW is not yet functioning, we include this reaction in library
 entry(
     index = 46,
     label = "HOCXO + Ni <=> CO2 + HX + Ni",
@@ -342,23 +352,25 @@ entry(
 
 
 #skip R47
-
-entry(
-    index = 48,
-    label = "CXHO + Ni <=> OCX + HX",
-    kinetics = SurfaceArrhenius(
-        A=(3.71E17, 'm^2/(mol*s)'),
-        n = 0.0,
-        Ea=(0.0, 'J/mol'),
-        Tmin = (200, 'K'),
-        Tmax = (3000, 'K'),
-    ),
-    shortDesc = u"""Default""",
-    longDesc = u"""R48"""
-)
+# moved to Surface_Dissociation/training
+#entry(
+#    index = 48,
+#    label = "CXHO + Ni <=> OCX + HX",
+#    kinetics = SurfaceArrhenius(
+#        A=(3.71E17, 'm^2/(mol*s)'),
+#        n = 0.0,
+#        Ea=(0.0, 'J/mol'),
+#        Tmin = (200, 'K'),
+#        Tmax = (3000, 'K'),
+#    ),
+#    shortDesc = u"""Default""",
+#    longDesc = u"""R48"""
+#)
 
 #skip R49
 
+#We don't yet have a template for breaking a double bond for surface dissociation.
+#when we do, this should move there.
 entry(
     index = 50,
     label = "CHX + OX <=> CXHO + Ni",
@@ -373,7 +385,8 @@ entry(
     longDesc = u"""R50"""
 )
 
-
+#similar to R38. curious O-insertion
+#not an elementary reaction according to our families
 entry(
     index = 52,
     label = "CXHO + HOX <=> HOCXO + HX",
