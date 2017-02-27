2617c3419838c174332f2ff89566813f8f499a74
zjburas <zjburas@mit.edu>
2017-02-26 19:57:50 -0500

    Added new kinetics libary for CH3+Indene PES in 2016 Mebel paper in Faraday Discussions.
    This small PES includes important steps in the methylation of indene
    that eventually leads to benzene.

---
+++ input/kinetics/libraries/2016_Mebel_Indene_CH3/reactions.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "2016_Mebel_Indene_CH3"
+shortDesc = u"Methyl Radical + Indene PES"
+longDesc = u"""
+TST calculated rates taken from:
+Mebel, A. M.; Georgievskii, Y.; Jasper, A. W.; Klippenstein, S. J., Pressure-dependent rate constants for PAH growth: 
+formation of indene and its conversion to naphthalene. Faraday Discuss. 2016, 195, 637-670.
+"""
+entry(
+    index = 1,
+    label = "3_methylindene + H <=> W1",
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (2.059e+08, 'cm^3/(mol*s)'),
+        n = 1.596,
+        Ea = (4.69, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+    longDesc = 
+u"""
+Originally from reaction library: Unclassified
+""",
+)
+
+entry(
+    index = 2,
+    label = "1_methylindene + H <=> W1",
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (5.904e+08, 'cm^3/(mol*s)'),
+        n = 1.534,
+        Ea = (3.418, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+    longDesc = 
+u"""
+Originally from reaction library: Unclassified
+""",
+)
+
+entry(
+    index = 3,
+    label = "Indene + CH3 <=> W1",
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (3206, 'cm^3/(mol*s)'),
+        n = 2.611,
+        Ea = (8.195, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+    longDesc = 
+u"""
+Originally from reaction library: Unclassified
+""",
+)
+
+entry(
+    index = 4,
+    label = "2_methylindene + H <=> W2",
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (1.441e+08, 'cm^3/(mol*s)'),
+        n = 1.514,
+        Ea = (2.403, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+    longDesc = 
+u"""
+Originally from reaction library: Unclassified
+""",
+)
+
+entry(
+    index = 5,
+    label = "Indene + CH3 <=> W2",
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (5064, 'cm^3/(mol*s)'),
+        n = 2.608,
+        Ea = (5.867, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+    longDesc = 
+u"""
+Originally from reaction library: Unclassified
+""",
+)
+
