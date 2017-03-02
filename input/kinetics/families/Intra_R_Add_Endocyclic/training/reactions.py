1692b7d22aff5d7f115ea009d192b1355005eca9
soumsrani <soumsrani@gmail.com>
2017-03-02 17:45:24 -0500

    Added training rxn in Intra_R_Add_Endocyc family
    Added a training rxn in Intra_R_Add_Endocyclic family for conversion of
    [CH]=Cc1ccccc1C=C to [CH]1CC=Cc2ccccc21. Rate for this reaction taken
    from, V. V. Kislov et al., J. Chem. Theory Comput., 2005, 1, 908-924

--- input/kinetics/families/Intra_R_Add_Endocyclic/training/reactions.py
+++ input/kinetics/families/Intra_R_Add_Endocyclic/training/reactions.py
@@ -1749,3 +1749,18 @@ u"""
 Taken from entry: W111 <=> W112
 """,
 )
+
+
+entry(
+    index = 86,
+    label = "C10H9-9 <=> C10H9-10",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(5.135e+12, 's^-1'), n=0.056, Ea=(2127, 'cal/mol'), T0=(1, 'K')),
+    rank = 3,
+    shortDesc = u"""Training reaction from kinetics library: C10H8_HACA2""",
+    longDesc = 
+u"""
+Taken from entry: A8 <=> A9
+""",
+)
+
