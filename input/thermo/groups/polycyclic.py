c7d35ef5b09fbb15a5850bc051de238308d06323
zjburas <zjburas@mit.edu>
2017-03-17 12:49:19 -0400

    Removed all  polycyclic thermo corrections that were manually estimated by the "bicyclic thermo estimation formula", since this formula has now been implemented in RMG-Py's overall thermo estimation algorithm.

--- input/thermo/groups/polycyclic.py
+++ input/thermo/groups/polycyclic.py
@@ -9116,9 +9116,6 @@ L1: PolycyclicRing
             L4: s2_3_6_diene_1_3
             L4: s2_3_6_diene_2_5
         L3: s2_3_6_ben
-        L3: s2_3_6_yne
-            L4: s2_3_6_yne_1
-            L4: s2_3_6_yne_2
     L2: s2_3_7
         L3: s2_3_7_ane
     L2: s2_3_8
@@ -9151,9 +9148,6 @@ L1: PolycyclicRing
             L4: s2_4_6_diene_2_6
             L4: s2_4_6_diene_5_7
         L3: s2_4_6_ben
-        L3: s2_4_6_yne
-            L4: s2_4_6_yne_1
-            L4: s2_4_6_yne_2
     L2: s2_5_5
         L3: s2_5_5_ane
         L3: s2_5_5_ene
@@ -9265,9 +9259,6 @@ L1: PolycyclicRing
             L4: s2_6_6_ben_ene_1
             L4: s2_6_6_ben_ene_2
         L3: s2_6_6_naphthalene
-        L3: s2_6_6_yne
-            L4: s2_6_6_yne_1
-            L4: s2_6_6_yne_2
     L2: s3_4_4
         L3: s3_4_4_ane
         L3: s3_4_4_ene
