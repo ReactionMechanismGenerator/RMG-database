485dae8b404591008e87bf30a07c1d28c004fd88
Alon Grinberg Dana <alongd@mit.edu>
2017-04-20 15:19:35 -0400

    Added a training reaction to intra_NO2_ONO_conversion family

--- input/kinetics/families/intra_NO2_ONO_conversion/training/reactions.py
+++ input/kinetics/families/intra_NO2_ONO_conversion/training/reactions.py
@@ -7,3 +7,20 @@ longDesc = u"""
 Put kinetic parameters for reactions to use as a training set for fitting
 group additivity values in this file.
 """
+
+entry(
+    index = 1,
+    label = "CH3NO2 <=> CH3ONO",
+    degeneracy = 1,
+    kinetics = ThirdBody(
+        arrheniusLow = Arrhenius(A=(7.05e+54, 'cm^3/(mol*s)'), n=-2.35, Ea=(62398, 'kcal/mol'), T0=(1, 'K'))),
+    rank = 2,
+    shortDesc = u"""""",
+    longDesc = 
+u"""
+R.S. Zhu, P. Raghunath, M.C. Lin, J. Phys. Chem. A, 2013, 117, 7308-7313, doi: 10.1021/jp401148q
+p. 7311
+calculations done at the UCCSD(T)/CBS//UB3LYP/6-311+G(3df,2p) level of theory
+""",
+)
+
