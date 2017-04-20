8e2126cb68af28b0a88ac0d6db516bcb83661fb0
Alon Grinberg Dana <alongd@mit.edu>
2017-04-20 14:43:44 -0400

    Added S_atom_triplet group to H_Abstraction family analogous to O_atom_triplet

--- input/kinetics/families/H_Abstraction/groups.py
+++ input/kinetics/families/H_Abstraction/groups.py
@@ -3422,6 +3422,16 @@ entry(
 """,
     kinetics = None,
 )
+ 
+entry(
+    index = 600,
+    label = "S_atom_triplet",
+    group = 
+"""
+1 *3 S u2
+""",
+    kinetics = None,
+)
 
 entry(
     index = 190,
@@ -6597,6 +6607,7 @@ L1: Y_rad_birad_trirad_quadrad
         L3: CH_doublet
     L2: Y_1centerbirad
         L3: O_atom_triplet
+        L3: S_atom_triplet
         L3: CH2_triplet
         L3: NH_triplet
     L2: Y_rad
