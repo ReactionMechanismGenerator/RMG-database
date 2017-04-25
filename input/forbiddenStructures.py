ab1b18ba0f32f32eecd10bfdbb187d2850507156
zjburas <zjburas@mit.edu>
2017-04-24 23:09:57 -0400

    Added another stained tricyclic to globally forbidden species list, because it causes RMG to crash.

--- input/forbiddenStructures.py
+++ input/forbiddenStructures.py
@@ -896,3 +896,26 @@ For certain unsaturated versions of this strained tricyclic, RMG's Clar optimiza
 to crash.
 """,
 )
+
+entry(
+    label = "strained_tricyclic_2",
+    group =
+"""
+1  R!H ux {2,[S,D,T,B]} {10,[S,D,T,B]}
+2  R!H ux {1,[S,D,T,B]} {3,[S,D,T,B]}
+3  R!H ux {2,[S,D,T,B]} {4,[S,D,T,B]} {8,[S,D,T,B]}
+4  R!H ux {3,[S,D,T,B]} {5,[S,D,T,B]}
+5  R!H ux {4,[S,D,T,B]} {6,[S,D,T,B]} {10,[S,D,T,B]}
+6  R!H ux {5,[S,D,T,B]} {7,[S,D,T,B]}
+7  R!H ux {6,[S,D,T,B]} {8,[S,D,T,B]}
+8  R!H ux {3,[S,D,T,B]} {7,[S,D,T,B]} {9,[S,D,T,B]}
+9  R!H ux {8,[S,D,T,B]} {10,[S,D,T,B]}
+10 R!H ux {1,[S,D,T,B]} {5,[S,D,T,B]} {9,[S,D,T,B]}
+""",
+    shortDesc = u"""""",
+    longDesc =
+u"""
+Certain unsaturated versions of this strained tricyclic cause RMG
+to crash.
+""",
+)
