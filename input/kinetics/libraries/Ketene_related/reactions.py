f7f54d512d086b06451ba45ca05569528ac6a22d
jimchu10 <jimchu@mit.edu>
2017-04-30 22:07:07 -0400

    Add more correction of ketene reaction 1. H+CH2CO<->CH3CO 2. C3H3(propargyl)+CH2CO<->C5H5O

--- input/kinetics/libraries/Ketene_related/reactions.py
+++ input/kinetics/libraries/Ketene_related/reactions.py
@@ -11,4 +11,22 @@ entry(
     label = "H + CH2CO <=> CH3 + CO",
     degeneracy = 1,
     kinetics = Arrhenius(A=(1.13e+13, 'cm^3/(mol*s)'), n=0, Ea=(3428, 'cal/mol'), T0=(1, 'K')),
+    longDesc = u"""Use the rate from GRI Mech 3.0""",
 )
+
+entry(
+    index = 2,
+    label = "CH2CO + H <=> CH3CO",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(2.3e+08, 'cm^3/(mol*s)'), n=1.61, Ea=(2627, 'cal/mol'), T0=(1, 'K')),
+    longDesc = u"""Use the rate from Glarborg library""",
+)
+
+entry(
+    index = 3,
+    label = "C3H3 + CH2CO <=> C5H5O",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(9.94e+03, 'cm^3/(mol*s)'), n=2.305, Ea=(5213, 'cal/mol'), T0=(1, 'K')),
+    longDesc = u"""Use the rate from the average value from master branch 04/30/2017""",
+)
+
