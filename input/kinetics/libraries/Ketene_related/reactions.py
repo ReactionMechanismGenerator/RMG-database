9f0ae20835681babeb65b830624b7b726334c344
jimchu10 <jimchu@mit.edu>
2017-05-03 15:44:03 -0400

    Add the rate of C3H3+CH2CO<->the second adduct from matser estimation to Ketene_library

--- input/kinetics/libraries/Ketene_related/reactions.py
+++ input/kinetics/libraries/Ketene_related/reactions.py
@@ -24,7 +24,7 @@ entry(
 
 entry(
     index = 3,
-    label = "C3H3 + CH2CO <=> C5H5O",
+    label = "C3H3 + CH2CO <=> C5H5O_1",
     degeneracy = 1,
     kinetics = Arrhenius(A=(9.94e+03, 'cm^3/(mol*s)'), n=2.305, Ea=(5213, 'cal/mol'), T0=(1, 'K')),
     longDesc = u"""Use the rate from the average value from master branch 04/30/2017""",
@@ -32,6 +32,14 @@ entry(
 
 entry(
     index = 4,
+    label = "C3H3 + CH2CO <=> C5H5O_2",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(2.08e+0, 'cm^3/(mol*s)'), n=3.05, Ea=(13100, 'cal/mol'), T0=(1, 'K')),
+    longDesc = u"""Use the rate from the average value from master branch 05/03/2017""",
+)
+
+entry(
+    index = 5,
     label = "CH3 + CH2CO <=> C3H5O_1",
     degeneracy = 1,
     kinetics = Arrhenius(A=(1.05855e+04, 'cm^3/(mol*s)'), n=2.41, Ea=(8055, 'cal/mol'), T0=(1, 'K')),
@@ -39,7 +47,7 @@ entry(
 )
 
 entry(
-    index = 5,
+    index = 6,
     label = "C3H5O_2 <=> CH3 + CH2CO",
     degeneracy = 1,
     kinetics = Arrhenius(A=(1e+14, 's^-1'), n=0, Ea=(31000, 'cal/mol'), T0=(1, 'K')),
