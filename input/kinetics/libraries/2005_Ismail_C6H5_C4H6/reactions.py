aeb50021a0b8db1bb3e2395cc2de5d2adadca9ee
zjburas <zjburas@mit.edu>
2017-05-09 18:42:24 -0400

    Removed parentheses from species names in 2005_Ismail_C6H5_C4H6 library, otherwise RMG will crash if this library is used as a seed mechanism.

--- input/kinetics/libraries/2005_Ismail_C6H5_C4H6/reactions.py
+++ input/kinetics/libraries/2005_Ismail_C6H5_C4H6/reactions.py
@@ -8,7 +8,7 @@ longDesc = u"""
 """
 entry(
     index = 1,
-    label = "phenyl + (1_3)butadiene <=> (4)phenyl_buten_3_yl",
+    label = "phenyl + 1_3_butadiene <=> 4_phenyl_buten_3_yl",
     degeneracy = 1,
     kinetics = Arrhenius(A=(813000, 'cm^3/(mol*s)'), n=2.56, Ea=(0, 'kJ/mol'), T0=(1, 'K')),
     longDesc =
@@ -19,7 +19,7 @@ Originally from reaction library: Unclassified
 
 entry(
     index = 2,
-    label = "phenyl + (1_3)butadiene <=> (3)phenyl_buten_4_yl",
+    label = "phenyl + 1_3_butadiene <=> 3_phenyl_buten_4_yl",
     degeneracy = 1,
     kinetics = Arrhenius(A=(47900, 'cm^3/(mol*s)'), n=2.65, Ea=(16.7, 'kJ/mol'), T0=(1, 'K')),
     longDesc =
@@ -30,7 +30,7 @@ Originally from reaction library: Unclassified
 
 entry(
     index = 3,
-    label = "phenyl + (1_3)butadiene <=> benzene + (1_3)butadien_2_yl",
+    label = "phenyl + 1_3_butadiene <=> benzene + 1_3_butadien_2_yl",
     degeneracy = 1,
     kinetics = Arrhenius(A=(8710, 'cm^3/(mol*s)'), n=3.12, Ea=(8.1, 'kJ/mol'), T0=(1, 'K')),
     longDesc =
@@ -41,7 +41,7 @@ Originally from reaction library: Unclassified
 
 entry(
     index = 4,
-    label = "phenyl + (1_3)butadiene <=> benzene + (1_3)butadien_1_yl",
+    label = "phenyl + 1_3_butadiene <=> benzene + 1_3_butadien_1_yl",
     degeneracy = 1,
     kinetics = Arrhenius(A=(31600, 'cm^3/(mol*s)'), n=3.11, Ea=(16.7, 'kJ/mol'), T0=(1, 'K')),
     longDesc =
@@ -52,7 +52,7 @@ Originally from reaction library: Unclassified
 
 entry(
     index = 5,
-    label = "(4)phenyl_buten_3_yl <=> trihydronaphthalene",
+    label = "4_phenyl_buten_3_yl <=> trihydronaphthalene",
     degeneracy = 1,
     kinetics = Arrhenius(A=(324000, 's^-1'), n=1.64, Ea=(110.61, 'kJ/mol'), T0=(1, 'K')),
     longDesc =
@@ -63,7 +63,7 @@ Originally from reaction library: Unclassified
 
 entry(
     index = 6,
-    label = "(4)phenyl_buten_3_yl <=> (1)phenyl_1_3_butadiene + H",
+    label = "4_phenyl_buten_3_yl <=> 1_phenyl_1_3_butadiene + H",
     degeneracy = 1,
     kinetics = Arrhenius(A=(6.61e+07, 's^-1'), n=2.11, Ea=(161.62, 'kJ/mol'), T0=(1, 'K')),
     longDesc =
@@ -74,7 +74,7 @@ Originally from reaction library: Unclassified
 
 entry(
     index = 7,
-    label = "trihydronaphthalene <=> (1_4)dihydro_naphthalene + H",
+    label = "trihydronaphthalene <=> 1_4_dihydro_naphthalene + H",
     degeneracy = 1,
     kinetics = Arrhenius(A=(1.38e+10, 's^-1'), n=1.25, Ea=(92.6, 'kJ/mol'), T0=(1, 'K')),
     longDesc =
