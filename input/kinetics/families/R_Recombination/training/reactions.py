c70d987e5157cf37eae148db3391a75fe4697922
alongd <alongd@mit.edu>
2017-05-10 20:49:24 -0400

    Added training reactions to R_Recombination from SOxNOx library
    Commented-out training reaction #8 which now has a better rate estimate
    (at combustion-relevant temperatures, the williams et al. study is for low T)
    Removed training reaction #9 which is incorrect: NO2-2 (O atom has the
    rad in this resonance structure) reacts with OH to form HOONO,
    not HNO3 as stated in this reaction; this is also not in agreement
    with the cited paper by Williams et al.

--- input/kinetics/families/R_Recombination/training/reactions.py
+++ input/kinetics/families/R_Recombination/training/reactions.py
@@ -179,68 +179,36 @@ Theoretical modeling of k0, k and Fc=0.38 exp(-T/4900K) led to consistency with 
 """,
 )
 
-entry(
-    index = 8,
-    label = "NO2 + OH <=> HNO3",
-    degeneracy = 1,
-    kinetics = Arrhenius(
-        A = (2.41e+07, 'm^3/(mol*s)'),
-        n = 0,
-        Ea = (0, 'kJ/mol'),
-        T0 = (1, 'K'),
-        Tmin = (200, 'K'),
-        Tmax = (400, 'K'),
-    ),
-    reference = Article(
-        authors = ["Williams, C.F.", "Pogrebnya, S.K.", "Clary, D.C."],
-        title = u'Quantum study on the branching ratio of the reaction NO2+OH',
-        journal = "J. Chem. Phys.",
-        volume = "126",
-        pages = """154321""",
-        year = "2007",
-        url = "http://kinetics.nist.gov/kinetics/Detail?id=2007WIL/POG154321:2",
-    ),
-    referenceType = "theory",
-    shortDesc = u"""Other theoretical""",
-    longDesc = 
-u"""
-Pressure dependence: Rate constant is high pressure limit
-
-Quantum dynamics calculations. Reaction potential energy suraface was studied using quantum chemistry.
-""",
-)
-
-entry(
-    index = 9,
-    label = "NO2-2 + OH <=> HNO3",
-    degeneracy = 1,
-    kinetics = Arrhenius(
-        A = (1.205e+06, 'm^3/(mol*s)'),
-        n = 0,
-        Ea = (0, 'kJ/mol'),
-        T0 = (1, 'K'),
-        Tmin = (200, 'K'),
-        Tmax = (400, 'K'),
-    ),
-    reference = Article(
-        authors = ["Williams, C.F.", "Pogrebnya, S.K.", "Clary, D.C."],
-        title = u'Quantum study on the branching ratio of the reaction NO2+OH',
-        journal = "J. Chem. Phys.",
-        volume = "126",
-        pages = """154321""",
-        year = "2007",
-        url = "http://kinetics.nist.gov/kinetics/Detail?id=2007WIL/POG154321:2",
-    ),
-    referenceType = "theory",
-    shortDesc = u"""Other theoretical""",
-    longDesc = 
-u"""
-Pressure dependence: Rate constant is high pressure limit
-Reference reaction: 2007WIL/POG154321:4
-Branching ration: 0.05
-Quantum dynamics calculations. Reaction potential energy suraface was studied using quantum chemistry.
-""",
-)
+#entry(
+#    index = 8,
+#    label = "NO2 + OH <=> HNO3",
+#    degeneracy = 1,
+#    kinetics = Arrhenius(
+#        A = (2.41e+07, 'm^3/(mol*s)'),
+#        n = 0,
+#        Ea = (0, 'kJ/mol'),
+#        T0 = (1, 'K'),
+#        Tmin = (200, 'K'),
+#        Tmax = (400, 'K'),
+#    ),
+#    reference = Article(
+#        authors = ["Williams, C.F.", "Pogrebnya, S.K.", "Clary, D.C."],
+#        title = u'Quantum study on the branching ratio of the reaction NO2+OH',
+#        journal = "J. Chem. Phys.",
+#        volume = "126",
+#        pages = """154321""",
+#        year = "2007",
+#        url = "http://kinetics.nist.gov/kinetics/Detail?id=2007WIL/POG154321:2",
+#    ),
+#    referenceType = "theory",
+#    shortDesc = u"""Other theoretical""",
+#    longDesc = 
+#u"""
+#Pressure dependence: Rate constant is high pressure limit
+#
+#Quantum dynamics calculations. Reaction potential energy suraface was studied using quantum chemistry.
+#""",
+#)
 
 entry(
     index = 10,
@@ -263,33 +231,26 @@ Taken from entry: ethyl + CPDyl <=> ethylCPD
 entry(
     index = 15,
     label = "CH3NO2 <=> CH3 + NO2",
-    kinetics = Arrhenius(A=(1.8e+16, 's^-1'), n=0, Ea=(58500, 'cal/mol'), T0=(1, 'K')),
-    rank = 1,
-    shortDesc = u"""Experimental, P. Glarborg, A.B. Bendtsen, J.A. Miller""",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(5.88e+24, 's^-1'), n=-2.35, Ea=(62398, 'cal/mol'), T0=(1, 'K'), Tmin = (500, 'K'), Tmax = (3000, 'K')),
+    rank = 2,
+    shortDesc = u"""Training reaction from kinetics library: SOxNOx""",
     longDesc = 
 u"""
+R.S. Zhu, P. Raghunath, M.C. Lin, J. Phys. Chem. A, 2013, 117, 7308-7313, doi: 10.1021/jp401148q
+p. 7311
+calculations done at the UCCSD(T)/CBS//UB3LYP/6-311+G(3df,2p) level of theory
+The high pressure limit rate is giving here. A 1 atm rate is also available from the same source.
+
+Also available (experimental) from:
 P. Glarborg, A.B. Bendtsen, J.A. Miller
 Nitromethane Dissociation: Implications for the CH3 + NO2 Reaction
 International Journal of Chemical Kinetics Volume 31, Issue 9, pages 591–602, 1999
 DOI: 10.1002/(SICI)1097-4601(1999)31:9<591::AID-KIN1>3.0.CO;2-E
+    kinetics = Arrhenius(A=(1.8e+16, 's^-1'), n=0, Ea=(58500, 'cal/mol'), T0=(1, 'K')),
 
 Also appears in the Nitrogen_Glarborg_Zhang_et_al library (index 671)
 and in the Nitrogen_Glarborg_Gimenez_et_al library (index 953)
-
-The high-pressure limit kinetics was taken. Troe coefficients are:
-    kinetics = Troe(
-        arrheniusHigh = Arrhenius(A=(1.8e+16, 's^-1'), n=0, Ea=(58500, 'cal/mol'), T0=(1, 'K')),
-        arrheniusLow = Arrhenius(
-            A = (1.259e+17, 'cm^3/(mol*s)'),
-            n = 0,
-            Ea = (42000, 'cal/mol'),
-            T0 = (1, 'K'),
-        ),
-        alpha = 0.183,
-        T3 = (1e-30, 'K'),
-        T1 = (1e+30, 'K'),
-        efficiencies = {},
-    ),
 """,
 )
 
@@ -338,6 +299,139 @@ S.J. Klippenstein, Y. Georgievskiia, L.B. Hardingb
 Predictive theory for the combination kinetics of two alkyl radicals
 Phys. Chem. Chem. Phys., 2006, 8, 1133-1147
 doi: 10.1039/B515914H
+""",
+)
+
+entry(
+    index = 30,
+    label = "N2H4 <=> NH2 + NH2",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(1.57e+21, 's^-1'), n=-1.04, Ea=(66565, 'cal/mol'), T0=(1, 'K'), Tmin = (700, 'K'), Tmax = (2000, 'K')),
+    rank = 2,
+    shortDesc = u"""Training reaction from kinetics library: SOxNOx""",
+    longDesc = 
+u"""
+P. Raghunath, N.T. Nghia, M.C. Lin, Advances in Quantum Chemistry, 2014, 69, 253-301, doi: 10.1016/B978-0-12-800345-9.00007-6
+p. 264
+calculations done at the RCCSD(T)/6-311þG(3df,2p)//B3LYP/6-311G(d,p) level of theoty
+Only High Pressure Limit rate was taken; low limit and 1 atm rate are also available from the same source
+""",
+)
+
+entry(
+    index = 31,
+    label = "N2H4 <=> N2H3 + H",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(1.57e+21, 's^-1'), n=-1.04, Ea=(66565, 'cal/mol'), T0=(1, 'K'), Tmin = (700, 'K'), Tmax = (2000, 'K')),
+    rank = 2,
+    shortDesc = u"""Training reaction from kinetics library: SOxNOx""",
+    longDesc = 
+u"""
+P. Raghunath, N.T. Nghia, M.C. Lin, Advances in Quantum Chemistry, 2014, 69, 253-301, doi: 10.1016/B978-0-12-800345-9.00007-6
+p. 264
+calculations done at the RCCSD(T)/6-311þG(3df,2p)//B3LYP/6-311G(d,p) level of theoty
+Only High Pressure Limit rate was taken; low limit and 1 atm rate are also available from the same source
+""",
+)
+
+entry(
+    index = 32,
+    label = "ONONO2 <=> NO + NO3-2",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(2.41e+21, 's^-1'), n=-1.76, Ea=(31535, 'cal/mol'), T0=(1, 'K'), Tmin = (700, 'K'), Tmax = (2000, 'K')),
+    rank = 2,
+    shortDesc = u"""Training reaction from kinetics library: SOxNOx""",
+    longDesc = 
+u"""
+P. Raghunath, N.T. Nghia, M.C. Lin, Advances in Quantum Chemistry, 2014, 69, 253-301, doi: 10.1016/B978-0-12-800345-9.00007-6
+p. 265
+calculations done at the RCCSD(T)/6-311þG(3df,2p)//B3LYP/6-311G(d,p) level of theoty
+Only High Pressure Limit rate was taken; low limit and 1 atm rate are also available from the same source
+""",
+)
+
+entry(
+    index = 33,
+    label = "NH2NHNO <=> N2H3 + NO",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(6.24e+15, 's^-1'), n=-0.15, Ea=(35611, 'cal/mol'), T0=(1, 'K'), Tmin = (250, 'K'), Tmax = (1500, 'K')),
+    rank = 2,
+    shortDesc = u"""Training reaction from kinetics library: SOxNOx""",
+    longDesc = 
+u"""
+P. Raghunath, N.T. Nghia, M.C. Lin, Advances in Quantum Chemistry, 2014, 69, 253-301, doi: 10.1016/B978-0-12-800345-9.00007-6
+p. 274
+calculations done at the CCSD(T)/6-311þG(3df,2p) level of theoty
+""",
+)
+
+entry(
+    index = 34,
+    label = "CH3ONO <=> CH3O + NO",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(1.58e+23, 's^-1'), n=-2.18, Ea=(41930, 'cal/mol'), T0=(1, 'K'), Tmin = (300, 'K'), Tmax = (3000, 'K')),
+    rank = 2,
+    shortDesc = u"""Training reaction from kinetics library: SOxNOx""",
+    longDesc = 
+u"""
+R.S. Zhu, P. Raghunath, M.C. Lin, J. Phys. Chem. A, 2013, 117, 7308-7313, doi: 10.1021/jp401148q
+p. 7311
+calculations done at the UCCSD(T)/CBS//UB3LYP/6-311+G(3df,2p) level of theory
+The high pressure limit rate is giving here. A 1 atm rate is akso available from the same source.
+""",
+)
+
+entry(
+    index = 35,
+    label = "CN + NCN <=> NCNCN",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(1.010e+09, 'cm^3/(mol*s)'), n=0, Ea=(-34691, 'cal/mol'), T0=(1, 'K'), Tmin = (2000, 'K'), Tmax = (4000, 'K')),
+    rank = 2,
+    shortDesc = u"""Training reaction from kinetics library: SOxNOx""",
+    longDesc = 
+u"""
+See Table 1 on p. 2397 in L.V Moskaleva, M.C. Lin, Proceedings of the Combustion Institute, 2000, 28(2), 2393-2401, doi: 10.1016/S0082-0784(00)80652-9
+Done at the G2M(RCC2)//B3LYP/6-311G(d,p) level of theory
+""",
+)
+
+entry(
+    index = 36,
+    label = "HSOO <=> SH + O2",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(4.41e+18, 's^-1'), n=-1.07, Ea=(7750, 'cal/mol'), T0=(1, 'K'), Tmin = (200, 'K'), Tmax = (2000, 'K')),
+    rank = 2,
+    shortDesc = u"""Training reaction from kinetics library: SOxNOx""",
+    longDesc = 
+u"""
+A. Goumri, J-D.R. Rocha, D. Laakso, C.E. Smith, P. Marshall, J. Phys. Chem. A, 1999, 103(51), 11328–11335 doi: 10.1021/jp9924070
+Table 7 on p. 11333
+calculations done at the QCISD(T)/6-311+G(3df,2p)//MP2=FULL/6-31G(d) level of theory
+
+Troe expression given, only k_inf taken here:
+kinetics = Troe(
+        arrheniusHigh = Arrhenius(A=(4.41e+18, 's^-1'), n=-1.07, Ea=(7750, 'cal/mol'), T0=(1, 'K'), Tmin = (200, 'K'), Tmax = (2000, 'K')),
+        arrheniusLow = Arrhenius(A=(1.56e+23, 'cm^3/(mol*s)'), n=-2.82, Ea=(-7450, 'cal/mol'), T0=(1, 'K'), Tmin = (200, 'K'), Tmax = (2000, 'K')),
+        alpha=1, T3=(1e-30, 'K'), T1=(1e+30, 'K'), efficiencies={}),
+""",
+)
+
+entry(
+    index = 37,
+    label = "OH + NO2-2 <=> HOONO",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(3.32e-06, 'cm^3/(mol*s)'), n=-0.24, Ea=(-200, 'cal/mol'), T0=(1, 'K'), Tmin = (200, 'K'), Tmax = (2000, 'K')),
+    rank = 3,
+    shortDesc = u"""Training reaction from kinetics library: SOxNOx""",
+    longDesc = 
+u"""
+R.S. Zhu, M.C. Lin, J. Chem. Phys., 2003, 119, 10667, doi: 10.1063/1.1619373
+
+Troe expression given, only k_inf taken here:
+kinetics = Troe(
+        arrheniusHigh = Arrhenius(A=(3.32e-06, 'cm^3/(mol*s)'), n=-0.24, Ea=(-200, 'cal/mol'), T0=(1, 'K'), Tmin = (200, 'K'), Tmax = (2000, 'K')),
+        arrheniusLow = Arrhenius(A=(1.14e+50, 'cm^6/(mol^2*s)'), n=-12.3, Ea=(5137, 'cal/mol'), T0=(1, 'K'), Tmin = (200, 'K'), Tmax = (2000, 'K')),
+        alpha=1, T3=(1e-30, 'K'), T1=(1e+30, 'K'), efficiencies={}),
 """,
 )
 
