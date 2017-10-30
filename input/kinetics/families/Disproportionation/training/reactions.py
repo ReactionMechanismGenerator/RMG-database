73df647bf19057b44c186edeb79ddec74986b9cd
alongd <alongd@mit.edu>
2017-10-30 12:17:48 -0400

    Degeneracy fixes in training reactions
    Disprop, H_Abs, R_Rec

--- input/kinetics/families/Disproportionation/training/reactions.py
+++ input/kinetics/families/Disproportionation/training/reactions.py
@@ -10,6 +10,7 @@ group additivity values in this file.
 entry(
     index = 1,
     label = "C2H + CH3O <=> C2H2 + CH2O",
+    degeneracy = 3,
     kinetics = Arrhenius(
         A = (3.61e+13, 'cm^3/(mol*s)', '*|/', 5),
         n = 0,
@@ -39,6 +40,7 @@ MRH 30-Aug-2009
 entry(
     index = 2,
     label = "C2H3 + O2 = C2H2_1 + HO2",
+    degeneracy = 4,
     kinetics = Arrhenius(
         A = (1.04E+16, 'cm^3/(mol*s)','*|/',5),
         n = -1.26,
