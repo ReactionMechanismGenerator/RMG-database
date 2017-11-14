27cdf874a1e81db46ba154ca08b98bf22cb48408
zjburas <zjburas@mit.edu>
2017-11-14 17:00:09 -0500

    Created new kinetic library, phenyl_diacetylene_effective, for effective isomerization of phenyl + diacetylene adduct to either benzofulvenyl or 2-naphthyl radical, taking into account the rate-limiting trans-cis isomerization of the adduct (CBS-QB3+TST calculated).

---
+++ input/kinetics/libraries/First_to_Second_Aromatic_Ring/phenyl_diacetylene_effective/reactions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "phenyl_diacetylene_effective"
+shortDesc = u"CBS-QB3"
+longDesc = u"""
+Effective rate for an adduct of phenyl radical + diacetylene to form either benzofulvenyl or 2-naphthyl radical.
+Rate-limiting step is trans-cis isomerization of the adduct, calculated using CBS-QB3+TST.
+"""
+entry(
+    index = 1,
+    label = "i2_trans <=> i3",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(2.992e+11, 's^-1'), n=0.670, Ea=(58.336, 'kcal/mol'), T0=(1, 'K')),
+    longDesc = 
+u"""
+Originally from reaction library: Unclassified
+""",
+)
+
+entry(
+    index = 2,
+    label = "i2_trans <=> i4",
+    degeneracy = 1,
+    kinetics=Arrhenius(A=(2.992e+11, 's^-1'), n=0.670, Ea=(58.336, 'kcal/mol'), T0=(1, 'K')),
+    longDesc = 
+u"""
+Originally from reaction library: Unclassified
+""",
+)
+
