9d5f2835fa1695fb5439306ade5606efde9cc717
zjburas <zjburas@mit.edu>
2017-11-27 13:47:05 -0500

    Forbid Benzofulvenyl radical from isomerizing to 1H_naphthalene in one step using Intra_R_Add_Exo_Scission family.
    No transition state was found.

--- input/kinetics/families/Intra_R_Add_Exo_scission/groups.py
+++ input/kinetics/families/Intra_R_Add_Exo_scission/groups.py
@@ -83,3 +83,95 @@ L1: rad
 L1: Cs-Cb
 """
 )
+
+forbidden(
+    label = "Benzofulvenyl_1",
+    group =
+"""
+1  *1 C u1 {4,S}
+4  *3 Cs u0 {1,S} {5,S} {12,S}
+5  Cd u0 {4,S} {6,D}
+6  Cd u0 {5,D} {7,S}
+7  Cb u0 {6,S} {8,B} {12,B}
+8  Cb u0 {7,B} {9,B}
+9  Cb u0 {8,B} {10,B}
+10 Cb u0 {9,B} {11,B}
+11 Cb u0 {10,B} {12,B}
+12 *2 Cb u0 {4,S} {7,B} {11,B}
+""",
+    shortDesc = u"""""",
+    longDesc =
+u"""
+Forbid a benzofulvenyl radical  from isomerizing to 1H_naphthalene in one step using this family. No transition state was
+found for such a reaction.
+""",
+)
+
+forbidden(
+    label = "Benzofulvenyl_2",
+    group =
+"""
+1  *1 C u1 {4,S}
+4  *3 Cs u0 {1,S} {5,S} {12,S}
+5  Cd u0 {4,S} {6,D}
+6  Cd u0 {5,D} {7,S}
+7  Cb u0 {6,S} {8,B} {12,B}
+8  Cb u0 {7,B} {9,B}
+9  Cb u0 {8,B} {10,B}
+10 Cb u0 {9,B} {11,B}
+11 Cb u0 {10,B} {12,B}
+12 *2 Cb u0 {4,S} {7,B} {11,B}
+""",
+    shortDesc = u"""""",
+    longDesc =
+u"""
+Forbid a benzofulvenyl radical  from isomerizing to 1H_naphthalene in one step using this family. No transition state was
+found for such a reaction.
+""",
+)
+
+forbidden(
+    label = "1H_naphthalene_1",
+    group =
+"""
+1 *1 Cs u1 {3,S} {11,S}
+3  Cd u0 {1,S} {4,D}
+4  Cd u0 {3,D} {5,S}
+5  Cb u0 {4,S} {6,B} {10,B}
+6  Cb u0 {5,B} {7,B} 
+7  Cb u0 {6,B} {8,B}
+8  Cb u0 {7,B} {9,B} 
+9  Cb u0 {8,B} {10,B}
+10 *2 Cb u0 {5,B} {9,B} {11,S}
+11 *3 C u0 {1,S} {10,S}
+""",
+    shortDesc = u"""""",
+    longDesc =
+u"""
+Forbid a 1H_naphthalene radical  from isomerizing to benzofulvenyl radical in one step using this family. No transition state was
+found for such a reaction.
+""",
+)
+
+forbidden(
+    label = "1H_naphthalene_2",
+    group =
+"""
+1 *1 Cs u1 {3,S} {11,S}
+3  Cd u0 {1,S} {4,D}
+4  Cd u0 {3,D} {5,S}
+5  Cb u0 {4,S} {6,B} {10,B}
+6  Cb u0 {5,B} {7,B} 
+7  Cb u0 {6,B} {8,B}
+8  Cb u0 {7,B} {9,B} 
+9  Cb u0 {8,B} {10,B}
+10 *2 Cb u0 {5,B} {9,B} {11,S}
+11 *3 C u0 {1,S} {10,S}
+""",
+    shortDesc = u"""""",
+    longDesc =
+u"""
+Forbid a 1H_naphthalene radical  from isomerizing to benzofulvenyl radical in one step using this family. No transition state was
+found for such a reaction.
+""",
+)
