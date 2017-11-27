44a52fa72a546b2354b7c0a2e5a4ba38b90ed78a
zjburas <zjburas@mit.edu>
2017-11-27 15:38:17 -0500

    Update recipes with floats for new families

--- input/kinetics/families/Intra_R_Add_Exo_scission/groups.py
+++ input/kinetics/families/Intra_R_Add_Exo_scission/groups.py
@@ -10,8 +10,8 @@ longDesc = u"""
 template(reactants=["Rn-Cs-Cb"], products=["Rn-Cs-Cb"], ownReverse=True)
 
 recipe(actions=[
-    ['BREAK_BOND', '*2', 'S', '*3'],
-    ['FORM_BOND', '*1', 'S', '*2'],
+    ['BREAK_BOND', '*2', 1, '*3'],
+    ['FORM_BOND', '*1', 1, '*2'],
     ['LOSE_RADICAL', '*1', '1'],
     ['GAIN_RADICAL', '*3', '1'],
 ])
