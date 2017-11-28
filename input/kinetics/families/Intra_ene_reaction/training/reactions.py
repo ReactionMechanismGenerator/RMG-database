cc759b64387f73d75130b4b2b76ce806012c4393
zjburas <zjburas@mit.edu>
2017-11-27 20:21:15 -0500

    Added non-R-add training reactions from 2009_Sharma_C5H5_CH3_highP library.

--- input/kinetics/families/Intra_ene_reaction/training/reactions.py
+++ input/kinetics/families/Intra_ene_reaction/training/reactions.py
@@ -80,3 +80,31 @@ Taken from entry: VIII <=> II
 """,
 )
 
+
+
+entry(
+    index = 6,
+    label = "C6H8 <=> C6H8-2",
+    degeneracy = 1.0,
+    kinetics = Arrhenius(A=(7.0333e+08, 's^-1'), n=1.2, Ea=(24.8, 'kcal/mol'), T0=(1, 'K')),
+    rank = 3,
+    shortDesc = u"""Training reaction from kinetics library: 2009_Sharma_C5H5_CH3_highP""",
+    longDesc = 
+u"""
+Taken from entry: C5H5CH3-5 <=> C5H5CH3-1
+""",
+)
+
+entry(
+    index = 7,
+    label = "C6H8-3 <=> C6H8-4",
+    degeneracy = 1.0,
+    kinetics = Arrhenius(A=(1.6539e+07, 's^-1'), n=2.1, Ea=(25.1, 'kcal/mol'), T0=(1, 'K')),
+    rank = 3,
+    shortDesc = u"""Training reaction from kinetics library: 2009_Sharma_C5H5_CH3_highP""",
+    longDesc = 
+u"""
+Taken from entry: C5H5CH3-1 <=> C5H5CH3-2
+""",
+)
+
