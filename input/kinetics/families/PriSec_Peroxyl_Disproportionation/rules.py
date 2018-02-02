32119f727112125396cd3384c792c695a2ca43e6
Colin Grambow <cgrambow@mit.edu>
2018-02-02 14:08:53 -0500

    Revert "Add default rules for peroxide families."
    This reverts commit 0d4f5c218584f4dc2378c3b599af7c588bf4c734.

--- input/kinetics/families/PriSec_Peroxyl_Disproportionation/rules.py
+++ input/kinetics/families/PriSec_Peroxyl_Disproportionation/rules.py
@@ -6,21 +6,3 @@ shortDesc = u""
 longDesc = u"""
 
 """
-entry(
-    index = 0,
-    label = "R2CHOO_rad;R2CHOO_rad",
-    kinetics = ArrheniusEP(
-        A = (5.0e6, 'm^3/(mol*s)'),
-        n = 0,
-        alpha = 0,
-        E0 = (20.0, 'kJ/mol'),
-        Tmin = (300, 'K'),
-        Tmax = (1500, 'K'),
-    ),
-    rank = 0,
-    shortDesc = u"""Default""",
-    longDesc =
-u"""
-
-""",
-)
--- input/kinetics/families/Tert_Peroxyl_Disproportionation/rules.py
+++ input/kinetics/families/Tert_Peroxyl_Disproportionation/rules.py
@@ -6,21 +6,3 @@ shortDesc = u""
 longDesc = u"""
 
 """
-entry(
-    index = 0,
-    label = "R2CHOO_rad;R2CHOO_rad",
-    kinetics = ArrheniusEP(
-        A = (1.48e9, 'm^3/(mol*s)'),
-        n = 0,
-        alpha = 0,
-        E0 = (35.6, 'kJ/mol'),
-        Tmin = (300, 'K'),
-        Tmax = (1500, 'K'),
-    ),
-    rank = 0,
-    shortDesc = u"""Default""",
-    longDesc =
-u"""
-
-""",
-)
