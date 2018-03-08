fea97c153055b8f2aa2342d24e94d1fb93352931
Max Liu <mjliu@mit.edu>
2018-03-08 13:51:22 -0500

    Use aromatic adjacency lists in 4 thermo libraries
    C10H11
    Fulvene_H
    Lai_Hexylbenzene
    naphthalene_H

--- input/thermo/libraries/naphthalene_H.py
+++ input/thermo/libraries/naphthalene_H.py
@@ -289,13 +289,13 @@ entry(
     molecule = 
 """
 multiplicity 2
-1  C u0 p0 c0 {2,S} {3,D} {4,S}
+1  C u0 p0 c0 {2,S} {3,B} {4,B}
 2  C u0 p0 c0 {1,S} {8,D} {11,S}
-3  C u0 p0 c0 {1,D} {5,S} {13,S}
-4  C u0 p0 c0 {1,S} {7,D} {17,S}
-5  C u0 p0 c0 {3,S} {6,D} {14,S}
-6  C u0 p0 c0 {5,D} {7,S} {15,S}
-7  C u0 p0 c0 {4,D} {6,S} {16,S}
+3  C u0 p0 c0 {1,B} {5,B} {13,S}
+4  C u0 p0 c0 {1,B} {7,B} {17,S}
+5  C u0 p0 c0 {3,B} {6,B} {14,S}
+6  C u0 p0 c0 {5,B} {7,B} {15,S}
+7  C u0 p0 c0 {4,B} {6,B} {16,S}
 8  C u0 p0 c0 {2,D} {9,S} {12,S}
 9  C u0 p0 c0 {8,S} {10,D} {18,S}
 10 C u1 p0 c0 {9,D} {19,S}
@@ -327,16 +327,16 @@ entry(
     label = "naphthalene",
     molecule = 
 """
-1  C u0 p0 c0 {2,S} {4,S} {5,D}
-2  C u0 p0 c0 {1,S} {3,S} {6,D}
-3  C u0 p0 c0 {2,S} {7,D} {11,S}
-4  C u0 p0 c0 {1,S} {8,D} {14,S}
-5  C u0 p0 c0 {1,D} {9,S} {15,S}
-6  C u0 p0 c0 {2,D} {10,S} {18,S}
-7  C u0 p0 c0 {3,D} {8,S} {12,S}
-8  C u0 p0 c0 {4,D} {7,S} {13,S}
-9  C u0 p0 c0 {5,S} {10,D} {16,S}
-10 C u0 p0 c0 {6,S} {9,D} {17,S}
+1  C u0 p0 c0 {2,B} {4,B} {5,B}
+2  C u0 p0 c0 {1,B} {3,B} {6,B}
+3  C u0 p0 c0 {2,B} {7,B} {11,S}
+4  C u0 p0 c0 {1,B} {8,B} {14,S}
+5  C u0 p0 c0 {1,B} {9,B} {15,S}
+6  C u0 p0 c0 {2,B} {10,B} {18,S}
+7  C u0 p0 c0 {3,B} {8,B} {12,S}
+8  C u0 p0 c0 {4,B} {7,B} {13,S}
+9  C u0 p0 c0 {5,B} {10,B} {16,S}
+10 C u0 p0 c0 {6,B} {9,B} {17,S}
 11 H u0 p0 c0 {3,S}
 12 H u0 p0 c0 {7,S}
 13 H u0 p0 c0 {8,S}
