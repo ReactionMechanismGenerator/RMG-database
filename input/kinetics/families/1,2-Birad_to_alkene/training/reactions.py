a7a74fa551b0019eca87134874653bdf9efa5f5a
Matt Johnson <mjohnson541@gmail.com>
2018-06-14 14:53:32 -0400

    rank update 1,2-Birad_to_alkene

--- input/kinetics/families/1,2-Birad_to_alkene/training/reactions.py
+++ input/kinetics/families/1,2-Birad_to_alkene/training/reactions.py
@@ -6,16 +6,14 @@ shortDesc = u"Kinetics used to train group additivity values"
 longDesc = u"""
 
 """
-
 entry(
-    index = 1,
+    index = 0,
     label = "SO2(T) => SO2(S)",
-    degeneracy = 1,
+    degeneracy = 1.0,
     reversible = False,
-    kinetics = Arrhenius(A=(1.0e+10, 's^-1'), n=0, Ea=(0, 'kcal/mol')),
+    kinetics = Arrhenius(A=(1e+10, 's^-1'), n=0, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
     rank = 1,
-    shortDesc = u"""""",
-    longDesc =
+    longDesc = 
 u"""
 taken from:
 F.B. Wampler, K. Otsuka, J.G. Calvert, E.K. Damon, Int. J. Chem. Kin., 1973, 5(4), 669-690, doi: 10.1002/kin.550050417
@@ -24,3 +22,4 @@ Similar rates were determined by:
 T.N. Rao, S.S. Collier, J.G. Calvet, JACS, 1969, 91(7), 1616-1612, doi: 10.1021/ja01035a006
 """,
 )
+
