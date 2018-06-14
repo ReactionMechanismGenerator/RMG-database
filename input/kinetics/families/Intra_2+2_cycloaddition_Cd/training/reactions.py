98b35d065b39fdbfb0e5d2773dc2fa25d18900a0
Matt Johnson <mjohnson541@gmail.com>
2018-06-14 15:03:00 -0400

    rank update Intra_2+2_cycloaddition_Cd

--- input/kinetics/families/Intra_2+2_cycloaddition_Cd/training/reactions.py
+++ input/kinetics/families/Intra_2+2_cycloaddition_Cd/training/reactions.py
@@ -7,14 +7,17 @@ longDesc = u"""
 Put kinetic parameters for reactions to use as a training set for fitting
 group additivity values in this file.
 """
-
-
 entry(
-    index = 1,
+    index = 0,
     label = "C6H6 <=> C6H6-2",
-    degeneracy = 1,
-    kinetics = Arrhenius(A=(4.99998e+11, 's^-1'), n=0.0559095, Ea=(122.413, 'kJ/mol'), T0=(1, 'K')),
-    rank = 3,
+    degeneracy = 1.0,
+    kinetics = Arrhenius(
+        A = (4.99998e+11, 's^-1'),
+        n = 0.0559095,
+        Ea = (122.413, 'kJ/mol'),
+        T0 = (1, 'K'),
+    ),
+    rank = 5,
     shortDesc = u"""Training reaction from kinetics library: 2003_Miller_Propargyl_Recomb_High_P""",
     longDesc = 
 u"""
