4497b6c269fac6b7b4a124cc746475f31d812058
alongd <alongd@mit.edu>
2018-07-09 08:47:44 -0400

    Generalized S in R_ROSR group of the ketoenol family

--- input/kinetics/families/ketoenol/groups.py
+++ input/kinetics/families/ketoenol/groups.py
@@ -28,10 +28,10 @@ entry(
     label = "R_ROSR",
     group = 
 """
-1 *2 R!H     u0 {2,S} {3,D}
-2 *3 [O,S2s] u0 {1,S} {4,S}
-3 *1 R!H     u0 {1,D}
-4 *4 R       u0 {2,S}
+1 *2 R!H   u0 {2,S} {3,D}
+2 *3 [O,S] u0 {1,S} {4,S}
+3 *1 R!H   u0 {1,D}
+4 *4 R     u0 {2,S}
 """,
     kinetics = None,
 )
