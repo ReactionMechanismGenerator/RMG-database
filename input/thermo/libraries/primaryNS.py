456f829c379f4174a55fdff4c31b3ffbad0939cd
HiroumiTani <hiroumi@mit.edu>
2018-09-12 03:56:50 +0900

    added thermo data of H2NN(T) and NH2NHNH2 to primaryNS.py.

--- input/thermo/libraries/primaryNS.py
+++ input/thermo/libraries/primaryNS.py
@@ -1784,3 +1784,123 @@ entry(
         =========== =========== =========== =========== ===========
     """,
 )
+
+entry(
+    index = 44,
+    label = "H2NN(T)",
+    molecule =
+"""
+multiplicity 3
+1 N u0 p1 c0 {2,S} {3,S} {4,S}
+2 N u2 p1 c0 {1,S}
+3 H u0 p0 c0 {1,S}
+4 H u0 p0 c0 {1,S}
+""",
+    thermo = NASA(
+        polynomials = [
+            NASAPolynomial(
+                coeffs = [4.04729, -0.00311119, 2.29222e-05, -2.62291e-08, 9.90206e-12, 43492.6, 5.33183],
+                Tmin = (10, 'K'),
+                Tmax = (786.77, 'K'),
+            ),
+            NASAPolynomial(
+                coeffs = [2.23132, 0.00869189, -4.48164e-06, 1.14412e-09, -1.15473e-13, 43698.8, 13.1517],
+                Tmin = (786.77, 'K'),
+                Tmax = (3000, 'K'),
+            ),
+        ],
+        Tmin = (10, 'K'),
+        Tmax = (3000, 'K'),
+        E0 = (361.629, 'kJ/mol'),
+        Cp0 = (33.2579, 'J/(mol*K)'),
+        CpInf = (83.1447, 'J/(mol*K)'),
+    ),
+    shortDesc = u"""CCSD(T)-F12/cc-pVTZ//M06-2x/cc-pVTZ""",
+    longDesc =
+    u"""
+    SMILES: N[N]
+
+    calculated by alongd (xc1089) at the CCSD(T)-F12/cc-pVTZ//M06-2x/cc-pVTZ level of theory
+
+
+    Thermodynamics for H2NN(T):
+      Enthalpy of formation (298 K)   =    88.859 kcal/mol
+      Entropy of formation (298 K)    =    56.175 cal/(mol*K)
+       =========== =========== =========== =========== ===========
+       Temperature Heat cap.   Enthalpy    Entropy     Free energy
+       (K)         (cal/mol*K) (kcal/mol)  (cal/mol*K) (kcal/mol)
+       =========== =========== =========== =========== ===========
+               300       9.040      88.877      56.236      72.007
+               400      10.026      89.830      58.969      66.242
+               500      11.054      90.884      61.317      60.226
+               600      12.023      92.039      63.419      53.987
+               800      13.622      94.611      67.108      40.924
+              1000      14.845      97.463      70.285      27.178
+              1500      16.816     105.431      76.720      -9.648
+              2000      17.873     114.129      81.717     -49.305
+              2400      18.407     121.391      85.025     -82.670
+       =========== =========== =========== =========== ===========
+    """,
+)
+
+entry(
+    index = 45,
+    label = "NH2NHNH2",
+    molecule =
+"""
+1 N u0 p1 c0 {2,S} {4,S} {5,S}
+2 N u0 p1 c0 {1,S} {3,S} {6,S}
+3 N u0 p1 c0 {2,S} {7,S} {8,S}
+4 H u0 p0 c0 {1,S}
+5 H u0 p0 c0 {1,S}
+6 H u0 p0 c0 {2,S}
+7 H u0 p0 c0 {3,S}
+8 H u0 p0 c0 {3,S}
+""",
+    thermo = NASA(
+        polynomials = [
+            NASAPolynomial(
+                coeffs = [3.79065, 0.0213782, -3.99354e-05, 9.75082e-08, -8.58502e-11, 22590.4, 6.83707],
+                Tmin = (10, 'K'),
+                Tmax = (429.355, 'K'),
+            ),
+            NASAPolynomial(
+                coeffs = [2.487, 0.0237268, -1.3915e-05, 3.96331e-09, -4.38596e-13, 22792.7, 13.0758],
+                Tmin = (429.355, 'K'),
+                Tmax = (3000, 'K'),
+            ),
+        ],
+        Tmin = (10, 'K'),
+        Tmax = (3000, 'K'),
+        E0 = (187.822, 'kJ/mol'),
+        Cp0 = (33.2579, 'J/(mol*K)'),
+        CpInf = (174.604, 'J/(mol*K)'),
+    ),
+    shortDesc = u"""CCSD(T)-F12/cc-pVTZ//M06-2x/cc-pVTZ""",
+    longDesc =
+    u"""
+    SMILES: NNN
+
+    calculated by alongd (xc1089) at the CCSD(T)-F12/cc-pVTZ//M06-2x/cc-pVTZ level of theory
+    rotors calculated at B3LYP/6-311++g(df,pd)
+
+    Thermodynamics for NH2NHNH2:
+      Enthalpy of formation (298 K)   =    48.625 kcal/mol
+      Entropy of formation (298 K)    =    67.011 cal/(mol*K)
+       =========== =========== =========== =========== ===========
+       Temperature Heat cap.   Enthalpy    Entropy     Free energy
+       (K)         (cal/mol*K) (kcal/mol)  (cal/mol*K) (kcal/mol)
+       =========== =========== =========== =========== ===========
+               300      16.985      48.659      67.124      28.521
+               400      19.862      50.501      72.406      21.539
+               500      22.534      52.624      77.131      14.059
+               600      24.866      54.997      81.451       6.126
+               800      28.640      60.366      89.147     -10.952
+              1000      31.445      66.388      95.855     -29.467
+              1500      35.619      83.287     109.502     -80.966
+              2000      37.696     101.665     120.062    -138.458
+              2400      38.787     116.973     127.036    -187.913
+       =========== =========== =========== =========== ===========
+    """,
+)
+
