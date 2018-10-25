4d79ebeb2dd88dcb62dd855b75d05b541ae20c23
alongd <alongd@mit.edu>
2018-10-25 08:34:47 -0400

    Added allow_pdep_route flag to NOx2018 kinetics library

--- input/kinetics/libraries/NOx2018/reactions.py
+++ input/kinetics/libraries/NOx2018/reactions.py
@@ -11205,6 +11205,7 @@ entry(
     index = 725,
     label = "N2H3 + NH2 <=> N2H2 + NH3",
     degeneracy = 1,
+    allow_pdep_route = True,
     kinetics = Arrhenius(
         A = (920000, 'cm^3/(mol*s)'),
         n = 1.94,
