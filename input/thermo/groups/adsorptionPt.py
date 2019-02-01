ab5dd8254fce98e0b35eb8d8233879f226b34c98
kblondal <katrin_blondal@brown.edu>
2019-02-01 14:11:37 -0500

    Commented out R-* in adsorptionPt
    It is just H with a single bond to Pt, which is already in the thermo database.

--- input/thermo/groups/adsorptionPt.py
+++ input/thermo/groups/adsorptionPt.py
@@ -27,30 +27,31 @@ This node should be empty, ensuring that one of the nodes below is used.
 """,
 )
 
-entry(
-    index = 1,
-    label = "R-*",
-    group =
-"""
-1 X  u0 p0 c0 {2,S}
-2 R  u0 p0 c0 {1,S}
-""",
-    thermo=ThermoData(
-        Tdata=([300, 400, 500, 600, 800, 1000, 1500], 'K'),
-        Cpdata=([-3.01, -1.78, -0.96, -0.41, 0.23, 0.56, 0.91], 'cal/(mol*K)'),
-        H298=(-86.29, 'kcal/mol'),
-        S298=(-26.39, 'cal/(mol*K)'),
-    ),
-    shortDesc=u"""Came from H single-bonded on Pt(111)""",
-    longDesc=u"""Calculated by Katrin Blondal at Brown University using statistical mechanics (files: compute_NASA_for_Pt-adsorbates.ipynb and compute_NASA_for_Pt-gas_phase.ipynb). Based on DFT calculations by Jelena Jelic at KIT.
-            DFT binding energy: -0.240 eV.
-            Linear scaling parameters: ref_adatom_H = -0.240 eV, psi = 0.00000 eV, gamma_H(X) = 1.000.
-
-   R
-   |
-***********
-"""
-)
+#entry(
+#    index = 1,
+#    label = "R-*",
+#    group =
+#"""
+#1 X  u0 p0 c0 {2,S}
+#2 R  u0 p0 c0 {1,S}
+#""",
+#    thermo=ThermoData(
+#        Tdata=([300, 400, 500, 600, 800, 1000, 1500], 'K'),
+#        Cpdata=([-3.01, -1.78, -0.96, -0.41, 0.23, 0.56, 0.91], 'cal/(mol*K)'),
+#        H298=(-86.29, 'kcal/mol'),
+#        S298=(-26.39, 'cal/(mol*K)'),
+#    ),
+#    shortDesc=u"""Came from H single-bonded on Pt(111)""",
+#    longDesc=u"""Calculated by Katrin Blondal at Brown University using statistical mechanics (files: compute_NASA_for_Pt-adsorbates.ipynb and compute_NASA_for_Pt-gas_phase.ipynb). Based on DFT calculations by Jelena Jelic at KIT.
+#            DFT binding energy: -0.240 eV.
+#            Linear scaling parameters: ref_adatom_H = -0.240 eV, psi = 0.00000 eV, gamma_H(X) = 1.000.
+#
+#   R
+#   |
+#***********
+#
+#"""
+#)
 
 entry(
     index = 2,
