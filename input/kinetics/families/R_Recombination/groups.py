a41b91bc887f1767cdb6a1d66ca58dfbd8126892
Matt Johnson <mjohnson541@gmail.com>
2019-06-06 17:15:52 -0400

    update R_Recombination with better comments

--- input/kinetics/families/R_Recombination/groups.py
+++ input/kinetics/families/R_Recombination/groups.py
@@ -2795,7 +2795,7 @@ entry(
     group = 
 """
 1 * H         u1
-2 * [C,O,S,N] u1 r0
+2 * [C,S,N,O] u1 r0
 """,
     kinetics = None,
 )
@@ -2829,7 +2829,7 @@ entry(
     group = 
 """
 1 * H       u1
-2 * [O,N,C] u1 r0
+2 * [N,C,O] u1 r0
 """,
     kinetics = None,
 )
@@ -3302,7 +3302,7 @@ entry(
     label = "Root_N-1R->H",
     group = 
 """
-1 * [Cl,S,C,Si,N,O] u1
+1 * [Cl,C,N,Si,S,O] u1
 2 * R               u1
 """,
     kinetics = None,
@@ -3524,7 +3524,7 @@ entry(
     label = "Root_N-1R->H_N-1CClNOSSi->N",
     group = 
 """
-1 * [O,S,C] u1
+1 * [S,C,O] u1
 2 * R       u1
 """,
     kinetics = None,
@@ -4070,7 +4070,7 @@ entry(
     label = "Root_N-1R->H_N-1CClNOSSi->N_N-1COS->O",
     group = 
 """
-1 * [S,C] u1
+1 * [C,S] u1
 2 * R     u1
 """,
     kinetics = None,
@@ -4395,7 +4395,7 @@ entry(
 3   R!H           ux {2,[S,D,T,B]}
 4   R!H           ux {2,[S,D,T,B]}
 5   R!H           ux {2,[S,D,T,B]}
-6   [Cl,O,N,S,Si] u0 r0 {1,[S,D,T,B]}
+6   [Cl,N,O,S,Si] u0 r0 {1,[S,D,T,B]}
 """,
     kinetics = None,
 )
@@ -4481,7 +4481,7 @@ entry(
 3   O             u0 {2,D}
 4   C             u0 {2,S}
 5   C             u0 r0 {1,S}
-6   [Cl,O,N,S,Si] u0 r0 {1,[S,D,T,B]}
+6   [Cl,N,O,S,Si] u0 r0 {1,[S,D,T,B]}
 """,
     kinetics = None,
 )
@@ -4495,7 +4495,7 @@ entry(
 2 * C             u1 {3,D} {4,S}
 3   O             u0 {2,D}
 4   C             u0 {2,S}
-5   [Cl,O,N,S,Si] u0 r0 {1,[S,D,T,B]}
+5   [Cl,N,O,S,Si] u0 r0 {1,[S,D,T,B]}
 """,
     kinetics = None,
 )
@@ -4831,7 +4831,7 @@ entry(
 1 * C             u1 r0 {4,[S,D,T,B]}
 2 * C             u1 {3,S}
 3   C             u0 r0 {2,S}
-4   [Cl,O,N,S,Si] u0 r0 {1,[S,D,T,B]}
+4   [Cl,N,O,S,Si] u0 r0 {1,[S,D,T,B]}
 """,
     kinetics = None,
 )
@@ -4958,7 +4958,7 @@ entry(
 1 * C             u1 r0 {4,D}
 2 * C             u1 r0 {3,D}
 3   O             u0 {2,D}
-4   [Cl,O,N,S,Si] u0 r0 {1,D}
+4   [Cl,N,O,S,Si] u0 r0 {1,D}
 """,
     kinetics = None,
 )
