db3d62ee0c66903e69f3db828b13836cf3770eed
Mark Payne <ampayne@mit.edu>
2019-06-13 16:54:51 -0400

    Remove Training Reactions that Violate Collision Limit in Substitution_O
    Missing thermochemistry for species COOOC seems to be the problem

--- input/kinetics/families/Substitution_O/training/reactions.py
+++ input/kinetics/families/Substitution_O/training/reactions.py
@@ -2462,28 +2462,6 @@ Converted to training reaction from rate rule: O-O2s(H)H;OsJ-Cs
 )
 
 entry(
-    index = 122,
-    label = "CH4O2-7 + CH3O-2 <=> C2H6O3-2 + H-2",
-    degeneracy = 1.0,
-    kinetics = Arrhenius(
-        A = (2.92558e-06, 'cm^3/(mol*s)'),
-        n = 5.89053,
-        Ea = (273.758, 'kJ/mol'),
-        T0 = (1, 'K'),
-        Tmin = (500, 'K'),
-        Tmax = (2000, 'K'),
-    ),
-    rank = 6,
-    shortDesc = u"""CB0-QB3 (1DHR)/UCCSD(T)-F12/vdz-f12""",
-    longDesc = 
-u"""
-Degeneracy not recalculated
-
-Converted to training reaction from rate rule: O-O2s(Cs)H;OsJ-Cs
-""",
-)
-
-entry(
     index = 123,
     label = "HO-2 + CH4O2-8 <=> H2O3-3 + CH3",
     degeneracy = 1.0,
@@ -2568,50 +2546,6 @@ u"""
 Degeneracy not recalculated
 
 Converted to training reaction from rate rule: O-Cs(HHH)Cs(HHH);OsJ-O2s
-""",
-)
-
-entry(
-    index = 127,
-    label = "HO-2 + C2H6O2-4 <=> CH4O3-4 + CH3",
-    degeneracy = 1.0,
-    kinetics = Arrhenius(
-        A = (21552.6, 'cm^3/(mol*s)'),
-        n = 2.45745,
-        Ea = (202.083, 'kJ/mol'),
-        T0 = (1, 'K'),
-        Tmin = (500, 'K'),
-        Tmax = (2000, 'K'),
-    ),
-    rank = 6,
-    shortDesc = u"""CB0-QB3 (1DHR)/UCCSD(T)-F12/vdz-f12""",
-    longDesc = 
-u"""
-Degeneracy not recalculated
-
-Converted to training reaction from rate rule: O-O2s(Cs)Cs(HHH);OsJ-H
-""",
-)
-
-entry(
-    index = 128,
-    label = "CH3O-2 + C2H6O2-4 <=> C2H6O3-2 + CH3",
-    degeneracy = 1.0,
-    kinetics = Arrhenius(
-        A = (0.0036016, 'cm^3/(mol*s)'),
-        n = 5.31614,
-        Ea = (66.9829, 'kJ/mol'),
-        T0 = (1, 'K'),
-        Tmin = (500, 'K'),
-        Tmax = (2000, 'K'),
-    ),
-    rank = 6,
-    shortDesc = u"""CB0-QB3 (1DHR)/UCCSD(T)-F12/vdz-f12""",
-    longDesc = 
-u"""
-Degeneracy not recalculated
-
-Converted to training reaction from rate rule: O-O2s(Cs)Cs(HHH);OsJ-Cs
 """,
 )
 
