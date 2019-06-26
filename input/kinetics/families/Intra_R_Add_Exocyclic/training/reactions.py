0ca585e16e86eb864f533968c3d9b8b7e823df7a
Mark Payne <ampayne@mit.edu>
2019-06-26 14:30:51 -0400

    Delete Reaction where comment and label disagree on the species involved

--- input/kinetics/families/Intra_R_Add_Exocyclic/training/reactions.py
+++ input/kinetics/families/Intra_R_Add_Exocyclic/training/reactions.py
@@ -664,44 +664,6 @@ Converted to training reaction from rate rule: R5_SD_D;doublebond_intra_HNd_pri;
 )
 
 entry(
-    index = 33,
-    label = "C4H7O <=> C4H7O-2",
-    degeneracy = 1.0,
-    kinetics = Arrhenius(
-        A = (2.724e+10, 's^-1', '*|/', 3),
-        n = 0.478,
-        Ea = (122.043, 'kJ/mol'),
-        T0 = (1, 'K'),
-        Tmin = (600, 'K'),
-        Tmax = (2000, 'K'),
-    ),
-    rank = 6,
-    shortDesc = u"""MRH CBS-QB3 calculations with 1d h.r. corrections""",
-    longDesc = 
-u"""
-MRH CBS-QB3 calculations for the reaction CH2=CH-CH2-OO => *CH2-cycle(CH-CH2-O-O)
-
-Previous RMG estimate for this reaction was an "Average of average" estimate.  This reaction was of
-interest to MRH/MHS because the butanol model was sensitive to allyl+O2 => CH2O+CH2CHO.  The high-p
-limit kinetics were necessary to estimate a k(T,P) for this PES.
-
-Reactant: 2 hindered rotors were considered (the OO and CH2OO torsions)
-TS: 0 hindered rotors were considered (the only low-frequency torisonal mode corresponded to
-	a hindered rotation within the cycle; MRH did not think treating this as a 1-d separable
-	hindered rotor was accurate)
-Product: 1 hindered rotor was considered (the *CH2 torsion)
-
-All external symmetry numbers were set equal to one.  The k(T) was calculated from 600 - 2000 K,
-in 200 K intervals, and the fitted Arrhenius expression from CanTherm was:
-k(T) = 2.724e+10 * (T/1K)^0.478 * exp(-29.169 kcal/mol / RT) cm3/mol/s.
-
-Converted to training reaction from rate rule: R5_SS_D;doublebond_intra_2H_pri;radadd_intra_O
-""",
-)
-
-
-
-entry(
     index = 41,
     label = "C5H7 <=> C5H7-2",
     degeneracy = 1.0,
