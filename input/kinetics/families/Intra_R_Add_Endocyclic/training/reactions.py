f28c28d0c0c760e5899e75078733805f2cfecb5a
Agnes <ajocher@mit.edu>
2019-07-26 15:40:01 -0400

    Delete training reactions with suspicious rate in Intra_R_Add_Endocyclic.

--- input/kinetics/families/Intra_R_Add_Endocyclic/training/reactions.py
+++ input/kinetics/families/Intra_R_Add_Endocyclic/training/reactions.py
@@ -1892,28 +1892,6 @@ From kinetics library: First_to_Second_Aromatic_Ring/2017_Buras_C6H5_C3H6_highP
 )
 
 entry(
-    index = 76,
-    label = "C9H11-5 <=> C9H11-6",
-    degeneracy = 1.0,
-    kinetics = Arrhenius(A=(4.255e+12, 's^-1'), n=0.347, Ea=(57.413, 'kcal/mol'), T0=(1, 'K')),
-    reference = Article(
-        authors = ['Z. J. Buras', 'T.-C. Chu', 'A. Jamal', 'N. W. Yee', 'J. E. Middaugh', 'W. H. Green'],
-        title = 'Phenyl radical + propene: a prototypical reaction surface for aromatic-catalyzed 1,2-hydrogen-migration and subsequent resonance-stabilized radical formation',
-        journal = 'Physical Chemistry Chemical Physics',
-        volume = '20',
-        pages = '13191-13214 ',
-        year = '2018',
-    ),
-    referenceType = "theory",
-    rank = 5,
-    longDesc = 
-u"""
-Calculations done at G3(MP2,CC)//B3LYP/6-311G** level of theory
-From kinetics library: First_to_Second_Aromatic_Ring/2017_Buras_C6H5_C3H6_highP
-""",
-)
-
-entry(
     index = 77,
     label = "C7H9-7 <=> C7H9-8",
     degeneracy = 2.0,
@@ -2232,29 +2210,6 @@ Taken from entry: pdt7 <=> pdt8
 )
 
 entry(
-    index = 91,
-    label = "C10H11-7 <=> C10H11-8",
-    degeneracy = 1.0,
-    kinetics = Arrhenius(A=(8.39e+10, 's^-1'), n=0.91, Ea=(36.4, 'kcal/mol'), T0=(1, 'K')),
-    reference = Article(
-        authors = ['A. J. Vervust', 'M. R. Djokic', 'S. S. Merchant', 'H.-H. Carstensen', 'A. E. Long', 'G. B. Marin', 'W. H. Green', 'K. M. Van Geem'],
-        title = 'Detailed Experimental and Kinetic Modeling Study of Cyclopentadiene Pyrolysis in the Presence of Ethene',
-        journal = 'Energy & Fuels',
-        volume = '32(3)',
-        pages = '3920-3934',
-        year = '2018',
-    ),
-    referenceType = "theory",
-    rank = 5,
-    longDesc = 
-u"""
-Calculations done at CBS-QB3 level of theory
-From kinetics library: C10H11
-Taken from entry: pdt8 <=> pdt9
-""",
-)
-
-entry(
     index = 92,
     label = "C10H11-9 <=> C10H11-10",
     degeneracy = 2.0,
@@ -2458,29 +2413,6 @@ u"""
 Calculations done at CBS-QB3 level of theory
 From kinetics library: C10H11
 Taken from entry: pdt14 <=> pdt23
-""",
-)
-
-entry(
-    index = 101,
-    label = "C10H11-27 <=> C10H11-28",
-    degeneracy = 1.0,
-    kinetics = Arrhenius(A=(1.27e+10, 's^-1'), n=1.01, Ea=(40.7, 'kcal/mol'), T0=(1, 'K')),
-    reference = Article(
-        authors = ['A. J. Vervust', 'M. R. Djokic', 'S. S. Merchant', 'H.-H. Carstensen', 'A. E. Long', 'G. B. Marin', 'W. H. Green', 'K. M. Van Geem'],
-        title = 'Detailed Experimental and Kinetic Modeling Study of Cyclopentadiene Pyrolysis in the Presence of Ethene',
-        journal = 'Energy & Fuels',
-        volume = '32(3)',
-        pages = '3920-3934',
-        year = '2018',
-    ),
-    referenceType = "theory",
-    rank = 5,
-    longDesc = 
-u"""
-Calculations done at CBS-QB3 level of theory
-From kinetics library: C10H11
-Taken from entry: pdt23 <=> pdt9
 """,
 )
 
