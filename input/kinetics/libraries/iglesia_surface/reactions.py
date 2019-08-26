bc14be5a42c22b07a5ea1cc049f3da4a18344d3f
alongd <alongd@mit.edu>
2019-08-26 10:52:39 -0400

    Modified igleasi_surface kinetics

--- input/kinetics/libraries/iglesia_surface/reactions.py
+++ input/kinetics/libraries/iglesia_surface/reactions.py
@@ -10,7 +10,7 @@ Manually added surface reactions with estimated rates
 entry(
     index = 0,
     label = "IC3H7 + HNO <=> propane + NO",
-    kinetics = Arrhenius(A=(1e+07, 'cm^3/(mol*s)'), n=2.0, Ea=(0.0, 'kcal/mol'), T0=(1, 'K')),
+    kinetics = Arrhenius(A=(1e+13, 'cm^3/(mol*s)'), n=0.0, Ea=(0.0, 'kcal/mol'), T0=(1, 'K')),
     longDesc =
 u"""
 
@@ -20,7 +20,7 @@ u"""
 entry(
     index = 1,
     label = "NC3H7 + HNO <=> propane + NO",
-    kinetics = Arrhenius(A=(1e+07, 'cm^3/(mol*s)'), n=2.0, Ea=(0.0, 'kcal/mol'), T0=(1, 'K')),
+    kinetics = Arrhenius(A=(1e+13, 'cm^3/(mol*s)'), n=0.0, Ea=(0.0, 'kcal/mol'), T0=(1, 'K')),
     longDesc =
 u"""
 
@@ -29,11 +29,11 @@ u"""
 
 entry(
     index = 2,
-    label = "C2H5 + HNO <=> C2H6 + HNO",
-    kinetics = Arrhenius(A=(1e+07, 'cm^3/(mol*s)'), n=2.0, Ea=(0.0, 'kcal/mol'), T0=(1, 'K')),
+    label = "C2H5 + HNO <=> C2H6 + NO",
+    kinetics = Arrhenius(A=(1e+13, 'cm^3/(mol*s)'), n=0.0, Ea=(0.0, 'kcal/mol'), T0=(1, 'K')),
     longDesc =
 u"""
-* new
+
 """,
 )
 
@@ -70,22 +70,42 @@ u"""
 entry(
     index = 6,
     label = "H + NO <=> HNO",
-    kinetics = Arrhenius(A=(1.0e+14, 'cm^3/(mol*s)'), n=0, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
+    kinetics = Arrhenius(A=(1.0e+16, 'cm^3/(mol*s)'), n=0, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
     longDesc =
 u"""
-high-P limit of C2H5 + NO
+
 """,
 )
+
+# entry(
+#     index = 6,
+#     label = "HNO <=> H + NO",
+#     kinetics = Arrhenius(A=(1.0e+10, 's^-1'), n=0, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
+#     longDesc =
+# u"""
+# high-P limit of C2H5 + NO
+# """,
+# )
 
 entry(
     index = 7,
     label = "CH3 + NO <=> CH3NO",
-    kinetics = Arrhenius(A=(1e+13, 'cm^3/(mol*s)'), n=0.0, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
+    kinetics = Arrhenius(A=(1e+15, 'cm^3/(mol*s)'), n=0.0, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
     longDesc =
 u"""
 an order of magnitude lower than "H + NO <=> HNO"
 """,
 )
+
+# entry(
+#     index = 7,
+#     label = "CH3NO <=> CH3 + NO",
+#     kinetics = Arrhenius(A=(1e+8, 's^-1'), n=0.0, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
+#     longDesc =
+# u"""
+# an order of magnitude lower than "H + NO <=> HNO"
+# """,
+# )
 
 entry(
     index = 8,
@@ -128,6 +148,17 @@ From primaryNitrogenLibrary by Page 1992 at CASSCF//(CASSCF and CISD), originall
 """,
 )
 
+# entry(
+#     index = 11,
+#     label = "H + HNO <=> H2 + NO",
+#     kinetics = Arrhenius(A=(4.46e+11, 'cm^3/(mol*s)'), n=0.72, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
+#     longDesc =
+# u"""
+# From primaryNitrogenLibrary by Page 1992 at CASSCF//(CASSCF and CISD), originally was:
+#     kinetics = Arrhenius(A=(4.46e+11, 'cm^3/(mol*s)'), n=0.72, Ea=(0.66, 'kcal/mol'), T0=(1, 'K')),
+# """,
+# )
+
 entry(
     index = 12,
     label = "CH3 + HNO <=> CH4 + NO",
@@ -138,12 +169,47 @@ u"""
 """,
 )
 
+# entry(
+#     index = 12,
+#     label = "CH3 + HNO <=> CH4 + NO",
+#     kinetics = Arrhenius(A=(4.46e+10, 'cm^3/(mol*s)'), n=0.72, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
+#     longDesc =
+# u"""
+#
+# """,
+# )
+
 entry(
     index = 13,
     label = "HNO + propenyl <=> C3H6 + NO",
-    kinetics = Arrhenius(A=(1.0e+11, 'cm^3/(mol*s)'), n=0.0, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
+    kinetics = Arrhenius(A=(1.0e+13, 'cm^3/(mol*s)'), n=0.0, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
     longDesc =
 u"""
-* new
+
 """,
 )
+
+# entry(
+#     index = 14,
+#     label = "IC3H7 + NO <=> NC3H7 + NO",
+#     kinetics = Arrhenius(A=(1.0e+11, 'cm^3/(mol*s)'), n=0.0, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
+#     longDesc =
+# u"""
+# * new
+# """,
+# )
+
+
+# entry(
+#     index = 14,
+#     label = "NO + C3H6 <=> C3H5 + HNO",
+#     kinetics = Arrhenius(A=(1.0e+14, 'cm^3/(mol*s)'), n=0.0, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
+#     longDesc =
+# u"""
+#
+# """,
+# )
+
+
+
+# add additional reactions for the intermediate species entering the cat reactor from reactor 1
