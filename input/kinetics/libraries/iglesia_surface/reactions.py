94ae5dc9c50504c546d1bf7bace5e8a2834cf0cd
alongd <alongd@mit.edu>
2019-09-06 15:35:03 -0400

    thermo & rates

--- input/kinetics/libraries/iglesia_surface/reactions.py
+++ input/kinetics/libraries/iglesia_surface/reactions.py
@@ -10,7 +10,7 @@ Manually added surface reactions with estimated rates
 entry(
     index = 0,
     label = "IC3H7 + HNO <=> propane + NO",
-    kinetics = Arrhenius(A=(1e+13, 'cm^3/(mol*s)'), n=0.0, Ea=(0.0, 'kcal/mol'), T0=(1, 'K')),
+    kinetics = Arrhenius(A=(1e+12, 'cm^3/(mol*s)'), n=0.0, Ea=(1.0, 'kcal/mol'), T0=(1, 'K')),
     longDesc =
 u"""
 too high, 1e12, small Ea, 1 kcal
@@ -20,7 +20,7 @@ too high, 1e12, small Ea, 1 kcal
 entry(
     index = 1,
     label = "NC3H7 + HNO <=> propane + NO",
-    kinetics = Arrhenius(A=(1e+13, 'cm^3/(mol*s)'), n=0.0, Ea=(0.0, 'kcal/mol'), T0=(1, 'K')),
+    kinetics = Arrhenius(A=(1e+12, 'cm^3/(mol*s)'), n=0.0, Ea=(1.0, 'kcal/mol'), T0=(1, 'K')),
     longDesc =
 u"""
 too high, 1e12, small Ea, 1 kcal
@@ -30,7 +30,7 @@ too high, 1e12, small Ea, 1 kcal
 entry(
     index = 2,
     label = "C2H5 + HNO <=> C2H6 + NO",
-    kinetics = Arrhenius(A=(1e+13, 'cm^3/(mol*s)'), n=0.0, Ea=(0.0, 'kcal/mol'), T0=(1, 'K')),
+    kinetics = Arrhenius(A=(1e+12, 'cm^3/(mol*s)'), n=0.0, Ea=(1.0, 'kcal/mol'), T0=(1, 'K')),
     longDesc =
 u"""
 too high, 1e12, small Ea, 1 kcal
@@ -40,19 +40,19 @@ too high, 1e12, small Ea, 1 kcal
 entry(
     index = 3,
     label = "IC3H7 + NO <=> C3H6 + HNO",
-    kinetics = Arrhenius(A=(1.0e+15, 'cm^3/(mol*s)'), n=0.0, Ea=(0.0, 'cal/mol'), T0=(1, 'K')),
+    kinetics = Arrhenius(A=(1.0e+13, 'cm^3/(mol*s)'), n=0.0, Ea=(0.0, 'cal/mol'), T0=(1, 'K')),
     longDesc =
 u"""
 A too high, 1e13
-two cases: if Eley-rideal case, 1e13
-if migrates on the surface, higher, calc ghas kinetic theory of collision w surface
+two cases: if Eley-Rideal case, 1e13
+if migrates on the surface, higher, calc gas kinetic theory of collision w surface
 """,
 )
 
 entry(
     index = 4,
     label = "NC3H7 + NO <=> C3H6 + HNO",
-    kinetics = Arrhenius(A=(1.0e+15, 'cm^3/(mol*s)'), n=0.0, Ea=(0.0, 'cal/mol'), T0=(1, 'K')),
+    kinetics = Arrhenius(A=(1.0e+13, 'cm^3/(mol*s)'), n=0.0, Ea=(0.0, 'cal/mol'), T0=(1, 'K')),
     longDesc =
 u"""
 
@@ -62,10 +62,10 @@ u"""
 entry(
     index = 5,
     label = "C2H5 + NO <=> C2H4 + HNO",
-    kinetics = Arrhenius(A=(1.0e+15, 'cm^3/(mol*s)'), n=0.0, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
+    kinetics = Arrhenius(A=(1.0e+13, 'cm^3/(mol*s)'), n=0.0, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
     longDesc =
 u"""
-same
+
 """,
 )
 
@@ -75,7 +75,7 @@ entry(
     kinetics = Arrhenius(A=(1.0e+13, 'cm^3/(mol*s)'), n=0, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
     longDesc =
 u"""
-corect if E-R, higher if collision
+It's correct if this is an Eley-Rideal behaviour, higher if collision behaviour
 """,
 )
 
@@ -142,7 +142,7 @@ u"""
 entry(
     index = 11,
     label = "H + HNO <=> H2 + NO",
-    kinetics = Arrhenius(A=(1.0e+12, 'cm^3/(mol*s)'), n=0.0, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
+    kinetics = Arrhenius(A=(4.46e+12, 'cm^3/(mol*s)'), n=0.72, Ea=(0.66, 'kcal/mol'), T0=(1, 'K')),
     longDesc =
 u"""
 From primaryNitrogenLibrary by Page 1992 at CASSCF//(CASSCF and CISD), originally was:
@@ -153,24 +153,13 @@ make it a bit higher A, small Ea
 """,
 )
 
-# entry(
-#     index = 11,
-#     label = "H + HNO <=> H2 + NO",
-#     kinetics = Arrhenius(A=(4.46e+11, 'cm^3/(mol*s)'), n=0.72, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
-#     longDesc =
-# u"""
-# From primaryNitrogenLibrary by Page 1992 at CASSCF//(CASSCF and CISD), originally was:
-#     kinetics = Arrhenius(A=(4.46e+11, 'cm^3/(mol*s)'), n=0.72, Ea=(0.66, 'kcal/mol'), T0=(1, 'K')),
-# """,
-# )
-
 entry(
     index = 12,
     label = "CH3 + HNO <=> CH4 + NO",
-    kinetics = Arrhenius(A=(1.0e+11, 'cm^3/(mol*s)'), n=0.0, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
+    kinetics = Arrhenius(A=(5.0e+11, 'cm^3/(mol*s)'), n=0.72, Ea=(0.66, 'kcal/mol'), T0=(1, 'K')),
     longDesc =
 u"""
-like 11 lower
+like 11, a bit lower
 """,
 )
 
@@ -187,34 +176,20 @@ like 11 lower
 entry(
     index = 13,
     label = "HNO + C3H5_allyl <=> C3H6 + NO",
-    kinetics = Arrhenius(A=(1.0e+13, 'cm^3/(mol*s)'), n=0.0, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
+    kinetics = Arrhenius(A=(5.0e+11, 'cm^3/(mol*s)'), n=0.0, Ea=(5, 'kcal/mol'), T0=(1, 'K')),
     longDesc =
 u"""
 high barrier, 5 kcal? A like 12
 """,
 )
 
-# entry(
-#     index = 14,
-#     label = "IC3H7 + NO <=> NC3H7 + NO",
-#     kinetics = Arrhenius(A=(1.0e+11, 'cm^3/(mol*s)'), n=0.0, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
-#     longDesc =
-# u"""
-# * new
-# """,
-# )
-
-
-# entry(
-#     index = 14,
-#     label = "NO + C3H6 <=> C3H5 + HNO",
-#     kinetics = Arrhenius(A=(1.0e+14, 'cm^3/(mol*s)'), n=0.0, Ea=(0, 'kcal/mol'), T0=(1, 'K')),
-#     longDesc =
-# u"""
-#
-# """,
-# )
-
-
-
-# add additional reactions for the intermediate species entering the cat reactor from reactor 1
+entry(
+    index = 14,
+    label = "HNO + HNO <=> H2 + NO + NO",
+    kinetics = Arrhenius(A=(2.0e13, 'cm^3/(mol*s)'), n=0.0, Ea=(25, 'kcal/mol'), T0=(1, 'K')),
+    longDesc =
+u"""
+A = kB T / h
+Ea = 2 * H(HNO), here taken for BDE = 65 kcal/mol
+""",
+)
