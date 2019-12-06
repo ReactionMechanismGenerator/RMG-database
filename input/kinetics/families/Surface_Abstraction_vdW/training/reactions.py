a4b4dc64daf81bf08125f2b93e7b300c014ca0bc
Emily Mazeau <mazeau.e@husky.neu.edu>
2019-12-06 15:48:06 -0500

    Adding in Surface Abstraction vdW family
    This is for a vdW species splitting, adsorbing to the surface, and transferring a functional group to a double, triple, or quadruple bonded surface species.  This was taken from "Mechanism of Methanol Synthesis on Cu through CO2 and CO Hydrogenation" by Grabow and Mavrikakis doi:10.1021/cs200055d

---
+++ input/kinetics/families/Surface_Abstraction_vdW/training/reactions.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "Surface_Abstraction_vdW/training"
+shortDesc = u"Reaction kinetics used to generate rate rules"
+longDesc = u"""
+Put kinetic parameters for specific reactions in this file to use as a
+training set for generating rate rules to populate this kinetics family.
+"""
+
+entry(
+    index = 16,
+    label = "OH_2* + OH_4* <=> H2O* + O*",
+    degeneracy = 2,
+    kinetics = SurfaceArrhenius(
+        A = (1.675e12, 'm^2/(mol*s)'),
+        n = 0.,
+        Ea = (14.0669343, 'kcal/mol'),
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    rank = 10,
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Reaction 16 from table 2 in "Mechanism of Methanol Synthesis on Cu through CO2
+and CO Hydrogenation", Grabow and Mavrikakis.  doi:10.1021/cs200055d
+"""
+)
+
+entry(
+    index = 40,
+    label = "OH_2* + HCO* <=> H2O* + CO*",
+    degeneracy = 1,
+    kinetics = SurfaceArrhenius(
+        A = (9.597e12, 'm^2/(mol*s)'),
+        n = 0.,
+        Ea = (6.9181644, 'kcal/mol'),
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    rank = 10,
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Reaction 40 from table 2 in "Mechanism of Methanol Synthesis on Cu through CO2
+and CO Hydrogenation", Grabow and Mavrikakis.  doi:10.1021/cs200055d
+"""
+)
+
+entry(
+    index = 41,
+    label = "HCOO* + HCO* <=> HCOOH* + CO*",
+    degeneracy = 1,
+    kinetics = SurfaceArrhenius(
+        A = (2.2e14, 'm^2/(mol*s)'),
+        n = 0.,
+        Ea = (13.8363288, 'kcal/mol'),
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    rank = 10,
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Reaction 41 from table 2 in "Mechanism of Methanol Synthesis on Cu through CO2
+and CO Hydrogenation", Grabow and Mavrikakis.  doi:10.1021/cs200055d
+"""
+)
+
+entry(
+    index = 45,
+    label = "CH3O* + HCO* <=> CH3OH* + CO*",
+    degeneracy = 1,
+    kinetics = SurfaceArrhenius(
+        A = (1.934e12, 'm^2/(mol*s)'),
+        n = 0.,
+        Ea = (8.76300824, 'kcal/mol'),
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    rank = 10,
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Reaction 45 from table 2 in "Mechanism of Methanol Synthesis on Cu through CO2
+and CO Hydrogenation", Grabow and Mavrikakis.  doi:10.1021/cs200055d
+"""
+)
