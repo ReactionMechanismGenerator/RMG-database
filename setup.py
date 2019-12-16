91aae4b8c70b4985635f6d43d42997412bd716fe
Max Liu <mjliu@mit.edu>
2019-12-16 16:19:02 -0500

    Increment RMG-database version to 3.0.0

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ scripts = ['scripts/' + scriptName for scriptName in scripts]
 # Initiate the build and/or installation
 setup(
     name='RMG-database',
-    version='2.4.0',
+    version='3.0.0',
     description='Reaction Mechanism Generator Database',
     author='William H. Green and the RMG Team',
     author_email='rmg_dev@mit.edu',
