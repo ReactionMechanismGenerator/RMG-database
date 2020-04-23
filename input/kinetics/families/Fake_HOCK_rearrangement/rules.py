2bd13b0d2cd57394929a1aebe4c60cb72a4b3a0a
Alon <alongd@mit.edu>
2020-04-23 14:57:03 -0400

    Added the Fake_HOCK_rearrangement reaction family

---
+++ input/kinetics/families/Fake_HOCK_rearrangement/rules.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "Fake_HOCK_rearrangement/rules"
+shortDesc = u""
+longDesc = u"""
+
+"""
+entry(
+    index = 0,
+    label = "CC-OOH",
+    kinetics = ArrheniusEP(
+        A = (3e+6, 's^-1'),
+        n = 0,
+        alpha = 0,
+        E0 = (8, 'kcal/mol'),
+        Tmin = (300, 'K'),
+        Tmax = (1500, 'K'),
+    ),
+    rank = 0,
+    shortDesc = u"""Default""",
+)
