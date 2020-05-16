950f5c289cae212c201bd7442d60d23981f34681
Alon <alongd@mit.edu>
2020-05-15 21:52:47 -0400

    Added the hemiaminal_hydrolysis reaction family

---
+++ input/kinetics/families/hemiaminal_hydrolysis/training/reactions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "hemiaminal_hydrolysis/training"
+shortDesc = u"Reaction kinetics used to generate rate rules"
+longDesc = u"""
+Put kinetic parameters for specific reactions in this file to use as a
+training set for generating rate rules to populate this kinetics family.
+"""
+
+entry(
+    index = 0,
+    label = "CCCCN(C)CO <=> CH2O + CCCCNC",
+    degeneracy = 1.0,
+    kinetics=Arrhenius(A=(5e+12, 's^-1'), n=2.0, Ea=(60, 'kJ/mol'),
+                       T0=(1, 'K'), Tmin=(275, 'K'), Tmax=(350, 'K')),
+    rank = 1,
+    longDesc =
+"""
+est.
+Ea is high on purpose to avoid endothermicity correction
+this rate is 4.5E+07 at 313 K
+""",
+)
