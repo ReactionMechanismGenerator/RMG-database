6965b886949fe4c70302875fa4f11665700cb21e
davidfarinajr <farina.d@husky.neu.edu>
2020-05-27 20:46:59 -0400

    removed vdw from HAN libraries

--- input/thermo/libraries/HAN_Ir.py
+++ input/thermo/libraries/HAN_Ir.py
@@ -15,7 +15,7 @@ entry(
 metal Ir
 1  O u0 p2 c0 {5,S} {8,S}
 2  O u0 p3 c-1 {6,S} {7,H}
-3  O u0 p3 c-1 {6,S} {8,H} {11,vdW}
+3  O u0 p3 c-1 {6,S} {8,H}
 4  O u0 p2 c0 {6,D}
 5  N u0 p0 c+1 {1,S} {7,S} {9,S} {10,S}
 6  N u0 p0 c+1 {2,S} {3,S} {4,D}
@@ -23,7 +23,7 @@ metal Ir
 8  H u0 p0 c0 {1,S} {3,H}
 9  H u0 p0 c0 {5,S}
 10 H u0 p0 c0 {5,S}
-11 X u0 p0 c0 {3,vdW}
+11 X u0 p0 c0
 """,
     thermo = ThermoData(
         Tdata=([300, 400, 500, 600, 800, 1000, 1500], 'K'), 
