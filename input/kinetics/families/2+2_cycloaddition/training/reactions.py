d9e574cee4e107bf3dcd3ce652262823b19b55ff
hwpang <hwpang@mit.edu>
2020-06-05 18:27:16 -0400

    Convert to training reactions from rules
    from 2+2_cycloaddition families

--- input/kinetics/families/2+2_cycloaddition/training/reactions.py
+++ input/kinetics/families/2+2_cycloaddition/training/reactions.py
@@ -55,3 +55,43 @@ Converted to training reaction from rate rule: CH2CHO;mb_CO_2H
 """,
 )
 
+entry(
+    index = 2,
+    label = "CH2O + C2H4<=> C3H6O",
+    kinetics = Arrhenius(A=(1.05605e+06,'cm^3/(mol*s)'), n=1.86, Ea=(232.9,'kJ/mol'), Tmin=(300,'K'), Tmax=(1500,'K')),
+    rank = 11,
+    shortDesc = """Converted to training reaction from rate rule: db;doublebond""",
+    longDesc = 
+"""
+Kinetics fitted from reverse direction using rate of 
+
+C3H6O <=> C2H4 + CH2O, low or high pressure extrapolation with thermal excitation technique, taken from 
+
+Zalotai, L. et al, Kinetics of gas phase decomposition of oxetan and oxetan-2,2-d2, Int. J. Chem. Kinet., 15, 505, 1983
+http://kinetics.nist.gov/kinetics/Detail?id=1983ZAL/HUN505:1
+""",
+)
+
+entry(
+    index = 3,
+    label = "CH2S + C2H4 <=> C3H6S",
+    kinetics = Arrhenius(A=(6.92e+10,'cm^3/(mol*s)'), n=0, Ea=(43.72,'kcal/mol'), Tmin=(300,'K'), Tmax=(1500,'K')),
+    rank = 11,
+    shortDesc = """Converted to training reaction from rate rule: CS;doublebond""",
+)
+
+entry(
+    index = 4,
+    label = "CH2O + C2H4 <=> C3H6O",
+    kinetics = Arrhenius(A=(6.92e+10,'cm^3/(mol*s)'), n=0, Ea=(43.72,'kcal/mol'), Tmin=(300,'K'), Tmax=(1500,'K')),
+    rank = 11,
+    shortDesc = """Converted to training reaction from rate rule: CO;doublebond""",
+)
+
+entry(
+    index = 5,
+    label = "C2H2O + C2H4 <=> C4H6O",
+    kinetics = Arrhenius(A=(6.92e+10,'cm^3/(mol*s)'), n=0, Ea=(43.72,'kcal/mol'), Tmin=(300,'K'), Tmax=(1500,'K')),
+    rank = 11,
+    shortDesc = """Converted to training reaction from rate rule: CCO;doublebond""",
+)
