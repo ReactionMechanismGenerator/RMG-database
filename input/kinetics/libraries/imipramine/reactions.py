521acab47fda00b9f673027e0f7b57ce9dee73f0
Oscar Wu <oscarwu@mit.edu>
2020-08-19 14:17:05 -0400

    Add imp_4_oo + meoh rate to lib

--- input/kinetics/libraries/imipramine/reactions.py
+++ input/kinetics/libraries/imipramine/reactions.py
@@ -294,6 +294,30 @@ Solvent: H2O:MeOH = 0.7:0.3 (mol%)
 """,
 )
 
+entry(
+    index = 1014,
+    label = "imipramine_4_oo + CH3OH <=> imipramine_4_ooh + CH2OH",
+    degeneracy = 3.0,
+    kinetics = Arrhenius(A=(7.69603e-05,'m^3/(mol*s)'), 
+                         n=0, 
+                         Ea=(141.246,'kJ/mol'), 
+                         T0=(1,'K'), 
+                         Tmin=(275,'K'), 
+                         Tmax=(350,'K'), 
+                         comment="""Fitted to 76 data points; dA = *|/ 1.27528, dn = +|- 0, dEa = +|- 0.627125 kJ/mol"""),
+    shortDesc = u"""MS-TST rate based on fragment""",
+    longDesc =
+"""
+Calculated by ACS using multiple-structure local-harmonic 
+conventional transition state theory with Eckart tunneling 
+(MS-LH-CTST/Eckart).
+
+Optfreq: wb97xd/def2svp in vacuum (freq scale factor: 0.986)
+SP: dlpno-ccsd(t)/def2-tzvp normalPNO + Cosmo-RS TZVPD-Fine 
+Solvent: H2O:MeOH = 0.7:0.3 (mol%)
+""",
+)
+
 
 # entry(
 #     index = 1010,
