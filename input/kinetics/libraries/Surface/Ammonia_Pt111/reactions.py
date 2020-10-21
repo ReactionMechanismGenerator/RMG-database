8348b795c115f5fabc6f6a6e43b41c1c9bde46b1
Tingchenlee <lee.ting@northeastern.edu>
2020-10-21 14:58:36 -0400

    add Ammonia_Pt111 and Ammonia_Pt_Al2O3_oxidation

---
+++ input/kinetics/libraries/Surface/Ammonia_Pt111/reactions.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "Ammonia_Pt111"
+shortDesc = u""
+longDesc = u"""
+Based primarily on 
+"Ammonia Dissociation on Pt(100), Pt(111), and Pt(211): A Comparative Density Functional Theory Study"
+W. K. Offermans, A. P. J. Jansen, R. A. van Santen, G. Novell-Leruth, J. M. Ricart, and J. Pérez-Ramírez
+The Journal of Physical Chemistry C 2007 111 (47), 17551-17557
+https://doi.org/10.1021/jp073083f
+"""
+
+#The unit of Pre Factor is [Hz]!
+#Kinetic parameters at 300 K for the dissociation of NHx, Ea is zero point energy corrected
+entry(
+    index = 1,
+    label = " NH3_X + X <=> NH2_X + H_X ",
+    kinetics = SurfaceArrhenius(
+        A=(8.6E11, 'm^2/(mol*s)'), #The value is for NH3_X(rot/vib), for NH3_X(vib), A=5.6E11
+        n = 0.0,
+        Ea = (93, 'kJ/mol'),  
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Ammonia Dissociation on Pt(100), Pt(111), and Pt(211): A Comparative Density Functional Theory Study
+"""
+)
+
+entry(
+    index = 2,
+    label = " NH2_X + X <=> NH_X + H_X ",
+    kinetics = SurfaceArrhenius(
+        A=(5E12, 'm^2/(mol*s)'), 
+        n = 0.0,
+        Ea = (110, 'kJ/mol'),  
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Ammonia Dissociation on Pt(100), Pt(111), and Pt(211): A Comparative Density Functional Theory Study
+"""
+)
+
+entry(
+    index = 3,
+    label = " NH_X + X <=> N_X + H_X ",
+    kinetics = SurfaceArrhenius(
+        A=(7.2E12, 'm^2/(mol*s)'), 
+        n = 0.0,
+        Ea = (118, 'kJ/mol'),  
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Ammonia Dissociation on Pt(100), Pt(111), and Pt(211): A Comparative Density Functional Theory Study
+"""
+)
+
+#Kinetic parameters at 300 K of the oxidation of NHx by Oads
+entry(
+    index = 4,
+    label = " NH3_X +O_X <=> NH2_X + OH_X ",
+    kinetics = SurfaceArrhenius(
+        A=(1.2E12, 'm^2/(mol*s)'), 
+        n = 0.0,
+        Ea = (42, 'kJ/mol'),    
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Ammonia Dissociation on Pt(100), Pt(111), and Pt(211): A Comparative Density Functional Theory Study
+"""
+)
+
+entry(
+    index = 5,
+    label = " NH2_X +O_X <=> NH_X + OH_X ",
+    kinetics = SurfaceArrhenius(
+        A=(6.1E12, 'm^2/(mol*s)'), 
+        n = 0.0,
+        Ea = (87, 'kJ/mol'),  
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Ammonia Dissociation on Pt(100), Pt(111), and Pt(211): A Comparative Density Functional Theory Study
+"""
+)
+
+entry(
+    index = 6,
+    label = " NH_X +O_X <=> N_X + OH_X ",
+    kinetics = SurfaceArrhenius(
+        A=(7.6E12, 'm^2/(mol*s)'), 
+        n = 0.0,
+        Ea = (84, 'kJ/mol'), 
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Ammonia Dissociation on Pt(100), Pt(111), and Pt(211): A Comparative Density Functional Theory Study
+"""
+)
+
+#Kinetic parameters at 300 K of the oxidation of NHx by OHads
+entry(
+    index = 7,
+    label = " NH3_X + OH_X <=> NH2_X + H2O_X  ",
+    kinetics = SurfaceArrhenius(
+        A=(1.6E11, 'm^2/(mol*s)'), 
+        n = 0.0,
+        Ea = (73, 'kJ/mol'),   
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Ammonia Dissociation on Pt(100), Pt(111), and Pt(211): A Comparative Density Functional Theory Study
+"""
+)
+
+entry(
+    index = 8,
+    label = " NH2_X + OH_X <=> NH_X + H2O_X  ",
+    kinetics = SurfaceArrhenius(
+        A=(3.5E12, 'm^2/(mol*s)'), 
+        n = 0.0,
+        Ea = (22, 'kJ/mol'),  
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Ammonia Dissociation on Pt(100), Pt(111), and Pt(211): A Comparative Density Functional Theory Study
+"""
+)
+
+entry(
+    index = 9,
+    label = " NH_X + OH_X <=> N_X + H2O_X  ",
+    kinetics = SurfaceArrhenius(
+        A=(5.1E11, 'm^2/(mol*s)'), 
+        n = 0.0,
+        Ea = (35, 'kJ/mol'),  
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Ammonia Dissociation on Pt(100), Pt(111), and Pt(211): A Comparative Density Functional Theory Study
+"""
+)
+
---
+++ input/kinetics/libraries/Surface/Ammonia_Pt_Al2O3_oxidation/reactions.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "Ammonia_Pt_Al2O3_oxidation"
+shortDesc = u""
+longDesc = u"""
+Based primarily on Pritpal S. Dhillon, Michael P. Harold, Di Wang, Ashok Kumar, Saurabh Joshi,
+Hydrothermal aging of Pt/Al2O3 monolith: 
+Washcoat morphology degradation effects studied using ammonia and propylene oxidation,
+Catalysis Today, Volume 320, 2019, Pages 20-29, ISSN 0920-5861,
+https://doi.org/10.1016/j.cattod.2017.12.023.
+"""
+#Need to use  generatedSpeciesConstraints(allowSingletO2=True) in input.py
+
+entry(
+    index = 1,
+    label = " NH3 + X <=> NH3_X ",
+    kinetics = StickingCoefficient(
+        A=1.25E1,                
+        n = 0.0,
+        Ea = (0, 'kJ/mol'),  #For the reverse rxn: A=2E4, Ea=106.16
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Hydrothermal aging of Pt/Al2O3 monolith: Washcoat morphology degradation effects studied using ammonia and propylene oxidation
+"""
+)
+
+entry(
+    index = 2,
+    label = " O2 + X + X <=> O_X + O_X",
+    kinetics = SurfaceArrhenius(
+        A=(2.6E1, 'm^2/(mol*s)'), 
+        n = 0.0,
+        Ea = (0, 'kJ/mol'),    
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Hydrothermal aging of Pt/Al2O3 monolith: Washcoat morphology degradation effects studied using ammonia and propylene oxidation
+"""
+)
+
+#might delete this
+entry(
+    index = 3,
+    label = " NH3_X + O_X <=> N_X + H2O + H_X ",
+    kinetics = SurfaceArrhenius(
+        A=(2.7E15, 'm^2/(mol*s)'), 
+        n = 0.0,
+        Ea = (140.70, 'kJ/mol'),   
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Hydrothermal aging of Pt/Al2O3 monolith: Washcoat morphology degradation effects studied using ammonia and propylene oxidation
+"""
+)
+
+entry(
+    index = 4,
+    label = " NO + X <=> NO_X ",
+    kinetics = SurfaceArrhenius(
+        A=(0.9E2, 'm^2/(mol*s)'), 
+        n = 0.0,
+        Ea = (0, 'kJ/mol'),   
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Hydrothermal aging of Pt/Al2O3 monolith: Washcoat morphology degradation effects studied using ammonia and propylene oxidation
+"""
+)
+
+entry(
+    index = 5,
+    label = " N_X + N_X  <=>  N2 + X + X ",
+    kinetics = SurfaceArrhenius(
+        A=(1.9E18, 'm^2/(mol*s)'), 
+        n = 0.0,
+        Ea = (174, 'kJ/mol'), 
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Hydrothermal aging of Pt/Al2O3 monolith: Washcoat morphology degradation effects studied using ammonia and propylene oxidation
+"""
+)
+
+entry(
+    index = 6,
+    label = " N_X + O_X  <=>  NO_X + X ",
+    kinetics = SurfaceArrhenius(
+        A=(2.2E12, 'm^2/(mol*s)'), 
+        n = 0.0,
+        Ea = (120.32, 'kJ/mol'), 
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Hydrothermal aging of Pt/Al2O3 monolith: Washcoat morphology degradation effects studied using ammonia and propylene oxidation
+"""
+)
+
+entry(
+    index = 7,
+    label = " N_X + NO_X   <=>  N2O + X + X ",
+    kinetics = SurfaceArrhenius(
+        A=(7.3E18, 'm^2/(mol*s)'), 
+        n = 0.0,
+        Ea = (130.5, 'kJ/mol'), 
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Hydrothermal aging of Pt/Al2O3 monolith: Washcoat morphology degradation effects studied using ammonia and propylene oxidation
+"""
+)
+
+entry(
+    index = 8,
+    label = " O_X + NO_X <=> NO2_X + X ",
+    kinetics = SurfaceArrhenius(
+        A=(6.4E13, 'm^2/(mol*s)'), 
+        n = 0.0,
+        Ea = (103.4, 'kJ/mol'), 
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Hydrothermal aging of Pt/Al2O3 monolith: Washcoat morphology degradation effects studied using ammonia and propylene oxidation
+"""
+)
+
+entry(
+    index = 9,
+    label = " NO2_X <=> NO2 + X ",
+    kinetics = SurfaceArrhenius(
+        A=(4E13, 'm^2/(mol*s)'), 
+        n = 0.0,
+        Ea = (120, 'kJ/mol'), 
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Hydrothermal aging of Pt/Al2O3 monolith: Washcoat morphology degradation effects studied using ammonia and propylene oxidation
+"""
+)
