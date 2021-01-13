990a52e944f2a0ab39df306bde59bdf8e8c0c2aa
davidfarinajr <farina.d@northeastern.edu>
2021-01-13 18:16:48 -0500

    revised Br_Abs rules

--- input/kinetics/families/Br_Abstraction/rules.py
+++ input/kinetics/families/Br_Abstraction/rules.py
@@ -25,39 +25,39 @@ entry(
     index = 0,
     label = "X_Br_or_Xrad_Br_Xbirad_Br_Xtrirad_Br;Y_rad_birad_trirad_quadrad",
     kinetics = ArrheniusEP(
-        A = (100000, 'cm^3/(mol*s)'),
+        A = (5.11e+13, 'cm^3/(mol*s)'),
         n = 0,
         alpha = 0,
-        E0 = (10, 'kcal/mol'),
-        Tmin = (300, 'K'),
-        Tmax = (1500, 'K'),
+        E0 = (5840, 'cal/mol'),
+        Tmin = (1, 'K'),
+        Tmax = (3000, 'K'),
     ),
     rank = 0,
     shortDesc = u"""Default""",
     longDesc =
 u"""
-If a biradical CH2JJ can abstract from RCH4 to make RCH3J and CH3J
-then a Y_rad CH3J should be able to abstract from RCH3J which means X_H needs
-to include Xrad_H. I.e. you can abstract from a radical. To make this possible
-a head node has been created X_H_or_Xrad_H which is a union of X_H and Xrad_H.
-The kinetics for it have just been copied from X_H and are only defined for
-abstraction by Y_rad_birad. I.e. the top level very approximate guess.
-
-Do better kinetics for this exist? Do we in fact use the reverse kinetics anyway?
+imported from 2-BTP model
+entry(
+    index = 1428,
+    label = "H + CH3BR <=> HBR + CH3",
+    degeneracy = 1.0,
+    kinetics = Arrhenius(A=(5.11e+13,'cm^3/(mol*s)'), n=0, Ea=(5840,'cal/mol'), T0=(1,'K')),
+    shortDesc = The chemkin file reaction is H + CH3BR <=> HBR + CH3,
+)
 """,
 )
 
-entry(
-    index = 1,
-    label = "X_H;Y_rad_birad_trirad_quadrad",
-    kinetics = ArrheniusEP(
-        A = (100000, 'cm^3/(mol*s)'),
-        n = 0,
-        alpha = 0,
-        E0 = (10, 'kcal/mol'),
-        Tmin = (300, 'K'),
-        Tmax = (1500, 'K'),
-    ),
-    rank = 0,
-    shortDesc = u"""Default""",
-)
+# entry(
+#     index = 1,
+#     label = "X_H;Y_rad_birad_trirad_quadrad",
+#     kinetics = ArrheniusEP(
+#         A = (100000, 'cm^3/(mol*s)'),
+#         n = 0,
+#         alpha = 0,
+#         E0 = (10, 'kcal/mol'),
+#         Tmin = (300, 'K'),
+#         Tmax = (1500, 'K'),
+#     ),
+#     rank = 0,
+#     shortDesc = u"""Default""",
+# )
