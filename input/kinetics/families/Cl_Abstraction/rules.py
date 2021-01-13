df7cdca71ba9d9aab37d31b78139e0006d38494f
davidfarinajr <farina.d@northeastern.edu>
2021-01-13 18:17:02 -0500

    revised Cl_Abs rules

--- input/kinetics/families/Cl_Abstraction/rules.py
+++ input/kinetics/families/Cl_Abstraction/rules.py
@@ -25,39 +25,34 @@ entry(
     index = 0,
     label = "X_Cl_or_Xrad_Cl_Xbirad_Cl_Xtrirad_Cl;Y_rad_birad_trirad_quadrad",
     kinetics = ArrheniusEP(
-        A = (100000, 'cm^3/(mol*s)'),
-        n = 0,
+        A = (7.046e+12, 'cm^3/(mol*s)'),
+        n = 1.73,
         alpha = 0,
-        E0 = (10, 'kcal/mol'),
+        E0 = (35.88, 'kJ/mol'),
         Tmin = (300, 'K'),
-        Tmax = (1500, 'K'),
+        Tmax = (3000, 'K'),
     ),
     rank = 0,
     shortDesc = u"""Default""",
     longDesc =
 u"""
-If a biradical CH2JJ can abstract from RCH4 to make RCH3J and CH3J
-then a Y_rad CH3J should be able to abstract from RCH3J which means X_H needs
-to include Xrad_H. I.e. you can abstract from a radical. To make this possible
-a head node has been created X_H_or_Xrad_H which is a union of X_H and Xrad_H.
-The kinetics for it have just been copied from X_H and are only defined for
-abstraction by Y_rad_birad. I.e. the top level very approximate guess.
-
-Do better kinetics for this exist? Do we in fact use the reverse kinetics anyway?
+CH3Cl + H -> CH3 + HCl
+A = 1.17E-11c m3/molecule/s * 6.02214E23 molecule/mol
+https://kinetics.nist.gov/kinetics/Detail?id=2004LOU/GON10586-10593:2
 """,
 )
 
-entry(
-    index = 1,
-    label = "X_H;Y_rad_birad_trirad_quadrad",
-    kinetics = ArrheniusEP(
-        A = (100000, 'cm^3/(mol*s)'),
-        n = 0,
-        alpha = 0,
-        E0 = (10, 'kcal/mol'),
-        Tmin = (300, 'K'),
-        Tmax = (1500, 'K'),
-    ),
-    rank = 0,
-    shortDesc = u"""Default""",
-)
+# entry(
+#     index = 1,
+#     label = "X_H;Y_rad_birad_trirad_quadrad",
+#     kinetics = ArrheniusEP(
+#         A = (100000, 'cm^3/(mol*s)'),
+#         n = 0,
+#         alpha = 0,
+#         E0 = (10, 'kcal/mol'),
+#         Tmin = (300, 'K'),
+#         Tmax = (1500, 'K'),
+#     ),
+#     rank = 0,
+#     shortDesc = u"""Default""",
+# )
