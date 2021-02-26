d83d0395bfe1b10488d79c07af4979fcdc09b505
Richard West <r.west@northeastern.edu>
2021-02-26 09:09:21 -0500

    Make Surface_Abstraction_Single_vdW its own reverse
    Think this might need some hard-coding in RMG-Py to switch the labels.
    
    Note: you must put group names in right order.
    If your tree is in a different order from your
    rules then the database tests fail.

--- input/kinetics/families/Surface_Abstraction_Single_vdW/rules.py
+++ input/kinetics/families/Surface_Abstraction_Single_vdW/rules.py
@@ -4,12 +4,13 @@
 name = "Surface_Abstraction_Single_vdW/rules"
 shortDesc = u""
 longDesc = u"""
-A vdW species splitting, adsorbing to the surface, and transferring a functional group to a single bonded surface species.
+A vdW species splitting, adsorbing to the surface, 
+and transferring a functional group to a single bonded surface species.
 """
 
 entry(
     index = 1,
-    label = "AdsorbateVdW1;Adsorbate1",
+    label = "Donating;Abstracting",
     kinetics = SurfaceArrheniusBEP(
         A = (1.0e13, 'm^2/(mol*s)'),
         n = 0,
