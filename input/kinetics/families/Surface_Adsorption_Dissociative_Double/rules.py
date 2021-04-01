79c24a709ac6f9444a53735dff62fcfbf9a64bf2
Tingchenlee <lee.ting@northeastern.edu>
2021-04-01 14:24:50 -0400

    remove O2 reaction in rules.py

--- input/kinetics/families/Surface_Adsorption_Dissociative_Double/rules.py
+++ input/kinetics/families/Surface_Adsorption_Dissociative_Double/rules.py
@@ -6,6 +6,7 @@ shortDesc = u""
 longDesc = u"""
 Dissociative adsorption of a gas-phase species forming two adsorbates, each with a double bond to a surface site
 """
+
 entry(
     index = 1,
     label = "Adsorbate;VacantSite1;VacantSite2",
@@ -20,29 +21,4 @@ entry(
     rank = 0,
     shortDesc = u"""Default""",
     longDesc = u"""Made up"""
-)
-
-entry(
-    index = 2,
-    label = "OO;VacantSite1;VacantSite2",
-    kinetics = StickingCoefficientBEP(
-        A = 4.36E-2,
-        n = -0.206,
-        E0=(1.5E3, 'J/mol'),
-        Tmin = (200, 'K'),
-        Tmax = (3000, 'K'),
-    ),
-    rank = 10,
-    shortDesc = u"""O2 dissociative adsorption""",
-    longDesc = u"""
-    Surface Reaction Kinetics of Steam- and CO2-Reforming as well as Oxidation of Methane over Nickel-Based Catalysts
-    Delgado et al
-    Catalysts, 2015, 5, 871-904
-    doi: 10.3390/catal5020871
-    
-    E0 is the paper's Ea
-    This is R3
-    
-    metal = 'Ni'
-    """
 )
