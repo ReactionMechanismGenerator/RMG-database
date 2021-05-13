0c95aa77d9fde40d635a19229e8d67973698aa97
Tingchenlee <lee.ting@northeastern.edu>
2021-05-13 12:46:52 -0400

    added training reactions from Novell, Offermans, Ralph, Ryan and Schneider_Pt111 libraries

--- input/kinetics/families/Surface_Abstraction_Single_vdW/training/reactions.py
+++ input/kinetics/families/Surface_Abstraction_Single_vdW/training/reactions.py
@@ -8,3 +8,45 @@ Put kinetic parameters for specific reactions in this file to use as a
 training set for generating rate rules to populate this kinetics family.
 """
 
+entry(
+    index = 1,
+    label = "HOX + H3NX <=> H2NX + H2OX",
+    degeneracy = 3.0,
+    kinetics = SurfaceArrhenius(A=(1.973e+22,'cm^2/(mol*s)'), n=0, Ea=(33772,'J/mol'), T0=(1,'K'), Tmin=(200,'K'), Tmax=(3000,'K')),
+    rank = 3,
+    shortDesc = """Surface_Abstraction_Single_vdW""",
+    longDesc = 
+"""
+Training reaction from kinetics library: Surface/Schneider_Pt111
+Original entry: NH3_X + OH_X <=> NH2_X + H2O_X
+"Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities."
+DMa, Hanyu; Schneider, William F.(2019). ACS Catalysis, 9(3), 2407-2414. 
+https://doi.org/10.1021/acscatal.8b04251
+This reaction used RMG's surface site density of Pt111 = 2.483E-9(mol/cm^2) to calculate the A factor.
+A = 4.9E13(1/s)/2.483E-9(mol/cm^2) = 1.973E22 cm^2/(mol*s)
+""",
+    metal = "Pt",
+    facet = "111",
+)
+
+entry(
+    index = 2,
+    label = "HOX + H3NX <=> H2NX + H2OX",
+    degeneracy = 3.0,
+    kinetics = SurfaceArrhenius(A=(6.444e+19,'cm^2/(mol*s)'), n=0, Ea=(73000,'J/mol'), T0=(1,'K'), Tmin=(200,'K'), Tmax=(3000,'K')),
+    rank = 3,
+    shortDesc = """Surface_Abstraction_Single_vdW""",
+    longDesc = 
+"""
+Training reaction from kinetics library: Surface/Offermans_Pt111
+Original entry: NH3_X + OH_X <=> NH2_X + H2O_X
+"Ammonia oxidation on platinum : a density functional theory study of surface reactivity."
+Offermans, W. K. (2007). Technische Universiteit Eindhoven. 
+https://doi.org/10.6100/IR630067
+This reaction used RMG's surface site density of Pt111 = 2.483E-9(mol/cm^2) to calculate the A factor.
+A (at 300K) = 1.6E11(1/s)/2.483E-9(mol/cm^2) = 6.444E19 cm^2/(mol*s)
+""",
+    metal = "Pt",
+    facet = "111",
+)
+
--- input/kinetics/families/Surface_Adsorption_vdW/training/reactions.py
+++ input/kinetics/families/Surface_Adsorption_vdW/training/reactions.py
@@ -67,3 +67,45 @@ entry(
     Catalysts, 2015, 5, 871-904""",
 	metal = "Ni",
 )
+entry(
+    index = 12,
+    label = "H3NX <=> H3N + X",
+    degeneracy = 1.0,
+    kinetics = SurfaceArrhenius(A=(3.09e+09,'1/s'), n=0, Ea=(60900,'J/mol'), T0=(1,'K'), Tmin=(200,'K'), Tmax=(3000,'K')),
+    rank = 3,
+    shortDesc = """Surface_Adsorption_vdW""",
+    longDesc = 
+"""
+Training reaction from kinetics library: Surface/Ralph_Pt111
+Original entry: NH3_X <=> NH3 + X
+"Ammonia Oxidation over Polycrystalline Platinum: 
+Surface Morphology and Kinetics at Atmospheric Pressure."
+Krähnert, Ralph(2005) A Doctoral Thesis.
+http://dx.doi.org/10.14279/depositonce-1270
+A = k/exp(Ea/RT) = 2.17(mol/m^2/s)/exp(60900J/mol / 8.314J/molK / 298K) = 4.09E9 cm^2/mol/s
+""",
+    metal = "Pt",
+    facet = "111",
+)
+
+entry(
+    index = 13,
+    label = "X + N2 <=> N2X",
+    degeneracy = 1.0,
+    kinetics = SurfaceArrhenius(A=(3.464e+21,'cm^3/(mol*s)'), n=0, Ea=(4000,'J/mol'), T0=(1,'K'), Tmin=(200,'K'), Tmax=(3000,'K')),
+    rank = 3,
+    shortDesc = """N2 Surface_Adsorption_vdW""",
+    longDesc = 
+"""
+Training reaction from kinetics library: Surface/Offermans_Pt111
+Original entry: N2 + X <=> N2_X
+"Ammonia oxidation on platinum : a density functional theory study of surface reactivity."
+Offermans, W. K. (2007). Technische Universiteit Eindhoven. 
+https://doi.org/10.6100/IR630067
+This reaction used RMG's surface site density of Pt111 = 2.483E-9(mol/cm^2) to calculate the A factor.
+A (at 300K from p.62)= 8.6E12(1/s)/2.483E-9(mol/cm^2) = 3.464E21 cm^2/(mol*s)
+""",
+    metal = "Pt",
+    facet = "111",
+)
+
