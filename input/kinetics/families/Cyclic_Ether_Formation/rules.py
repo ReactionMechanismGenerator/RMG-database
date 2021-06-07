8c0b9a55d55f67d64198286c02c81495374a0dea
Xiaorui Dong <xiaorui@mit.edu>
2021-06-07 11:57:36 -0400

    Remove the only entry in the rules.py
    This will be replaced by new rules created by ATG anyway

--- input/kinetics/families/Cyclic_Ether_Formation/rules.py
+++ input/kinetics/families/Cyclic_Ether_Formation/rules.py
@@ -6,17 +6,3 @@ shortDesc = u""
 longDesc = u"""
 
 """
-entry(
-    index = 812,
-    label = "RnOO;Y_rad_intra;OO_intra",
-    kinetics = ArrheniusEP(
-        A = (1e+11, 's^-1'),
-        n = 0,
-        alpha = 0,
-        E0 = (10, 'kcal/mol'),
-        Tmin = (300, 'K'),
-        Tmax = (1500, 'K'),
-    ),
-    rank = 0,
-)
-
