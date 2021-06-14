693045fd70be892a0e7ebebc019aefcba004beda
jonzheng <jonzheng@mit.edu>
2021-06-14 14:55:18 -0400

    trained tree (ran BM notebook), set reversible to false

--- input/kinetics/families/Baeyer-Villiger_step2/training/reactions.py
+++ input/kinetics/families/Baeyer-Villiger_step2/training/reactions.py
@@ -2,34 +2,34 @@
 # encoding: utf-8
 
 name = "Baeyer-Villiger_step2/training"
-shortDesc = u"Reaction kinetics used to generate rate rules"
-longDesc = u"""
+shortDesc = "Reaction kinetics used to generate rate rules"
+longDesc = """
 Put kinetic parameters for specific reactions in this file to use as a
 training set for generating rate rules to populate this kinetics family.
 """
+entry(
+    index = 0,
+    label = "acetone_peracetic_criegee <=> methyl_acetate + acetic_acid",
+    degeneracy = 2.0,
+    kinetics = Arrhenius(A=(1.69425e+11,'s^-1'), n=0.82328, Ea=(126.358,'kJ/mol'), T0=(1,'K')),
+    rank = 6,
+    shortDesc = """CBS-QB3 calculation without HR""",
+    longDesc = 
+"""
+CBS-QB3 calculation without HR fitted over range from 300-600 K with Eckart tunneling
+""",
+)
 
 entry(
     index = 1,
-    label = "acetone_peracetic_criegee <=> methyl_acetate + acetic_acid",
+    label = "cyclohexanone_peracetic_criegee <=> caprolactone + acetic_acid",
     degeneracy = 2.0,
-    kinetics = Arrhenius(A=(1.69425e11, 's^-1'), n=0.82328, Ea=(126.358, 'kJ/mol'), T0=(1, 'K')),
+    kinetics = Arrhenius(A=(1.52064e+11,'s^-1'), n=0.572241, Ea=(93.6294,'kJ/mol'), T0=(1,'K')),
     rank = 6,
-    shortDesc = u"""CBS-QB3 calculation without HR""",
-    longDesc =
-u"""
+    shortDesc = """CBS-QB3 calculation without HR""",
+    longDesc = 
+"""
 CBS-QB3 calculation without HR fitted over range from 300-600 K with Eckart tunneling
 """,
 )
 
-entry(
-    index = 2,
-    label = "cyclohexanone_peracetic_criegee <=> caprolactone + acetic_acid",
-    degeneracy = 2.0,
-    kinetics = Arrhenius(A=(1.52064e+11, 's^-1'), n=0.572241, Ea=(93.6294, 'kJ/mol'), T0=(1, 'K')),
-    rank = 6,
-    shortDesc = u"""CBS-QB3 calculation without HR""",
-    longDesc =
-u"""
-CBS-QB3 calculation without HR fitted over range from 300-600 K with Eckart tunneling
-""",
-)
