ee4fc220c1e167cc89475d97eaef97b8a208a010
Xiaorui Dong <xiaorui@mit.edu>
2021-06-14 12:36:16 -0400

    Removed a duplicate entry before generating new tree
    Previously, when converting from rules to training reactions, rule 7 and 8 were converted to the same reaction. Now only keep the one that has a more specific rate estimation.

--- input/kinetics/families/ketoenol/training/reactions.py
+++ input/kinetics/families/ketoenol/training/reactions.py
@@ -134,29 +134,6 @@ entry(
     kinetics = Arrhenius(
         A = (7040, 's^-1'),
         n = 2.66,
-        Ea = (313.8, 'kJ/mol'),
-        T0 = (1, 'K'),
-        Tmin = (300, 'K'),
-        Tmax = (1500, 'K'),
-    ),
-    rank = 11,
-    shortDesc = u"""W.H. Green estimate
-                   A,n from R_ROR;R1_doublebond_CH2;R2_doublebond_H;R_O_H
-                   Ea = W.H. Green estimate
-                   """,
-    longDesc = 
-u"""
-Converted to training reaction from rate rule: R_ROR;R1_doublebond_CH2;R2_doublebond_H;R_O_R
-""",
-)
-
-entry(
-    index = 8,
-    label = "C3H6O <=> C3H6O-2",
-    degeneracy = 1.0,
-    kinetics = Arrhenius(
-        A = (7040, 's^-1'),
-        n = 2.66,
         Ea = (351.456, 'kJ/mol'),
         T0 = (1, 'K'),
         Tmin = (300, 'K'),
