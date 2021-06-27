16cb5d4f6b3212fd9b34f28c1c2c258ea80f7257
Tingchenlee <lee.ting@northeastern.edu>
2021-06-26 23:46:13 -0400

    revised A factors in Schneider_Pd211 based on the models

--- input/kinetics/libraries/Surface/Schneider_Pd211/reactions.py
+++ input/kinetics/libraries/Surface/Schneider_Pd211/reactions.py
@@ -15,7 +15,7 @@ entry(
     index = 1,
     label = "O2 + X + X <=> O_X + O_X",
     kinetics = StickingCoefficient(
-        A = 1,
+        A = 0.1,
         n = 0,
         Ea = (28947, 'J/mol'),
         Tmin = (200, 'K'),
@@ -39,7 +39,7 @@ entry(
     index = 2,
     label = "NH3 + X <=> NH3_X",
    kinetics = StickingCoefficient(
-       A = 1,
+       A = 0.1,
         n = 0,
         Ea = (0, 'J/mol'),
         Tmin = (200, 'K'),
@@ -62,7 +62,7 @@ entry(
     index = 3,
     label = "NH3_X +O_X <=> NH2_X + OH_X",
     kinetics = SurfaceArrhenius(
-        A = (2.44E21, 'cm^2/(mol*s)'), 
+        A = (1.93E21, 'cm^2/(mol*s)'), 
         n = 0.0,
         Ea = (39560.9, 'J/mol'),   
         Tmin = (200, 'K'),
@@ -89,7 +89,7 @@ entry(
     index = 4,
     label = "NH2_X +O_X <=> NH_X + OH_X",
     kinetics = SurfaceArrhenius(
-        A = (2.44E21, 'cm^2/(mol*s)'), 
+        A = (2.05E21, 'cm^2/(mol*s)'), 
         n = 0.0,
         Ea = (104209.2, 'J/mol'),   
         Tmin = (200, 'K'),
@@ -116,7 +116,7 @@ entry(
     index = 5,
     label = "NH_X + O_X <=> N_X + OH_X",
     kinetics = SurfaceArrhenius(
-        A = (2.44E21, 'cm^2/(mol*s)'), 
+        A = (2.9E21, 'cm^2/(mol*s)'), 
         n = 0.0,
         Ea = (23157.6, 'J/mol'),   
         Tmin = (200, 'K'),
@@ -143,7 +143,7 @@ entry(
     index = 6,
     label = "NH3_X + OH_X <=> NH2_X + H2O_X",
     kinetics = SurfaceArrhenius(
-        A = (2.44E21, 'cm^2/(mol*s)'), 
+        A = (1.82E22, 'cm^2/(mol*s)'), 
         n = 0.0,
         Ea = (70437.7, 'J/mol'),   
         Tmin = (200, 'K'),
@@ -170,7 +170,7 @@ entry(
     index = 7,
     label = "NH2_X + OH_X <=> NH_X + H2O_X",
     kinetics = SurfaceArrhenius(
-        A = (2.44E21, 'cm^2/(mol*s)'), 
+        A = (5.58E21, 'cm^2/(mol*s)'), 
         n = 0.0,
         Ea = (73332.4, 'J/mol'),  
         Tmin = (200, 'K'),
@@ -197,7 +197,7 @@ entry(
     index = 8,
     label = "NH_X + OH_X <=> N_X + H2O_X",
     kinetics = SurfaceArrhenius(
-        A = (2.44E21, 'cm^2/(mol*s)'), 
+        A = (2.19E21, 'cm^2/(mol*s)'), 
         n = 0.0,
         Ea = (41490.7, 'J/mol'),  
         Tmin = (200, 'K'),
@@ -224,7 +224,7 @@ entry(
     index = 9,
     label = "OH_X + OH_X <=> O_X + H2O_X",
     kinetics = SurfaceArrhenius(
-        A = (2.44E21, 'cm^2/(mol*s)'),  
+        A = (1.15E20, 'cm^2/(mol*s)'),  #revised value from A=1.15E21 to A=1.15E20 base one the models
         n = 0.0,
         Ea = (71402.6, 'J/mol'),  
         Tmin = (200, 'K'),
@@ -247,13 +247,39 @@ This is reaction (9) in Table S2
     facet = "211",
 )
 
+# entry(
+#     index = 10,
+#     label = "H2O_X <=> H2O + X",
+#     kinetics = SurfaceArrhenius(
+#         A = (5.9E7, '1/s'),
+#         n = 0.0,
+#         Ea = (30876.8, 'J/mol'),  
+#         Tmin = (200, 'K'),
+#         Tmax = (3000, 'K'),
+#     ),
+#     shortDesc = u"""Surface_Adsorption_vdW""",
+#     longDesc = u"""
+# "DFT and microkinetic comparison of Pt, Pd and Rh-catalyzed ammonia oxidation"
+# Hanyu Ma, and William F.Schneider
+# Journal of Catalysis 383 (2020) 322–330
+# https://doi.org/10.1016/j.jcat.2020.01.029
+# Ea = 0.32eV = 30876.8J/mol
+
+# This is reaction (10) in Table S2
+# """,
+#       metal = "Pd",
+#       facet = "211",
+# )
+
+#Add R10 in the reverse direction, assume sticking coefficient = 0.1
+#The reaction in forward direction from the paper is too fast.
 entry(
     index = 10,
     label = "H2O + X <=> H2O_X",
     kinetics = StickingCoefficient(
-        A = 1,
+        A = 0.1,
         n = 0.0,
-        Ea = (30876.8, 'J/mol'),  
+        Ea = (0, 'J/mol'),  
         Tmin = (200, 'K'),
         Tmax = (3000, 'K'),
     ),
@@ -263,7 +289,6 @@ entry(
 Hanyu Ma, and William F.Schneider
 Journal of Catalysis 383 (2020) 322–330
 https://doi.org/10.1016/j.jcat.2020.01.029
-Ea = 0.32eV = 30876.8J/mol
 
 This is reaction (10) in Table S2
 """,
@@ -273,9 +298,9 @@ This is reaction (10) in Table S2
 
 entry(
     index = 11,
-    label = "N2 + X + X <=> N_X + N_X",
-    kinetics = StickingCoefficient(
-        A = 1,
+    label = "N_X + N_X <=> N2 + X + X",
+    kinetics = SurfaceArrhenius(
+        A = (3.42E21, 'cm^2/(mol*s)'),
         n = 0.0,
         Ea = (119647.6, 'J/mol'),  
         Tmin = (200, 'K'),
@@ -296,11 +321,37 @@ This is reaction (11) in Table S2
     facet = "211",
 )
 
+#Reverse reaction of R11
+# entry(
+#     index = 11,
+#     label = "N2 + X + X <=> N_X + N_X",
+#     kinetics = StickingCoefficient(
+#         A = 1,
+#         n = 0.0,
+#         Ea = (177541.6, 'J/mol'),  
+#         Tmin = (200, 'K'),
+#         Tmax = (3000, 'K'),
+#     ),
+#     shortDesc = u"""N2 Surface_Adsorption_Dissociative""",
+#     longDesc = u"""
+# "DFT and microkinetic comparison of Pt, Pd and Rh-catalyzed ammonia oxidation"
+# Hanyu Ma, and William F.Schneider
+# Journal of Catalysis 383 (2020) 322–330
+# https://doi.org/10.1016/j.jcat.2020.01.029
+
+# Ea = 1.84eV = 177541.6J/mol
+
+# This is reaction (11) in Table S2
+# """,
+#     metal = "Pd",
+#     facet = "211",
+# )
+
 entry(
     index = 12,
     label = "N_X + O_X <=> NO_X + X",
     kinetics = SurfaceArrhenius(
-        A = (2.44E21, 'cm^2/(mol*s)'),  
+        A = (3.09E21, 'cm^2/(mol*s)'),  
         n = 0.0,
         Ea = (82981.4, 'J/mol'),  
         Tmin = (200, 'K'),
@@ -314,7 +365,6 @@ Journal of Catalysis 383 (2020) 322–330
 https://doi.org/10.1016/j.jcat.2020.01.029
 
 This reaction used RMG's surface site density of Pd211 = 2.688E-9(mol/cm^2) to calculate the A factor.
-The A factor is calculated by equation (5) which assumed qTS/qIS = 1
 Ea = 0.86eV = 82981.4J/mol
 
 This is reaction (12) in Table S2
@@ -327,7 +377,7 @@ entry(
     index = 13,
     label = "NO_X <=> NO + X",
     kinetics = SurfaceArrhenius(
-        A = (6.55E14, '1/s'),  
+        A = (6.55E13, '1/s'),  #revised value from A=6.55E14 to A=6.55E13 base one the models
         n = 0.0,
         Ea = (225786.6, 'J/mol'),  
         Tmin = (200, 'K'),
@@ -352,7 +402,7 @@ entry(
     index = 14,
     label = "N_X + NO_X <=> N2O_X + X",
     kinetics = SurfaceArrhenius(
-        A = (2.44E21, 'cm^2/(mol*s)'),   
+        A = (1.6E19, 'cm^2/(mol*s)'),   #revised value from A=1.6E21 to A=1.6E19 base one the models
         n = 0.0,
         Ea = (191050.2, 'J/mol'), 
         Tmin = (200, 'K'),
@@ -366,7 +416,6 @@ Journal of Catalysis 383 (2020) 322–330
 https://doi.org/10.1016/j.jcat.2020.01.029
 
 This reaction used RMG's surface site density of Pd211 = 2.688E-9(mol/cm^2) to calculate the A factor.
-The A factor is calculated by equation (5) which assumed qTS/qIS = 1
 Ea = 1.98eV = 191050.2J/mol
 
 This is reaction (14) in Table S2
@@ -379,7 +428,7 @@ entry(
     index = 15,
     label = "N2O_X <=> N2O + X",
     kinetics = SurfaceArrhenius(
-        A = (1.69E13, '1/s'), 
+        A = (1.69E13, '1/s'), #This is the value calculated from the prefactor paper
         n = 0.0,
         Ea = (36666.2, 'J/mol'),  
         Tmin = (200, 'K'),
@@ -404,7 +453,7 @@ entry(
     index = 16,
     label = "NH3_X + X <=> NH2_X + H_X",
     kinetics = SurfaceArrhenius(
-        A = (2.06E23, 'cm^2/(mol*s)'), 
+        A = (3.52E20, 'cm^2/(mol*s)'), 
         n = 0.0,
         Ea = (97454.9, 'J/mol'),
         Tmin = (200, 'K'),
@@ -418,8 +467,8 @@ Journal of Catalysis 383 (2020) 322–330
 https://doi.org/10.1016/j.jcat.2020.01.029
 
 This reaction used RMG's surface site density of Pd211 = 2.688E-9(mol/cm^2) to calculate the A factor.
-Using the method proposed by Campbell et al. to calculate A actor.
 Ea = 1.01eV = 97454.9/mol
+
 This is reaction (1) in Table S4
 """,
     metal = "Pd",
@@ -430,7 +479,7 @@ entry(
     index = 17,
     label = "NH2_X + X <=> NH_X + H_X",
     kinetics = SurfaceArrhenius(
-        A = (2.23E23, 'cm^2/(mol*s)'), 
+        A = (3.19E21, 'cm^2/(mol*s)'), 
         n = 0.0,
         Ea = (152454.2, 'J/mol'),
         Tmin = (200, 'K'),
@@ -444,7 +493,6 @@ Journal of Catalysis 383 (2020) 322–330
 https://doi.org/10.1016/j.jcat.2020.01.029
 
 This reaction used RMG's surface site density of Pd211 = 2.688E-9(mol/cm^2) to calculate the A factor.
-Using the method proposed by Campbell et al. to calculate A actor.
 Ea = 1.58eV = 152454.2J/mol
 
 This is reaction (2) in Table S4
@@ -457,7 +505,7 @@ entry(
     index = 18,
     label = "NH_X + X <=> N_X + H_X",
     kinetics = SurfaceArrhenius(
-        A = (2.23E23, 'cm^2/(mol*s)'), 
+        A = (3.35E21, 'cm^2/(mol*s)'), 
         n = 0.0,
         Ea = (118682.7, 'J/mol'),
         Tmin = (200, 'K'),
@@ -471,7 +519,6 @@ Journal of Catalysis 383 (2020) 322–330
 https://doi.org/10.1016/j.jcat.2020.01.029
 
 This reaction used RMG's surface site density of Pd211 = 2.688E-9(mol/cm^2) to calculate the A factor.
-Using the method proposed by Campbell et al. to calculate A actor.
 Ea = 1.23eV = 118682.7J/mol
 
 This is reaction (3) in Table S4
@@ -484,7 +531,7 @@ entry(
     index = 19,
     label = "H_X + O_X <=> OH_X + X",
     kinetics = SurfaceArrhenius(
-        A = (2.44E21, 'cm^2/(mol*s)'), 
+        A = (1.18E21, 'cm^2/(mol*s)'), 
         n = 0.0,
         Ea = (123507.2, 'J/mol'),
         Tmin = (200, 'K'),
@@ -498,7 +545,6 @@ Journal of Catalysis 383 (2020) 322–330
 https://doi.org/10.1016/j.jcat.2020.01.029
 
 This reaction used RMG's surface site density of Pd211 = 2.688E-9(mol/cm^2) to calculate the A factor.
-The A factor is calculated by equation (5) which assumed qTS/qIS = 1
 Ea = 1.28eV = 123507.2J/mol
 
 This is reaction (4) in Table S4
@@ -511,7 +557,7 @@ entry(
     index = 20,
     label = "H_X + OH_X <=> H2O_X + X",
     kinetics = SurfaceArrhenius(
-        A = (2.44E21, 'cm^2/(mol*s)'), 
+        A = (1.88E21, 'cm^2/(mol*s)'), 
         n = 0.0,
         Ea = (91665.5, 'J/mol'),
         Tmin = (200, 'K'),
@@ -525,7 +571,6 @@ Journal of Catalysis 383 (2020) 322–330
 https://doi.org/10.1016/j.jcat.2020.01.029
 
 This reaction used RMG's surface site density of Pd211 = 2.688E-9(mol/cm^2) to calculate the A factor.
-The A factor is calculated by equation (5) which assumed qTS/qIS = 1
 Ea = 0.95eV = 91665.5J/mol
 
 This is reaction (5) in Table S4
