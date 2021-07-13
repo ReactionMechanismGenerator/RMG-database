425c0637ea14fcafda3d74b01b7ee84bc0fddb13
davidfarinajr <farina.d@northeastern.edu>
2021-07-13 17:51:17 -0400

    added 1,2_XY_interchange family

---
+++ input/kinetics/families/1,2_XY_interchange/groups.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "1,2_XY_interchange/groups"
+shortDesc = u""
+longDesc = u"""
+
+X1-R2-R3-Y4    ->    Y4-R2-R3-X1
+
+Y = F,Cl,Br,I
+X = F,Cl,Br,I,O
+"""
+
+template(reactants=["XY"], products=["YX"], ownReverse=True)
+
+reversible = True
+
+recipe(actions=[
+    ['BREAK_BOND', '*1', 1, '*2'],
+    ['BREAK_BOND', '*3', 1, '*4'],
+    ['FORM_BOND', '*1', 1, '*3'],
+    ['FORM_BOND', '*2', 1, '*4'],
+])
+
+entry(
+    index = 0,
+    label = "XY",
+    group = "OR{YY,OY}",
+    kinetics = None,
+)
+
+entry(
+    index = 1,
+    label = "YY",
+    group =
+"""
+1 *1 Val7   u0 {2,S}
+2 *2 Cs     u0 {1,S} {3,S}
+3 *3 Cs     u0 {2,S} {4,S}
+4 *4 Val7   u0 {3,S}
+""",
+    kinetics = None,
+)
+
+entry(
+    index = 2,
+    label = "FF",
+    group =
+"""
+1 *1 F1s   u0 {2,S}
+2 *2 Cs    u0 {1,S} {3,S}
+3 *3 Cs    u0 {2,S} {4,S}
+4 *4 F1s   u0 {3,S}
+""",
+    kinetics = None,
+)
+
+entry(
+    index = 3,
+    label = "ClCl",
+    group =
+"""
+1 *1 Cl1s  u0 {2,S}
+2 *2 Cs    u0 {1,S} {3,S}
+3 *3 Cs    u0 {2,S} {4,S}
+4 *4 Cl1s  u0 {3,S}
+""",
+    kinetics = None,
+)
+
+entry(
+    index = 4,
+    label = "BrBr",
+    group =
+"""
+1 *1 Br1s  u0 {2,S}
+2 *2 Cs    u0 {1,S} {3,S}
+3 *3 Cs    u0 {2,S} {4,S}
+4 *4 Br1s  u0 {3,S}
+""",
+    kinetics = None,
+)
+
+entry(
+    index = 5,
+    label = "FCl",
+    group =
+"""
+1 *1 F1s  u0 {2,S}
+2 *2 Cs   u0 {1,S} {3,S}
+3 *3 Cs   u0 {2,S} {4,S}
+4 *4 Cl1s u0 {3,S}
+""",
+    kinetics = None,
+)
+
+entry(
+    index = 6,
+    label = "FBr",
+    group =
+"""
+1 *1 F1s   u0 {2,S}
+2 *2 Cs  u0 {1,S} {3,S}
+3 *3 Cs  u0 {2,S} {4,S}
+4 *4 Br1s  u0 {3,S}
+""",
+    kinetics = None,
+)
+
+entry(
+    index = 7,
+    label = "ClBr",
+    group =
+"""
+1 *1 Cl1s  u0 {2,S}
+2 *2 Cs    u0 {1,S} {3,S}
+3 *3 Cs    u0 {2,S} {4,S}
+4 *4 Br1s  u0 {3,S}
+""",
+    kinetics = None,
+)
+
+entry(
+    index = 8,
+    label = "OY",
+    group =
+"""
+1 *1 O2s    u0 {2,S}
+2 *2 Cs     u0 {1,S} {3,S}
+3 *3 Cs     u0 {2,S} {4,S}
+4 *4 Val7   u0 {3,S}
+""",
+    kinetics = None,
+)
+
+entry(
+    index = 9,
+    label = "OF",
+    group =
+"""
+1 *1 O2s  u0 {2,S}
+2 *2 Cs   u0 {1,S} {3,S}
+3 *3 Cs   u0 {2,S} {4,S}
+4 *4 F1s  u0 {3,S}
+""",
+    kinetics = None,
+)
+
+
+entry(
+    index = 10,
+    label = "OCl",
+    group =
+"""
+1 *1 O2s  u0 {2,S}
+2 *2 Cs   u0 {1,S} {3,S}
+3 *3 Cs   u0 {2,S} {4,S}
+4 *4 Cl1s u0 {3,S}
+""",
+    kinetics = None,
+)
+
+
+entry(
+    index = 11,
+    label = "OBr",
+    group =
+"""
+1 *1 O2s  u0 {2,S}
+2 *2 Cs   u0 {1,S} {3,S}
+3 *3 Cs   u0 {2,S} {4,S}
+4 *4 Br1s u0 {3,S}
+""",
+    kinetics = None,
+)
+
+
+tree(
+"""
+L1: XY
+    L2: YY
+        L3: FF
+        L3: ClCl
+        L3: BrBr
+        L3: FCl
+        L3: FBr
+        L3: ClBr
+    L2: OY
+        L3: OF
+        L3: OBr
+        L3: OCl
+"""
+)
---
+++ input/kinetics/families/1,2_XY_interchange/rules.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "1,2_XY_interchange/rules"
+shortDesc = u""
+longDesc = u"""
+Sources:
+[1] Ju-Sung Kim, Laura M. Brandt, George L. Heard, and Bert E. Holmes.
+    Computational study of the threshold energy for the 1,2-interchange of X and R 
+    (X, R = halogens, pseudohalogens, and monovalent hydrocarbon groups) on CH2XCH2R. 
+    Canadian Journal of Chemistry. 94(12): 1038-1043. https://doi.org/10.1139/cjc-2016-0293
+
+    Table 1. Threshold energy barriers for interchange reactions (kJ/mol) calculated at 
+    the B3PW91/6-311+G(2d,p) level of theory and basis set
+"""
+
+entry(
+    index = 0,
+    label = "XY",
+    kinetics = ArrheniusEP(
+        A = (1e+10, 's^-1'),
+        n = 0,
+        alpha = 0,
+        E0 = (250, 'kJ/mol'),
+        Tmin = (300, 'K'),
+        Tmax = (1500, 'K'),
+    ),
+    rank = 0,
+    shortDesc = u"""default""",
+)
+
+entry(
+    index = 1,
+    label = "YY",
+    kinetics = ArrheniusEP(
+        A = (1e+10, 's^-1'),
+        n = 0,
+        alpha = 0,
+        E0 = (200, 'kJ/mol'),
+        Tmin = (300, 'K'),
+        Tmax = (1500, 'K'),
+    ),
+    rank = 0,
+    shortDesc = u"""default""",
+)
+
+entry(
+    index = 2,
+    label = "FF",
+    kinetics = ArrheniusEP(
+        A = (1e+10, 's^-1'),
+        n = 0,
+        alpha = 0,
+        E0 = (289, 'kJ/mol'),
+        Tmin = (300, 'K'),
+        Tmax = (1500, 'K'),
+    ),
+    rank = 0,
+    shortDesc = u"""E0 from Table 1 in [1]""",
+)
+
+entry(
+    index = 3,
+    label = "ClCl",
+    kinetics = ArrheniusEP(
+        A = (1e+10, 's^-1'),
+        n = 0,
+        alpha = 0,
+        E0 = (183, 'kJ/mol'),
+        Tmin = (300, 'K'),
+        Tmax = (1500, 'K'),
+    ),
+    rank = 0,
+    shortDesc = u"""E0 from Table 1 in [1]""",
+)
+
+entry(
+    index = 4,
+    label = "BrBr",
+    kinetics = ArrheniusEP(
+        A = (1e+10, 's^-1'),
+        n = 0,
+        alpha = 0,
+        E0 = (141, 'kJ/mol'),
+        Tmin = (300, 'K'),
+        Tmax = (1500, 'K'),
+    ),
+    rank = 0,
+    shortDesc = u"""E0 from Table 1 in [1]""",
+)
+
+entry(
+    index = 5,
+    label = "FCl",
+    kinetics = ArrheniusEP(
+        A = (1e+10, 's^-1'),
+        n = 0,
+        alpha = 0,
+        E0 = (236, 'kJ/mol'),
+        Tmin = (300, 'K'),
+        Tmax = (1500, 'K'),
+    ),
+    rank = 0,
+    shortDesc = u"""E0 from Table 1 in [1]""",
+)
+
+entry(
+    index = 6,
+    label = "ClBr",
+    kinetics = ArrheniusEP(
+        A = (1e+10, 's^-1'),
+        n = 0,
+        alpha = 0,
+        E0 = (162, 'kJ/mol'),
+        Tmin = (300, 'K'),
+        Tmax = (1500, 'K'),
+    ),
+    rank = 0,
+    shortDesc = u"""E0 from Table 1 in [1]""",
+)
+
+entry(
+    index = 7,
+    label = "FBr",
+    kinetics = ArrheniusEP(
+        A = (1e+10, 's^-1'),
+        n = 0,
+        alpha = 0,
+        E0 = (216, 'kJ/mol'),
+        Tmin = (300, 'K'),
+        Tmax = (1500, 'K'),
+    ),
+    rank = 0,
+    shortDesc = u"""E0 from Table 1 in [1]""",
+)
+
+entry(
+    index = 8,
+    label = "OY",
+    kinetics = ArrheniusEP(
+        A = (1e+10, 's^-1'),
+        n = 0,
+        alpha = 0,
+        E0 = (250, 'kJ/mol'),
+        Tmin = (300, 'K'),
+        Tmax = (1500, 'K'),
+    ),
+    rank = 0,
+    shortDesc = u"""default""",
+)
+
+entry(
+    index = 9,
+    label = "OF",
+    kinetics = ArrheniusEP(
+        A = (1e+10, 's^-1'),
+        n = 0,
+        alpha = 0,
+        E0 = (299, 'kJ/mol'),
+        Tmin = (300, 'K'),
+        Tmax = (1500, 'K'),
+    ),
+    rank = 0,
+    shortDesc = u"""E0 from Table 1 in [1]""",
+    longDesc = 
+u"""
+Average of OCH3 and OH
+""",
+)
+
+entry(
+    index = 10,
+    label = "OCl",
+    kinetics = ArrheniusEP(
+        A = (1e+10, 's^-1'),
+        n = 0,
+        alpha = 0,
+        E0 = (244.5, 'kJ/mol'),
+        Tmin = (300, 'K'),
+        Tmax = (1500, 'K'),
+    ),
+    rank = 0,
+    shortDesc = u"""E0 from Table 1 in [1]""",
+    longDesc = 
+u"""
+Average of OCH3 and OH
+""",
+)
+
+entry(
+    index = 11,
+    label = "OBr",
+    kinetics = ArrheniusEP(
+        A = (1e+10, 's^-1'),
+        n = 0,
+        alpha = 0,
+        E0 = (222.5, 'kJ/mol'),
+        Tmin = (300, 'K'),
+        Tmax = (1500, 'K'),
+    ),
+    rank = 0,
+    shortDesc = u"""E0 from Table 1 in [1]""",
+    longDesc = 
+u"""
+Average of OCH3 and OH
+""",
+)
