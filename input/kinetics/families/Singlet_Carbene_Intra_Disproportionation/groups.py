1e19fd479374efaae47f0006fbb0592ae1cfdd5c
davidfarinajr <farina.d@northeastern.edu>
2021-07-16 18:20:06 -0400

    prep `Singlet_Carbene_Intra_Disproportionation` for autotree gen

--- input/kinetics/families/Singlet_Carbene_Intra_Disproportionation/groups.py
+++ input/kinetics/families/Singlet_Carbene_Intra_Disproportionation/groups.py
@@ -10,8 +10,10 @@ Reaction site *1 should always be a singlet in this family.
 template(reactants=["singletcarbene_CH"], products=["CH_C_unsaturated"], ownReverse=False)
 
 reverse = "SingletCarbenefromMultipleBond"
-
+reactantNum = 1
+productNum = 1
 reversible = True
+
 recipe(actions=[
     ['LOSE_PAIR', '*1', '1'],
     ['FORM_BOND', '*1', 1, '*3'],
