cca8a57c36bf7d4053d86d82531ecfd490e911ef
Tingchenlee <lee.ting@northeastern.edu>
2021-07-23 19:07:20 -0400

    add training reactions from Popa_Rh111 library

--- input/kinetics/families/Surface_Abstraction_vdW/training/reactions.py
+++ input/kinetics/families/Surface_Abstraction_vdW/training/reactions.py
@@ -233,3 +233,90 @@ A (at 300K) = 5.1E11(1/s)/2.483E-9(mol/cm^2) = 2.05E20 cm^2/(mol*s)
     facet = "111",
 )
 
+entry(
+    index = 11,
+    label = "O* + H3NX <=> H2NX + OH_4*",
+    degeneracy = 3.0,
+    kinetics = SurfaceArrhenius(A=(6.4e+20,'cm^2/(mol*s)'), n=0, Ea=(92630.4,'J/mol'), T0=(1,'K'), Tmin=(200,'K'), Tmax=(3000,'K')),
+    rank = 3,
+    shortDesc = """Surface_Abstraction_vdW""",
+    longDesc = 
+"""
+Training reaction from kinetics library: Surface/Ammonia/Popa_Rh111
+Original entry: NH3_X +O_X <=> NH2_X + OH_X
+Based primarily on 
+"Density-functional theory study of NHx oxidation 
+and reverse reactions on the Rh (111) surface."
+C. Popa, R. A. van Santen, and A. P. J. JansenJ.
+Phys. Chem. C 2007, 111, 9839– 9852.
+https://doi.org/10.1021/jp071072g
+
+This reaction used RMG's surface site density of Rh111 = 2.656E-09(mol/cm^2) to calculate the A factor.
+A (at 300K)= 1.7E12(1/s)/2.656E-9(mol/cm^2) = 6.40E20 cm^2/(mol*s)
+
+Ea = 0.96eV = 92630.4J/mol
+
+This is reaction 2a. of TABLE 4.
+""",
+    metal = "Rh",
+    facet = "111",
+)
+
+entry(
+    index = 12,
+    label = "H2NX-2 + OH_2* <=> H2O* + HNX",
+    degeneracy = 1.0,
+    kinetics = SurfaceArrhenius(A=(1.5e+21,'cm^2/(mol*s)'), n=0, Ea=(13508.6,'J/mol'), T0=(1,'K'), Tmin=(200,'K'), Tmax=(3000,'K')),
+    rank = 3,
+    shortDesc = """Surface_Abstraction_vdW""",
+    longDesc = 
+"""
+Training reaction from kinetics library: Surface/Ammonia/Popa_Rh111
+Original entry: NH2_X + OH_X <=> NH_X + H2O_X
+Based primarily on 
+"Density-functional theory study of NHx oxidation 
+and reverse reactions on the Rh (111) surface."
+C. Popa, R. A. van Santen, and A. P. J. JansenJ.
+Phys. Chem. C 2007, 111, 9839– 9852.
+https://doi.org/10.1021/jp071072g
+
+This reaction used RMG's surface site density of Rh111 = 2.656E-09(mol/cm^2) to calculate the A factor.
+A (at 300K)= 3.98E12(1/s)/2.656E-9(mol/cm^2) = 1.50E21 cm^2/(mol*s)
+
+Ea = 0.14eV = 13508.6J/mol
+
+This is reaction 3a. of TABLE 5.
+""",
+    metal = "Rh",
+    facet = "111",
+)
+
+entry(
+    index = 13,
+    label = "HNX-2 + OH_2* <=> H2O* + NX",
+    degeneracy = 1.0,
+    kinetics = SurfaceArrhenius(A=(4.52e+20,'cm^2/(mol*s)'), n=0, Ea=(22192.7,'J/mol'), T0=(1,'K'), Tmin=(200,'K'), Tmax=(3000,'K')),
+    rank = 3,
+    shortDesc = """Surface_Abstraction_vdW""",
+    longDesc = 
+"""
+Training reaction from kinetics library: Surface/Ammonia/Popa_Rh111
+Original entry: NH_X + OH_X <=> N_X + H2O_X
+Based primarily on 
+"Density-functional theory study of NHx oxidation 
+and reverse reactions on the Rh (111) surface."
+C. Popa, R. A. van Santen, and A. P. J. JansenJ.
+Phys. Chem. C 2007, 111, 9839– 9852.
+https://doi.org/10.1021/jp071072g
+
+This reaction used RMG's surface site density of Rh111 = 2.656E-09(mol/cm^2) to calculate the A factor.
+A (at 300K)= 1.2E12(1/s)/2.656E-9(mol/cm^2) = 4.52E20 cm^2/(mol*s)
+
+Ea = 0.23eV = 22192.7J/mol
+
+This is reaction 6a. of TABLE 5.
+""",
+    metal = "Rh",
+    facet = "111",
+)
+
