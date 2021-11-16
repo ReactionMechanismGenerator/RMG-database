c7b8f53e66e415f0f0d29622b4f873090be30e41
Xiaorui Dong <xiaorui@mit.edu>
2021-11-16 01:59:06 -0500

    Add training from alkene_chemistry to intra H mig

--- input/kinetics/families/intra_H_migration/training/reactions.py
+++ input/kinetics/families/intra_H_migration/training/reactions.py
@@ -9123,3 +9123,185 @@ Species are optimized and calculated by the CBS-QB3 method, no hindered rotors c
 """,
 )
 
+entry(
+    index = 440,
+    label = "C5H11O2-19 <=> C5H11O2-20",
+    degeneracy = 3.0,
+    kinetics = Arrhenius(A=(1180,'s^-1'), n=2.5, Ea=(19.073,'kcal/mol'), T0=(1,'K'), Tmin=(400,'K'), Tmax=(1500,'K')),
+    rank = 3,
+    shortDesc = """calculated at QCISD(T)/CBS//B3LYP/6-311++G(d,p) 1DHR from [4] by Ye et al.""",
+    longDesc = 
+"""
+Training reaction from kinetics library: alkene_chemistry
+Original entry: iC5H11OOJ-1 <=> CCC([CH2])COO
+""",
+)
+
+entry(
+    index = 441,
+    label = "C5H11O2-21 <=> C5H11O2-22",
+    degeneracy = 1.0,
+    kinetics = Arrhenius(A=(8140,'s^-1'), n=2.34, Ea=(24.345,'kcal/mol'), T0=(1,'K'), Tmin=(400,'K'), Tmax=(1500,'K')),
+    rank = 3,
+    shortDesc = """calculated at QCISD(T)/CBS//B3LYP/6-311++G(d,p) 1DHR from [4] by Ye et al.""",
+    longDesc = 
+"""
+Training reaction from kinetics library: alkene_chemistry
+Original entry: iC5H11OOJ-1 <=> CC[C](C)COO
+""",
+)
+
+entry(
+    index = 442,
+    label = "C5H11O2-23 <=> C5H11O2-24",
+    degeneracy = 2.0,
+    kinetics = Arrhenius(A=(2810,'s^-1'), n=2.22, Ea=(16.431,'kcal/mol'), T0=(1,'K'), Tmin=(400,'K'), Tmax=(1500,'K')),
+    rank = 3,
+    shortDesc = """calculated at QCISD(T)/CBS//B3LYP/6-311++G(d,p) 1DHR from [4] by Ye et al.""",
+    longDesc = 
+"""
+Training reaction from kinetics library: alkene_chemistry
+Original entry: iC5H11OOJ-1 <=> C[CH]C(C)COO
+""",
+)
+
+entry(
+    index = 443,
+    label = "C5H11O2-25 <=> C5H11O2-26",
+    degeneracy = 3.0,
+    kinetics = Arrhenius(A=(3570,'s^-1'), n=2.66, Ea=(18.496,'kcal/mol'), T0=(1,'K'), Tmin=(400,'K'), Tmax=(1500,'K')),
+    rank = 3,
+    shortDesc = """calculated at QCISD(T)/CBS//B3LYP/6-311++G(d,p) 1DHR from [4] by Ye et al.""",
+    longDesc = 
+"""
+Training reaction from kinetics library: alkene_chemistry
+Original entry: iC5H11OOJ-1 <=> [CH2]CC(C)COO
+""",
+)
+
+entry(
+    index = 444,
+    label = "C5H11O2-27 <=> C5H11O2-28",
+    degeneracy = 2.0,
+    kinetics = Arrhenius(A=(4790,'s^-1'), n=2.6, Ea=(27.506,'kcal/mol'), T0=(1,'K'), Tmin=(400,'K'), Tmax=(1500,'K')),
+    rank = 3,
+    shortDesc = """calculated at QCISD(T)/CBS//B3LYP/6-311++G(d,p) 1DHR from [4] by Ye et al.""",
+    longDesc = 
+"""
+Training reaction from kinetics library: alkene_chemistry
+Original entry: C2CCCOO <=> CC(C)[CH]COO
+""",
+)
+
+entry(
+    index = 445,
+    label = "C5H11O2-13 <=> C5H11O2-29",
+    degeneracy = 1.0,
+    kinetics = Arrhenius(A=(31400,'s^-1'), n=2.08, Ea=(15.407,'kcal/mol'), T0=(1,'K'), Tmin=(400,'K'), Tmax=(1500,'K')),
+    rank = 3,
+    shortDesc = """calculated at QCISD(T)/CBS//B3LYP/6-311++G(d,p) 1DHR from [4] by Ye et al.""",
+    longDesc = 
+"""
+Training reaction from kinetics library: alkene_chemistry
+Original entry: C2CCCOO <=> C[C](C)CCOO
+""",
+)
+
+entry(
+    index = 446,
+    label = "C5H11O2-30 <=> C5H11O2-31",
+    degeneracy = 6.0,
+    kinetics = Arrhenius(A=(573,'s^-1'), n=2.47, Ea=(19.74,'kcal/mol'), T0=(1,'K'), Tmin=(400,'K'), Tmax=(1500,'K')),
+    rank = 3,
+    shortDesc = """calculated at QCISD(T)/CBS//B3LYP/6-311++G(d,p) 1DHR from [4] by Ye et al.""",
+    longDesc = 
+"""
+Training reaction from kinetics library: alkene_chemistry
+Original entry: C2CCCOO <=> [CH2]C(C)CCOO
+""",
+)
+
+entry(
+    index = 447,
+    label = "C5H11O2-32 <=> C5H11O2-33",
+    degeneracy = 3.0,
+    kinetics = Arrhenius(A=(12600,'s^-1'), n=2.6, Ea=(29.755,'kcal/mol'), T0=(1,'K'), Tmin=(400,'K'), Tmax=(1500,'K')),
+    rank = 3,
+    shortDesc = """calculated at QCISD(T)/CBS//B3LYP/6-311++G(d,p) 1DHR from [4] by Ye et al.""",
+    longDesc = 
+"""
+Training reaction from kinetics library: alkene_chemistry
+Original entry: iC5H11OOJ-3 <=> CC(C)C([CH2])OO
+""",
+)
+
+entry(
+    index = 448,
+    label = "C5H11O2-5 <=> C5H11O2-34",
+    degeneracy = 1.0,
+    kinetics = Arrhenius(A=(67200,'s^-1'), n=2.21, Ea=(25.647,'kcal/mol'), T0=(1,'K'), Tmin=(400,'K'), Tmax=(1500,'K')),
+    rank = 3,
+    shortDesc = """calculated at QCISD(T)/CBS//B3LYP/6-311++G(d,p) 1DHR from [4] by Ye et al.""",
+    longDesc = 
+"""
+Training reaction from kinetics library: alkene_chemistry
+Original entry: iC5H11OOJ-3 <=> C[C](C)C(C)OO
+""",
+)
+
+entry(
+    index = 449,
+    label = "C5H11O2-35 <=> C5H11O2-36",
+    degeneracy = 6.0,
+    kinetics = Arrhenius(A=(253000,'s^-1'), n=1.95, Ea=(20.961,'kcal/mol'), T0=(1,'K'), Tmin=(400,'K'), Tmax=(1500,'K')),
+    rank = 3,
+    shortDesc = """calculated at QCISD(T)/CBS//B3LYP/6-311++G(d,p) 1DHR from [4] by Ye et al.""",
+    longDesc = 
+"""
+Training reaction from kinetics library: alkene_chemistry
+Original entry: iC5H11OOJ-3 <=> [CH2]C(C)C(C)OO
+""",
+)
+
+entry(
+    index = 450,
+    label = "C5H11O2-37 <=> C5H11O2-38",
+    degeneracy = 6.0,
+    kinetics = Arrhenius(A=(152,'s^-1'), n=2.99, Ea=(28.116,'kcal/mol'), T0=(1,'K'), Tmin=(400,'K'), Tmax=(1500,'K')),
+    rank = 3,
+    shortDesc = """calculated at QCISD(T)/CBS//B3LYP/6-311++G(d,p) 1DHR from [4] by Ye et al.""",
+    longDesc = 
+"""
+Training reaction from kinetics library: alkene_chemistry
+Original entry: iC5H11OOJ-2 <=> CCC(C)([CH2])OO
+""",
+)
+
+entry(
+    index = 451,
+    label = "C5H11O2-7 <=> C5H11O2-39",
+    degeneracy = 3.0,
+    kinetics = Arrhenius(A=(2240,'s^-1'), n=2.4, Ea=(18.43,'kcal/mol'), T0=(1,'K'), Tmin=(400,'K'), Tmax=(1500,'K')),
+    rank = 3,
+    shortDesc = """calculated at QCISD(T)/CBS//B3LYP/6-311++G(d,p) 1DHR from [4] by Ye et al.""",
+    longDesc = 
+"""
+Training reaction from kinetics library: alkene_chemistry
+Original entry: iC5H11OOJ-2 <=> [CH2]CC(C)(C)OO
+""",
+)
+
+entry(
+    index = 452,
+    label = "C5H11O2-3 <=> C5H11O2-40",
+    degeneracy = 2.0,
+    kinetics = Arrhenius(A=(236,'s^-1'), n=2.8, Ea=(24.854,'kcal/mol'), T0=(1,'K'), Tmin=(400,'K'), Tmax=(1500,'K')),
+    rank = 3,
+    shortDesc = """calculated at QCISD(T)/CBS//B3LYP/6-311++G(d,p) 1DHR from [4] by Ye et al.""",
+    longDesc = 
+"""
+Training reaction from kinetics library: alkene_chemistry
+Original entry: iC5H11OOJ-2 <=> C[CH]C(C)(C)OO
+""",
+)
+
