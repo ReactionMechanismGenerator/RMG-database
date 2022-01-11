cc7ef729baf0636f2aa1bf93e62c65d7f747a4dc
Matt Johnson <mjohnson541@gmail.com>
2022-01-11 14:57:33 -0500

    bulk tree generation commit

--- input/kinetics/families/Li_Abstraction/training/reactions.py
+++ input/kinetics/families/Li_Abstraction/training/reactions.py
@@ -2,13 +2,13 @@
 # encoding: utf-8
 
 name = "Li_Abstraction/training"
-shortDesc = u"Reaction kinetics used to generate rate rules"
-longDesc = u"""
+shortDesc = "Reaction kinetics used to generate rate rules"
+longDesc = """
 Put kinetic parameters for specific reactions in this file to use as a
 training set for generating rate rules to populate this kinetics family.
 """
 entry(
-    index = 1,
+    index = 0,
     label = "FLi + CH3 <=> CH3F + Li",
     degeneracy = 1.0,
     kinetics = Arrhenius(A=(4.14134e+06,'cm^3/(mol*s)'), n=2.18948, Ea=(140.779,'kJ/mol'), T0=(1,'K'), Tmin=(300,'K'), Tmax=(3000,'K'), solute=SoluteData(S=1.8681935378213759,B=2.373498032935028,E=0.32859248603303737,L=5.004153890392606,A=0.8849787213003473,comment='')),
@@ -46,7 +46,7 @@ ccsd(t)-f12/cc-pvdz-f12//b3lyp/6-311++G(d,p)used COSMO TZPD-Fine with energy fil
 )
 
 entry(
-    index = 2,
+    index = 1,
     label = "ClLi + CH3 <=> CH3Cl + Li",
     degeneracy = 1.0,
     kinetics = Arrhenius(A=(1.57736e+07,'cm^3/(mol*s)'), n=1.91726, Ea=(119.96,'kJ/mol'), T0=(1,'K'), Tmin=(300,'K'), Tmax=(3000,'K'), solute=SoluteData(S=2.40096476096442,B=0.7611537028291462,E=0.36695032193123533,L=5.182274882516309,A=0.578711830957878,comment='')),
