fbdbc4e0b1c8aaac79e69e887c36e1efd607dbae
kblondal <katrin_blondal@brown.edu>
2022-03-10 13:48:50 -0500

    Added Surface_Dissociation_to_Bidentate family
    and three training reactions belonging to it.

---
+++ input/kinetics/families/Surface_Dissociation_to_Bidentate/rules.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "Surface_Dissociation_to_Bidentate/rules"
+shortDesc = u""
+longDesc = u"""
+"""
+
+entry(
+    index = 1,
+    label = "Combined;VacantSite1;VacantSite2",
+    kinetics = SurfaceArrheniusBEP(
+        A = (2.528E19, 'm^4/(mol^2*s)'), 
+        n = 0.698,  
+        alpha = 0.075, 
+        E0 = (11.12, 'kcal/mol'), 
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
+    ),
+    rank = 0,
+    shortDesc = u"""Default""",
+    longDesc = u"""
+A and n factors are from the average rates of training reactions 1-3, and alpha and E0 are BEP
+parameters from training reactions 1-3 (in the Surface_Dissociation_to_Bidentate 
+forward direction).
+The A factor has been divided by 2.33 here to account for the average degeneracy of the training reactions.
+"""
+)
