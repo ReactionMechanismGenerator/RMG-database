c7492de66c407936ffd5fe0cf2450b7474845ae4
Alon Grinberg Dana <alongd@mit.edu>
2022-06-11 22:05:30 +0300

    change amino_to_amide+hydheyde fam

--- input/kinetics/families/imine_to_hemiaminal/groups.py
+++ input/kinetics/families/imine_to_hemiaminal/groups.py
@@ -7,10 +7,10 @@ longDesc = u"""
 This family describes solution-phase hydrolysis of amine groups:
 R=NR' + H2O <=> H + RN(OH)R'
 atom labels:
-R[*1]=N[*2]R' + H[*4]O[*3]H <=> H[*4] + RC[*1](OH[*3])N[*2]R'
+RC[*1]=N[*2]R' + H[*4]O[*3]H <=>  RC[*1](OH[*3])N[*2](H[*4])R'
 """
 
-template(reactants=["CdN", "H2O"], products=["CdNOH", "H"], ownReverse=False)
+template(reactants=["CdN", "H2O"], products=["CdNOH"], ownReverse=False)
 
 reverse = "hydroxylamine_to_imine"
 
@@ -22,7 +22,7 @@ recipe(actions=[
     ['CHANGE_BOND', '*1', -1, '*2'],
     ['BREAK_BOND', '*3', 1, '*4'],
     ['FORM_BOND', '*1', 1, '*3'],
-    ['GAIN_RADICAL', '*4' , '1'],
+    ['FORM_BOND', '*4', 1, '*2'],
 ])
 
 entry(
@@ -30,8 +30,11 @@ entry(
     label="CdN",
     group=
 """
-1 *1 C ux px cx {2,D} 
-2 *2 N u0 p1 c0 {1,D}
+1    R ux px cx {2,S} 
+2 *1 C u0 p0 c0 {1,S} {3,D} {5,S}
+3 *2 N u0 p1 c0 {2,D} {4,S}
+4    R ux px cx {3,S}
+5    H u0 p0 c0 {2,S}
 """,
     kinetics=None,
 )
