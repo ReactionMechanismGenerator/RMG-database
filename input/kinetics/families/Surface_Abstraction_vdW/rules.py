a1477b880927e86660689d8bf1a9dc0ae794e709
Chris B <56306881+ChrisBNEU@users.noreply.github.com>
2022-10-03 14:57:45 -0400

    updating BEP E0 and alpha from Fajin et al paper

--- input/kinetics/families/Surface_Abstraction_vdW/rules.py
+++ input/kinetics/families/Surface_Abstraction_vdW/rules.py
@@ -14,12 +14,16 @@ entry(
     kinetics = SurfaceArrheniusBEP(
         A = (1.0e13, 'm^2/(mol*s)'),
         n = 0,
-        alpha = 0.5,
-        E0 = (0, 'kcal/mol'),
+        alpha = 0.57,
+        E0 = (15.62, 'kcal/mol'),
         Tmin = (200, 'K'),
         Tmax = (3000, 'K'),
     ),
     rank = 0,
-    shortDesc = u"""Default""",
-    longDesc = u"""Made up"""
+    shortDesc = u"""source from http://dx.doi.org/10.1021/acs.jctc.6b00168""",
+    longDesc = u"""adapted from table 3 in 'Effect of the Exchange-Correlation Potential on the Transferability of 
+    Brønsted–Evans–Polanyi Relationships in Heterogeneous Catalysis'
+    This is for H dissociationn, so it is more appropriate when another group is being abstracted to an empty site. nevertheless, we'll use it for
+    this and the surface abstraction single vdw family in lieu of a better estimate. 
+    """
 )
