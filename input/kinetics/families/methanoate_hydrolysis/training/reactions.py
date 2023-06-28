cbd253a390cedb13f8260daadc76e6b880fa0c52
Alon Grinberg Dana <alon@technion.ac.il>
2023-06-28 10:26:59 +0300

    f! family

--- input/kinetics/families/methanoate_hydrolysis/training/reactions.py
+++ input/kinetics/families/methanoate_hydrolysis/training/reactions.py
@@ -7,3 +7,16 @@ longDesc = u"""
 Put kinetic parameters for specific reactions in this file to use as a
 training set for generating rate rules to populate this kinetics family.
 """
+
+entry(
+    index = 1,
+    label = "BNX + H2O <=> P1 + P2",
+    degeneracy = 1.0,
+    kinetics = Arrhenius(A=(1e+6, 'cm^3/(mol*s)'), n=0, Ea=(10, 'kcal/mol')),
+    rank = 5,
+    shortDesc = u"""est.""",
+    longDesc =
+u"""
+
+""",
+)
