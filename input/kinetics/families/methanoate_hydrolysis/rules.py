146c793a43f626542ecead1b1a03cd787714dbd9
Alon Grinberg Dana <alon@technion.ac.il>
2023-07-05 21:46:17 +0300

    tmp

--- input/kinetics/families/methanoate_hydrolysis/rules.py
+++ input/kinetics/families/methanoate_hydrolysis/rules.py
@@ -10,8 +10,7 @@ longDesc = u"""
 entry(
     index=0,
     label="methanoate;H2O",
-    kinetics=Arrhenius(A=(2.07329e-08, 'cm^3/(mol*s)'), n=5.04178, Ea=(166.415, 'kJ/mol'),
-                       T0=(1, 'K'), Tmin=(290, 'K'), Tmax=(2000, 'K')),
+    kinetics=ArrheniusEP(A=(1.7e+13, 'cm^3/(mol*s)'), n=0, alpha=0, E0=(25, 'kcal/mol')),
     rank=0,
     shortDesc=u"""Default""",
 )
--- input/kinetics/families/methanoate_hydrolysis/training/reactions.py
+++ input/kinetics/families/methanoate_hydrolysis/training/reactions.py
@@ -10,17 +10,13 @@ training set for generating rate rules to populate this kinetics family.
 
 entry(
     index = 1,
-    label="BNX + H2O <=> P1 + P2",
-    degeneracy=1.0,
-    kinetics=Arrhenius(A=(2.07329e-08, 'cm^3/(mol*s)'), n=5.04178, Ea=(166.415, 'kJ/mol'),
-                       T0=(1, 'K'), Tmin=(290, 'K'), Tmax=(2000, 'K'),
-                       comment="""Fitted to 15 points; dA = *|/ 76.452, dn = +|- 0.574177, dEa = +|- 2.95278 kJ/mol"""),
-    rank=5,
-    shortDesc=u"""DFT""",
-    longDesc=
-    u"""
-    xdeg1018b
-    wb97xd/def2tzvp SMD water
-    computed for a fragment CC(=O)OCCN, no rotors
-    """,
+    label = "BNX + H2O <=> P1 + P2",
+    degeneracy = 1.0,
+    kinetics = Arrhenius(A=(1.7e+13, 'cm^3/(mol*s)'), n=0, Ea=(25, 'kcal/mol')),
+    rank = 5,
+    shortDesc = u"""est.""",
+    longDesc =
+u"""
+
+""",
 )
