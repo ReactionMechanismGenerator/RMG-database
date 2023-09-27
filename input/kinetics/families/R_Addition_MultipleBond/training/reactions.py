274cdbf42fd4a7d3cc4a08eac7166a684d22472a
NellyMitnik <nellymitnik@gmail.com>
2023-09-27 10:31:26 +0300

    added training sets rxn of R_addition_MultipleBond

--- input/kinetics/families/R_Addition_MultipleBond/training/reactions.py
+++ input/kinetics/families/R_Addition_MultipleBond/training/reactions.py
@@ -59545,3 +59545,35 @@ Electronic structures: Gaussian 16, kinetic constants: MESS
 """,
 )
 
+entry(
+    index = 2956,
+    label = "C5H7O2_W1_rad <=> CH2O_rad + furan_1",
+    degeneracy = 1.0,
+    kinetics = Arrhenius(
+        A = (4.06e+13,'s^-1'),
+        n = 0.11,
+        Ea = (30694,'cal/mol'),
+        T0 = (1, 'K'),
+        Tmin=(298,'K'),
+        Tmax=(2500,'K')
+    ),
+    reference = Article(
+        authors = ["'L. Xing'", "'Y. He'", "'J. Wang'", "'L. Lian'", "'Z. cheng'", "'X. Wang'", "'M. Liu'"],
+        title = 'The reactions of 2-furfuryl alcohol with hydrogen atom: A theoretical calculation and kinetic modeling analysis',
+        journal = "'Combustion and Flame'",
+        volume = "'250'",
+        pages = "'112627'",
+        year = "'2023'",
+        url = 'https://linkinghub.elsevier.com/retrieve/pii/S0010218023000123',
+    ),
+    referenceType = "theory",
+    rank = 5,
+    shortDesc = u"""CCSD(T)/CBS//M06–2X/def2-TZVP level""",
+    longDesc =
+u"""
+Quantum chemistry calculations at the CCSD(T)/CBS//M06–2X/def2-TZVP level.
+Table 3 high pressure limit rates (0.01, 0.1 1 and 10 atm) : FFOH + H <=> W1
+Electronic structures: Gaussian 16, kinetic constants: MESS
+""",
+)
+
