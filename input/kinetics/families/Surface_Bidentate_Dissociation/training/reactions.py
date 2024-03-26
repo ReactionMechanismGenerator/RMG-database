402aea74425c2b2e4661935f832c3618330cfc93
Bjarne Kreitz <bjarne_kreitz@brown.edu>
2024-03-26 17:20:46 -0400

    update bidentate dissociation

--- input/kinetics/families/Surface_Bidentate_Dissociation/training/reactions.py
+++ input/kinetics/families/Surface_Bidentate_Dissociation/training/reactions.py
@@ -8,31 +8,24 @@ Put kinetic parameters for specific reactions in this file to use as a
 training set for generating rate rules to populate this kinetics family.
 """
 
-#!/usr/bin/env python
-# encoding: utf-8
-
-name = "Surface_Bidentate_Dissociation/training"
-shortDesc = u"Reaction kinetics used to generate rate rules"
-longDesc = u"""
-Put kinetic parameters for specific reactions in this file to use as a
-training set for generating rate rules to populate this kinetics family.
-"""
-
 entry(
     index = 1,
     label = "CC_2X <=> CX_3 + CX_4  ",
     degeneracy = 1,
     kinetics = SurfaceArrhenius(
-        A=(2.202E12, '1/s'), 
-        n = 0.09, 
-        Ea=(103497.2, 'J/mol'),
+        A=(4.22E12, '1/s'),
+        n = 0.0,
+        Ea=(104, 'kJ/mol'),
         Tmin = (298, 'K'),
         Tmax = (2000, 'K'),
     ),
     rank=8,
     shortDesc = u"""Default""",
     longDesc = u"""
-Calculated with DFT by Katrín Blöndal at Brown University, using the vdW-DF-cx functional in Quantum ESPRESSO. 
+Calculated with DFT by Katrín Blöndal and Bjarne Kreitz at Brown University, using the vdW-DF-cx functional in Quantum ESPRESSO. 
+Details on the computational method to derive the rate constants are provided in "Automatic mechanism generation involving 
+kinetics of surface reactions with bidentate adsorbates" by B. Kreitz, K. Blöndal, K. Badger, R. H. West and C. F. Goldsmith, Digital Discovery, 2024, 3, 173
+doi:10.1039/d3dd00184a
 """,
     metal = "Pt",
 )
@@ -42,16 +35,19 @@ entry(
     label = "CCH_2X <=> CX_3 + CHX_4",
     degeneracy = 1,
     kinetics = SurfaceArrhenius(
-        A=(5.272E11, '1/s'), 
+        A=(1.3E12, '1/s'),
         n = 0.126, 
-        Ea=(76699.8, 'J/mol'),
+        Ea=(77, 'kJ/mol'),
         Tmin = (298, 'K'),
         Tmax = (2000, 'K'),
     ),
     rank=8,
     shortDesc = u"""Default""",
     longDesc = u"""
-Calculated with DFT by Katrín Blöndal at Brown University, using the vdW-DF-cx functional in Quantum ESPRESSO. 
+Calculated with DFT by Katrín Blöndal and Bjarne Kreitz at Brown University, using the vdW-DF-cx functional in Quantum ESPRESSO. 
+Details on the computational method to derive the rate constants are provided in "Automatic mechanism generation involving 
+kinetics of surface reactions with bidentate adsorbates" by B. Kreitz, K. Blöndal, K. Badger, R. H. West and C. F. Goldsmith, Digital Discovery, 2024, 3, 173
+doi:10.1039/d3dd00184a 
 """,
     metal = "Pt",
 )
@@ -61,35 +57,41 @@ entry(
     label = "HCCH_2X <=> CHX_3 + CHX_4",
     degeneracy = 1,
     kinetics = SurfaceArrhenius(
-        A=(7.062E11, '1/s'), 
-        n = 0.320, 
-        Ea=(88220.0, 'J/mol'),
-        Tmin = (298, 'K'),
-        Tmax = (2000, 'K'),
-    ),
-    rank=10,
-    shortDesc = u"""Default""",
-    longDesc = u"""
-Calculated with DFT by Katrín Blöndal at Brown University, using the vdW-DF-cx functional in Quantum ESPRESSO. 
-""",
-    metal = "Pt",
-)
-
-entry( 
-    index = 4,
-    label = "CHX_3 + CH2X_4 <=> HCCH2_2X",
-    degeneracy = 1,
-    kinetics = SurfaceArrhenius(
-        A=(4.256E17, 'm^2/(mol*s)'),
-        n = 0.106, 
-        Ea=(138024.4, 'J/mol'),
+        A=(7.93E12, '1/s'),
+        n = 0.0,
+        Ea=(90, 'kJ/mol'),
         Tmin = (298, 'K'),
         Tmax = (2000, 'K'),
     ),
     rank=8,
     shortDesc = u"""Default""",
     longDesc = u"""
-Calculated with DFT by Katrín Blöndal at Brown University, using the vdW-DF-cx functional in Quantum ESPRESSO.
+Calculated with DFT by Katrín Blöndal and Bjarne Kreitz at Brown University, using the vdW-DF-cx functional in Quantum ESPRESSO. 
+Details on the computational method to derive the rate constants are provided in "Automatic mechanism generation involving 
+kinetics of surface reactions with bidentate adsorbates" by B. Kreitz, K. Blöndal, K. Badger, R. H. West and C. F. Goldsmith, Digital Discovery, 2024, 3, 173
+doi:10.1039/d3dd00184a
+""",
+    metal = "Pt",
+)
+
+entry( 
+    index = 4,
+    label = "HCCH2_2X <=> CHX_3 + CH2X_4",
+    degeneracy = 1,
+    kinetics = SurfaceArrhenius(
+        A=(2.74E13, '1/s'),
+        n = 0.0,
+        Ea=(140, 'kJ/mol'),
+        Tmin = (298, 'K'),
+        Tmax = (2000, 'K'),
+    ),
+    rank=8,
+    shortDesc = u"""Default""",
+    longDesc = u"""
+Calculated with DFT by Katrín Blöndal and Bjarne Kreitz at Brown University, using the vdW-DF-cx functional in Quantum ESPRESSO. 
+Details on the computational method to derive the rate constants are provided in "Automatic mechanism generation involving 
+kinetics of surface reactions with bidentate adsorbates" by B. Kreitz, K. Blöndal, K. Badger, R. H. West and C. F. Goldsmith, Digital Discovery, 2024, 3, 173
+doi:10.1039/d3dd00184a
 """,
     metal = "Pt",
 )
@@ -99,16 +101,19 @@ entry(
     label = "CH2X_3 + CH2X_4 <=> H2CCH2_2X",
     degeneracy = 1,
     kinetics = SurfaceArrhenius(
-        A=(1.161E19, 'm^2/(mol*s)'),
-        n = 0.281, 
-        Ea=(152439.3, 'J/mol'),
+        A=(9.89E23, 'cm^2/(mol*s)'),
+        n = 0.0,
+        Ea=(154, 'kJ/mol'),
         Tmin = (298, 'K'),
         Tmax = (2000, 'K'),
     ),
     rank=8,
     shortDesc = u"""Default""",
     longDesc = u"""
-Calculated with DFT by Katrín Blöndal at Brown University, using the vdW-DF-cx functional in Quantum ESPRESSO.
+Calculated with DFT by Katrín Blöndal and Bjarne Kreitz at Brown University, using the vdW-DF-cx functional in Quantum ESPRESSO. 
+Details on the computational method to derive the rate constants are provided in "Automatic mechanism generation involving 
+kinetics of surface reactions with bidentate adsorbates" by B. Kreitz, K. Blöndal, K. Badger, R. H. West and C. F. Goldsmith, Digital Discovery, 2024, 3, 173
+doi:10.1039/d3dd00184a
 """,
     metal = "Pt",
 )
@@ -118,16 +123,19 @@ entry(
     label = "CHX_3 + OX_4 <=> HCO_2X",
     degeneracy = 1,
     kinetics = SurfaceArrhenius(
-        A=(6.922E17, 'm^2/(mol*s)'),
-        n = 0.049, 
-        Ea=(142325.0, 'J/mol'),
+        A=(6.54E21, 'cm^2/(mol*s)'),
+        n = 0.0,
+        Ea=(142, 'kJ/mol'),
         Tmin = (298, 'K'),
         Tmax = (2000, 'K'),
     ),
     rank=8,
     shortDesc = u"""Default""",
     longDesc = u"""
-Calculated with DFT by Katrín Blöndal at Brown University, using the vdW-DF-cx functional in Quantum ESPRESSO.
+Calculated with DFT by Katrín Blöndal and Bjarne Kreitz at Brown University, using the vdW-DF-cx functional in Quantum ESPRESSO. 
+Details on the computational method to derive the rate constants are provided in "Automatic mechanism generation involving 
+kinetics of surface reactions with bidentate adsorbates" by B. Kreitz, K. Blöndal, K. Badger, R. H. West and C. F. Goldsmith, Digital Discovery, 2024, 3, 173
+doi:10.1039/d3dd00184a
 """,
     metal = "Pt",
 )
@@ -137,16 +145,19 @@ entry(
     label = "CH2X_3 + OX_4 <=> H2CO_2X",
     degeneracy = 1,
     kinetics = SurfaceArrhenius(
-        A=(1.174E18, 'm^2/(mol*s)'),
-        n = 0.082, 
-        Ea=(114251.7, 'J/mol'),
+        A=(2.2E22, 'cm^2/(mol*s)'),
+        n = 0.0,
+        Ea=(115, 'kJ/mol'),
         Tmin = (298, 'K'),
         Tmax = (2000, 'K'),
     ),
     rank=8,
     shortDesc = u"""Default""",
     longDesc = u"""
-Calculated with DFT by Katrín Blöndal at Brown University, using the vdW-DF-cx functional in Quantum ESPRESSO.
+Calculated with DFT by Katrín Blöndal and Bjarne Kreitz at Brown University, using the vdW-DF-cx functional in Quantum ESPRESSO. 
+Details on the computational method to derive the rate constants are provided in "Automatic mechanism generation involving 
+kinetics of surface reactions with bidentate adsorbates" by B. Kreitz, K. Blöndal, K. Badger, R. H. West and C. F. Goldsmith, Digital Discovery, 2024, 3, 173
+doi:10.1039/d3dd00184a
 """,
     metal = "Pt",
 )
