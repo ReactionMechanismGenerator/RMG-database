67565014c47eb036f7d1886a29de042c83246226
ssun30 <sun.su@northeastern.edu>
2024-04-11 15:40:58 -0400

    Enabled surface proton electron reduction families

--- input/kinetics/families/recommended.py
+++ input/kinetics/families/recommended.py
@@ -154,11 +154,11 @@ surface_development = {
 
 # Electrochemical families!
 electrochem = {
-    # 'Surface_Proton_Electron_Reduction_Alpha',
+    'Surface_Proton_Electron_Reduction_Alpha',
     # 'Surface_Proton_Electron_Reduction_Alpha_vdW',
-    # 'Surface_Proton_Electron_Reduction_Beta',
+    'Surface_Proton_Electron_Reduction_Beta',
     # 'Surface_Proton_Electron_Reduction_Beta_vdW',
-    # 'Surface_Proton_Electron_Reduction_Beta_Dissociation',
+    'Surface_Proton_Electron_Reduction_Beta_Dissociation',
     '1,2_Elimination_LiR',
     '1,2_Intra_Elimination_LiR',
     'Cation_Addition_MultipleBond',
