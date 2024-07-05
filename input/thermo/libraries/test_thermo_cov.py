1146d0d3f43814e5b17ca8d5ed3e326d575c41d3
12Chao <xu.chao@northeastern.edu>
2024-07-05 12:31:43 -0400

    Add in the cov-dep thermo paraemter for CO on Ni

--- input/thermo/libraries/test_thermo_cov.py
+++ input/thermo/libraries/test_thermo_cov.py
@@ -26,36 +26,36 @@ entry(
     facet = "111",
 )
 
-entry(
-    index = 2,
-    label = "O_ads",
-    molecule =
-"""
-1 X  u0 p0 c0 {2,D}
-2 O  u0 p2 c0 {1,D}
-""",
-    thermo = NASA(
-        polynomials = [
-            NASAPolynomial(coeffs=[1.79722382E-01, 1.25453156E-02, -2.29924588E-05, 1.94187177E-08, -6.22414099E-12, -1.73402246E+04, -2.22409728E+00], Tmin=(298.0,'K'), Tmax=(1000.0, 'K')),
-            NASAPolynomial(coeffs=[2.92050897E+00, -2.70455589E-04, 5.15610634E-07, -2.93911213E-10, 5.54030466E-14, -1.78369003E+04, -1.50940536E+01], Tmin=(1000.0,'K'), Tmax=(2000.0, 'K')),
-        ],
-        Tmin = (298.0, 'K'),
-        Tmax = (2000.0, 'K'),
-        thermo_coverage_dependence = {"""
-                                      1 X  u0 p0 c0 {2,D}
-                                      2 O  u0 p2 c0 {1,D}
-                                      """:{'model': 'polynomial', 'enthalpy-coefficients':[-0.04321765,1.04225839,0], 'entropy-coefficients':[0,0,0]}},
-    ),
-    longDesc = u"""Calculated by Katrin Blondal at Brown University using statistical mechanics (file: compute_NASA_for_Pt-adsorbates.ipynb). 
-            Based on DFT calculations by Jelena Jelic at KIT.
-            DFT binding energy: -3.586 eV.
-            Linear scaling parameters: ref_adatom_O = -3.586 eV, psi = 0.00000 eV, gamma_O(X) = 1.000.""",
-    metal = "Pt",
-    facet = "111",
-)
+# entry(
+#     index = 2,
+#     label = "O_ads",
+#     molecule =
+# """
+# 1 X  u0 p0 c0 {2,D}
+# 2 O  u0 p2 c0 {1,D}
+# """,
+#     thermo = NASA(
+#         polynomials = [
+#             NASAPolynomial(coeffs=[1.79722382E-01, 1.25453156E-02, -2.29924588E-05, 1.94187177E-08, -6.22414099E-12, -1.73402246E+04, -2.22409728E+00], Tmin=(298.0,'K'), Tmax=(1000.0, 'K')),
+#             NASAPolynomial(coeffs=[2.92050897E+00, -2.70455589E-04, 5.15610634E-07, -2.93911213E-10, 5.54030466E-14, -1.78369003E+04, -1.50940536E+01], Tmin=(1000.0,'K'), Tmax=(2000.0, 'K')),
+#         ],
+#         Tmin = (298.0, 'K'),
+#         Tmax = (2000.0, 'K'),
+#         thermo_coverage_dependence = {"""
+#                                       1 X  u0 p0 c0 {2,D}
+#                                       2 O  u0 p2 c0 {1,D}
+#                                       """:{'model': 'polynomial', 'enthalpy-coefficients':[-0.04321765,1.04225839,0], 'entropy-coefficients':[0,0,0]}},
+#     ),
+#     longDesc = u"""Calculated by Katrin Blondal at Brown University using statistical mechanics (file: compute_NASA_for_Pt-adsorbates.ipynb). 
+#             Based on DFT calculations by Jelena Jelic at KIT.
+#             DFT binding energy: -3.586 eV.
+#             Linear scaling parameters: ref_adatom_O = -3.586 eV, psi = 0.00000 eV, gamma_O(X) = 1.000.""",
+#     metal = "Pt",
+#     facet = "111",
+# )
 
 entry(
-    index = 3,
+    index = 2,
     label = "CO*",
     molecule =  
 """
@@ -74,7 +74,7 @@ entry(
                                       1 C u0 p0 {2,D} {3,D}
                                       2 O u0 p2 {1,D} 
                                       3 X u0 p0 {1,D} 
-                                      """:{'model': 'polynomial', 'enthalpy-coefficients':[-0.0503, 0.8575, 0], 'entropy-coefficients':[0,0,0]}},
+                                      """:{'model': 'polynomial', 'enthalpy-coefficients':[-0.18638701, 0.46212068, 1.2758056], 'entropy-coefficients':[0,0,0]}},
     ),
     shortDesc = u"""CO adsorbed on nickel (?)""",
     longDesc =  u"""Estimated via CFG-TiC
