edee3046c0960a542b187eb147a8665f42ac2889
Chris B <56306881+ChrisBNEU@users.noreply.github.com>
2024-07-10 18:14:03 -0400

    Dissociation Beta vdW

--- input/kinetics/families/Surface_Dissociation_Beta_vdW/rules.py
+++ input/kinetics/families/Surface_Dissociation_Beta_vdW/rules.py
@@ -62,23 +62,3 @@ E0 and alpha are taken from Table 5 for all metals from Sutton and Vlachos, "Eth
 Pre-exponential coefficient is calculated from 1e13 s^-1 (standard guess from transition state theory) divided by 2.39e-9 mol cm^-2 (surface site density of Pt(111)
 """
 )
-
-entry(
-    index = 4,
-    label = "CH3;VacantSite",
-    kinetics = SurfaceArrheniusBEP(
-        A = (1.393e17, 'm^2/(mol*s)'),
-        n = 0.,
-        alpha =0.58,
-        E0 = (112.9, 'kJ/mol'),
-        Tmin = (200, 'K'),
-        Tmax = (3000, 'K'),
-    ),
-    rank = 0,
-    shortDesc = u"""Default""",
-    longDesc = u"""
-E0 and alpha are taken from Table 6 for oxygenates for 3x3 cell size from "A Theoretical and Computational Analysis of Linear Free Energy Relations for the Estimation of Activation Energies" Jonathan E. Sutton, Dionisios G. Vlachos, ACS Catal., 2012, 2, 1624-1634, DOI:10.1021/cs3003269.
-Pre-exponential coefficient is calculated from 1e13 s^-1 (standard guess from transition state theory) divided by 2.39e-9 mol cm^-2 (surface site density of Pt(111)
-A divided by 3 because of reaction path degeneracy for -R-CH3 dissociation (3 equivalent H atoms)
-"""
-)
