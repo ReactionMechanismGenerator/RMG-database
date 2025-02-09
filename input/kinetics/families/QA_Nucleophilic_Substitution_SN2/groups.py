0f5ec0aced5dadba7638c76c8933cf7a34a5db5c
NastPlot <nastasia@campus.technion.ac.il>
2025-02-09 22:15:52 +0200

    edited QA degradation files

--- input/kinetics/families/QA_Nucleophilic_Substitution_SN2/groups.py
+++ input/kinetics/families/QA_Nucleophilic_Substitution_SN2/groups.py
@@ -35,7 +35,7 @@ recipe(actions=[
    
 ])
 
-group(
+entry(
     index = 0,
     label="RCH2(N+)R3",
     group=
@@ -58,7 +58,7 @@ entry(
     group =
 """
 1 *3 O u0 p3 c-1 {2,S} 
-2    H u0 p0 c0 {1,s}
+2    H u0 p0 c0 {1,S}
 """,
     kinetics = None,
 )
@@ -91,45 +91,37 @@ entry(
     kinetics = None,
 )
 
-group(
+entry(
     index = 4,
     label="C6H5-CH2-(N+)(CH3)3)", #TMBA
     group=
 """
-1 *2 N u0 p1 c1 {2,S} {8,S} {9,S} {10,S}
-2 *1 C u0 p0 c0 {1,S} {3,S} {4,S} {5,S}
+1 *2 N u0 p0 c+1 {2,S} {7,S} {8,S} {9,S}
+2 *1 C u0 p0 c0 {1,S} {3,S} {4,B} {6,B}
 3    H u0 p0 c0 {2,S}
-4    H u0 p0 c0 {2,S}
-5    C u0 p0 c0 {2,S} {11,B} {16,B}
-6    C u0 p0 c0 {11,B} {7,B} {12,S}
-7    C u0 p0 c0 {6,B} {8,B} {13,S}
-8    C u0 p0 c0 {1,S} {7,B} {9,B}
-9    C u0 p0 c0 {1,S} {8,B} {10,S}
-10   C u0 p0 c0 {1,S} {9,B} {15,B}
-11   C u0 p0 c0 {5,B} {6,B} {14,S}
+4    C u0 p0 c0 {2,B} {5,B} {10,S}
+5    C u0 p0 c0 {4,B} {6,B} {11,S}
+6    C u0 p0 c0 {2,B} {5,B} {12,S}
+7    C u0 p0 c0 {1,S} {13,S} {14,S} {15,S}
+8    C u0 p0 c0 {1,S} {16,S} {17,S} {18,S}
+9    C u0 p0 c0 {1,S} {19,S} {20,S} {21,S}
+10   H u0 p0 c0 {4,S}
+11   H u0 p0 c0 {5,S}
 12   H u0 p0 c0 {6,S}
 13   H u0 p0 c0 {7,S}
-14   H u0 p0 c0 {11,S}
-15   H u0 p0 c0 {10,S}
-16   H u0 p0 c0 {5,B}
-17   C u0 p0 c0 {8,S} {18,S} {19,S} {20,S}
-18   H u0 p0 c0 {17,S}
-19   H u0 p0 c0 {17,S}
-20   H u0 p0 c0 {17,S}
-21   C u0 p0 c0 {9,S} {22,S} {23,S} {24,S}
-22   H u0 p0 c0 {21,S}
-23   H u0 p0 c0 {21,S}
-24   H u0 p0 c0 {21,S}
-25   C u0 p0 c0 {10,S} {26,S} {27,S} {28,S}
-26   H u0 p0 c0 {25,S}
-27   H u0 p0 c0 {25,S}
-28   H u0 p0 c0 {25,S}
+14   H u0 p0 c0 {7,S}
+15   H u0 p0 c0 {7,S}
+16   H u0 p0 c0 {8,S}
+17   H u0 p0 c0 {8,S}
+18   H u0 p0 c0 {8,S}
+19   H u0 p0 c0 {9,S}
+20   H u0 p0 c0 {9,S}
+21   H u0 p0 c0 {9,S}
 """,
     kinetics=None,
 )
 
-#C6H5-CH2-(N+)R2-CH3 adjacency
-group(
+entry(
     index = 5,
     label="C6H5-CH2-(N+)R2-CH3", 
     group=
@@ -162,6 +154,66 @@ group(
 """,
     kinetics=None,
 )
+
+entry(
+    index = 6,
+    label="R-CH2-(N+)R2-CH3", 
+    group=
+"""
+1 *2 N  u0 p0 c+1 {2,S} {5,S} {6,S} {7,S}
+2 *1 C  u0 p0 c0 {1,S} {3,S} {4,S} {8,S}
+3    R  u0 p0 c0 {2,S}
+4    H  u0 p0 c0 {2,S}
+5    R  u0 p0 c0 {1,S}
+6    R  u0 p0 c0 {1,S}
+7    C  u0 p0 c0 {1,S} {9,S} {10,S} {11,S}
+8    H  u0 p0 c0 {2,S}
+9    H  u0 p0 c0 {7,S}
+10   H  u0 p0 c0 {7,S}
+11   H  u0 p0 c0 {7,S}
+""",
+    kinetics=None,
+)
+
+entry(
+    index = 7,
+    label="C6H5-CH2-(N+)(CH2CH3)3", 
+    group=
+"""
+1 *2 N  u0 p0 c+1 {2,S} {7,S} {11,S} {15,S}
+2 *1 C  u0 p0 c0 {1,S} {3,S} {4,B} {6,B}
+3    C  u0 p0 c0 {2,S} {8,B} {9,B}
+4    C  u0 p0 c0 {2,B} {5,B} {10,S}
+5    C  u0 p0 c0 {4,B} {6,B} {12,S}
+6    C  u0 p0 c0 {2,B} {5,B} {13,S}
+7    C  u0 p0 c0 {1,S} {16,S} {17,S} {18,S}
+8    C  u0 p0 c0 {3,B} {9,B} {14,S}
+9    C  u0 p0 c0 {3,B} {8,B} {15,S}
+10   H  u0 p0 c0 {4,S}
+11   C  u0 p0 c0 {1,S} {19,S} {20,S} {21,S}
+12   H  u0 p0 c0 {5,S}
+13   H  u0 p0 c0 {6,S}
+14   H  u0 p0 c0 {8,S}
+15   C  u0 p0 c0 {1,S} {9,S} {22,S} {23,S}
+16   H  u0 p0 c0 {7,S}
+17   H  u0 p0 c0 {7,S}
+18   C  u0 p0 c0 {7,S} {24,S} {25,S} {26,S}
+19   H  u0 p0 c0 {11,S}
+20   H  u0 p0 c0 {11,S}
+21   H  u0 p0 c0 {11,S}
+22   H  u0 p0 c0 {15,S}
+23   H  u0 p0 c0 {15,S}
+24   C  u0 p0 c0 {18,S} {27,S} {28,S} {29,S}
+25   H  u0 p0 c0 {18,S}
+26   H  u0 p0 c0 {18,S}
+27   H  u0 p0 c0 {24,S}
+28   H  u0 p0 c0 {24,S}
+29   H  u0 p0 c0 {24,S}
+""",
+    kinetics=None,
+)
+
+
 
 tree(
 """
--- input/kinetics/families/QA_Nucleophilic_Substitution_SN2/training/reactions.py
+++ input/kinetics/families/QA_Nucleophilic_Substitution_SN2/training/reactions.py
@@ -11,7 +11,7 @@ entry(
     index = 0,
     label = "C6H5-CH2-(N+)(CH3)3 + (O-)H <=> C6H5-CH2-OH + N(CH3)3", #TMBA degradation
     degeneracy = 1,
-    kinetics = Arrhenius(A=(4.14134e+06,'cm^3/(mol*s)'), n=2.18948, Ea=(140.779,'kJ/mol'), T0=(1,'K'), Tmin=(300,'K'), Tmax=(3000,'K'), solute=SoluteData(S=1.8681935378213759,B=2.373498032935028,E=0.32859248603303737,L=5.004153890392606,A=0.8849787213003473,comment='')),
+    kinetics = Arrhenius(A=(4.14134e+06,'cm^3/(mol*s)'), n=2.18948, Ea=(140.779,'kJ/mol'), T0=(1,'K'), Tmin=(270,'K'), Tmax=(600,'K'), solute=SoluteData(S=1.8681935378213759,B=2.373498032935028,E=0.32859248603303737,L=5.004153890392606,A=0.8849787213003473,comment='')),
     rank = 3, # means that the data is calculated using high-level theoretical methods
     longDesc = 
 """
