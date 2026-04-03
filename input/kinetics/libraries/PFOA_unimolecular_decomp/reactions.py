5053cfa9a320a71218cf80164c0c3d9ad0e63371
Nora Khalil <khalil.nor@northeastern.edu>
2026-04-03 14:21:02 -0400

    made library of proposed rates for PFOA unimolecular decomposition. Back calculated reaction rates from Figure 1 of Westmoreland's recent publication: 'Finding Mechanisms and Conditions for Pyrolysis of Perfluorooctanoic Acid Using a Detailed Kinetic Model' 2025  https://pubs.acs.org/doi/10.1021/acs.iecr.5c00671?fig=fig1&ref=pdf. Motivation: Currently in RMG, homolytic scission reactions for PFOA degradation are coming from R_recombination family, at nodes with training data that isn't very representative of the reactions we want to estimate for. Commit this library in hopes of better rates for these homolytic scission reactions

---
+++ input/kinetics/libraries/PFOA_unimolecular_decomp/reactions.py
@@ -0,0 +1,99 @@
+entry(
+    index = 1,
+    label = "PF8acid <=> CF2s + PF7acid",
+    degeneracy = 1.0,
+    kinetics = Arrhenius(A=(1.16993e+13,'s^-1'), n=0, Ea=(356505,'J/mol'), T0=(1,'K')),
+)
+
+    
+    
+entry(
+    index = 2,
+    label = "PF8acid <=> CF3r + rPF7acid",
+    degeneracy = 1.0,
+    kinetics = Arrhenius(A=(5.42053e+15,'s^-1'), n=0, Ea=(363267,'J/mol'), T0=(1,'K')),
+)
+
+    
+    
+entry(
+    index = 3,
+    label = "PF8acid <=> PF2alkylr + rPF6acid",
+    degeneracy = 1.0,
+    kinetics = Arrhenius(A=(6.81764e+16,'s^-1'), n=0, Ea=(345596,'J/mol'), T0=(1,'K')),
+)
+
+    
+    
+entry(
+    index = 4,
+    label = "PF8acid <=> PF3alkylr + rPF5acid",
+    degeneracy = 1.0,
+    kinetics = Arrhenius(A=(5.77293e+18,'s^-1'), n=0, Ea=(345168,'J/mol'), T0=(1,'K')),
+)
+
+    
+    
+entry(
+    index = 5,
+    label = "PF8acid <=> PF4alkylr + rPF4acid",
+    degeneracy = 1.0,
+    kinetics = Arrhenius(A=(3.7357e+17,'s^-1'), n=0, Ea=(335622,'J/mol'), T0=(1,'K')),
+)
+
+    
+    
+entry(
+    index = 6,
+    label = "PF8acid <=> PF5alkylr + rPF3acid",
+    degeneracy = 1.0,
+    kinetics = Arrhenius(A=(8.04493e+16,'s^-1'), n=0, Ea=(349065,'J/mol'), T0=(1,'K')),
+)
+
+    
+    
+entry(
+    index = 7,
+    label = "PF8acid <=> PF6alkylr + rPF2acid",
+    degeneracy = 1.0,
+    kinetics = Arrhenius(A=(9.97238e+16,'s^-1'), n=0, Ea=(338695,'J/mol'), T0=(1,'K')),
+)
+
+    
+    
+entry(
+    index = 8,
+    label = "PF8acid <=> CO2 + PF7-1-H",
+    degeneracy = 1.0,
+    kinetics = Arrhenius(A=(5.66565e+13,'s^-1'), n=0, Ea=(301929,'J/mol'), T0=(1,'K')),
+)
+
+    
+    
+entry(
+    index = 9,
+    label = "PF8acid <=> CO2 + HF + PF7-1-ene",
+    degeneracy = 1.0,
+    kinetics = Arrhenius(A=(1.11328e+13,'s^-1'), n=0, Ea=(283306,'J/mol'), T0=(1,'K')),
+)
+
+    
+    
+entry(
+    index = 10,
+    label = "PF8acid <=> PF7alkylr + rCOOH",
+    degeneracy = 1.0,
+    kinetics = Arrhenius(A=(1.0044e+16,'s^-1'), n=0, Ea=(355535,'J/mol'), T0=(1,'K')),
+)
+
+    
+    
+entry(
+    index = 11,
+    label = "PF8acid <=> HF + PF8lactone",
+    degeneracy = 1.0,
+    kinetics = Arrhenius(A=(1.55075e+12,'s^-1'), n=0, Ea=(222334,'J/mol'), T0=(1,'K')),
+)
+
+    
+    
