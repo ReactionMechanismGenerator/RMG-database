2d73924336a3037233f0e9f1a5bd087963ccafce
Kirk Badger <kirk_badger@brown.edu>
2026-04-10 15:36:24 -0400

    adding the other 3 families with charge separation

---
+++ input/kinetics/families/Surface_Dissociation_Charge_Separation/rules.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "Surface_Dissociation_Charge_Separation/rules"
+shortDesc = u""
+longDesc = u"""
+"""
+entry(
+    index = 1,
+    label = "Combined;VacantSite",
+    kinetics = SurfaceArrheniusBEP(
+        A = (8.38e21, 'cm^2/(mol*s)'),
+        n = 0,
+        alpha = 0.768,
+        E0 = (124, 'kJ/mol'),
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
+    ),
+    rank = 0,
+    shortDesc = u"""Default""",
+    longDesc = u"""
+"""
+)
