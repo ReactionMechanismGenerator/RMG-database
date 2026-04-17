a0a2d85ca97aeef19c1652256a6df03406f7383b
Kirk Badger <kirk_badger@brown.edu>
2026-04-17 16:08:33 -0400

    Using Val5, and opening RMG-Py #2929

--- input/kinetics/families/Surface_Abstraction_Charge_Single/groups.py
+++ input/kinetics/families/Surface_Abstraction_Charge_Single/groups.py
@@ -31,6 +31,8 @@ recipe(actions=[
     ['CHANGE_BOND', '*3', -1, '*5'],
     ['LOSE_PAIR','*6','1'],
     ['GAIN_PAIR', '*3', '1'],
+    ['LOSE_CHARGE','*3','1'],
+    ['GAIN_CHARGE', '*6', '1'],
 ])
 
 entry(
@@ -50,7 +52,7 @@ entry(
     group =
 """
 1 *4 R u0 px c0 {2,S}
-2 *3 N u0 p0 c+1 {1,S} {3,D} {4,S}
+2 *3 Val5 u0 p0 c+1 {1,S} {3,D} {4,S}
 3 *5 Xo u0 p0 c0 {2,D}
 4 *6 R!H u0 p[1,2,3] c-1 {2,S}
 """,
@@ -69,7 +71,7 @@ forbidden(
     group =
 """
 1 *4 R u0 px c0 {2,S} {5,[S,D,T]}
-2 *3 N u0 p0 c+1 {1,S} {3,D} {4,S}
+2 *3 Val5 u0 p0 c+1 {1,S} {3,D} {4,S}
 3 *5 Xo u0 p0 c0 {2,D}
 4 *6 R!H u0 p[1,2,3] c-1 {2,S}
 5 Xo u0 c0 {1,[S,D,T]}
