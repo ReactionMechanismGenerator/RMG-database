2e8147c41ab5dfc3d863e69454c65894e3b5f443
ssun30 <sun.su@northeastern.edu>
2026-04-28 20:39:45 -0400

    [echem] Updated recommended libraries and kinetics families for CO2RR

--- input/kinetics/families/recommended.py
+++ input/kinetics/families/recommended.py
@@ -152,7 +152,7 @@ surface_development = {
     'Surface_Migration',
 }
 
-# Electrochemical families!
+# Electrochemical families.
 electrochem = {
     'Surface_Proton_Electron_Reduction_Alpha',
     'Surface_Proton_Electron_Reduction_Alpha_vdW',
@@ -171,23 +171,23 @@ electrochem = {
     'R_Addition_MultipleBond_Disprop',
 }
 
-# CO2RR families
+# Electrocatalytic CO2 Reduction families.
 surface_CO2 = {
-    'Surface_Adsorption_Single',
-    'Surface_Adsorption_vdW',
-    'Surface_Adsorption_Dissociative',
-    'Surface_Dissociation',
     'Surface_Abstraction',
+    'Surface_Abstraction_vdW',
+    'Surface_Abstraction_Single_vdW',
+    'Surface_Abstraction_Beta_double_vdW',
+    'Surface_Adsorption_Dissociative',
+    'Surface_Adsorption_Dissociative_Double',
+    'Surface_Adsorption_vdW',
+    'Surface_Dissociation',
+    'Surface_Dissociation_Double_vdW',
+    'Surface_Dissociation_vdW',
     'Surface_EleyRideal_Addition_Multiple_Bond',
     'Surface_Migration',
-    'Surface_Dissociation_Double_vdW',
-    'Surface_Addition_Single_vdW',
-    'Surface_Dissociation_vdW',
-    'Surface_Abstraction_vdW',
-    'Surface_Dissociation_Beta',
-    'Surface_Adsorption_Bidentate',
-    'Surface_Bidentate_Dissociation',
-    'Surface_vdW_to_Bidentate',
-    'Surface_Abstraction_Single_vdW',
-    'Surface_Adsorption_Dissociative_Double',
+    'Surface_Proton_Electron_Reduction_Alpha',
+    'Surface_Proton_Electron_Reduction_Alpha_vdW',
+    'Surface_Proton_Electron_Reduction_Beta',
+    'Surface_Proton_Electron_Reduction_Beta_vdW',
+    'Surface_Proton_Electron_Reduction_Beta_Dissociation',
 }
